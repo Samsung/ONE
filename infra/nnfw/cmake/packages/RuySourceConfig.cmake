2c2b4aa3efc0475df52106b26e35fddb19ac4dce
JiHwan Yeo <jihwan.yeo@samsung.com>
2020-04-17 14:53:54 +0900

    Update ruy version

--- infra/nnfw/cmake/packages/RuySourceConfig.cmake
+++ infra/nnfw/cmake/packages/RuySourceConfig.cmake
@@ -2,9 +2,9 @@ function(_RuySource_import)
   nnas_include(ExternalSourceTools)
   nnas_include(OptionTools)
 
-  # NOTE TensorFlow master (2020-04-06) downloads Ruy from the following URL
+  # NOTE TensorFlow master (2020-04-10) downloads Ruy from the following URL
   envoption(EXTERNAL_DOWNLOAD_SERVER "https://github.com")
-  set(RUY_URL ${EXTERNAL_DOWNLOAD_SERVER}/google/ruy/archive/91d62808498cea7ccb48aa59181e218b4ad05701.zip)
+  set(RUY_URL ${EXTERNAL_DOWNLOAD_SERVER}/google/ruy/archive/2e2658f964638ab7aa562d4b48b76007d44e38f0.zip)
   ExternalSource_Get("ruy" ${DOWNLOAD_RUY} ${RUY_URL})
 
   set(RuySource_DIR ${ruy_SOURCE_DIR} PARENT_SCOPE)
