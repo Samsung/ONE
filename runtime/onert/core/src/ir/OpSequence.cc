61b19e78f897f6205f2b225a7abf0a962248ad97
Yongseop Kim <yons.kim@samsung.com>
2020-04-17 15:24:53 +0900

    [onert] Enhance getStr of OpSequence
    Enhance getStr of OpSequence for debug
    
    Signed-off-by: Yongseop Kim <yons.kim@samsung.com>

--- runtime/onert/core/src/ir/OpSequence.cc
+++ runtime/onert/core/src/ir/OpSequence.cc
@@ -33,7 +33,7 @@ void OpSequence::accept(OperationVisitor &v) const { v.visit(*this); }
 // TODO: Impl Dumper instead of this method
 std::string OpSequence::getStr() const
 {
-  // "  OpSequence IN(xx,xx,xx) -> { op0, op1, op2 } -> OUT(yy,yy,yy)"
+  // "  OpSequence IN(0,1,2) -> { op0(0,1,2:3), op1(3:4), op2(4:5) } -> OUT(5)"
   std::stringstream ss;
   ss << "  OpSequence IN(";
   for (const auto &index : getInputs())
@@ -43,7 +43,28 @@ std::string OpSequence::getStr() const
   ss << " ) -> {";
   for (const auto &elem : _operations)
   {
-    ss << " " << elem.index.value() << "(" << elem.node->name() << ")";
+    // input string
+    std::string inputs;
+    for (const auto &ind : elem.node->getInputs())
+    {
+      inputs += std::to_string(ind.value());
+      inputs.push_back(',');
+    }
+    if (inputs.back() == ',')
+      inputs.pop_back();
+
+    // output string
+    std::string outputs;
+    for (const auto &ind : elem.node->getOutputs())
+    {
+      outputs += std::to_string(ind.value());
+      outputs.push_back(',');
+    }
+    if (outputs.back() == ',')
+      outputs.pop_back();
+
+    ss << " " << elem.index.value() << "(" << elem.node->name() << ":" << inputs << ":" << outputs
+       << ")";
   }
   ss << " } -> OUT(";
   for (const auto &index : getOutputs())
