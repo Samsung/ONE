17fae7cc5a611713f871818b6de2da1ea87c3425
Yongseop Kim <dragon.seop@gmail.com>
2020-04-21 12:13:21 +0900

    [onert] Enhance getStr of OpSequence (#35)
    * [onert] Enhance getStr of OpSequence
    
    Enhance getStr of OpSequence for debug
    
    Signed-off-by: Yongseop Kim <yons.kim@samsung.com>
    
    * Revise getStr code

--- runtime/onert/core/src/ir/OpSequence.cc
+++ runtime/onert/core/src/ir/OpSequence.cc
@@ -18,6 +18,24 @@
 #include "ir/OperationVisitor.h"
 #include <sstream>
 
+namespace
+{
+
+std::string getStrFromIndice(const onert::ir::OperandIndexSequence &indice)
+{
+  std::string str;
+  for (const auto &ind : indice)
+  {
+    str += std::to_string(ind.value());
+    str.push_back(',');
+  }
+  if (str.back() == ',')
+    str.pop_back();
+
+  return str;
+}
+}
+
 namespace onert
 {
 namespace ir
@@ -33,24 +51,16 @@ void OpSequence::accept(OperationVisitor &v) const { v.visit(*this); }
 // TODO: Impl Dumper instead of this method
 std::string OpSequence::getStr() const
 {
-  // "  OpSequence IN(xx,xx,xx) -> { op0, op1, op2 } -> OUT(yy,yy,yy)"
+  // "  OpSequence IN(0,1,2) -> { op0(0,1,2:3), op1(3:4), op2(4:5) } -> OUT(5)"
   std::stringstream ss;
-  ss << "  OpSequence IN(";
-  for (const auto &index : getInputs())
-  {
-    ss << " " << index.value();
-  }
-  ss << " ) -> {";
+  ss << "  OpSequence IN(" << getStrFromIndice(getInputs()) << ") -> {";
   for (const auto &elem : _operations)
   {
-    ss << " " << elem.index.value() << "(" << elem.node->name() << ")";
+    ss << " " << elem.index.value() << "(" << elem.node->name() << ":"
+       << getStrFromIndice(elem.node->getInputs()) << ":"
+       << getStrFromIndice(elem.node->getOutputs()) << ")";
   }
-  ss << " } -> OUT(";
-  for (const auto &index : getOutputs())
-  {
-    ss << " " << index.value();
-  }
-  ss << " )";
+  ss << " } -> OUT(" << getStrFromIndice(getOutputs()) << ")";
   return ss.str();
 }
 
