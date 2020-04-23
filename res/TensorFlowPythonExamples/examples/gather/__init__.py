e8b2a4dc38264330ecb2539e74a14e655ebf0174
Seok Namkoong <seok9311@naver.com>
2020-04-23 09:46:11 +0900

    [res] Add gather operation cases (#211)
    This commit will add `gather` operation related cases
    
    Signed-off-by: llFreetimell <seok9311@naver.com>

---
+++ res/TensorFlowPythonExamples/examples/gather/__init__.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+param_ = tf.compat.v1.placeholder(dtype=tf.float32, shape=(1, 2, 3, 4), name="Hole")
+indices_ = tf.constant([1, 2])
+op_ = tf.gather(param_, indices_, axis=2)
