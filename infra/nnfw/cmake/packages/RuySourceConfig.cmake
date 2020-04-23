04b3a37553a91a8b0c5da2089eea0d4bf0960183
ragmani <ragmani0216@gmail.com>
2020-04-23 16:43:41 +0900

    [tizen] Add ruy tar ball to packaging for supporting ruy on tizen
    This commit adds ruy tar ball to packaging for supporting ruy on tizen.
    
    Signed-off-by: ragmani <ragmani0216@gmail.com>

--- infra/nnfw/cmake/packages/RuySourceConfig.cmake
+++ infra/nnfw/cmake/packages/RuySourceConfig.cmake
@@ -4,7 +4,7 @@ function(_RuySource_import)
 
   # NOTE Downloads source from latest ruy library (2020-04-10)
   envoption(EXTERNAL_DOWNLOAD_SERVER "https://github.com")
-  set(RUY_URL ${EXTERNAL_DOWNLOAD_SERVER}/google/ruy/archive/2e2658f964638ab7aa562d4b48b76007d44e38f0.zip)
+  set(RUY_URL ${EXTERNAL_DOWNLOAD_SERVER}/google/ruy/archive/2e2658f964638ab7aa562d4b48b76007d44e38f0.tar.gz)
   ExternalSource_Get("ruy" ${DOWNLOAD_RUY} ${RUY_URL})
 
   set(RuySource_DIR ${ruy_SOURCE_DIR} PARENT_SCOPE)
