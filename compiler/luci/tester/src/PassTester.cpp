2d94c7ed0f97ba2e60a043486b5f65dc5c47c9a1
seongwoo <mhs4670go@naver.com>
2020-04-23 13:51:48 +0900

    Draft PassTester

---
+++ compiler/luci/tester/src/PassTester.cpp
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Model.h"
+
+#include <luci/Importer.h>
+#include <luci/Pass/ResolveCustomOpBatchMatMulPass.h>
+#include <luci/Service/Validate.h>
+#include <luci/CircleExporter.h>
+#include <oops/InternalExn.h>
+
+#include <fstream>
+#include <iostream>
+
+namespace
+{
+
+void show_help_message(const char *progname, std::ostream &os)
+{
+  os << "USAGE: " << progname << " circlefile_in circlefile_out" << std::endl << std::endl;
+}
+
+void show_error_message(const char *progname, std::ostream &os, const std::string &msg)
+{
+  os << "ERROR: " << msg << std::endl;
+  os << std::endl;
+
+  show_help_message(progname, os);
+}
+
+struct CircleExpContract : public luci::CircleExporter::Contract
+{
+public:
+  CircleExpContract(loco::Graph *graph, const std::string &filename)
+      : _graph(graph), _filepath(filename)
+  {
+    // NOTHING TO DO
+  }
+  CircleExpContract(luci::Module *module, const std::string &filename)
+      : _module(module), _filepath(filename)
+  {
+    // NOTHING TO DO
+  }
+  virtual ~CircleExpContract() = default;
+
+public:
+  loco::Graph *graph(void) const final { return _graph; }
+
+  luci::Module *module(void) const final { return _module; }
+
+public:
+  bool store(const char *ptr, const size_t size) const final;
+
+private:
+  loco::Graph *_graph;
+  luci::Module *_module;
+  const std::string _filepath;
+};
+
+bool CircleExpContract::store(const char *ptr, const size_t size) const
+{
+  if (!ptr)
+    INTERNAL_EXN("Graph was not serialized by FlatBuffer for some reason");
+
+  std::ofstream fs(_filepath.c_str(), std::ofstream::binary);
+  fs.write(ptr, size);
+
+  return fs.good();
+}
+}
+
+/*
+ * @brief Test how good optimizing pass works
+*/
+int main(int argc, char **argv)
+{
+  if (argc != 3)
+  {
+    show_error_message(argv[0], std::cerr, "In/Out Circle file path is not specified");
+    return 255;
+  }
+
+  std::string input_path = argv[1];
+  std::string output_path = argv[2];
+
+  std::cout << "[INFO] Circle from '" << input_path << "' to '" << output_path << "'" << std::endl;
+
+  // Load model from the file
+  std::unique_ptr<luci::Model> model = luci::load_model(input_path);
+  if (model == nullptr)
+  {
+    std::cerr << "ERROR: Failed to load '" << input_path << "'" << std::endl;
+    return 255;
+  }
+
+  const circle::Model *input_model = model->model();
+  if (input_model == nullptr)
+  {
+    std::cerr << "ERROR: Failed to read '" << input_path << "'" << std::endl;
+    return 255;
+  }
+
+  // Import from input Circle file
+  luci::Importer importer;
+  auto module = importer.importModule(input_model);
+  assert(module->size() > 0);
+
+  for (size_t g = 0; g < module->size(); ++g)
+  {
+    auto graph = module->graph(g);
+    if (graph == nullptr)
+      return 255;
+
+    {
+      luci::ResolveCustomOpBatchMatMulPass pass;
+      while (pass.run(graph) == true)
+        ;
+    }
+
+    if (!luci::validate(graph))
+      return 255;
+  }
+
+  // Export to output Circle file
+  luci::CircleExporter exporter;
+
+  CircleExpContract contract(module.get(), output_path);
+
+  return exporter.invoke(&contract) ? 0 : 255;
+}
