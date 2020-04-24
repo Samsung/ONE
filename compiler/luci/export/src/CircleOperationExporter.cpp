a35e1a1e471716b261fae8b3b3820ea0bd287a0e
seongwoo <mhs4670go@naver.com>
2020-04-24 16:13:25 +0900

    [luci] Export BatchMatMul and Custom op
    This commit enables luci to export BatchMatMul and Custom op
    
    Signed-off-by: seongwoo <mhs4670go@naver.com>

--- compiler/luci/export/src/CircleOperationExporter.cpp
+++ compiler/luci/export/src/CircleOperationExporter.cpp
@@ -51,11 +51,13 @@ public:
   void visit(luci::CircleAdd *) final;
   void visit(luci::CircleArgMax *) final;
   void visit(luci::CircleAveragePool2D *) final;
+  void visit(luci::CircleBatchMatMul *) final;
   void visit(luci::CircleBatchToSpaceND *) final;
   void visit(luci::CircleConcatenation *) final;
   void visit(luci::CircleConst *) final{/* skip, everything is done in exportOpDefinedTensors */};
   void visit(luci::CircleConv2D *) final;
   void visit(luci::CircleCos *) final;
+  void visit(luci::CircleCustom *) final;
   void visit(luci::CircleDepthwiseConv2D *) final;
   void visit(luci::CircleDiv *) final;
   void visit(luci::CircleExp *) final;
@@ -173,6 +175,19 @@ void OperationExporter::visit(luci::CircleAveragePool2D *node)
   export_pool_2d<luci::CircleAveragePool2D>(node, circle::BuiltinOperator_AVERAGE_POOL_2D);
 }
 
+void OperationExporter::visit(luci::CircleBatchMatMul *node)
+{
+  uint32_t op_idx = md.registerBuiltinOpcode(circle::BuiltinOperator_BATCH_MATMUL);
+  std::vector<int32_t> inputs_vec{get_tensor_index(node->x()), get_tensor_index(node->y())};
+  std::vector<int32_t> outputs_vec{get_tensor_index(static_cast<loco::Node *>(node))};
+  auto inputs = builder.CreateVector(inputs_vec);
+  auto outputs = builder.CreateVector(outputs_vec);
+  auto options = CreateBatchMatMulOptions(builder, node->adj_x(), node->adj_y());
+  auto op_offset = CreateOperator(builder, op_idx, inputs, outputs,
+                                  circle::BuiltinOptions_BatchMatMulOptions, options.Union());
+  gd._operators.push_back(op_offset);
+}
+
 void OperationExporter::visit(luci::CircleConcatenation *node)
 {
   uint32_t op_idx = md.registerBuiltinOpcode(circle::BuiltinOperator_CONCATENATION);
@@ -241,6 +256,26 @@ void OperationExporter::visit(luci::CircleCos *node)
   // Make COS operator
   auto op_offset = CreateOperator(builder, op_idx, inputs, outputs,
                                   circle::BuiltinOptions_CosOptions, options.Union());
+  gd._operators.push_back(op_offset);
+}
+
+void OperationExporter::visit(luci::CircleCustom *node)
+{
+  uint32_t op_idx = md.registerBuiltinOpcode(circle::BuiltinOperator_CUSTOM);
+  std::vector<int32_t> inputs_vec;
+  for (uint32_t i = 0; i < node->numInputs(); i++)
+  {
+    inputs_vec.push_back(get_tensor_index(node->inputs(i)));
+  }
+  std::vector<int32_t> outputs_vec{get_tensor_index(static_cast<loco::Node *>(node))};
+  auto inputs = builder.CreateVector(inputs_vec);
+  auto outputs = builder.CreateVector(outputs_vec);
+  flatbuffers::Offset<flatbuffers::Vector<uint8_t>> circle_custom_options;
+  std::vector<uint8_t> custom_options_vec{node->custom_options().begin(),
+                                          node->custom_options().end()};
+  circle_custom_options = builder.CreateVector(custom_options_vec);
+  auto op_offset = CreateOperator(builder, op_idx, inputs, outputs, circle::BuiltinOptions_NONE,
+                                  flatbuffers::Offset<void>(), circle_custom_options);
   gd._operators.push_back(op_offset);
 }
 
