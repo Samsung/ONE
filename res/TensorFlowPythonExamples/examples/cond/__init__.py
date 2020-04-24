5f5a39cb4f94a2ad2cd3676d5d5ab9c33cb06817
SaeHie Park <saehie.park@gmail.com>
2020-04-24 09:59:09 +0900

    [res] TensorFlowPythonExamples for cond (#252)
    This will introduce TensorFlowPythonExamples for cond
    
    Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ res/TensorFlowPythonExamples/examples/cond/__init__.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+x_ = tf.compat.v1.placeholder(dtype=tf.float32, shape=[], name="HoleX")
+y_ = tf.compat.v1.placeholder(dtype=tf.float32, shape=[], name="HoleY")
+z_ = tf.compat.v1.placeholder(dtype=tf.float32, shape=[], name="HoleZ")
+
+
+def fn1(a, b):
+    return tf.math.multiply(a, b, name="HoleM")
+
+
+def fn2(a, b):
+    return tf.math.add(a, b, name="HoleA")
+
+
+pr_ = tf.compat.v1.placeholder(tf.bool, shape=[], name="HoleC")
+op_ = tf.cond(pr_, lambda: fn1(x_, y_), lambda: fn2(y_, z_), name="Cond")
+re_ = tf.identity(op_, name="HoleR")
