b4a007ed5755f48f0a1a829d262b273aabb58877
Hyuk Jin Jeong <hj1.jeong@samsung.com>
2020-04-27 11:17:38 +0900

    [luci] Enalbe export for Tile operation (#311)
    This enables export for Tile operation
    
    Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

--- compiler/luci/export/src/CircleOperationExporter.cpp
+++ compiler/luci/export/src/CircleOperationExporter.cpp
@@ -82,6 +82,7 @@ public:
   void visit(luci::CircleSquaredDifference *) final;
   void visit(luci::CircleSub *) final;
   void visit(luci::CircleTanh *) final;
+  void visit(luci::CircleTile *) final;
   void visit(luci::CircleTranspose *) final;
   void visit(luci::CircleTransposeConv *) final;
   void visit(luci::CircleUnpack *) final;
@@ -632,6 +633,20 @@ void OperationExporter::visit(luci::CircleTanh *node)
   auto inputs = builder.CreateVector(inputs_vec);
   auto outputs = builder.CreateVector(outputs_vec);
   auto op_offset = CreateOperator(builder, op_idx, inputs, outputs);
+  gd._operators.push_back(op_offset);
+}
+
+void OperationExporter::visit(luci::CircleTile *node)
+{
+  uint32_t op_idx = md.registerBuiltinOpcode(circle::BuiltinOperator_TILE);
+  std::vector<int32_t> inputs_vec{get_tensor_index(node->input()),
+                                  get_tensor_index(node->multiples())};
+  std::vector<int32_t> outputs_vec{get_tensor_index(static_cast<loco::Node *>(node))};
+  auto inputs = builder.CreateVector(inputs_vec);
+  auto outputs = builder.CreateVector(outputs_vec);
+  auto options = CreateTileOptions(builder);
+  auto op_offset = CreateOperator(builder, op_idx, inputs, outputs,
+                                  circle::BuiltinOptions_TileOptions, options.Union());
   gd._operators.push_back(op_offset);
 }
 
