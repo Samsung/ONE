d6e0c0f9c5574a2f647c44fb810c0ea624934b31
Hyeongseok Oh <hseok82.oh@samsung.com>
2020-04-27 17:40:06 +0900

    [onert/cmake] Remove unused build flag
    Remove unused cmake build flag
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/options/options_aarch64-linux.cmake
+++ infra/nnfw/cmake/options/options_aarch64-linux.cmake
@@ -2,4 +2,3 @@
 # aarch64 linux cmake options
 #
 option(DOWNLOAD_NEON2SSE "Download NEON2SSE library source" OFF)
-option(BUILD_SRCN_KERNEL "Build srcn kernel" ON)
--- infra/nnfw/cmake/options/options_armv7l-linux.cmake
+++ infra/nnfw/cmake/options/options_armv7l-linux.cmake
@@ -2,5 +2,4 @@
 # armv7l linux cmake options
 #
 option(DOWNLOAD_NEON2SSE "Download NEON2SSE library source" OFF)
-option(BUILD_SRCN_KERNEL "Build srcn kernel" ON)
 option(BUILD_OPENCL_TOOL "Build OpenCL tool" ON)
