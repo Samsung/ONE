f3739761e8bfe58a16eb28ebfc8c1018e516ae5d
toomuchsalt <3951558+toomuchsalt@users.noreply.github.com>
2020-04-27 22:13:49 +0300

    [tflchef] Add support for squeeze operation
    Support generation of networks with squeeze operation
    
    ONE-DCO-1.0-Signed-off-by: Vladimir Plazun v.plazun@samsung.com

---
+++ compiler/tflchef/tflite/src/Op/Squeeze.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Squeeze.h"
+
+#include "Convert.h"
+
+namespace tflchef
+{
+
+void TFliteOpSqueeze::filler(const tflite::Operator *op, TFliteImport *import,
+                             tflchef::ModelRecipe *model_recipe) const
+{
+  // Nothing to do with filler
+}
+
+tflchef::Operation *TFliteOpSqueeze::build(const tflite::Operator *op, TFliteImport *import,
+                                           tflchef::ModelRecipe *model_recipe) const
+{
+  auto op_params = op->builtin_options_as_SqueezeOptions();
+  assert(op_params != nullptr);
+
+  auto operation = model_recipe->add_operation();
+
+  operation->set_type("Squeeze");
+
+  auto op_options = operation->mutable_squeeze_options();
+
+  std::vector<int32_t> squeeze_dims = as_index_vector(op_params->squeeze_dims());
+
+  for (auto dim : squeeze_dims)
+  {
+    op_options->add_squeeze_dim(dim);
+  }
+
+  return operation;
+}
+
+} // namespace tflchef
