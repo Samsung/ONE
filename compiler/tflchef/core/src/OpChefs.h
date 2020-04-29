273fe338f7eb7b382a430cdae0dbee04f81b81e9
Denis Krylov <krylovdenism@gmail.com>
2020-04-29 11:55:16 +0300

    [tflchef] Support Less operation
    Support Less operation in tflchef core, tflite, proto
    
    ONE-DCO-1.0-Signed-off-by: Denis Krylov <krylovdenism@gmail.com>

--- compiler/tflchef/core/src/OpChefs.h
+++ compiler/tflchef/core/src/OpChefs.h
@@ -34,6 +34,7 @@
 #include "Op/FullyConnected.h"
 #include "Op/Gather.h"
 #include "Op/If.h"
+#include "Op/Less.h"
 #include "Op/LogicalOr.h"
 #include "Op/LogicalNot.h"
 #include "Op/Logistic.h"
--- compiler/tflchef/tflite/src/TFliteOpRegistry.h
+++ compiler/tflchef/tflite/src/TFliteOpRegistry.h
@@ -71,6 +71,7 @@ private:
     REG_TFL_OP(FLOOR_DIV, TFliteOpFloorDiv);
     REG_TFL_OP(FULLY_CONNECTED, TFliteOpFullyConnected);
     REG_TFL_OP(GATHER, TFliteOpGather);
+    REG_TFL_OP(LESS, TFliteOpLess);
     REG_TFL_OP(LOGICAL_NOT, TFliteOpLogicalNot);
     REG_TFL_OP(LOGICAL_OR, TFliteOpLogicalOr);
     REG_TFL_OP(LOGISTIC, TFliteOpLogistic);
