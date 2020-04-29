6fe64c6fae56cd283992d37fd30d7c2aed85f48e
ragmani <ragmani0216@gmail.com>
2020-04-29 15:08:20 +0900

    [onert] Introduce a backend for controlflow
    This commit introduces a backend for controlflow into BackendManager.
      - Introduce controlflow backend
      - Make BackendManager loads controlflow backend
    
    Signed-off-by: ragmani <ragmani0216@gmail.com>

---
+++ runtime/onert/core/src/backend/controlflow/KernelGenerator.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ONERT_BACKEND_CONTROLFLOW_KERNEL_GENERATOR_H__
+#define __ONERT_BACKEND_CONTROLFLOW_KERNEL_GENERATOR_H__
+
+#include <backend/IKernelGenerator.h>
+#include <backend/ITensorBuilder.h>
+#include <exec/IExecutor.h>
+#include <ir/Operands.h>
+#include <ir/Operations.Include.h>
+
+namespace onert
+{
+namespace backend
+{
+namespace controlflow
+{
+
+class KernelGenerator : public IKernelGenerator
+{
+public:
+  KernelGenerator(const ir::Operands &operand_ctx);
+
+  using IKernelGenerator::visit;
+
+  void visit(const ir::OpSequence &) override;
+  void visit(const ir::operation::While &) override;
+
+private:
+  const ir::Operands &_operand_ctx;
+  const std::shared_ptr<TensorBuilderSet> _tensor_builder_set;
+  const std::shared_ptr<exec::ExecutorMap> _executor_map;
+};
+
+} // namespace controlflow
+} // namespace backend
+} // namespace onert
+
+#endif // __ONERT_BACKEND_CONTROLFLOW_KERNEL_GENERATOR_H__
