d636670d3ff70200b684334b6dc3aaca1324ca5d
SaeHie Park <saehie.park@gmail.com>
2020-05-06 15:48:21 +0900

    [res] TF example for batch_to_space (#488)
    This will introduce TF python example for batch_to_space Op
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ res/TensorFlowPythonExamples/examples/batch_to_space/__init__.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+
+in_ = tf.compat.v1.placeholder(tf.float32, shape=[4, 1, 1, 1], name="Hole")
+cr_ = tf.constant([[0, 0], [0, 0]], name="Hole")
+op_ = tf.batch_to_space(in_, cr_, 2)
