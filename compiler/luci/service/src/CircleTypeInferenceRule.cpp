9927965c8d121ca80fd2138c598acf19af8924be
SaeHie Park <saehie.park@gmail.com>
2020-05-11 15:10:25 +0900

    [luci] Shape, dtype inf for ReduceProd (#651)
    This will enable shape and dtype inference for ReduceProd Op
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/service/src/CircleTypeInferenceRule.cpp
+++ compiler/luci/service/src/CircleTypeInferenceRule.cpp
@@ -152,6 +152,11 @@ struct TypeInferenceAlgorithm final : public luci::CircleNodeVisitor<loco::DataT
 
   loco::DataType visit(const luci::CircleMul *node) final { return loco::dtype_get(node->x()); }
 
+  loco::DataType visit(const luci::CircleReduceProd *node) final
+  {
+    return loco::dtype_get(node->input());
+  }
+
   loco::DataType visit(const luci::CircleRelu *node) final
   {
     return loco::dtype_get(node->features());
