d573a6e8ccba0dbe43dd44a33e26f6932b3b2ce5
SaeHie Park <saehie.park@gmail.com>
2020-05-11 16:03:18 +0900

    [luci] Export ReduceProp (#650)
    This will export of ReduceProp Op
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/export/src/CircleOperationExporter.cpp
+++ compiler/luci/export/src/CircleOperationExporter.cpp
@@ -74,6 +74,7 @@ public:
   void visit(luci::CircleMean *) final;
   void visit(luci::CircleMul *) final;
   void visit(luci::CirclePack *) final;
+  void visit(luci::CircleReduceProd *) final;
   void visit(luci::CirclePad *) final;
   void visit(luci::CircleRelu *) final;
   void visit(luci::CircleRelu6 *) final;
@@ -550,6 +551,20 @@ void OperationExporter::visit(luci::CirclePad *node)
   auto options = CreatePadOptions(builder);
   auto op_offset = CreateOperator(builder, op_idx, inputs, outputs,
                                   circle::BuiltinOptions_PadOptions, options.Union());
+  gd._operators.push_back(op_offset);
+}
+
+void OperationExporter::visit(luci::CircleReduceProd *node)
+{
+  uint32_t op_idx = md.registerBuiltinOpcode(circle::BuiltinOperator_REDUCE_PROD);
+  std::vector<int32_t> inputs_vec{get_tensor_index(node->input()),
+                                  get_tensor_index(node->reduction_indices())};
+  std::vector<int32_t> outputs_vec{get_tensor_index(static_cast<loco::Node *>(node))};
+  auto inputs = builder.CreateVector(inputs_vec);
+  auto outputs = builder.CreateVector(outputs_vec);
+  auto options = CreateReducerOptions(builder, node->keep_dims());
+  auto op_offset = CreateOperator(builder, op_idx, inputs, outputs,
+                                  circle::BuiltinOptions_ReducerOptions, options.Union());
   gd._operators.push_back(op_offset);
 }
 
