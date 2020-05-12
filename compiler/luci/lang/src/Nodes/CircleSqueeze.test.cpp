3ad1daff0df217957eec01c260ac57a036bd2ee8
toomuchsalt <3951558+toomuchsalt@users.noreply.github.com>
2020-05-12 15:34:58 +0300

    [luci/lang] add support for Squeeze operator
    Add IR support for Squeeze operator
    
    ONE-DCO-1.0-Signed-off-by: Vladimir Plazun v.plazun@samsung.com

---
+++ compiler/luci/lang/src/Nodes/CircleSqueeze.test.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "luci/IR/Nodes/CircleSqueeze.h"
+
+#include "luci/IR/CircleDialect.h"
+
+#include <gtest/gtest.h>
+
+TEST(CircleSqueezeTest, constructor_P)
+{
+  luci::CircleSqueeze squeeze;
+
+  ASSERT_EQ(luci::CircleDialect::get(), squeeze.dialect());
+  ASSERT_EQ(luci::CircleOpcode::SQUEEZE, squeeze.opcode());
+
+  ASSERT_EQ(nullptr, squeeze.input());
+  ASSERT_EQ(0, squeeze.squeeze_dim_num());
+}
+
+TEST(CircleSqueezeTest, squeeze_dims)
+{
+  luci::CircleSqueeze squeeze;
+
+  squeeze.squeeze_dim_num(2);
+  ASSERT_EQ(2, squeeze.squeeze_dim_num());
+
+  squeeze.squeeze_dim(0, 1);
+  squeeze.squeeze_dim(1, 2);
+
+  ASSERT_EQ(1, squeeze.squeeze_dim(0));
+  ASSERT_EQ(2, squeeze.squeeze_dim(1));
+}
