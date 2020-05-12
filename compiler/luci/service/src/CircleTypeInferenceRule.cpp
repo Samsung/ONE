70905538289e79f9c3c4dbe8198761bc543d201c
Seok Namkoong <seok9311@naver.com>
2020-05-12 15:35:40 +0900

    [luci] Introduce Type/Shape inference for Sum
    This commit will introduce type/shape inference for `Sum` operation
    
    ONE-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>

--- compiler/luci/service/src/CircleTypeInferenceRule.cpp
+++ compiler/luci/service/src/CircleTypeInferenceRule.cpp
@@ -210,6 +210,11 @@ struct TypeInferenceAlgorithm final : public luci::CircleNodeVisitor<loco::DataT
 
   loco::DataType visit(const luci::CircleSub *node) final { return loco::dtype_get(node->x()); }
 
+  loco::DataType visit(const luci::CircleSum *node) final
+  {
+    return loco::dtype_get(node->input());
+  }
+
   loco::DataType visit(const luci::CircleTanh *node) final { return loco::dtype_get(node->x()); }
 
   loco::DataType visit(const luci::CircleTile *node) final
