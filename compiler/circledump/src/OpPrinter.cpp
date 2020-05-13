53030e24366011b3e0a67dc73f07c413029273e3
SaeHie Park <saehie.park@gmail.com>
2020-05-13 15:21:32 +0900

    [circledump] Comment for Select
    This will add comment for Select as there is no option to dump
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/circledump/src/OpPrinter.cpp
+++ compiler/circledump/src/OpPrinter.cpp
@@ -430,6 +430,7 @@ OpPrinterRegistry::OpPrinterRegistry()
   // There is no Option for ReLU and ReLU6
   _op_map[circle::BuiltinOperator_REDUCE_PROD] = make_unique<ReducerPrinter>();
   _op_map[circle::BuiltinOperator_RESHAPE] = make_unique<ReshapePrinter>();
+  // There is no Option for SELECT
   // There is no Option for SIN
   _op_map[circle::BuiltinOperator_SOFTMAX] = make_unique<SoftmaxPrinter>();
   // There is no Option for SPACE_TO_BATCH_ND
