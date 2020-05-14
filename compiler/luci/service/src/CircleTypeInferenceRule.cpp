dae168f36f7b5320e5659047e0b35a09a03af1c8
Sunchit Sharma <sun.sharma@samsung.com>
2020-05-14 13:24:46 +0530

    [luci/service] Added Logical_And operator support in luci/service Added files to support Logical_And operator in luci/service
    Signed-off-by: Sunchit Sharma <sun.sharma@samsung.com>

--- compiler/luci/service/src/CircleTypeInferenceRule.cpp
+++ compiler/luci/service/src/CircleTypeInferenceRule.cpp
@@ -109,6 +109,11 @@ struct TypeInferenceAlgorithm final : public luci::CircleNodeVisitor<loco::DataT
     return loco::dtype_get(node->input(1));
   }
 
+  loco::DataType visit(const luci::CircleLogicalAnd *node) final
+  {
+    return loco::dtype_get(node->x());
+  }
+
   loco::DataType visit(const luci::CircleLogicalNot *node) final
   {
     return loco::dtype_get(node->x());
