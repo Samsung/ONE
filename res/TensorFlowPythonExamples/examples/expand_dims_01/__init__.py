d57b5dc0fcbf5c2753a0791bb33d5c5be2095504
Hyun Sik Yoon <63768207+hyunsik-yoon@users.noreply.github.com>
2020-05-14 12:01:03 +0900

    [res/TensorFlowPythonExamples] Adding expand_dims example (#796)
    * [res/TensorFlowPythonExamples] Adding expand_dims example
    
    This adds expand_dims example into res/TensorFlowPythonExamples.
    
    Signed-off-by: Hyun Sik Yoon <hyunsik.yoon.1024@gmail.com>
    
    * put each example into separate dir

---
+++ res/TensorFlowPythonExamples/examples/expand_dims_01/__init__.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+# example 2 where input has unknown dim and axis is const
+
+in_ = tf.compat.v1.placeholder(dtype=tf.int32, shape=(None, None), name="Hole")
+expand_dim_ = tf.compat.v1.expand_dims(in_, 1, name="ExpandDims")
+
+# note: the code above will produce tflite file where output shape of ExpandDims is [1, 1, 1]
+#       (Tflite converter replace "None" to "1")
+#       axis can be negative integer.
