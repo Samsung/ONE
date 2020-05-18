af6691893b2fcdb8d8ba55d1550fb759515d920c
VT <b.theogaraj@samsung.com>
2020-05-18 15:11:39 +0900

    [Logical OR] New Opeator Addition
    Changes to support Logical OR operation on tensors.
    
    Signed-off-by: VT <b.theogaraj@samsung.com>

--- runtime/onert/backend/cpu/ShapeFixer.cc
+++ runtime/onert/backend/cpu/ShapeFixer.cc
@@ -209,6 +209,8 @@ void ShapeFixer::visit(const ir::operation::ZerosLike &) { /* DO NOTHING */}
 
 void ShapeFixer::visit(const ir::operation::Tile &) { /* DO NOTHING */}
 
+void ShapeFixer::visit(const ir::operation::LogicalOr &) { /* DO NOTHING */}
+
 } // namespace cpu
 } // namespace backend
 } // namespace onert
