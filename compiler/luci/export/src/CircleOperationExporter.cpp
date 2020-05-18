789fa4d127102d57487c4ced0974ea94d21e30ed
d.krylov <57215295+d-krylov@users.noreply.github.com>
2020-05-18 20:16:25 +0300

    [luci]  Support Greater in export (#1069)
    Support Greater in luci export
    
    ONE-DCO-1.0-Signed-off-by: Denis Krylov <krylovdenism@gmail.com>

--- compiler/luci/export/src/CircleOperationExporter.cpp
+++ compiler/luci/export/src/CircleOperationExporter.cpp
@@ -67,6 +67,7 @@ public:
   void visit(luci::CircleFloorMod *) final;
   void visit(luci::CircleFullyConnected *) final;
   void visit(luci::CircleGather *) final;
+  void visit(luci::CircleGreater *) final;
   void visit(luci::CircleIf *) final;
   void visit(luci::CircleLogicalAnd *) final;
   void visit(luci::CircleLogicalNot *) final;
@@ -457,6 +458,23 @@ void OperationExporter::visit(luci::CircleGather *node)
   // Make GATHER operator
   auto op_offset = CreateOperator(builder, op_idx, inputs, outputs,
                                   circle::BuiltinOptions_GatherOptions, options.Union());
+  gd._operators.push_back(op_offset);
+}
+
+void OperationExporter::visit(luci::CircleGreater *node)
+{
+  uint32_t opcode_idx = md.registerBuiltinOpcode(circle::BuiltinOperator_GREATER);
+  std::vector<int32_t> inputs{get_tensor_index(node->x()), get_tensor_index(node->y())};
+  std::vector<int32_t> outputs{get_tensor_index(node)};
+
+  auto fb_inputs = builder.CreateVector(inputs);
+  auto fb_outputs = builder.CreateVector(outputs);
+
+  auto options = CreateGreaterOptions(builder);
+
+  auto op_offset = CreateOperator(builder, opcode_idx, fb_inputs, fb_outputs,
+                                  circle::BuiltinOptions_GreaterOptions, options.Union());
+
   gd._operators.push_back(op_offset);
 }
 
