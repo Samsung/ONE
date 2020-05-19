782d2c33a2c6ee51bb8b27caa2e24abdeeb913bc
d.krylov <57215295+d-krylov@users.noreply.github.com>
2020-05-19 21:46:22 +0300

    [luci] Support Less operator in service (#1107)
    Support Less operator in luci service
    
    ONE-DCO-1.0-Signed-off-by: Denis Krylov <krylovdenism@gmail.com>

--- compiler/luci/service/src/CircleTypeInferenceRule.cpp
+++ compiler/luci/service/src/CircleTypeInferenceRule.cpp
@@ -123,6 +123,8 @@ struct TypeInferenceAlgorithm final : public luci::CircleNodeVisitor<loco::DataT
     return loco::dtype_get(node->input(1));
   }
 
+  loco::DataType visit(const luci::CircleLess *) final { return loco::DataType::BOOL; }
+
   loco::DataType visit(const luci::CircleLogicalAnd *node) final
   {
     return loco::dtype_get(node->x());
