0ccfcf1ecc0fe592ff73d1d5d3b111831a30ae0c
SaeHie Park <saehie.park@gmail.com>
2020-05-19 16:23:57 +0900

    [res] TensorFlowPythonModels for RELU_N1_TO_1 (#1102)
    This will introduce TensorFlowPythonModels for RELU_N1_TO_1 Op
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ res/TensorFlowPythonModels/tfpem.py
@@ -0,0 +1,25 @@
+# TensorFlow Python Example Manager
+
+import tensorflow as tf
+import importlib
+import argparse
+
+parser = argparse.ArgumentParser(description='Process TensorFlow Python Models')
+
+parser.add_argument('--mode', metavar='MODE', choices=['pbtxt'], default='pbtxt')
+parser.add_argument('examples', metavar='EXAMPLES', nargs='+')
+
+args = parser.parse_args()
+
+if args.mode == 'pbtxt':
+    for example in args.examples:
+        print("Generate '" + example + ".pbtxt'")
+
+        tf.compat.v1.reset_default_graph()
+        # https://stackoverflow.com/questions/37808866/proper-way-to-dynamically-import-a-module-with-relative-imports
+        importlib.import_module("examples." + example)
+
+        with open(example + ".pbtxt", "w") as f:
+            f.write(str(tf.compat.v1.get_default_graph().as_graph_def(add_shapes=True)))
+
+        print("Generate '" + example + ".pbtxt' - Done")
