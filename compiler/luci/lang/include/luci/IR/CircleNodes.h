111aae8c82e78c000c43b8a1505c8dff1e6eabd8
Andrey Kvochko <kvochko@users.noreply.github.com>
2020-05-21 00:58:37 +0300

    [luci] Support Range operator in luci IR (#1215)
    This adds Range IR Node in luci
    
    ONE-DCO-1.0-Signed-off-by: Andrey Kvochko <a.kvochko@samsung.com>
    
    Co-authored-by: binarman <efimov.alexander@gmail.com>

--- compiler/luci/lang/include/luci/IR/CircleNodes.h
+++ compiler/luci/lang/include/luci/IR/CircleNodes.h
@@ -55,6 +55,7 @@
 #include "Nodes/CircleOneHot.h"
 #include "Nodes/CirclePack.h"
 #include "Nodes/CirclePad.h"
+#include "Nodes/CircleRange.h"
 #include "Nodes/CircleReduceAny.h"
 #include "Nodes/CircleReduceProd.h"
 #include "Nodes/CircleRelu6.h"
