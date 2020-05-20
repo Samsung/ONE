6301b279a8b699f97caa8c1a5add2c818b1f9a55
Hyun Sik Yoon <63768207+hyunsik-yoon@users.noreply.github.com>
2020-05-20 16:53:59 +0900

    [onert] Moving shape inference into each op's *.cc file (#1172)
    * [onert] Moving shape inference into each op's C file
    
    Since shapeinference.cc file size is getting huge, this commit splits the file into each op's C file.
    
    Signed-off-by: Hyun Sik Yoon <hyunsik.yoon.1024@gmail.com>
    
    * uncommented

--- runtime/onert/core/src/util/shapeinf/ShapeInference.cc
+++ runtime/onert/core/src/util/shapeinf/ShapeInference.cc
@@ -100,14 +100,12 @@ std::pair<int, int> calcConvLikeHeightAndWidth(const int in_h, const int in_w, c
 // Shape inference
 //
 
-// Define shape calculation for operations. List them in alphabetic order.
-// Remove TODO when the function name matching the alphabet is added
-
 Shapes inferEltwiseShape(const ir::Shape &lhs_shape, const ir::Shape &rhs_shape)
 {
   return {broadcastShapes(lhs_shape, rhs_shape)};
 }
 
+// TODO move this when Avgpool.cc is created in util/shapeinf
 Shapes inferAvgPoolShape(const ir::Shape &in_shape, const ir::operation::AvgPool2D::Param &param,
                          const ir::Layout layout)
 {
@@ -119,6 +117,7 @@ Shapes inferAvgPoolShape(const ir::Shape &in_shape, const ir::operation::AvgPool
   return {ir::Shape{ifm_shape.N, out_h_w.first, out_h_w.second, ifm_shape.C}};
 }
 
+// TODO move this when Concat.cc is created in util/shapeinf
 ir::Shape inferConcatShape(const Shapes &in_shapes, const ir::operation::Concat::Param &param)
 {
   const int32_t concat_axis = param.axis;
@@ -140,6 +139,7 @@ ir::Shape inferConcatShape(const Shapes &in_shapes, const ir::operation::Concat:
   return out_shape;
 }
 
+// TODO move this when Conv2D.cc is created in util/shapeinf
 Shapes inferConv2DShape(const ir::Shape &in_shape, const ir::Shape &ker_shape,
                         const ir::operation::Conv2D::Param &param, ir::Layout layout)
 {
@@ -156,6 +156,7 @@ Shapes inferConv2DShape(const ir::Shape &in_shape, const ir::Shape &ker_shape,
   return {ir::Shape{ifm_shape.N, out_h_w.first, out_h_w.second, kf_shape.N}};
 }
 
+// TODO move this when DepthwiseConv2D.cc is created in util/shapeinf
 Shapes inferDepthwiseConv2DShape(const ir::Shape &in_shape, const ir::Shape &ker_shape,
                                  const ir::operation::DepthwiseConv2D::Param &param,
                                  ir::Layout layout)
@@ -174,6 +175,7 @@ Shapes inferDepthwiseConv2DShape(const ir::Shape &in_shape, const ir::Shape &ker
   return {ir::Shape{ifm_shape.N, out_h_w.first, out_h_w.second, kf_shape.C}};
 }
 
+// TODO move this when FullyConnected.cc is created in util/shapeinf
 Shapes inferFullyConnectedShape(const ir::Shape &in_shape, const ir::Shape &ker_shape)
 {
   assert(in_shape.rank() >= 2);
@@ -188,9 +190,7 @@ Shapes inferFullyConnectedShape(const ir::Shape &in_shape, const ir::Shape &ker_
   return {{ir::Shape({static_cast<int32_t>(batch_size), num_units})}};
 }
 
-// TODO write op starting from G
-// TODO write op starting from L
-
+// TODO move this when MaxPool.cc is created in util/shapeinf
 Shapes inferMaxPoolShape(const ir::Shape &in_shape, const ir::operation::MaxPool2D::Param &param,
                          const ir::Layout layout)
 {
@@ -202,21 +202,13 @@ Shapes inferMaxPoolShape(const ir::Shape &in_shape, const ir::operation::MaxPool
   return {ir::Shape{ifm_shape.N, out_h_w.first, out_h_w.second, ifm_shape.C}};
 }
 
-// TODO write op starting from N
-// TODO write op starting from P
-// TODO write op starting from R
-// TODO write op starting from S
-// TODO write op starting from T
-// TODO write op starting from U
-// TODO write op starting from Z
-
 /*
   StaticInferer
-
-  - Define visitors for operations. List them in alphabetic order.
-  - Remove TODO when any op starting from the alphabet is added
+  - Write methods except visit()
+  - For visit() of each operator, find each op's C file
 */
 
+// TODO move this into Add.cc
 void StaticInferer::visit(const ir::operation::Add &op)
 {
   const auto lhs_idx{op.getInputs().at(ir::operation::Add::Input::LHS)};
@@ -237,6 +229,7 @@ void StaticInferer::visit(const ir::operation::Add &op)
   output.info().shape(new_shape);
 }
 
+// TODO move this into Concat.cc
 void StaticInferer::visit(const ir::operation::Concat &op)
 {
   const auto input_count = op.getInputs().size();
@@ -265,198 +258,11 @@ void StaticInferer::visit(const ir::operation::Concat &op)
   output.info().shape(out_shape);
 }
 
-// TODO write op starting from D
-// TODO write op starting from E
-// TODO write op starting from F
-// TODO write op starting from G
-// TODO write op starting from L
-// TODO write op starting from M
-// TODO write op starting from N
-// TODO write op starting from P
-
-void StaticInferer::visit(const ir::operation::Reshape &op)
-{
-  const auto input_idx{op.getInputs().at(ir::operation::Reshape::Input::INPUT)};
-  const auto &input = _operands.at(input_idx);
-
-  // get mutable output operand
-  const auto output_idx = op.getOutputs().at(0);
-  ir::Operand &output = _operands.at(output_idx);
-
-  // if input is dynamic, output also becomes dynamic
-  if (input.info().isDynamic())
-  {
-    output.info().setDynamic();
-    return;
-  }
-
-  if (op.getInputs().size() == 1)
-  {
-    // no change on output shape
-    return;
-  }
-
-  // Let's check the second input
-  const auto shape_idx{op.getInputs().at(ir::operation::Reshape::Input::SHAPE)};
-  const auto &shape = _operands.at(shape_idx);
-
-  if (shape.isConstant())
-  {
-    // if shape is from Const, TFLC put the shape of output into tensor
-    // no change on output shape
-    return;
-  }
-
-  // if shape is NOT Const, set output shape to be dynamic_
-  output.info().setDynamic();
-}
-
-// TODO write op starting from S
-
-void StaticInferer::visit(const ir::operation::Tanh &op)
-{
-  const auto input_idx{op.getInputs().at(ir::operation::Tanh::Input::INPUT)};
-  const auto &input = _operands.at(input_idx);
-
-  // get mutable output operand
-  const auto output_idx = op.getOutputs().at(0);
-  ir::Operand &output = _operands.at(output_idx);
-
-  // if input is dynamic, output also becomes dynamic
-  if (input.info().isDynamic())
-  {
-    output.info().setDynamic();
-    return;
-  }
-
-  // re-sizing output shape
-  ir::Shape new_shape = input.info().shape();
-  output.info().shape(new_shape);
-}
-
-// TODO write op starting from U
-// TODO write op starting from Z
-
-} // namespace shape_inference
-} // namespace onert
-
-// namespace for helper function of DynamicInferer
-namespace
-{
-
-using onert::backend::ITensor;
-
-bool isReshapableShape(const ITensor *input, const onert::ir::Shape &shape)
-{
-  size_t input_elem_conut = 1;
-  {
-    for (size_t axis = 0; axis < input->num_dimensions(); axis++)
-      input_elem_conut *= input->dimension(axis);
-  }
-
-  return (input_elem_conut == shape.num_elements());
-}
-
-} // namespace
-
-namespace onert
-{
-namespace shape_inference
-{
-
 /*
  * DynamicInferer
-
-  - Define visitors for operations. List them in alphabetic order.
-  - Remove TODO when any op starting from the alphabet is added
+  - Write methods except visit()
+  - For visit() of each operator, find each op's C file
  */
-
-// TODO write op starting from A
-// TODO write op starting from C
-// TODO write op starting from D
-// TODO write op starting from E
-// TODO write op starting from F
-// TODO write op starting from G
-// TODO write op starting from L
-// TODO write op starting from M
-// TODO write op starting from N
-// TODO write op starting from P
-
-void DynamicInferer::visit(const ir::operation::Reshape &op)
-{
-  // check if output is not dynamic
-  auto output_ind = op.getOutputs().at(0);
-  auto *output = _tensor_registry->getITensor(output_ind);
-  if (!output->is_dynamic())
-    return;
-
-  // from op, access the buffer of second input to read new shape
-  auto new_shape_ind = op.getInputs().at(ir::operation::Reshape::Input::SHAPE);
-  auto &new_shape_op = _operands.at(new_shape_ind);
-
-  // if shape is from Const, TFLC put the shape of output into tensor
-  if (new_shape_op.isConstant())
-  {
-    // no change on output shape
-    return;
-  }
-
-  // getting output shape by reading new_shape tensor buffer
-  auto new_shape = _tensor_registry->getITensor(new_shape_ind);
-  assert(new_shape);
-
-  int32_t *new_shape_buf = reinterpret_cast<int32_t *>(new_shape->buffer());
-  assert(new_shape_buf);
-
-  auto new_rank = new_shape->dimension(0);
-
-  ir::Shape output_shape(new_rank);
-  for (size_t d = 0; d < new_rank; d++)
-    output_shape.dim(d) = new_shape_buf[d];
-
-  // sanity check
-  {
-    auto input_ind = op.getInputs().at(ir::operation::Reshape::Input::INPUT);
-    auto input = _tensor_registry->getITensor(input_ind);
-    assert(input);
-
-    if (!isReshapableShape(input, output_shape))
-      throw std::runtime_error("Reshape: 2nd param is not compatible with the shape of input");
-  }
-
-  // set output shape and output buffer
-  setShape(output, output_shape);
-
-  assert(output->buffer() == nullptr);
-  _dynamic_tensor_manager->allocate(output_ind, output_shape);
-  assert(output->buffer() != nullptr);
-}
-
-// TODO write op starting from S
-
-void DynamicInferer::visit(const ir::operation::Tanh &op)
-{
-  // check if output is not dynamic
-  auto output_ind = op.getOutputs().at(0);
-  auto *output = _tensor_registry->getITensor(output_ind);
-  if (!output->is_dynamic())
-    return;
-
-  // getting output shape
-  auto input_ind = op.getInputs().at(ir::operation::Tanh::Input::INPUT);
-  auto *input = _tensor_registry->getITensor(input_ind);
-  auto output_shape = getShape(input);
-
-  // set output shape and output buffer
-  setShape(output, output_shape);
-
-  assert(output->buffer() == nullptr);
-  _dynamic_tensor_manager->allocate(output_ind, output_shape);
-  assert(output->buffer() != nullptr);
-}
-
-// TODO write op starting from U
-// TODO write op starting from Z
 
 } // namespace shape_inference
 } // namespace onert
