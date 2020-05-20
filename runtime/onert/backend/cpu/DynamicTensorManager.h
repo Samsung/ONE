60d40303ffdab5bb69c206662cd7f16f9d3ad7b7
Hyun Sik Yoon <hyunsik.yoon.1024@gmail.com>
2020-05-20 16:38:17 +0900

    remove todo from doxygen comment

--- runtime/onert/backend/cpu/DynamicTensorManager.h
+++ runtime/onert/backend/cpu/DynamicTensorManager.h
@@ -30,9 +30,10 @@ namespace backend
 namespace cpu
 {
 
+// TODO Find optimized algorithm to manage memory.
+
 /**
  * @brief Class to manage dynamic tensor and its memory
- * @todo  Optimization is needed
  */
 class DynamicTensorManager : public backend::IDynamicTensorManager
 {
