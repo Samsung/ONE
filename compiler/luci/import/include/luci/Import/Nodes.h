c5f4f2b9d3bcd6e70e6922c557b601fc974be8cb
Denis Krylov <krylovdenism@gmail.com>
2020-05-21 11:37:04 +0300

    [luci] Support Reduce Max in import
    Support Reduce Max in luci import
    
    Apply format.patch
    
    ONE-DCO-1.0-Signed-off-by: Denis Krylov <krylovdenism@gmail.com>

--- compiler/luci/import/include/luci/Import/Nodes.h
+++ compiler/luci/import/include/luci/Import/Nodes.h
@@ -60,6 +60,7 @@
 #include "Nodes/CirclePow.h"
 #include "Nodes/CircleRange.h"
 #include "Nodes/CircleReduceAny.h"
+#include "Nodes/CircleReduceMax.h"
 #include "Nodes/CircleReduceProd.h"
 #include "Nodes/CircleRelu.h"
 #include "Nodes/CircleReluN1To1.h"
