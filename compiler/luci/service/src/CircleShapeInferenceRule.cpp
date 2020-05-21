f41e808fdb56e4e73a9a39862432e0626217958d
toomuchsalt <3951558+toomuchsalt@users.noreply.github.com>
2020-05-21 16:37:05 +0300

    [luci/service] Pow support shape and type inference
    Add shape and type inference support for Pow operator
    
    ONE-DCO-1.0-Signed-off-by: Vladimir Plazun v.plazun@samsung.com

--- compiler/luci/service/src/CircleShapeInferenceRule.cpp
+++ compiler/luci/service/src/CircleShapeInferenceRule.cpp
@@ -956,6 +956,16 @@ public:
     return loco::NodeShape{output_shape};
   }
 
+  loco::NodeShape visit(const luci::CirclePow *node) final
+  {
+    auto x_shape = loco::shape_get(node->x()).as<loco::TensorShape>();
+    auto y_shape = loco::shape_get(node->y()).as<loco::TensorShape>();
+
+    auto output_shape = broadcast_shape(x_shape, y_shape);
+
+    return loco::NodeShape{output_shape};
+  }
+
   loco::NodeShape visit(const luci::CircleRange *node) final
   {
     loco::TensorShape output_shape;
