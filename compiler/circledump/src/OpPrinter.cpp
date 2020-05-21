a4fc94fccb135521d4e1c7ed9f7e5daf8dad92b9
Andrey Kvochko <a.kvochko@samsung.com>
2020-05-21 15:06:07 +0300

    [circledump] Enable LOCAL_RESPONSE_NORMALIZATION
    This will enable dumping of LOCAL_RESPONSE_NORMALIZATION Op
    
    ONE-DCO-1.0-Signed-off-by: Andrey Kvochko <a.kvochko@samsung.com>

--- compiler/circledump/src/OpPrinter.cpp
+++ compiler/circledump/src/OpPrinter.cpp
@@ -243,6 +243,23 @@ public:
   }
 };
 
+class LocalResponseNormalizationPrinter : public OpPrinter
+{
+public:
+  void options(const circle::Operator *op, std::ostream &os) const override
+  {
+    if (auto *params = op->builtin_options_as_LocalResponseNormalizationOptions())
+    {
+      os << "    ";
+      os << "radius(" << params->radius() << ") ";
+      os << "bias(" << params->bias() << ") ";
+      os << "alpha(" << params->alpha() << ") ";
+      os << "beta(" << params->beta() << ") ";
+      os << std::endl;
+    }
+  }
+};
+
 class MirrorPadPrinter : public OpPrinter
 {
 public:
@@ -513,6 +530,8 @@ OpPrinterRegistry::OpPrinterRegistry()
   _op_map[circle::BuiltinOperator_GATHER] = make_unique<GatherPrinter>();
   _op_map[circle::BuiltinOperator_IF] = make_unique<IfPrinter>();
   // There is no Option for LOGISTIC
+  _op_map[circle::BuiltinOperator_LOCAL_RESPONSE_NORMALIZATION] =
+      make_unique<LocalResponseNormalizationPrinter>();
   _op_map[circle::BuiltinOperator_MAX_POOL_2D] = make_unique<Pool2DPrinter>();
   _op_map[circle::BuiltinOperator_MIRROR_PAD] = make_unique<MirrorPadPrinter>();
   _op_map[circle::BuiltinOperator_MUL] = make_unique<MulPrinter>();
