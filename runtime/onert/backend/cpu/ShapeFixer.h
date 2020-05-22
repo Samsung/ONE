b0dff5475fb1ddb07c725b8d21407371c7d2a19d
Jang Jiseob <ragmani0216@gmail.com>
2020-05-22 15:48:10 +0900

    [onert] Support float32 ReLU on cpu backend (#1249)
    This commit makes cpu backend supports float32 ReLU.
    
    Signed-off-by: ragmani <ragmani0216@gmail.com>

--- runtime/onert/backend/cpu/ShapeFixer.h
+++ runtime/onert/backend/cpu/ShapeFixer.h
@@ -68,6 +68,7 @@ public:
   void visit(const ir::operation::ReduceAny &) override;
   void visit(const ir::operation::ReduceMax &) override;
   void visit(const ir::operation::ReduceMin &) override;
+  void visit(const ir::operation::ReLU &) override;
   void visit(const ir::operation::Select &) override;
   void visit(const ir::operation::Slice &) override;
   void visit(const ir::operation::StridedSlice &) override;
