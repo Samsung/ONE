57f9f0ccd8c80746b2e90a1cd3fb34178a79aac1
Shedko Andrey/AI Tools Lab /SRR/Engineer/Samsung Electronics <a.shedko@samsung.com>
2020-05-25 12:47:54 +0300

    [luci/service] added L2 Normalize to luci service
    added L2 Normalize to luci service
    
    Signed-off-by: Shedko Andrey/AI Tools Lab /SRR/Engineer/Samsung Electronics <a.shedko@samsung.com>

--- compiler/luci/service/src/CircleTypeInferenceRule.cpp
+++ compiler/luci/service/src/CircleTypeInferenceRule.cpp
@@ -138,6 +138,11 @@ struct TypeInferenceAlgorithm final : public luci::CircleNodeVisitor<loco::DataT
     return loco::dtype_get(node->input(1));
   }
 
+  loco::DataType visit(const luci::CircleL2Normalize *node) final
+  {
+    return loco::dtype_get(node->x());
+  }
+
   loco::DataType visit(const luci::CircleLeakyRelu *node) final
   {
     return loco::dtype_get(node->features());
