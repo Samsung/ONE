f641da66878ec5d36637314a3687f8a034ba8592
mhs4670go <mhs4670go@naver.com>
2020-06-01 10:12:06 +0900

    [TensorFlowPythonExamples] fix placeholder shape in expand_dims_00 (#1619)
    This commit fixes shape of placeholder in expand_dims_00
    
    ONE-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>

--- res/TensorFlowPythonExamples/examples/expand_dims_00/__init__.py
+++ res/TensorFlowPythonExamples/examples/expand_dims_00/__init__.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 # example 1 where input has all known dims and axis is const
 
 in_ = tf.compat.v1.placeholder(dtype=tf.int32, shape=(2, 3), name="Hole")
-axis_ = tf.compat.v1.placeholder(dtype=tf.int32, shape=0, name="Axis")
+axis_ = tf.compat.v1.placeholder(dtype=tf.int32, shape=(), name="Axis")
 expand_dim_ = tf.compat.v1.expand_dims(in_, axis_, name="ExpandDims")
 
 # note: the code above will produce tflite file where output of ExpandDims is int32 scalar,
