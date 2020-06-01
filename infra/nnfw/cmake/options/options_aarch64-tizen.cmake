9b96a934b9b817f75cae71fa12a81464904143fe
Hyeongseok Oh <hseok82.oh@samsung.com>
2020-06-01 19:53:26 +0900

    [onert] Remove experimental nnpack backend (#1716)
    Remove experimental nnpack backend and related package managing scripts
    High-performance cpu backend is handled by cker with optimized kernel(and ruy)
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/options/options_aarch64-tizen.cmake
+++ infra/nnfw/cmake/options/options_aarch64-tizen.cmake
@@ -5,7 +5,6 @@ option(BUILD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
 option(BUILD_TENSORFLOW_LITE "Build TensorFlow Lite from the downloaded source" OFF)
 option(DOWNLOAD_EIGEN "Download Eigen source" OFF)
 option(DOWNLOAD_NEON2SSE "Download NEON2SSE library source" OFF)
-option(DOWNLOAD_NNPACK "Download NNPACK source" OFF)
 
 option(BUILD_LOGGING "Build logging runtime" OFF)
 option(BUILD_TFLITE_RUN "Build tflite-run" OFF)
