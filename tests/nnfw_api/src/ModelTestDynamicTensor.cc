fd4a4fb03b1d219162fe2b4000881a356bea5390
Hyun Sik Yoon <hyunsik.yoon.1024@gmail.com>
2020-06-01 12:13:15 +0900

    [onert] TC for graph input with unknown dim
    This adds two test cases to check graph inputs with unknown dim.
    
    Signed-off-by: Hyun Sik Yoon <hyunsik.yoon.1024@gmail.com>

--- tests/nnfw_api/src/ModelTestDynamicTensor.cc
+++ tests/nnfw_api/src/ModelTestDynamicTensor.cc
@@ -169,8 +169,116 @@ TEST_F(TestDynamicTensorReshapeModelLoaded, neg_reshape_multiple_executions)
   set_input_output_and_run(new_shape, expected);
 }
 
-// TODO add more test including
-
 // Unknown Dimension Test
+//
+
+class TestInputUnknownDimInputConcatModelLoaded
+    : public ValidationTestModelLoaded<NNPackages::UNKNOWN_DIM_INPUT_CONCAT>
+{
+protected:
+  void prepare_apply_set_input_output(const std::vector<float> &input0,
+                                      const std::vector<float> &input1,
+                                      std::vector<float> *actual_output, nnfw_tensorinfo input0_ti)
+  {
+    ASSERT_EQ(nnfw_prepare(_session), NNFW_STATUS_NO_ERROR);
+    ASSERT_EQ(nnfw_apply_tensorinfo(_session, 0, input0_ti), NNFW_STATUS_NO_ERROR);
+
+    ASSERT_EQ(nnfw_set_input(_session, 0, NNFW_TYPE_TENSOR_FLOAT32, input0.data(),
+                             sizeof(float) * input0.size()),
+              NNFW_STATUS_NO_ERROR);
+    ASSERT_EQ(nnfw_set_input(_session, 1, NNFW_TYPE_TENSOR_FLOAT32, input1.data(),
+                             sizeof(float) * input1.size()),
+              NNFW_STATUS_NO_ERROR);
+
+    ASSERT_EQ(nnfw_set_output(_session, 0, NNFW_TYPE_TENSOR_FLOAT32, actual_output->data(),
+                              sizeof(float) * actual_output->size()),
+              NNFW_STATUS_NO_ERROR);
+  }
+};
+
+/**
+ * @brief Testing the following model:
+ *
+ *        #0 = placeholder([None, None])
+ *        #1 = placeholder([2, 3])
+ *        #2 = concat (#0, #1, axis=0)
+ *
+ *        Calling sequence:
+ *        - nnfw_prepare()
+ *        - nnfw_apply_tensor_info(#0, [1, 3])
+ *        - nnfw_set_input()
+ *        - nnfw_run()
+ *
+ * @note Run this test with "cpu" backend
+ */
+TEST_F(TestInputUnknownDimInputConcatModelLoaded, concat_input0_to_2x3)
+{
+  ASSERT_EQ(nnfw_set_available_backends(_session, "cpu"), NNFW_STATUS_NO_ERROR);
+
+  const std::vector<float> input0 = {1, 2, 3};          // of shape [1, 3]
+  const std::vector<float> input1 = {4, 5, 6, 7, 8, 9}; // of shape [2, 3]
+
+  const std::vector<float> expected = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::vector<float> actual_output(expected.size());
+
+  // input reshaping to [1, 3]
+  nnfw_tensorinfo ti;
+  {
+    ti.dtype = NNFW_TYPE_TENSOR_FLOAT32;
+    ti.rank = 2;
+    ti.dims[0] = 1;
+    ti.dims[1] = 3;
+  }
+
+  prepare_apply_set_input_output(input0, input1, &actual_output, ti);
+
+  // Do inference
+  NNFW_STATUS res = nnfw_run(_session);
+  ASSERT_EQ(res, NNFW_STATUS_NO_ERROR);
+
+  // output value check
+  for (int i = 0; i < expected.size(); ++i)
+    ASSERT_EQ(expected[i], actual_output[i]);
+}
+
+/**
+ * @brief Negative Test: Testing the following model:
+ *
+ *        #0 = placeholder([None, None])
+ *        #1 = placeholder([2, 3])
+ *        #2 = concat (#0, #1, axis=0)
+ *
+ *        Calling sequence:
+ *        - nnfw_prepare()
+ *        - nnfw_apply_tensor_info(#0, [3, 1]) ---> input shape is not matched for concat to work
+ *        - nnfw_set_input()
+ *        - nnfw_run()
+ *
+ * @note Run this test with "cpu" backend and "linear" executor
+ */
+TEST_F(TestInputUnknownDimInputConcatModelLoaded, neg_concat_input0_to_wrong_shape)
+{
+  ASSERT_EQ(nnfw_set_available_backends(_session, "cpu"), NNFW_STATUS_NO_ERROR);
+
+  const std::vector<float> input0 = {1, 2, 3};          // of shape [3, 1], wrong shape
+  const std::vector<float> input1 = {4, 5, 6, 7, 8, 9}; // of shape [2, 3]
+
+  std::vector<float> actual_output(100); // whatever size
+
+  // input reshaping to [3, 1]
+  nnfw_tensorinfo ti;
+  {
+    ti.dtype = NNFW_TYPE_TENSOR_FLOAT32;
+    ti.rank = 2;
+    ti.dims[0] = 3;
+    ti.dims[1] = 1;
+  }
+
+  prepare_apply_set_input_output(input0, input1, &actual_output, ti);
+
+  // Do inference
+  NNFW_STATUS res = nnfw_run(_session);
+  ASSERT_EQ(res, NNFW_STATUS_ERROR);
+}
 
 // Complex Model Test
