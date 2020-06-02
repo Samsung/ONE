044398aa162335bf014772dfc414278c59cef6f5
Andrey Shedko <sirius.frozen@gmail.com>
2020-06-02 13:47:03 +0300

    Changed input tensor for op
    Changed input tensor for op to reference the correct tensor.
    
    Signed-off-by: Andrey Shedko <a.shedko@samsung.com>

--- res/TensorFlowPythonExamples/examples/prelu/__init__.py
+++ res/TensorFlowPythonExamples/examples/prelu/__init__.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 
-lhs_ = tf.compat.v1.placeholder(dtype=tf.float32, name="input", shape=[1, 4, 4, 3])
+input_tensor = tf.compat.v1.placeholder(
+    dtype=tf.float32, name="input", shape=[1, 4, 4, 3])
 prelu = tf.keras.layers.PReLU(shared_axes=[1, 2])
 op_ = prelu(input_tensor)
