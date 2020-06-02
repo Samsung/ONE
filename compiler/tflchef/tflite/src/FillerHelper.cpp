821021c6a3bdb444262f7cfeaa071f1a6fd95276
SaeHie Park <saehie.park@gmail.com>
2020-06-02 16:13:13 +0900

    [tflchef] Filler helper (#1752)
    This will introduce filler helper function for explicit fill types
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ compiler/tflchef/tflite/src/FillerHelper.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "FillerHelper.h"
+
+#include "Convert.h"
+
+namespace tflchef
+{
+
+void fill_tensor_to_import(int32_t idx, TFliteImport *import)
+{
+  const tflite::Tensor *tensor = import->tensors()->Get(idx);
+  if (tensor != nullptr)
+  {
+    if (tensor->type() == tflite::TensorType::TensorType_INT32)
+    {
+      const tflite::Buffer *buffer = import->buffers()->Get(tensor->buffer());
+      if (buffer && buffer->data())
+      {
+        auto vec = extract_buffer<int32_t>(buffer);
+        import->set_tensor_filler(idx, vec);
+      }
+    }
+    else if (tensor->type() == tflite::TensorType::TensorType_FLOAT32)
+    {
+      const tflite::Buffer *buffer = import->buffers()->Get(tensor->buffer());
+      if (buffer && buffer->data())
+      {
+        auto vec = extract_buffer<float>(buffer);
+        import->set_tensor_filler(idx, vec);
+      }
+    }
+  }
+}
+
+} // namespace tflchef
