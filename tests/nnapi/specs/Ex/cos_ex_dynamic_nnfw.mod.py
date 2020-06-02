02bd6d36d44076aa06f3156541dc0962ed112aae
Sungho Lee <41365503+intom@users.noreply.github.com>
2020-06-02 18:43:02 +0900

    Add shape inference for Cos operation (#1454)
    I added shape inference for COS operation.
    
    ONE-DCO-1.0-Signed-off-by: sungho22.lee <sungho22.lee@samsung.com>

---
+++ tests/nnapi/specs/Ex/cos_ex_dynamic_nnfw.mod.py
@@ -0,0 +1,64 @@
+#
+# Copyright (C) 2018 The Android Open Source Project
+# Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# refer to tanh_v1_dynamic.mod.py about the structore
+
+# This adds reshape as the first op in a model and
+# returns output of reshape, which is dynamic tensor
+
+'''
+Testing Cos op when the input is dynamic.
+      input [1, 2, 3]  shape [3]  (value of shape will be [1, 2, 3])
+          |             |
+          +-------------+
+          |
+       Reshape (added by DynamicInputGenerator since it generates its output to be dynamic)
+          |
+          | dynamic tensor at compilation time but the shape will be [1, 2, 3] at execution time
+          |
+         Cos
+          |
+        output (dynamic tensor, [1, 2, 3] at execution time)
+'''
+
+import dynamic_tensor
+
+model = Model()
+
+model_input_shape = [1, 2, 3]
+
+dynamic_layer = dynamic_tensor.DynamicInputGenerator(model, model_input_shape)
+
+test_node_input = dynamic_layer.getTestNodeInput()
+
+# write COS test. input is `test_input`
+
+# note output shape is used by expected output's shape
+model_output = Output("output", "TENSOR_FLOAT32", "{1, 2, 3}")
+
+model.Operation("COS_EX", test_node_input).To(model_output)
+
+model_input_data = [12.0, 36.1, 2.0, 90, 1.0, 0.012]
+model_output_data = [0.843853959, -0.028311733, -0.416146837, -0.448073616, 0.540302306, 0.999928001]
+
+Example({
+    # use these two as input
+    dynamic_layer.getModelInput(): model_input_data,
+    dynamic_layer.getShapeInput() : model_input_shape,
+
+    model_output: model_output_data,
+})
