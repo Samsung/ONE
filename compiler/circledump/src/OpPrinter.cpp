78c70f146703ce2cb445a61cd19fe8ac0d33c276
Andrey Shedko <sirius.frozen@gmail.com>
2020-06-02 14:34:24 +0300

    [circle dump] Added L2Norm to circle dump (#1590)
    Added L2Norm to circle dump
    
    Signed-off-by: Andrey Shedko <a.shedko@samsung.com>

--- compiler/circledump/src/OpPrinter.cpp
+++ compiler/circledump/src/OpPrinter.cpp
@@ -274,6 +274,21 @@ public:
   }
 };
 
+class L2NormPrinter : public OpPrinter
+{
+public:
+  void options(const circle::Operator *op, std::ostream &os) const override
+  {
+    if (auto *params = op->builtin_options_as_L2NormOptions())
+    {
+      os << "    ";
+      os << "Activation(" << EnumNameActivationFunctionType(params->fused_activation_function())
+         << ") ";
+      os << std::endl;
+    }
+  }
+};
+
 class LeakyReluPrinter : public OpPrinter
 {
 public:
@@ -588,6 +603,7 @@ OpPrinterRegistry::OpPrinterRegistry()
   _op_map[circle::BuiltinOperator_FULLY_CONNECTED] = make_unique<FullyConnectedPrinter>();
   _op_map[circle::BuiltinOperator_GATHER] = make_unique<GatherPrinter>();
   _op_map[circle::BuiltinOperator_IF] = make_unique<IfPrinter>();
+  _op_map[circle::BuiltinOperator_L2_NORMALIZATION] = make_unique<L2NormPrinter>();
   _op_map[circle::BuiltinOperator_L2_POOL_2D] = make_unique<Pool2DPrinter>();
   _op_map[circle::BuiltinOperator_LEAKY_RELU] = make_unique<LeakyReluPrinter>();
   _op_map[circle::BuiltinOperator_LOCAL_RESPONSE_NORMALIZATION] =
