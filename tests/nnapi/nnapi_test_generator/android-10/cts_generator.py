687aeb9eb2dca45d59564011c254c4b663c78c15
Sanggyu Lee <takepencil@naver.com>
2020-06-02 11:03:16 +0900

    [onert] nnapi test generator will use current environment's python
    Currently, nnapi tests are generated with system-wide python3.
    It requires us to install numpy module in system-wide.
    
    To support vritualenv or something, it changes to use `env`
    instead of `/usr/bin/python3`.
    
    ONE-DCO-1.0-Signed-off-by: Sanggyu Lee <sg5.lee@samsung.com>

--- tests/nnapi/nnapi_test_generator/android-10/cts_generator.py
+++ tests/nnapi/nnapi_test_generator/android-10/cts_generator.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+# Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
 # Copyright 2018, The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
