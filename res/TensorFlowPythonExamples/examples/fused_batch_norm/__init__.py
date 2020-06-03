6a5fb5386a0ee9a24467ce1ceac183c8688ae11d
SaeHie Park <saehie.park@gmail.com>
2020-06-04 06:38:26 +0900

    [res] TF example for BatchNorm, FusedBatchNorm (#1806)
    This will introduce TensorFlowPythonExamples for batch_normalization and fused_batch_norm
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>
    ONE-DCO-1.0-Signed-off-by: Alexander Efimov <a.efimov@samsung.com>

---
+++ res/TensorFlowPythonExamples/examples/fused_batch_norm/__init__.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+scale = tf.compat.v1.constant([1., 2., 3.])
+offset = tf.compat.v1.constant([4., 5., 6.])
+mean = tf.constant([1., 2., 3.])
+variance = tf.constant([4., 5., 6.])
+
+in_ = tf.compat.v1.placeholder(dtype=tf.float32, shape=(3, 3, 3, 3), name="Hole")
+fbn_ = tf.compat.v1.nn.fused_batch_norm(
+    in_, scale, offset, mean, variance, is_training=False)
