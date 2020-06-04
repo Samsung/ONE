726d371fdac13b3996d46f8aba68a8ce0d8cfaff
Leehansol <hansolu.lee@samsung.com>
2020-06-04 16:16:55 +0900

    [onert] Add Shape inference for Pow and its test case
    This PR adds shape inference for Pow and its test case.
    
    Signed-off-by: LeeHanSol <hansolu.lee@samsung.com>

--- runtime/onert/core/include/util/ShapeInference.h
+++ runtime/onert/core/include/util/ShapeInference.h
@@ -130,6 +130,7 @@ private:
   // TODO write op starting from O
   void visit(const ir::operation::Pack &op);
   void visit(const ir::operation::Permute &op);
+  void visit(const ir::operation::Pow &op);
   // TODO write op starting from Q
   void visit(const ir::operation::Reshape &op);
   void visit(const ir::operation::Round &op);
@@ -201,6 +202,7 @@ public:
   // TODO write op starting from O
   void visit(const ir::operation::Pack &op);
   void visit(const ir::operation::Permute &op);
+  void visit(const ir::operation::Pow &op);
   // TODO write op starting from Q
   void visit(const ir::operation::Reshape &op);
   void visit(const ir::operation::Round &op);
