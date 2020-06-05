Samsung/ONE

commit 6112606571c97d942aa67c3e8ad27fdf9ce09bee

[onert] Modify `ir::OpSequence` to be index based (#1660)
This commit modifies `ir::OpSequence` to be index based.
  - Remove "onert::ir::Element"
  - Fix incorrect use of address-of operator
  - Adjust visit functions of `ir::OpSequence` by `OperationVisitor`

Signed-off-by: ragmani <ragmani0216@gmail.com>

runtime/onert/core/src/exec/LinearExecutor.cc
@@ -27,9 +27,9 @@ namespace exec
 #ifdef RUY_PROFILER
 namespace
 {
-char *seq_to_label(const onert::ir::OpSequence *op_seq)
+char *seq_to_label(const onert::ir::OpSequence *op_seq, onert::ir::Operations &operations)
 {
-  auto node_name = op_seq->operations().at(0).node->name();
+  auto node_name = operations.at(*op_seq->begin()).name();
   char *cstr = new char[node_name.length() + 1];
   std::strcpy(cstr, node_name.c_str());
   return cstr;
@@ -46,7 +46,7 @@ void LinearExecutor::executeImpl()
     const auto backend = code.lower_info->backend();
 // TODO : Move ruy profiler into ExecutionObserver
 #ifdef RUY_PROFILER
-    ruy::profiler::ScopeLabel label(seq_to_label(op_seq));
+    ruy::profiler::ScopeLabel label(seq_to_label(op_seq), _graph.operations());
 #endif
     _subject.notifyJobBegin(this, op_seq, backend);
     code.fn_seq->run();

runtime/onert/backend/acl_cl/Backend.h
@@ -46,11 +46,12 @@ public:
                                              bool is_linear_executor) const override
   {
     const auto &operands = graph.operands();
+    const auto &operations = graph.operations();
     auto context = std::make_unique<BackendContext>(this, &graph);
     auto tb = std::make_shared<TensorBuilder>(operands, createTensorManager(is_linear_executor));
     context->tensor_builder = tb;
     context->constant_initializer = std::make_shared<ConstantInitializer>(operands, tb);
-    context->kernel_gen = std::make_shared<KernelGenerator>(operands, tb);
+    context->kernel_gen = std::make_shared<KernelGenerator>(operands, operations, tb);
     context->shape_fixer = std::make_shared<ShapeFixer>(operands, tb);
     context->tensor_register = nullptr;
     context->optimizer = std::make_shared<Optimizer>(context.get());

runtime/onert/backend/acl_cl/KernelGenerator.cc
@@ -43,9 +43,11 @@ using ::onert::backend::acl_common::asAclClFunction;
 using ActivationBuilder = ::onert::backend::acl_common::AclActivationBuilder<
     ::arm_compute::ICLTensor, ::arm_compute::CLActivationLayer, acl_common::AclClFunction>;
 
-KernelGenerator::KernelGenerator(const ir::Operands &ctx,
+KernelGenerator::KernelGenerator(const ir::Operands &operands_ctx,
+                                 const ir::Operations &operations_ctx,
                                  const std::shared_ptr<TensorBuilder> &tensor_builder)
-    : _ctx(ctx), _tensor_builder(tensor_builder), _current_op_seq_layout(ir::Layout::UNKNOWN)
+    : _ctx(operands_ctx), _operations_ctx(operations_ctx), _tensor_builder(tensor_builder),
+      _current_op_seq_layout(ir::Layout::UNKNOWN)
 {
   // DO NOTHING
 }
@@ -57,9 +59,9 @@ void KernelGenerator::visit(const ir::OpSequence &op_seq)
   assert(!_return_fn_seq);
   _return_fn_seq = std::make_unique<exec::FunctionSequence>();
   _current_op_seq_layout = op_seq.getLayout();
-  for (const auto &e : op_seq.operations())
+  for (const auto &operation_idx : op_seq.operations())
   {
-    const auto &node = *(e.node);
+    const auto &node = _operations_ctx.at(operation_idx);
     node.accept(*this);
     _return_fn_seq->append(releaseFunction());
   }

runtime/onert/backend/acl_cl/KernelGenerator.h
@@ -32,7 +32,8 @@ namespace acl_cl
 class KernelGenerator : public IKernelGenerator
 {
 public:
-  KernelGenerator(const ir::Operands &ctx, const std::shared_ptr<TensorBuilder> &tensor_builder);
+  KernelGenerator(const ir::Operands &operands_ctx, const ir::Operations &operations_ctx,
+                  const std::shared_ptr<TensorBuilder> &tensor_builder);
 
   void visit(const ir::OpSequence &) override;
   void visit(const ir::operation::BatchToSpaceND &) override;
@@ -104,6 +105,7 @@ public:
 
 private:
   const ir::Operands &_ctx;
+  const ir::Operations &_operations_ctx;
   std::shared_ptr<TensorBuilder> _tensor_builder;
   ir::Layout _current_op_seq_layout;
 };

runtime/onert/backend/acl_neon/Backend.h
@@ -47,11 +47,12 @@ public:
                                              bool is_linear_executor) const override
   {
     const auto &operands = graph.operands();
+    const auto &operations = graph.operations();
     auto context = std::make_unique<BackendContext>(this, &graph);
     auto tb = std::make_shared<TensorBuilder>(operands, createTensorManager(is_linear_executor));
     context->tensor_builder = tb;
     context->constant_initializer = std::make_shared<ConstantInitializer>(operands, tb);
-    context->kernel_gen = std::make_shared<KernelGenerator>(operands, tb);
+    context->kernel_gen = std::make_shared<KernelGenerator>(operands, operations, tb);
     context->shape_fixer = std::make_shared<ShapeFixer>(operands, tb);
     context->tensor_register = nullptr;
     context->optimizer = std::make_shared<Optimizer>(context.get());

runtime/onert/backend/acl_neon/KernelGenerator.cc
@@ -43,9 +43,11 @@ using ::onert::backend::acl_common::asAclFunction;
 using ActivationBuilder = ::onert::backend::acl_common::AclActivationBuilder<
     ::arm_compute::ITensor, ::arm_compute::NEActivationLayer, acl_common::AclFunction>;
 
-KernelGenerator::KernelGenerator(const ir::Operands &ctx,
+KernelGenerator::KernelGenerator(const ir::Operands &operands_ctx,
+                                 const ir::Operations &operations_ctx,
                                  const std::shared_ptr<TensorBuilder> &tensor_builder)
-    : _ctx(ctx), _tensor_builder(tensor_builder), _current_op_seq_layout(ir::Layout::UNKNOWN)
+    : _ctx(operands_ctx), _operations_ctx(operations_ctx), _tensor_builder(tensor_builder),
+      _current_op_seq_layout(ir::Layout::UNKNOWN)
 {
   // DO NOTHING
 }
@@ -57,9 +59,9 @@ void KernelGenerator::visit(const ir::OpSequence &op_seq)
   assert(!_return_fn_seq);
   _return_fn_seq = std::make_unique<exec::FunctionSequence>();
   _current_op_seq_layout = op_seq.getLayout();
-  for (const auto &e : op_seq.operations())
+  for (const auto &operation_idx : op_seq.operations())
   {
-    const auto &node = *(e.node);
+    const auto &node = _operations_ctx.at(operation_idx);
     node.accept(*this);
     _return_fn_seq->append(releaseFunction());
   }

runtime/onert/backend/acl_neon/KernelGenerator.h
@@ -32,7 +32,8 @@ namespace acl_neon
 class KernelGenerator : public IKernelGenerator
 {
 public:
-  KernelGenerator(const ir::Operands &ctx, const std::shared_ptr<TensorBuilder> &tensor_builder);
+  KernelGenerator(const ir::Operands &operands_ctx, const ir::Operations &operations_ctx,
+                  const std::shared_ptr<TensorBuilder> &tensor_builder);
 
   void visit(const ir::OpSequence &) override;
   void visit(const ir::operation::Abs &) override;
@@ -102,6 +103,7 @@ public:
 
 private:
   const ir::Operands &_ctx;
+  const ir::Operations &_operations_ctx;
   std::shared_ptr<TensorBuilder> _tensor_builder;
   ir::Layout _current_op_seq_layout;
 };

runtime/onert/backend/cpu/Backend.h
@@ -45,11 +45,12 @@ public:
                                              bool) const override
   {
     const auto &operands = graph.operands();
+    const auto &operations = graph.operations();
     auto context = std::make_unique<BackendContext>(this, &graph);
     auto tb = std::make_shared<TensorBuilder>();
     context->tensor_builder = tb;
     context->constant_initializer = std::make_shared<ConstantInitializer>(operands, tb);
-    context->kernel_gen = std::make_shared<KernelGenerator>(operands, tb, kb);
+    context->kernel_gen = std::make_shared<KernelGenerator>(operands, operations, tb, kb);
     context->shape_fixer = std::make_shared<ShapeFixer>(operands);
     context->tensor_register = nullptr;
     context->optimizer = nullptr;

commit 888c32d05ca6e2eafdb320a1b324803e9bdc00b3

[onert] Fix build error for ruy_profiler
Fix build error for ruy_profiler caused by #1660
- Fix the position of parenthesis
- Add missing const for OpSequence

ONE-DCO-1.0-Signed-off-by: JiHwan Yeo <jihwan.yeo@samsung.com>

runtime/onert/core/src/exec/LinearExecutor.cc
@@ -27,7 +27,7 @@ namespace exec
 #ifdef RUY_PROFILER
 namespace
 {
-char *seq_to_label(const onert::ir::OpSequence *op_seq, onert::ir::Operations &operations)
+char *seq_to_label(const onert::ir::OpSequence *op_seq, const onert::ir::Operations &operations)
 {
   auto node_name = operations.at(*op_seq->begin()).name();
   char *cstr = new char[node_name.length() + 1];
@@ -46,7 +46,7 @@ void LinearExecutor::executeImpl()
     const auto backend = code.lower_info->backend();
 // TODO : Move ruy profiler into ExecutionObserver
 #ifdef RUY_PROFILER
-    ruy::profiler::ScopeLabel label(seq_to_label(op_seq), _graph.operations());
+    ruy::profiler::ScopeLabel label(seq_to_label(op_seq, _graph.operations()));
 #endif
     _subject.notifyJobBegin(this, op_seq, backend);
     code.fn_seq->run();