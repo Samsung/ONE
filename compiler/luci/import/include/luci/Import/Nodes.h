d7adbf386ba10911b0cf34a7b248d444a0fcdbb7
Denis Krylov <krylovdenism@gmail.com>
2020-06-05 14:47:12 +0300

    [luci] Support ReverseSequence operator
    Support ReverseSequence operator in luci
    
    ONE-DCO-1.0-Signed-off-by: Denis Krylov <krylovdenism@gmail.com>

--- compiler/luci/import/include/luci/Import/Nodes.h
+++ compiler/luci/import/include/luci/Import/Nodes.h
@@ -82,6 +82,7 @@
 #include "Nodes/CircleReshape.h"
 #include "Nodes/CircleResizeBilinear.h"
 #include "Nodes/CircleResizeNearestNeighbor.h"
+#include "Nodes/CircleReverseSequence.h"
 #include "Nodes/CircleRound.h"
 #include "Nodes/CircleRsqrt.h"
 #include "Nodes/CircleScatterNd.h"
