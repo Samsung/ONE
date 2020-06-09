c78c89909bc34f1f540f97ddfe29fbcf3afe17bc
struss <rrstrous@nate.com>
2020-06-09 15:58:53 +0900

    [luci-interpreter]Add Elu Kernel
    This commit Add Elu kernel on luci-interpreter.
    
    ONE-DCO-1.0-Signed-off-by: KiDeuk Bang <rrstrous@nate.com>

--- compiler/luci-interpreter/src/KernelBuilder.h
+++ compiler/luci-interpreter/src/KernelBuilder.h
@@ -40,6 +40,7 @@ public:
   std::unique_ptr<Kernel> visit(const luci::CircleConv2D *node) override;
   std::unique_ptr<Kernel> visit(const luci::CircleConst *node) override;
   std::unique_ptr<Kernel> visit(const luci::CircleDepthwiseConv2D *node) override;
+  std::unique_ptr<Kernel> visit(const luci::CircleElu *node) override;
   std::unique_ptr<Kernel> visit(const luci::CircleFullyConnected *node) override;
   std::unique_ptr<Kernel> visit(const luci::CircleLogistic *node) override;
   std::unique_ptr<Kernel> visit(const luci::CircleInput *node) override;
