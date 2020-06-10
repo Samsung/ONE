973ca5f238783584d918b862529a8c724ed7328c
SaeHie Park <saehie.park@gmail.com>
2020-06-11 08:10:12 +0900

    [luci] Negative tests for CircleBatchToSpaceND IR
    This will add some negative tests for CircleBatchToSpaceND IR
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/lang/src/Nodes/CircleBatchToSpaceND.test.cpp
+++ compiler/luci/lang/src/Nodes/CircleBatchToSpaceND.test.cpp
@@ -17,6 +17,7 @@
 #include "luci/IR/Nodes/CircleBatchToSpaceND.h"
 
 #include "luci/IR/CircleDialect.h"
+#include "luci/IR/CircleNodeVisitor.h"
 
 #include <gtest/gtest.h>
 
@@ -30,4 +31,56 @@ TEST(CircleBatchToSpaceNDTest, constructor)
   ASSERT_EQ(nullptr, bts_node.input());
   ASSERT_EQ(nullptr, bts_node.block_shape());
   ASSERT_EQ(nullptr, bts_node.crops());
+}
+
+TEST(CircleBatchToSpaceNDTest, input_NEG)
+{
+  luci::CircleBatchToSpaceND bts_node;
+  luci::CircleBatchToSpaceND node;
+
+  bts_node.input(&node);
+  bts_node.block_shape(&node);
+  bts_node.crops(&node);
+  ASSERT_NE(nullptr, bts_node.input());
+  ASSERT_NE(nullptr, bts_node.block_shape());
+  ASSERT_NE(nullptr, bts_node.crops());
+
+  bts_node.input(nullptr);
+  bts_node.block_shape(nullptr);
+  bts_node.crops(nullptr);
+  ASSERT_EQ(nullptr, bts_node.input());
+  ASSERT_EQ(nullptr, bts_node.block_shape());
+  ASSERT_EQ(nullptr, bts_node.crops());
+}
+
+TEST(CircleBatchToSpaceNDTest, arity_NEG)
+{
+  luci::CircleBatchToSpaceND bts_node;
+
+  ASSERT_NO_THROW(bts_node.arg(2));
+  ASSERT_THROW(bts_node.arg(3), std::out_of_range);
+}
+
+TEST(CircleBatchToSpaceNDTest, visit_mutable_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeMutableVisitor<void>
+  {
+  };
+
+  luci::CircleBatchToSpaceND bts_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(bts_node.accept(&tv), std::exception);
+}
+
+TEST(CircleBatchToSpaceNDTest, visit_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeVisitor<void>
+  {
+  };
+
+  luci::CircleBatchToSpaceND bts_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(bts_node.accept(&tv), std::exception);
 }
