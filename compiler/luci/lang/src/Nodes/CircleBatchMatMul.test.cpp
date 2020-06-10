c60a182d36a6fa524a845b9eda0edf9c08d8bb3a
SaeHie Park <saehie.park@gmail.com>
2020-06-11 08:07:53 +0900

    [luci] Negative tests for CircleBatchMatMul IR
    This will add some negative tests for CircleBatchMatMul IR
    
    ONE-DCO-1.0-ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/lang/src/Nodes/CircleBatchMatMul.test.cpp
+++ compiler/luci/lang/src/Nodes/CircleBatchMatMul.test.cpp
@@ -17,6 +17,7 @@
 #include "luci/IR/Nodes/CircleBatchMatMul.h"
 
 #include "luci/IR/CircleDialect.h"
+#include "luci/IR/CircleNodeVisitor.h"
 
 #include <gtest/gtest.h>
 
@@ -32,4 +33,52 @@ TEST(CircleBatchMatMulTest, constructor)
 
   ASSERT_FALSE(batchmatmul_node.adj_x());
   ASSERT_FALSE(batchmatmul_node.adj_y());
+}
+
+TEST(CircleBatchMatMulTest, input_NEG)
+{
+  luci::CircleBatchMatMul batchmatmul_node;
+  luci::CircleBatchMatMul node;
+
+  batchmatmul_node.x(&node);
+  batchmatmul_node.y(&node);
+  ASSERT_NE(nullptr, batchmatmul_node.x());
+  ASSERT_NE(nullptr, batchmatmul_node.y());
+
+  batchmatmul_node.x(nullptr);
+  batchmatmul_node.y(nullptr);
+  ASSERT_EQ(nullptr, batchmatmul_node.x());
+  ASSERT_EQ(nullptr, batchmatmul_node.y());
+}
+
+TEST(CircleBatchMatMulTest, arity_NEG)
+{
+  luci::CircleBatchMatMul batchmatmul_node;
+
+  ASSERT_NO_THROW(batchmatmul_node.arg(1));
+  ASSERT_THROW(batchmatmul_node.arg(2), std::out_of_range);
+}
+
+TEST(CircleBatchMatMulTest, visit_mutable_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeMutableVisitor<void>
+  {
+  };
+
+  luci::CircleBatchMatMul batchmatmul_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(batchmatmul_node.accept(&tv), std::exception);
+}
+
+TEST(CircleBatchMatMulTest, visit_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeVisitor<void>
+  {
+  };
+
+  luci::CircleBatchMatMul batchmatmul_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(batchmatmul_node.accept(&tv), std::exception);
 }
