6cdd2cf45b7f82dd7a7e70261a60a90d8b5101bc
binarman <efimov.alexander@gmail.com>
2020-06-10 19:24:55 +0300

    [res] TensorFlow python examples of conv operator
    Add examples on ordinary and dilated conv operators
    
    ONE-DCO-1.0-Signed-off-by: Alexander Efimov <a.efimov@samsung.com>

---
+++ res/TensorFlowPythonExamples/examples/conv_2/__init__.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import numpy as np
+
+in_ = tf.compat.v1.placeholder(tf.float32, shape=(1, 32, 32, 3), name="Hole")
+
+filters = np.array(
+    np.random.uniform(low=-1., high=1, size=[5, 5, 3, 32]), dtype=np.float32)
+strides = (1, 2, 2, 1)
+dilations = (2, 2)
+
+op_ = tf.compat.v1.nn.conv2d(
+    in_, filters, strides, "VALID", data_format="NHWC", dilations=dilations)
