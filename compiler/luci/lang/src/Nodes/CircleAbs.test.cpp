8426b2fda23cead0c0cfa780d5907f7cffa54408
SaeHie Park <saehie.park@gmail.com>
2020-06-10 16:58:22 +0900

    [luci] Negative tests for CircleAbs IR (#2096)
    This will add some negative tests for CircleAbs IR
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/lang/src/Nodes/CircleAbs.test.cpp
+++ compiler/luci/lang/src/Nodes/CircleAbs.test.cpp
@@ -17,8 +17,11 @@
 #include "luci/IR/Nodes/CircleAbs.h"
 
 #include "luci/IR/CircleDialect.h"
+#include "luci/IR/CircleNodeVisitor.h"
 
 #include <gtest/gtest.h>
+
+#include <memory>
 
 TEST(CircleAbsTest, constructor)
 {
@@ -28,4 +31,64 @@ TEST(CircleAbsTest, constructor)
   ASSERT_EQ(luci::CircleOpcode::ABS, abs_node.opcode());
 
   ASSERT_EQ(nullptr, abs_node.x());
+}
+
+TEST(CircleAbsTest, common_NEG)
+{
+  luci::CircleAbs abs_node;
+
+  abs_node.name("name");
+  ASSERT_EQ("name", abs_node.name());
+
+  auto q = std::make_unique<luci::CircleQuantParam>();
+  abs_node.quantparam(std::move(q));
+  ASSERT_NE(nullptr, abs_node.quantparam());
+
+  ASSERT_EQ(luci::ShapeStatus::UNDEFINED, abs_node.shape_status());
+  abs_node.shape_status(luci::ShapeStatus::NOSHAPE);
+  ASSERT_NE(luci::ShapeStatus::UNDEFINED, abs_node.shape_status());
+}
+
+TEST(CircleAbsTest, input_NEG)
+{
+  luci::CircleAbs abs_node;
+  luci::CircleAbs node;
+
+  abs_node.x(&node);
+  ASSERT_NE(nullptr, abs_node.x());
+
+  abs_node.x(nullptr);
+  ASSERT_EQ(nullptr, abs_node.x());
+}
+
+TEST(CircleAbsTest, arity_NEG)
+{
+  luci::CircleAbs abs_node;
+
+  ASSERT_NO_THROW(abs_node.arg(0));
+  ASSERT_THROW(abs_node.arg(1), std::out_of_range);
+}
+
+TEST(CircleAbsTest, visit_mutable_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeMutableVisitor<void>
+  {
+  };
+
+  luci::CircleAbs abs_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(abs_node.accept(&tv), std::exception);
+}
+
+TEST(CircleAbsTest, visit_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeVisitor<void>
+  {
+  };
+
+  luci::CircleAbs abs_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(abs_node.accept(&tv), std::exception);
 }
