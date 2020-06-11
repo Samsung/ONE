a227fce431704f257dd7edbddd073c824617a4f0
SaeHie Park <saehie.park@gmail.com>
2020-06-11 15:47:20 +0900

    [luci] Negative tests for CircleStridedSlice +2 (#2163)
    This will add some negative tests for CircleStridedSlice, CircleSub and CircleSum IR
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/lang/src/Nodes/CircleSub.test.cpp
+++ compiler/luci/lang/src/Nodes/CircleSub.test.cpp
@@ -17,6 +17,7 @@
 #include "luci/IR/Nodes/CircleSub.h"
 
 #include "luci/IR/CircleDialect.h"
+#include "luci/IR/CircleNodeVisitor.h"
 
 #include <gtest/gtest.h>
 
@@ -29,4 +30,52 @@ TEST(CircleSubTest, constructor_P)
 
   ASSERT_EQ(nullptr, sub_node.x());
   ASSERT_EQ(nullptr, sub_node.y());
+}
+
+TEST(CircleSubTest, input_NEG)
+{
+  luci::CircleSub sub_node;
+  luci::CircleSub node;
+
+  sub_node.x(&node);
+  sub_node.y(&node);
+  ASSERT_NE(nullptr, sub_node.x());
+  ASSERT_NE(nullptr, sub_node.y());
+
+  sub_node.x(nullptr);
+  sub_node.y(nullptr);
+  ASSERT_EQ(nullptr, sub_node.x());
+  ASSERT_EQ(nullptr, sub_node.y());
+}
+
+TEST(CircleSubTest, arity_NEG)
+{
+  luci::CircleSub sub_node;
+
+  ASSERT_NO_THROW(sub_node.arg(1));
+  ASSERT_THROW(sub_node.arg(2), std::out_of_range);
+}
+
+TEST(CircleSubTest, visit_mutable_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeMutableVisitor<void>
+  {
+  };
+
+  luci::CircleSub sub_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(sub_node.accept(&tv), std::exception);
+}
+
+TEST(CircleSubTest, visit_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeVisitor<void>
+  {
+  };
+
+  luci::CircleSub sub_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(sub_node.accept(&tv), std::exception);
 }
