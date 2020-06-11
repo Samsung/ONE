108e685dca07a222ef08dc1f2d18030ca40d0aad
binarman <efimov.alexander@gmail.com>
2020-06-11 12:30:55 +0300

    review fix 2

--- res/TensorFlowPythonExamples/examples/conv2d_2/__init__.py
+++ res/TensorFlowPythonExamples/examples/conv2d_2/__init__.py
@@ -3,8 +3,7 @@ import numpy as np
 
 in_ = tf.compat.v1.placeholder(tf.float32, shape=(1, 32, 32, 3), name="Hole")
 
-filters = np.array(
-    np.random.uniform(low=-1., high=1, size=[5, 5, 3, 32]), dtype=np.float32)
+filters = np.random.uniform(low=-1., high=1, size=[5, 5, 3, 32]).astype(np.float32)
 strides = (1, 2, 2, 1)
 dilations = (1, 2, 2, 1)
 
