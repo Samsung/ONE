b02daf35b510f2ed8c905de40d0325202904c4a5
SaeHie Park <saehie.park@gmail.com>
2020-06-11 11:03:43 +0900

    [luci] Negative tests for CircleLog +3
    This will add some negative tests for CircleLocalResponseNormalization, CircleLog, CircleLogistic and CircleMaxPool2D IR
    - also revise attributes tests for CircleMaxPool2D
    
    ONE-DCO-1.0-ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/lang/src/Nodes/CircleLocalResponseNormalization.test.cpp
+++ compiler/luci/lang/src/Nodes/CircleLocalResponseNormalization.test.cpp
@@ -17,6 +17,7 @@
 #include "luci/IR/Nodes/CircleLocalResponseNormalization.h"
 
 #include "luci/IR/CircleDialect.h"
+#include "luci/IR/CircleNodeVisitor.h"
 
 #include <gtest/gtest.h>
 
@@ -33,4 +34,57 @@ TEST(CircleLocalResponseNormalizationTest, constructor_P)
   ASSERT_EQ(1.0f, local_response_normalization_node.bias());
   ASSERT_EQ(1.0f, local_response_normalization_node.alpha());
   ASSERT_EQ(0.5f, local_response_normalization_node.beta());
+}
+
+TEST(CircleLocalResponseNormalizationTest, input_NEG)
+{
+  luci::CircleLocalResponseNormalization local_response_normalization_node;
+  luci::CircleLocalResponseNormalization node;
+
+  local_response_normalization_node.input(&node);
+  ASSERT_NE(nullptr, local_response_normalization_node.input());
+
+  local_response_normalization_node.input(nullptr);
+  ASSERT_EQ(nullptr, local_response_normalization_node.input());
+
+  local_response_normalization_node.radius(100);
+  local_response_normalization_node.bias(100.0f);
+  local_response_normalization_node.alpha(100.0f);
+  local_response_normalization_node.beta(100.0f);
+  ASSERT_NE(5, local_response_normalization_node.radius());
+  ASSERT_NE(1.0f, local_response_normalization_node.bias());
+  ASSERT_NE(1.0f, local_response_normalization_node.alpha());
+  ASSERT_NE(0.5f, local_response_normalization_node.beta());
+}
+
+TEST(CircleLocalResponseNormalizationTest, arity_NEG)
+{
+  luci::CircleLocalResponseNormalization local_response_normalization_node;
+
+  ASSERT_NO_THROW(local_response_normalization_node.arg(0));
+  ASSERT_THROW(local_response_normalization_node.arg(1), std::out_of_range);
+}
+
+TEST(CircleLocalResponseNormalizationTest, visit_mutable_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeMutableVisitor<void>
+  {
+  };
+
+  luci::CircleLocalResponseNormalization local_response_normalization_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(local_response_normalization_node.accept(&tv), std::exception);
+}
+
+TEST(CircleLocalResponseNormalizationTest, visit_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeVisitor<void>
+  {
+  };
+
+  luci::CircleLocalResponseNormalization local_response_normalization_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(local_response_normalization_node.accept(&tv), std::exception);
 }
