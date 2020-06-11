420c899ba0009a00277b8b6c49f16ff9a25a2249
binarman <efimov.alexander@gmail.com>
2020-06-11 11:20:17 +0300

    review fix

--- res/TensorFlowPythonExamples/examples/conv2d_2/__init__.py
+++ res/TensorFlowPythonExamples/examples/conv2d_2/__init__.py
@@ -6,7 +6,7 @@ in_ = tf.compat.v1.placeholder(tf.float32, shape=(1, 32, 32, 3), name="Hole")
 filters = np.array(
     np.random.uniform(low=-1., high=1, size=[5, 5, 3, 32]), dtype=np.float32)
 strides = (1, 2, 2, 1)
-dilations = (2, 2)
+dilations = (1, 2, 2, 1)
 
 op_ = tf.compat.v1.nn.conv2d(
     in_, filters, strides, "VALID", data_format="NHWC", dilations=dilations)
