f6e8f6dd7946fae550926ec3a137846ecb2525b0
SaeHie Park <saehie.park@gmail.com>
2020-06-11 13:15:51 +0900

    [luci] Negative tests for CircleOneHot +3 (#2149)
    This will add some negative tests for CircleOneHot, CirclePack, CirclePad and CirclePow IR
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/lang/src/Nodes/CirclePow.test.cpp
+++ compiler/luci/lang/src/Nodes/CirclePow.test.cpp
@@ -17,6 +17,7 @@
 #include "luci/IR/Nodes/CirclePow.h"
 
 #include "luci/IR/CircleDialect.h"
+#include "luci/IR/CircleNodeVisitor.h"
 
 #include <gtest/gtest.h>
 
@@ -29,4 +30,52 @@ TEST(CirclePowTest, constructor_P)
 
   ASSERT_EQ(nullptr, pow_node.x());
   ASSERT_EQ(nullptr, pow_node.y());
+}
+
+TEST(CirclePowTest, input_NEG)
+{
+  luci::CirclePow pow_node;
+  luci::CirclePow node;
+
+  pow_node.x(&node);
+  pow_node.y(&node);
+  ASSERT_NE(nullptr, pow_node.x());
+  ASSERT_NE(nullptr, pow_node.y());
+
+  pow_node.x(nullptr);
+  pow_node.y(nullptr);
+  ASSERT_EQ(nullptr, pow_node.x());
+  ASSERT_EQ(nullptr, pow_node.y());
+}
+
+TEST(CirclePowTest, arity_NEG)
+{
+  luci::CirclePow pow_node;
+
+  ASSERT_NO_THROW(pow_node.arg(1));
+  ASSERT_THROW(pow_node.arg(2), std::out_of_range);
+}
+
+TEST(CirclePowTest, visit_mutable_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeMutableVisitor<void>
+  {
+  };
+
+  luci::CirclePow pow_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(pow_node.accept(&tv), std::exception);
+}
+
+TEST(CirclePowTest, visit_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeVisitor<void>
+  {
+  };
+
+  luci::CirclePow pow_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(pow_node.accept(&tv), std::exception);
 }
