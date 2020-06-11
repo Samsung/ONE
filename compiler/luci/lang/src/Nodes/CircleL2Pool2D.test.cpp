cd327aea2e542d6081485957baf038e1e7b0a3fd
SaeHie Park <saehie.park@gmail.com>
2020-06-11 10:59:28 +0900

    [luci] Negative tests for CircleInstanceNorm +3
    This will add some negative tests for CircleInstanceNorm, CircleL2Pool2D, CircleLess and CircleLessEqual IR
    - and revise CircleL2Pool2D attributes test in CTOR
    
    ONE-DCO-1.0-ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/lang/src/Nodes/CircleL2Pool2D.test.cpp
+++ compiler/luci/lang/src/Nodes/CircleL2Pool2D.test.cpp
@@ -17,6 +17,7 @@
 #include "luci/IR/Nodes/CircleL2Pool2D.h"
 
 #include "luci/IR/CircleDialect.h"
+#include "luci/IR/CircleNodeVisitor.h"
 
 #include <gtest/gtest.h>
 
@@ -28,6 +29,66 @@ TEST(CircleL2Pool2DTest, constructor)
   ASSERT_EQ(luci::CircleOpcode::L2_POOL_2D, l2pool2d_node.opcode());
 
   ASSERT_EQ(nullptr, l2pool2d_node.value());
-  ASSERT_NE(l2pool2d_node.filter(), nullptr);
-  ASSERT_NE(l2pool2d_node.stride(), nullptr);
+  ASSERT_EQ(1, l2pool2d_node.filter()->h());
+  ASSERT_EQ(1, l2pool2d_node.filter()->w());
+  ASSERT_EQ(1, l2pool2d_node.stride()->h());
+  ASSERT_EQ(1, l2pool2d_node.stride()->w());
+  ASSERT_EQ(luci::FusedActFunc::UNDEFINED, l2pool2d_node.fusedActivationFunction());
+}
+
+TEST(CircleL2Pool2DTest, input_NEG)
+{
+  luci::CircleL2Pool2D l2pool2d_node;
+  luci::CircleL2Pool2D node;
+
+  l2pool2d_node.value(&node);
+  ASSERT_NE(nullptr, l2pool2d_node.value());
+
+  l2pool2d_node.value(nullptr);
+  ASSERT_EQ(nullptr, l2pool2d_node.value());
+
+  l2pool2d_node.stride()->h(2);
+  l2pool2d_node.stride()->w(2);
+  ASSERT_EQ(2, l2pool2d_node.stride()->h());
+  ASSERT_EQ(2, l2pool2d_node.stride()->w());
+
+  l2pool2d_node.filter()->h(2);
+  l2pool2d_node.filter()->w(2);
+  ASSERT_EQ(2, l2pool2d_node.filter()->h());
+  ASSERT_EQ(2, l2pool2d_node.filter()->w());
+
+  l2pool2d_node.fusedActivationFunction(luci::FusedActFunc::RELU);
+  ASSERT_NE(luci::FusedActFunc::UNDEFINED, l2pool2d_node.fusedActivationFunction());
+}
+
+TEST(CircleL2Pool2DTest, arity_NEG)
+{
+  luci::CircleL2Pool2D l2pool2d_node;
+
+  ASSERT_NO_THROW(l2pool2d_node.arg(0));
+  ASSERT_THROW(l2pool2d_node.arg(1), std::out_of_range);
+}
+
+TEST(CircleL2Pool2DTest, visit_mutable_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeMutableVisitor<void>
+  {
+  };
+
+  luci::CircleL2Pool2D l2pool2d_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(l2pool2d_node.accept(&tv), std::exception);
+}
+
+TEST(CircleL2Pool2DTest, visit_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeVisitor<void>
+  {
+  };
+
+  luci::CircleL2Pool2D l2pool2d_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(l2pool2d_node.accept(&tv), std::exception);
 }
