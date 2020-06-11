ae6ddbb2bbd3a2f73500b8b204c2df7921d6b8d6
SaeHie Park <saehie.park@gmail.com>
2020-06-11 12:58:25 +0900

    [luci] Negative tests for CircleReshape +2
    This will add some negative tests for CircleReshape, CircleResizeBilinear and CircleResizeNearestNeighbor IR
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/lang/src/Nodes/CircleReshape.test.cpp
+++ compiler/luci/lang/src/Nodes/CircleReshape.test.cpp
@@ -17,6 +17,7 @@
 #include "luci/IR/Nodes/CircleReshape.h"
 
 #include "luci/IR/CircleDialect.h"
+#include "luci/IR/CircleNodeVisitor.h"
 
 #include <gtest/gtest.h>
 
@@ -45,4 +46,52 @@ TEST(CircleReshapeTest, alloc_new_shape_P)
   auto &const_reshape = const_cast<const luci::CircleReshape &>(reshape);
   ASSERT_EQ(0, const_reshape.newShape()->dim(0));
   ASSERT_EQ(1, const_reshape.newShape()->dim(1));
+}
+
+TEST(CircleReshapeTest, input_NEG)
+{
+  luci::CircleReshape reshape_node;
+  luci::CircleReshape node;
+
+  reshape_node.tensor(&node);
+  reshape_node.shape(&node);
+  ASSERT_NE(nullptr, reshape_node.tensor());
+  ASSERT_NE(nullptr, reshape_node.shape());
+
+  reshape_node.tensor(nullptr);
+  reshape_node.shape(nullptr);
+  ASSERT_EQ(nullptr, reshape_node.tensor());
+  ASSERT_EQ(nullptr, reshape_node.shape());
+}
+
+TEST(CircleReshapeTest, arity_NEG)
+{
+  luci::CircleReshape reshape_node;
+
+  ASSERT_NO_THROW(reshape_node.arg(1));
+  ASSERT_THROW(reshape_node.arg(2), std::out_of_range);
+}
+
+TEST(CircleReshapeTest, visit_mutable_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeMutableVisitor<void>
+  {
+  };
+
+  luci::CircleReshape reshape_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(reshape_node.accept(&tv), std::exception);
+}
+
+TEST(CircleReshapeTest, visit_NEG)
+{
+  struct TestVisitor final : public luci::CircleNodeVisitor<void>
+  {
+  };
+
+  luci::CircleReshape reshape_node;
+
+  TestVisitor tv;
+  ASSERT_THROW(reshape_node.accept(&tv), std::exception);
 }
