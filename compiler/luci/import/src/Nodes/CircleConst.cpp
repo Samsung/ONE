ad0a3027b6965102c166310fbbff346a8cf45b1e
Seok Namkoong <seok9311@naver.com>
2020-06-12 14:49:18 +0900

    [luci] Introduce S16 type in luci
    This commit will introduce S16 type in `luci`
    
    ONE-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>

--- compiler/luci/import/src/Nodes/CircleConst.cpp
+++ compiler/luci/import/src/Nodes/CircleConst.cpp
@@ -110,6 +110,10 @@ CircleConst *create_circleconst(GraphBuilderContext *context, int32_t tensor_ind
         copy_data<loco::DataType::U8>(buffer, num_elements, const_node);
         break;
 
+      case loco::DataType::S16:
+        copy_data<loco::DataType::S16>(buffer, num_elements, const_node);
+        break;
+
       case loco::DataType::S32:
         copy_data<loco::DataType::S32>(buffer, num_elements, const_node);
         break;
