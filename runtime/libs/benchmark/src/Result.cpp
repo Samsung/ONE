Samsung/ONE

commit 95f301dc907c56a1550ed1b97f88b69002d7fa6a

Revise libbenchmark for nnpackage_run and tflite_run (#2204)
- Expose the only Phases and Result classes
- Phases: a role for acting benchmark such as latency and memory
- Result: the result of benchmark

Signed-off-by: Yongseop Kim <yons.kim@samsung.com>

runtime/libs/benchmark/src/Result.cpp
@@ -1,0 +1,227 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "benchmark/Result.h"
+#include "benchmark/Phases.h"
+#include "benchmark/CsvWriter.h"
+
+#include <string>
+#include <numeric>
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+namespace
+{
+
+template <class T, class R> R average(const std::vector<T> &v)
+{
+  T sum = std::accumulate(v.begin(), v.end(), 0);
+  R avg = sum / static_cast<R>(v.size());
+  return avg;
+}
+
+double averageTimeMs(const benchmark::Phase &phase)
+{
+  double avg_us = average<uint64_t, double>(phase.time);
+  return avg_us / 1e3;
+}
+
+double maxTimeMs(const benchmark::Phase &phase)
+{
+  auto it = max_element(std::begin(phase.time), std::end(phase.time));
+  return *it / 1e3;
+}
+
+double minTimeMs(const benchmark::Phase &phase)
+{
+  auto it = min_element(std::begin(phase.time), std::end(phase.time));
+  return *it / 1e3;
+}
+
+double geomeanTimeMs(const benchmark::Phase &phase)
+{
+  double log_sum = 0.0;
+  for (auto t_us : phase.time)
+  {
+    log_sum += std::log(t_us / 1e3);
+  }
+
+  // Calculating geometric mean with logs
+  //   "Geometric Mean of (V1, V2, ... Vn)"
+  // = (V1*V2*...*Vn)^(1/n)
+  // = exp(log((V1*V2*...*Vn)^(1/n)))
+  // = exp(log((V1*V2*...*Vn)/n)))
+  // = exp((log(V1) + log(V2) + ... + log(Vn))/n)
+  // = exp(_log_sum/num)
+  return std::exp(log_sum / static_cast<double>(phase.time.size()));
+}
+
+uint32_t averageMemoryKb(const benchmark::Phase &phase, int type)
+{
+  return average<uint32_t, uint32_t>(phase.memory[type]);
+}
+
+uint32_t peakMemory(const uint32_t memory[benchmark::PhaseEnum::END_OF_PHASE]
+                                         [benchmark::MemoryType::END_OF_MEM_TYPE],
+                    int type)
+{
+  using namespace benchmark;
+  return std::max({memory[PhaseEnum::MODEL_LOAD][type], memory[PhaseEnum::PREPARE][type],
+                   memory[PhaseEnum::EXECUTE][type]});
+}
+
+void printResultTime(
+    const double time[benchmark::PhaseEnum::END_OF_PHASE][benchmark::FigureType::END_OF_FIG_TYPE])
+{
+  using namespace benchmark;
+
+  std::cout << "===================================" << std::endl;
+
+  std::streamsize ss_precision = std::cout.precision();
+  std::cout << std::setprecision(3);
+  std::cout << std::fixed;
+
+  for (int i = PhaseEnum::MODEL_LOAD; i <= PhaseEnum::EXECUTE; ++i)
+  {
+    // Note. Tricky. Ignore WARMUP
+    if (i == PhaseEnum::WARMUP)
+      continue;
+    std::cout << std::setw(12) << std::left << getPhaseString(i) << " takes "
+              << time[i][FigureType::MEAN] << " ms" << std::endl;
+  }
+
+  for (int j = FigureType::MEAN; j <= FigureType::GEOMEAN; ++j)
+  {
+    std::cout << "- " << std::setw(9) << std::left << getFigureTypeString(j) << ":  "
+              << time[PhaseEnum::EXECUTE][j] << " ms" << std::endl;
+  }
+
+  std::cout << std::setprecision(ss_precision);
+  std::cout << std::defaultfloat;
+
+  std::cout << "===================================" << std::endl;
+}
+
+void printResultMemory(const uint32_t memory[benchmark::PhaseEnum::END_OF_PHASE]
+                                            [benchmark::MemoryType::END_OF_MEM_TYPE])
+{
+  using namespace benchmark;
+
+  for (int j = MemoryType::RSS; j <= MemoryType::PSS; ++j)
+  {
+    std::cout << getMemoryTypeString(j) << std::endl;
+    for (int i = PhaseEnum::MODEL_LOAD; i <= PhaseEnum::PREPARE; ++i)
+    {
+      std::cout << "- " << std::setw(12) << std::left << getPhaseString(i) << " takes "
+                << memory[i][j] << " kb" << std::endl;
+    }
+    // Tricky. Handle WARMUP as EXECUTE
+    std::cout << "- " << std::setw(12) << std::left << getPhaseString(PhaseEnum::EXECUTE)
+              << " takes " << memory[PhaseEnum::WARMUP][j] << " kb" << std::endl;
+    std::cout << "- " << std::setw(12) << std::left << "PEAK"
+              << " takes " << peakMemory(memory, j) << " kb" << std::endl;
+    std::cout << "===================================" << std::endl;
+  }
+}
+
+} // namespace
+
+namespace benchmark
+{
+
+Result::Result(const Phases &phases)
+{
+  const auto option = phases.option();
+  {
+    for (int i = PhaseEnum::MODEL_LOAD; i <= PhaseEnum::PREPARE; ++i)
+    {
+      auto phase = phases.at(gPhaseStrings[i]);
+      time[i][FigureType::MEAN] = averageTimeMs(phase);
+    }
+
+    int i = PhaseEnum::EXECUTE;
+    auto exec_phase = phases.at(gPhaseStrings[i]);
+    time[i][FigureType::MEAN] = averageTimeMs(exec_phase);
+    time[i][FigureType::MAX] = maxTimeMs(exec_phase);
+    time[i][FigureType::MIN] = minTimeMs(exec_phase);
+    time[i][FigureType::GEOMEAN] = geomeanTimeMs(exec_phase);
+  }
+  if (option.memory)
+  {
+    print_memory = true;
+    for (int i = PhaseEnum::MODEL_LOAD; i <= PhaseEnum::EXECUTE; ++i)
+    {
+      auto phase = phases.at(gPhaseStrings[i]);
+      for (int j = MemoryType::RSS; j <= MemoryType::PSS; ++j)
+      {
+        memory[i][j] = averageMemoryKb(phase, j);
+      }
+    }
+  }
+}
+
+void printResult(const Result &result)
+{
+  printResultTime(result.time);
+
+  if (result.print_memory == false)
+    return;
+
+  printResultMemory(result.memory);
+}
+
+// TODO There are necessary for a kind of output data file so that it doesn't have to be csv file
+// format.
+void writeResult(const Result &result, const std::string &exec, const std::string &model,
+                 const std::string &backend)
+{
+  std::string csv_filename = exec + "-" + model + "-" + backend + ".csv";
+
+  // write to csv
+  CsvWriter writer(csv_filename);
+  writer << model << backend;
+
+  // TODO Add GEOMEAN
+  // time
+  auto time = result.time;
+  writer << time[PhaseEnum::MODEL_LOAD][FigureType::MEAN]
+         << time[PhaseEnum::PREPARE][FigureType::MEAN] << time[PhaseEnum::EXECUTE][FigureType::MIN]
+         << time[PhaseEnum::EXECUTE][FigureType::MAX] << time[PhaseEnum::EXECUTE][FigureType::MEAN];
+
+  // memory
+  auto memory = result.memory;
+  for (int j = MemoryType::RSS; j <= MemoryType::PSS; ++j)
+  {
+    // Tricky. Handle WARMUP as EXECUTE
+    for (int i = PhaseEnum::MODEL_LOAD; i <= PhaseEnum::WARMUP; ++i)
+    {
+      writer << memory[i][j];
+    }
+    writer << peakMemory(memory, j);
+  }
+
+  bool done = writer.done();
+
+  if (!done)
+  {
+    std::cerr << "Writing to " << csv_filename << " is failed" << std::endl;
+  }
+}
+
+} // namespace benchmark

runtime/libs/benchmark/include/benchmark.h
@@ -17,9 +17,7 @@
 #ifndef __NNFW_BENCHMARK_H__
 #define __NNFW_BENCHMARK_H__
 
+#include "benchmark/Phases.h"
 #include "benchmark/Result.h"
-#include "benchmark/MemoryPoller.h"
-#include "benchmark/CsvWriter.h"
-#include "benchmark/Util.h"
 
 #endif // __NNFW_BENCHMARK_H__

runtime/libs/benchmark/include/benchmark/MemoryPoller.h
@@ -27,7 +27,7 @@
 #include <condition_variable>
 #include <list>
 
-#include "Phase.h"
+#include "Types.h"
 
 namespace benchmark
 {
@@ -48,11 +48,11 @@ public:
     _thread.join();
   }
 
-  bool start(Phase phase);
-  bool end(Phase phase);
-  const std::unordered_map<Phase, uint32_t> &getRssMap() const { return _rss_map; }
-  const std::unordered_map<Phase, uint32_t> &getHwmMap() const { return _hwm_map; }
-  const std::unordered_map<Phase, uint32_t> &getPssMap() const { return _pss_map; }
+  bool start(PhaseEnum phase);
+  bool end(PhaseEnum phase);
+  const std::unordered_map<PhaseEnum, uint32_t> &getRssMap() const { return _rss_map; }
+  const std::unordered_map<PhaseEnum, uint32_t> &getHwmMap() const { return _hwm_map; }
+  const std::unordered_map<PhaseEnum, uint32_t> &getPssMap() const { return _pss_map; }
 
 private:
   void process();
@@ -65,10 +65,10 @@ private:
 private:
   std::chrono::milliseconds _duration;
   std::thread _thread;
-  std::list<Phase> _phases;
-  std::unordered_map<Phase, uint32_t> _rss_map;
-  std::unordered_map<Phase, uint32_t> _hwm_map;
-  std::unordered_map<Phase, uint32_t> _pss_map;
+  std::list<PhaseEnum> _phases;
+  std::unordered_map<PhaseEnum, uint32_t> _rss_map;
+  std::unordered_map<PhaseEnum, uint32_t> _hwm_map;
+  std::unordered_map<PhaseEnum, uint32_t> _pss_map;
 
   std::mutex _mutex;
   std::mutex _mutex_started;

runtime/libs/benchmark/include/benchmark/Phase.h
@@ -17,45 +17,50 @@
 #ifndef __NNFW_BENCHMARK_PHASE_H__
 #define __NNFW_BENCHMARK_PHASE_H__
 
+#include "Types.h"
+
 #include <string>
-#include <cassert>
+#include <vector>
 
 namespace benchmark
 {
 
-enum class Phase
+struct Phase
 {
-  MODEL_LOAD,
-  PREPARE,
-  EXECUTE,
+  Phase(const std::string &t, uint32_t c) : tag(t), count(c)
+  {
+    // DO NOTHING
+  }
+
+  const std::string tag;
+  uint32_t count;
+  std::vector<uint64_t> time;                                // us
+  std::vector<uint32_t> memory[MemoryType::END_OF_MEM_TYPE]; // kB
 };
 
-inline std::string getPhaseString(Phase phase)
+struct PhaseOption
 {
-  switch (phase)
-  {
-    case Phase::MODEL_LOAD:
-      return "MODEL_LOAD";
-    case Phase::PREPARE:
-      return "PREPARE";
-    case Phase::EXECUTE:
-      return "EXECUTE";
-    default:
-      assert(false);
-      return "";
-  }
-}
+  PhaseOption() : memory(false), memory_gpu(false) {}
+  PhaseOption(bool m) : memory(m) {}
+  PhaseOption(bool m, int64_t ms) : memory(m), memory_gpu(false), memory_interval(ms) {}
+  PhaseOption(bool m, bool mg) : memory(m), memory_gpu(mg) {}
+  PhaseOption(bool m, bool mg, int64_t ms) : memory(m), memory_gpu(mg), memory_interval(ms) {}
+
+  bool memory = false;
+  bool memory_gpu = false;     // very specific...
+  int64_t memory_interval = 5; // ms
+};
 
 } // namespace benchmark
 
 namespace std
 {
 
-template <> struct hash<benchmark::Phase>
+template <> struct hash<benchmark::PhaseEnum>
 {
-  size_t operator()(benchmark::Phase value) const noexcept
+  size_t operator()(benchmark::PhaseEnum value) const noexcept
   {
-    using type = typename std::underlying_type<benchmark::Phase>::type;
+    using type = typename std::underlying_type<benchmark::PhaseEnum>::type;
     return hash<type>()(static_cast<type>(value));
   }
 };

runtime/libs/benchmark/include/benchmark/Phases.h
@@ -1,0 +1,65 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __NNFW_BENCHMARK_PHASES_H__
+#define __NNFW_BENCHMARK_PHASES_H__
+
+#include "Phase.h"
+#include "MemoryPoller.h"
+
+#include <string>
+#include <functional>
+#include <unordered_map>
+
+namespace benchmark
+{
+
+class Phases
+{
+public:
+  Phases(const PhaseOption &option);
+
+  using PhaseFunc = std::function<void(const Phase &, uint32_t)>;
+
+  void run(const std::string &tag, const PhaseFunc &exec, uint32_t loop_num = 1,
+           bool option_disable = false)
+  {
+    run(tag, exec, nullptr, loop_num, option_disable);
+  }
+
+  void run(const std::string &tag, const PhaseFunc &exec, const PhaseFunc &post,
+           uint32_t loop_num = 1, bool option_disable = false)
+  {
+    run(tag, exec, &post, loop_num, option_disable);
+  }
+
+  const PhaseOption &option() const { return _option; }
+  const MemoryPoller &mem_poll() const { return _mem_poll; }
+  const Phase &at(const std::string &tag) const { return _phases.at(tag); }
+
+private:
+  void run(const std::string &tag, const PhaseFunc &exec, const PhaseFunc *post, uint32_t loop_num,
+           bool option_disable);
+
+private:
+  const PhaseOption _option;
+  std::unordered_map<std::string, Phase> _phases;
+  MemoryPoller _mem_poll;
+};
+
+} // namespace benchmark
+
+#endif // __NNFW_BENCHMARK_PHASES_H__

runtime/libs/benchmark/include/benchmark/Result.h
@@ -17,30 +17,10 @@
 #ifndef __NNFW_BENCHMARK_RESULT_H__
 #define __NNFW_BENCHMARK_RESULT_H__
 
-#include "Phase.h"
-#include "MemoryPoller.h"
+#include "Types.h"
+#include "Phases.h"
 
 #include <string>
-#include <vector>
-#include <numeric>
-#include <algorithm>
-#include <cassert>
-#include <memory>
-#include <cmath>
-
-namespace
-{
-
-uint32_t maxMemory(const std::unordered_map<benchmark::Phase, uint32_t> &map)
-{
-  auto answer = *std::max_element(
-      map.begin(), map.end(),
-      [](const std::pair<benchmark::Phase, uint32_t> &p1,
-         const std::pair<benchmark::Phase, uint32_t> &p2) { return p1.second < p2.second; });
-  return answer.second;
-}
-
-} // namespace
 
 namespace benchmark
 {
@@ -49,126 +29,19 @@ namespace benchmark
 class Result
 {
 public:
-  Result(double model_load_time, double prepare_time, const std::vector<double> &execute_times)
-      : _model_load_time(model_load_time), _prepare_time(prepare_time), _model_load_rss(0),
-        _prepare_rss(0), _execute_rss(0), _peak_rss(0), _model_load_hwm(0), _prepare_hwm(0),
-        _execute_hwm(0), _peak_hwm(0), _model_load_pss(0), _prepare_pss(0), _execute_pss(0),
-        _peak_pss(0)
-  {
-    // execute
-    double sum = 0.0;
-    double log_sum = 0.0;
-    double min = std::numeric_limits<double>::max();
-    double max = std::numeric_limits<double>::lowest();
-    for (auto t : execute_times)
-    {
-      sum += t;
-      log_sum += std::log(t);
-      min = std::min(min, t);
-      max = std::max(max, t);
-    }
-    _execute_time_mean = sum / static_cast<double>(execute_times.size());
-    _execute_time_min = min;
-    _execute_time_max = max;
+  Result(const Phases &phases);
 
-    // Calculating geometric mean with logs
-    //   "Geometric Mean of (V1, V2, ... Vn)"
-    // = (V1*V2*...*Vn)^(1/n)
-    // = exp(log((V1*V2*...*Vn)^(1/n)))
-    // = exp(log((V1*V2*...*Vn)/n)))
-    // = exp((log(V1) + log(V2) + ... + log(Vn))/n)
-    // = exp(_log_sum/num)
-    _execute_time_geomean = std::exp(log_sum / static_cast<double>(execute_times.size()));
-  }
-
-  Result(double model_load_time, double prepare_time, const std::vector<double> &execute_times,
-         const std::unique_ptr<MemoryPoller> &memory_poller)
-      : Result(model_load_time, prepare_time, execute_times)
-  {
-    if (!memory_poller)
-      return;
-
-    const auto &rss = memory_poller->getRssMap();
-    const auto &hwm = memory_poller->getHwmMap();
-    const auto &pss = memory_poller->getPssMap();
-
-    // rss
-    assert(rss.size() > 0);
-    assert(rss.find(Phase::MODEL_LOAD) != rss.end());
-    assert(rss.find(Phase::PREPARE) != rss.end());
-    assert(rss.find(Phase::EXECUTE) != rss.end());
-    _model_load_rss = rss.at(Phase::MODEL_LOAD);
-    _prepare_rss = rss.at(Phase::PREPARE);
-    _execute_rss = rss.at(Phase::EXECUTE);
-    _peak_rss = maxMemory(rss);
-
-    // hwm
-    assert(hwm.size() > 0);
-    assert(hwm.find(Phase::MODEL_LOAD) != hwm.end());
-    assert(hwm.find(Phase::PREPARE) != hwm.end());
-    assert(hwm.find(Phase::EXECUTE) != hwm.end());
-    _model_load_hwm = hwm.at(Phase::MODEL_LOAD);
-    _prepare_hwm = hwm.at(Phase::PREPARE);
-    _execute_hwm = hwm.at(Phase::EXECUTE);
-    _peak_hwm = maxMemory(hwm);
-
-    // pss
-    assert(pss.size() > 0);
-    assert(pss.find(Phase::MODEL_LOAD) != pss.end());
-    assert(pss.find(Phase::PREPARE) != pss.end());
-    assert(pss.find(Phase::EXECUTE) != pss.end());
-    _model_load_pss = pss.at(Phase::MODEL_LOAD);
-    _prepare_pss = pss.at(Phase::PREPARE);
-    _execute_pss = pss.at(Phase::EXECUTE);
-    _peak_pss = maxMemory(pss);
-  }
-
-public:
-  double getModelLoadTime() const { return _model_load_time; }
-  double getPrepareTime() const { return _prepare_time; }
-  double getExecuteTimeMean() const { return _execute_time_mean; }
-  double getExecuteTimeGeoMean() const { return _execute_time_geomean; }
-  double getExecuteTimeMin() const { return _execute_time_min; }
-  double getExecuteTimeMax() const { return _execute_time_max; }
-
-  uint32_t getModelLoadRss() const { return _model_load_rss; }
-  uint32_t getPrepareRss() const { return _prepare_rss; }
-  uint32_t getExecuteRss() const { return _execute_rss; }
-  uint32_t getPeakRss() const { return _peak_rss; }
-
-  uint32_t getModelLoadHwm() const { return _model_load_hwm; }
-  uint32_t getPrepareHwm() const { return _prepare_hwm; }
-  uint32_t getExecuteHwm() const { return _execute_hwm; }
-  uint32_t getPeakHwm() const { return _peak_hwm; }
-
-  uint32_t getModelLoadPss() const { return _model_load_pss; }
-  uint32_t getPreparePss() const { return _prepare_pss; }
-  uint32_t getExecutePss() const { return _execute_pss; }
-  uint32_t getPeakPss() const { return _peak_pss; }
-
-private:
-  double _model_load_time;
-  double _prepare_time;
-  double _execute_time_mean;
-  double _execute_time_geomean;
-  double _execute_time_min;
-  double _execute_time_max;
-
-  uint32_t _model_load_rss;
-  uint32_t _prepare_rss;
-  uint32_t _execute_rss;
-  uint32_t _peak_rss;
-
-  uint32_t _model_load_hwm;
-  uint32_t _prepare_hwm;
-  uint32_t _execute_hwm;
-  uint32_t _peak_hwm;
-
-  uint32_t _model_load_pss;
-  uint32_t _prepare_pss;
-  uint32_t _execute_pss;
-  uint32_t _peak_pss;
+  double time[PhaseEnum::END_OF_PHASE][FigureType::END_OF_FIG_TYPE];
+  uint32_t memory[PhaseEnum::END_OF_PHASE][MemoryType::END_OF_MEM_TYPE];
+  bool print_memory = false;
 };
+
+// TODO Support not only stdout but also ostream
+void printResult(const Result &result);
+
+// TODO Support not only csv but also other datafile format such as xml, json, ...
+void writeResult(const Result &result, const std::string &exec, const std::string &model,
+                 const std::string &backend);
 
 } // namespace benchmark
 

runtime/libs/benchmark/include/benchmark/Types.h
@@ -1,0 +1,133 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <string>
+#include <vector>
+
+#ifndef __NNFW_BENCHMARK_TYPES_H__
+#define __NNFW_BENCHMARK_TYPES_H__
+
+namespace benchmark
+{
+
+enum MemoryType
+{
+  RSS,
+  HWM,
+  PSS,
+  END_OF_MEM_TYPE
+};
+
+inline std::string getMemoryTypeString(MemoryType type)
+{
+  switch (type)
+  {
+    case MemoryType::RSS:
+      return "RSS";
+    case MemoryType::HWM:
+      return "HWM";
+    case MemoryType::PSS:
+      return "PSS";
+    default:
+      return "END_OF_MEM_TYPE";
+  }
+}
+
+inline std::string getMemoryTypeString(int type)
+{
+  return getMemoryTypeString(static_cast<MemoryType>(type));
+}
+
+enum PhaseEnum
+{
+  MODEL_LOAD,
+  PREPARE,
+  WARMUP,
+  EXECUTE,
+  END_OF_PHASE,
+};
+
+inline std::string getPhaseString(PhaseEnum phase)
+{
+  switch (phase)
+  {
+    case PhaseEnum::MODEL_LOAD:
+      return "MODEL_LOAD";
+    case PhaseEnum::PREPARE:
+      return "PREPARE";
+    case PhaseEnum::WARMUP:
+      return "WARMUP";
+    case PhaseEnum::EXECUTE:
+      return "EXECUTE";
+    default:
+      return "END_OF_PHASE";
+  }
+}
+
+inline std::string getPhaseString(int phase)
+{
+  return getPhaseString(static_cast<PhaseEnum>(phase));
+}
+
+inline PhaseEnum getPhaseEnum(const std::string &phase)
+{
+  if (phase.compare("MODEL_LOAD") == 0)
+    return PhaseEnum::MODEL_LOAD;
+  if (phase.compare("PREPARE") == 0)
+    return PhaseEnum::PREPARE;
+  if (phase.compare("WARMUP") == 0)
+    return PhaseEnum::WARMUP;
+  if (phase.compare("EXECUTE") == 0)
+    return PhaseEnum::EXECUTE;
+  return PhaseEnum::END_OF_PHASE;
+}
+
+const std::vector<std::string> gPhaseStrings{"MODEL_LOAD", "PREPARE", "WARMUP", "EXECUTE"};
+
+enum FigureType
+{
+  MEAN,
+  MAX,
+  MIN,
+  GEOMEAN,
+  END_OF_FIG_TYPE
+};
+
+inline std::string getFigureTypeString(FigureType type)
+{
+  switch (type)
+  {
+    case MEAN:
+      return "MEAN";
+    case MAX:
+      return "MAX";
+    case MIN:
+      return "MIN";
+    case GEOMEAN:
+      return "GEOMEAN";
+    default:
+      return "END_OF_FIG_TYPE";
+  }
+}
+
+inline std::string getFigureTypeString(int type)
+{
+  return getFigureTypeString(static_cast<FigureType>(type));
+}
+
+} // namespace benchmark
+
+#endif // __NNFW_BENCHMARK_TYPES_H__

runtime/libs/benchmark/include/benchmark/Util.h
@@ -1,114 +1,0 @@
-/*
- * Copyright (c) 2019 Samsung Electronics Co., Ltd. All Rights Reserved
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#ifndef __NNFW_BENCHMARK_UTIL_H__
-#define __NNFW_BENCHMARK_UTIL_H__
-
-#include "Result.h"
-#include "CsvWriter.h"
-
-#include <chrono>
-#include <string>
-#include <iostream>
-
-namespace benchmark
-{
-
-inline uint64_t nowMicros()
-{
-  auto time_point = std::chrono::high_resolution_clock::now();
-  auto since_epoch = time_point.time_since_epoch();
-  // default precision of high resolution clock is 10e-9 (nanoseconds)
-  return std::chrono::duration_cast<std::chrono::microseconds>(since_epoch).count();
-}
-
-// TODO Support not only stdout but also ostream
-inline void printResult(const Result &result, bool print_memory)
-{
-  std::cout << "===================================" << std::endl;
-  std::cout << getPhaseString(Phase::MODEL_LOAD) << " takes " << result.getModelLoadTime() / 1e3
-            << " ms" << std::endl;
-  std::cout << getPhaseString(Phase::PREPARE) << "    takes " << result.getPrepareTime() / 1e3
-            << " ms" << std::endl;
-  std::cout << getPhaseString(Phase::EXECUTE) << "    takes " << std::endl;
-  std::cout << "- Min:  " << result.getExecuteTimeMin() / 1e3 << " ms" << std::endl;
-  std::cout << "- Max:  " << result.getExecuteTimeMax() / 1e3 << " ms" << std::endl;
-  std::cout << "- Mean: " << result.getExecuteTimeMean() / 1e3 << " ms" << std::endl;
-  std::cout << "- GeoMean: " << result.getExecuteTimeGeoMean() / 1e3 << " ms" << std::endl;
-  std::cout << "===================================" << std::endl;
-
-  if (print_memory == false)
-    return;
-
-  std::cout << "RSS" << std::endl;
-  std::cout << "- " << getPhaseString(Phase::MODEL_LOAD) << " takes " << result.getModelLoadRss()
-            << " kb" << std::endl;
-  std::cout << "- " << getPhaseString(Phase::PREPARE) << "    takes " << result.getPrepareRss()
-            << " kb" << std::endl;
-  std::cout << "- " << getPhaseString(Phase::EXECUTE) << "    takes " << result.getExecuteRss()
-            << " kb" << std::endl;
-  std::cout << "- PEAK "
-            << "      takes " << result.getPeakRss() << " kb" << std::endl;
-  std::cout << "===================================" << std::endl;
-  std::cout << "HWM" << std::endl;
-  std::cout << "- " << getPhaseString(Phase::MODEL_LOAD) << " takes " << result.getModelLoadHwm()
-            << " kb" << std::endl;
-  std::cout << "- " << getPhaseString(Phase::PREPARE) << "    takes " << result.getPrepareHwm()
-            << " kb" << std::endl;
-  std::cout << "- " << getPhaseString(Phase::EXECUTE) << "    takes " << result.getExecuteHwm()
-            << " kb" << std::endl;
-  std::cout << "- PEAK "
-            << "      takes " << result.getPeakHwm() << " kb" << std::endl;
-  std::cout << "===================================" << std::endl;
-  std::cout << "PSS" << std::endl;
-  std::cout << "- " << getPhaseString(Phase::MODEL_LOAD) << " takes " << result.getModelLoadPss()
-            << " kb" << std::endl;
-  std::cout << "- " << getPhaseString(Phase::PREPARE) << "    takes " << result.getPreparePss()
-            << " kb" << std::endl;
-  std::cout << "- " << getPhaseString(Phase::EXECUTE) << "    takes " << result.getExecutePss()
-            << " kb" << std::endl;
-  std::cout << "- PEAK "
-            << "      takes " << result.getPeakPss() << " kb" << std::endl;
-  std::cout << "===================================" << std::endl;
-}
-
-// TODO Support not only csv but also other datafile format such as xml, json, ...
-inline void writeResult(const Result &result, const std::string &exec, const std::string &model,
-                        const std::string &backend)
-{
-  std::string csv_filename = exec + "-" + model + "-" + backend + ".csv";
-
-  // write to csv
-  CsvWriter writer(csv_filename);
-  writer << model << backend << result.getModelLoadTime() / 1e3 << result.getPrepareTime() / 1e3
-         << result.getExecuteTimeMin() / 1e3 << result.getExecuteTimeMax() / 1e3
-         << result.getExecuteTimeMean() / 1e3 << result.getModelLoadRss() << result.getPrepareRss()
-         << result.getExecuteRss() << result.getPeakRss() << result.getModelLoadHwm()
-         << result.getPrepareHwm() << result.getExecuteHwm() << result.getPeakHwm()
-         << result.getModelLoadPss() << result.getPreparePss() << result.getExecutePss()
-         << result.getPeakPss();
-
-  bool done = writer.done();
-
-  if (!done)
-  {
-    std::cerr << "Writing to " << csv_filename << " is failed" << std::endl;
-  }
-}
-
-} // namespace benchmark
-
-#endif // __NNFW_BENCHMARK_UTIL_H__

commit d6b0d13be50236fc1ad7c5e5680956d7575122f4

[libbenchmark] fix bug on peakMemory
- caused by #2204
- handle WARMUP as EXECUTE
  - it's because measuring memory on EXECUTE takes overhead a quite

ONE-DCO-1.0-Signed-off-by: Yongseop Kim <yons.kim@samsung.com>

runtime/libs/benchmark/src/Result.cpp
@@ -82,8 +82,9 @@ uint32_t peakMemory(const uint32_t memory[benchmark::PhaseEnum::END_OF_PHASE]
                     int type)
 {
   using namespace benchmark;
+  // tricky. handle WARMUP as EXECUTE
   return std::max({memory[PhaseEnum::MODEL_LOAD][type], memory[PhaseEnum::PREPARE][type],
-                   memory[PhaseEnum::EXECUTE][type]});
+                   memory[PhaseEnum::WARMUP][type]});
 }
 
 void printResultTime(