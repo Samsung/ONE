4703790f9d90db3fa3348bfc03f24a45ce96a4c2
dayo <dayg502@gmail.com>
2020-06-19 10:50:03 +0900

    [onert] fix Range operation in cpu backend (#2424)
    - instead of using pointer as T, use value as T
    
    ONE-DCO-1.0-Signed-off-by: dayo09 <dayg502@gmail.com>

--- compute/cker/include/cker/operation/Range.h
+++ compute/cker/include/cker/operation/Range.h
@@ -28,23 +28,36 @@ namespace cker
 {
 template <typename T> inline int GetSize(T start, T limit, T delta)
 {
+  if (!((start > limit && delta < 0) || (start < limit && delta > 0)))
+  {
+    throw std::runtime_error("Range: invalid input values");
+  }
+
   int size = (std::is_integral<T>::value
-                  ? ((std::abs(*limit - *start) + std::abs(*delta) - 1) / std::abs(*delta))
-                  : std::ceil(std::abs((*limit - *start) / *delta)));
+                  ? ((std::abs(limit - start) + std::abs(delta) - 1) / std::abs(delta))
+                  : std::ceil(std::abs((limit - start) / delta)));
   return size;
 }
+
 template <typename T>
-inline void Range(const T start_data, const T limit_data, const T delta_data, T output_data)
+inline void Range(const T *start_data, const T *limit_data, const T *delta_data, T *output_data)
 {
-  const int num_elements = GetSize<T>(start_data, limit_data, delta_data);
-  T start = start_data;
+  const T start_value = *start_data;
+  const T delta_value = *delta_data;
+  const T limit_value = *limit_data;
+
+  const int num_elements = GetSize<T>(start_value, limit_value, delta_value);
+  T value = start_value;
 
   for (int i = 0; i < num_elements; ++i)
   {
-    output_data[i] = *start;
-    *start += *delta_data;
+    output_data[i] = value;
+    value += delta_value;
+
+    assert(value < limit_value);
   }
 }
+
 } // namespace cker
 } // namespace nnfw
 
