d19fbc5a78604fc3455716693fd918c89cd3446b
struss <rrstrous@nate.com>
2020-06-19 14:05:27 +0900

    Update commented Contents.

--- compiler/luci-interpreter/src/kernels/TestUtils.h
+++ compiler/luci-interpreter/src/kernels/TestUtils.h
@@ -20,6 +20,8 @@
 
 #include "luci_interpreter/core/Tensor.h"
 
+#include <type_traits>
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -63,7 +65,7 @@ std::vector<::testing::Matcher<float>> ArrayFloatNear(const std::vector<float> &
 template <typename T>
 inline std::vector<T> quantize(const std::vector<float> &data, float scale, int32_t zero_point)
 {
-  assert(getElementType<T>() != DataType::FLOAT32);
+  assert(!std::is_floating_point<T>::value);
   std::vector<T> q;
   for (const auto &f : data)
   {
@@ -77,7 +79,7 @@ inline std::vector<T> quantize(const std::vector<float> &data, float scale, int3
 template <typename T>
 inline std::vector<float> dequantize(const std::vector<T> &data, float scale, int32_t zero_point)
 {
-  assert(getElementType<T>() != DataType::FLOAT32);
+  assert(!std::is_floating_point<T>::value);
   std::vector<float> f;
   for (const T &q : data)
   {
@@ -88,7 +90,7 @@ inline std::vector<float> dequantize(const std::vector<T> &data, float scale, in
 
 template <typename T> std::pair<float, int32_t> quantizationParams(float f_min, float f_max)
 {
-  if (getElementType<T>() == DataType::FLOAT32)
+  if (std::is_floating_point<T>::value)
   {
     return {0.0, 0};
   }
