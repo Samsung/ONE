7a420785b4baf7bc390b2b3db14f835bcbbc3ada
Hanjoung Lee <hanjoung.lee@samsung.com>
2020-06-24 17:27:30 +0900

    [onert] Introduce controlflow::UserTensor
    Introduce `backend::controlflow::UserTensor` for model input and output
    tensors. All the input/output tensors are going to be wrapped with
    `UserTensor` so it can be treated the same way with others.
    
    It is to remove explicit copies(Source/Sink) when starting execution.
    
    Draft: #1325
    
    ONE-DCO-1.0-Signed-off-by: Hanjoung Lee <hanjoung.lee@samsung.com>

---
+++ runtime/onert/core/src/backend/controlflow/UserTensor.cc
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "UserTensor.h"
+
+namespace onert
+{
+namespace backend
+{
+namespace controlflow
+{
+
+size_t UserTensor::calcOffset(const ir::Coordinates &coords) const
+{
+  size_t rank = num_dimensions();
+  size_t offset = 0;
+  for (size_t i = 0; i < rank; ++i)
+  {
+    offset = offset * dimension(i) + coords[i];
+  }
+  offset *= sizeOfDataType(data_type());
+  return offset;
+}
+
+} // namespace controlflow
+} // namespace backend
+} // namespace onert
