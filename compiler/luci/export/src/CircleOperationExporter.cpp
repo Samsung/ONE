2de4923e477eea359ceaffa95dff74296a47262e
Denis Krylov <krylovdenism@gmail.com>
2020-07-08 13:55:47 +0300

    Use export_simple in exporter
    ONE-DCO-1.0-Signed-off-by: Denis Krylov <krylovdenism@gmail.com>

--- compiler/luci/export/src/CircleOperationExporter.cpp
+++ compiler/luci/export/src/CircleOperationExporter.cpp
@@ -633,15 +633,8 @@ void OperationExporter::visit(luci::CircleLogSoftmax *node)
 
 void OperationExporter::visit(luci::CircleMatrixDiag *node)
 {
-  uint32_t op_idx = md.registerBuiltinOpcode(circle::BuiltinOperator_MATRIX_DIAG);
-  std::vector<int32_t> inputs_vec{get_tensor_index(node->diagonal())};
-  std::vector<int32_t> outputs_vec{get_tensor_index(static_cast<loco::Node *>(node))};
-  auto inputs = builder.CreateVector(inputs_vec);
-  auto outputs = builder.CreateVector(outputs_vec);
-  auto options = CreateMatrixDiagOptions(builder);
-  auto op_offset = CreateOperator(builder, op_idx, inputs, outputs,
-                                  circle::BuiltinOptions_MatrixDiagOptions, options.Union());
-  gd._operators.push_back(op_offset);
+  export_simple(node, circle::BuiltinOperator_MATRIX_DIAG, circle::BuiltinOptions_MatrixDiagOptions,
+                CreateMatrixDiagOptions(builder).Union());
 }
 
 void OperationExporter::visit(luci::CircleMaximum *node)
