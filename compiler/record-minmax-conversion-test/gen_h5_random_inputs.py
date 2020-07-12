6810b4f92d8d0069bf0af33d9b83684c8c30ad85
seongwoo chae <mhs4670go@naver.com>
2020-07-13 06:14:33 +0900

    [record-minmax-conversion-test] Add output path option (#3009)
    * [record-minmax-conversion-test] Add output path option
    
    This commit adds output path option to script
    
    ONE-DCO-1.0-ONE-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>
    
    * fix option name
    
    * fix bug

--- compiler/record-minmax-conversion-test/gen_h5_random_inputs.py
+++ compiler/record-minmax-conversion-test/gen_h5_random_inputs.py
@@ -8,24 +8,27 @@ import argparse
 # This script generates a pack of random input data (.h5) expected by the input tflite model
 #
 # Basic usage:
-#   gen_h5_inputs.py --model <path/to/tflite/model> --num_data <number/of/data>
-#   ex: gen_h5_inputs.py --model add.tflite --num_data 3
+#   gen_h5_inputs.py --model <path/to/tflite/model> --num_data <number/of/data> --output <path/to/output/data>
+#   ex: gen_h5_inputs.py --model add.tflite --num_data 3 --output add.tflite.input.h5
 #   (This will create add.tflite.input.h5 composed of three random inputs in the same directory as the model)
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, required=True)
 parser.add_argument('--num_data', type=int, required=True)
+parser.add_argument('--output', type=str, required=True)
 args = parser.parse_args()
 
 model = args.model
 
 num_data = args.num_data
 
+output_path = args.output
+
 # Build TFLite interpreter. (to get the information of model input)
 interpreter = tf.lite.Interpreter(model)
 input_details = interpreter.get_input_details()
 
 # Create h5 file
-h5_file = h5.File(model + ".input.h5", 'w')
+h5_file = h5.File(output_path, 'w')
 group = h5_file.create_group("value")
 group.attrs['desc'] = "Input data for " + model
 
