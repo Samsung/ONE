f7eb0dc9da1058caa6238cba1440b27448be61eb
seongwoo <mhs4670go@naver.com>
2020-07-14 16:46:59 +0900

    [record-minmax-conversion-test] Use common-artifacts
    This commit make module use common-artifacts
    
    ONE-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>

--- compiler/record-minmax-conversion-test/requires.cmake
+++ compiler/record-minmax-conversion-test/requires.cmake
@@ -1,3 +1,4 @@
+require("common-artifacts")
 require("record-minmax")
 require("tflite2circle")
 require("tflchef")
--- compiler/record-minmax-conversion-test/testall.sh
+++ compiler/record-minmax-conversion-test/testall.sh
@@ -11,8 +11,9 @@
 GEN_SOURCE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 GEN_SCRIPT_PATH="${GEN_SOURCE_PATH}/gen_h5_random_inputs.py"
 CONFIG_PATH="$1"; shift
+BIN_PATH=$(dirname "$CONFIG_PATH")
 WORKDIR="$1"; shift
-VIRTUALENV="${WORKDIR}/venv"
+VIRTUALENV="${WORKDIR}/venv_1_13_2"
 
 source "${CONFIG_PATH}"
 
@@ -29,10 +30,10 @@ for TESTCASE in "$@"; do
 
   TESTCASE_FILE="${WORKDIR}/${TESTCASE}"
 
-  PASSED_TAG="${TESTCASE_FILE}.passed"
+  PASSED_TAG="${BIN_PATH}/${TESTCASE}.passed"
   rm -f "${PASSED_TAG}"
 
-  cat > "${TESTCASE_FILE}.log" <(
+  cat > "${BIN_PATH}/${TESTCASE}.log" <(
     exec 2>&1
     set -ex
 
@@ -41,7 +42,7 @@ for TESTCASE in "$@"; do
     "${VIRTUALENV}/bin/python" "${GEN_SCRIPT_PATH}" \
     --model "${TESTCASE_FILE}.tflite" \
     --num_data 3 \
-    --output "${TESTCASE_FILE}.tflite.input.h5"
+    --output "${BIN_PATH}/${TESTCASE}.tflite.input.h5"
 
     if [[ $? -ne 0 ]]; then
       echo "FAILED TO GENERATE INPUT"
@@ -51,8 +52,8 @@ for TESTCASE in "$@"; do
     # Run record-minmax
     "${RECORD_MINMAX_PATH}" \
       --input_model "${TESTCASE_FILE}.circle" \
-      --input_data "${TESTCASE_FILE}.tflite.input.h5" \
-      --output_model "${TESTCASE_FILE}.out.circle" 
+      --input_data "${BIN_PATH}/${TESTCASE}.tflite.input.h5" \
+      --output_model "${BIN_PATH}/${TESTCASE}.out.circle"
 
     if [[ $? -eq 0 ]]; then
       touch "${PASSED_TAG}"
