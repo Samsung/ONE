f818375c8594bec49f8f8d8e6c8d1845016cb772
SaeHie Park <saehie.park@gmail.com>
2020-07-15 16:48:01 +0900

    [infra] Introduce unittest_compiler_xml script
    This will introduce unittest_compiler_xml script to generate unit test xml reports
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ infra/scripts/compiler_modules.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Don't run this script
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && echo "Please don't execute ${BASH_SOURCE[0]}, source it" && return
+
+DEBUG_BUILD_ITEMS="angkor;cwrap;pepper-str;pepper-strcast;pp;stdex"
+DEBUG_BUILD_ITEMS+=";oops;pepper-assert"
+DEBUG_BUILD_ITEMS+=";hermes;hermes-std"
+DEBUG_BUILD_ITEMS+=";loco;locop;locomotiv;logo-core;logo"
+DEBUG_BUILD_ITEMS+=";foder;souschef;arser"
+DEBUG_BUILD_ITEMS+=";safemain;mio-circle;mio-tflite"
+DEBUG_BUILD_ITEMS+=";tflite2circle"
+DEBUG_BUILD_ITEMS+=";luci"
+DEBUG_BUILD_ITEMS+=";luci-interpreter"
+DEBUG_BUILD_ITEMS+=";luci-value-test"
+DEBUG_BUILD_ITEMS+=";circle2circle;record-minmax;circle-quantizer"
+DEBUG_BUILD_ITEMS+=";circle-verify"
+DEBUG_BUILD_ITEMS+=";tflchef;circlechef"
+DEBUG_BUILD_ITEMS+=";common-artifacts"
+DEBUG_BUILD_ITEMS+=";circle2circle-dredd-recipe-test"
+DEBUG_BUILD_ITEMS+=";record-minmax-conversion-test"
+DEBUG_BUILD_ITEMS+=";tf2tfliteV2-conversion-test"
+DEBUG_BUILD_ITEMS+=";tflite2circle-conversion-test"
