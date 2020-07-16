9b2c5a3621b1a84f9eacc5bde9982322bf328da5
Yongseop Kim <yons.kim@samsung.com>
2020-07-16 16:28:20 +0900

    [onert] Introduce StaticTensorManager for cpu backend
    Introduce StaticTensorManager for cpu backend. Compared to
    cpu_common::StaticTensorManager, cpu::StaticTensorManager doesn't use
    const tensor manager because it handles const tensors as ExternalTensor.
    FOr not, it is not used but will be soon.
    
    ONE-DCO-1.0-Signed-off-by: Yongseop Kim <yons.kim@samsung.com>

--- runtime/onert/backend/cpu/StaticTensorManager.cc
+++ runtime/onert/backend/cpu/StaticTensorManager.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Samsung Electronics Co., Ltd. All Rights Reserved
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,8 @@
  * limitations under the License.
  */
 
-#include "backend/cpu_common/StaticTensorManager.h"
+#include "StaticTensorManager.h"
+#include "Tensor.h"
 
 #include <util/logging.h>
 
@@ -22,31 +23,13 @@ namespace onert
 {
 namespace backend
 {
-namespace cpu_common
+namespace cpu
 {
 
-StaticTensorManager::StaticTensorManager(const std::shared_ptr<TensorRegistry> &reg)
-    : _const_mgr{new DynamicMemoryManager()}, _nonconst_mgr{new MemoryManager()}, _tensors{reg}
+StaticTensorManager::StaticTensorManager(const std::shared_ptr<cpu_common::TensorRegistry> &reg)
+    : _nonconst_mgr{new cpu_common::MemoryManager()}, _tensors{reg}
 {
   // DO NOTHING
-}
-
-void StaticTensorManager::allocateConsts(void)
-{
-  for (auto &pair : _tensors->managed_tensors())
-  {
-    const auto &ind = pair.first;
-    auto tensor = pair.second;
-    if (_as_constants[ind])
-    {
-      auto mem_alloc = _const_mgr->allocate(ind, tensor->total_size());
-      tensor->setBuffer(mem_alloc);
-      auto buffer = mem_alloc->base();
-      VERBOSE(CPU_COMMON_StaticTensorManager) << "CONSTANT TENSOR(#" << ind.value()
-                                              << "): " << static_cast<void *>(buffer)
-                                              << "size : " << tensor->total_size() << std::endl;
-    }
-  }
 }
 
 void StaticTensorManager::allocateNonconsts(void)
@@ -62,13 +45,11 @@ void StaticTensorManager::allocateNonconsts(void)
       auto *buffer = _nonconst_mgr->getBuffer(ind);
       tensor->setBuffer(buffer);
 
-      VERBOSE(CPU_COMMON_StaticTensorManager) << "TENSOR(#" << ind.value()
-                                              << "): " << static_cast<void *>(buffer) << std::endl;
+      VERBOSE(CPU_StaticTensorManager) << "TENSOR(#" << ind.value()
+                                       << "): " << static_cast<void *>(buffer) << std::endl;
     }
   }
 }
-
-void StaticTensorManager::deallocateConsts(void) { _const_mgr->deallocate(); }
 
 void StaticTensorManager::deallocateNonconsts(void) { _nonconst_mgr->deallocate(); }
 
@@ -76,18 +57,26 @@ void StaticTensorManager::buildTensor(const ir::OperandIndex &ind,
                                       const ir::OperandInfo &tensor_info, ir::Layout backend_layout,
                                       bool as_const)
 {
-  assert(!_tensors->getManagedTensor(ind));
-  auto tensor = std::make_shared<Tensor>(tensor_info, backend_layout);
-  _tensors->setManagedTensor(ind, tensor);
+  assert(!_tensors->getITensor(ind));
+  if (as_const)
+  {
+    auto tensor = std::make_shared<ExternalTensor>(tensor_info, backend_layout);
+    _tensors->setExternalTensor(ind, tensor);
+  }
+  else
+  {
+    auto tensor = std::make_shared<Tensor>(tensor_info, backend_layout);
+    _tensors->setManagedTensor(ind, tensor);
+  }
   _as_constants[ind] = as_const;
 }
 
 void StaticTensorManager::claimPlan(const ir::OperandIndex &ind, uint32_t size)
 {
-  assert(_tensors->getManagedTensor(ind));
+  assert(_tensors->getITensor(ind));
 
   // This method is called only when a tensor has proper shape
-  assert(!_tensors->getManagedTensor(ind)->is_dynamic());
+  assert(!_tensors->getITensor(ind)->is_dynamic());
 
   if (!_as_constants[ind])
     _nonconst_mgr->claimPlan(ind, size);
@@ -95,10 +84,10 @@ void StaticTensorManager::claimPlan(const ir::OperandIndex &ind, uint32_t size)
 
 void StaticTensorManager::releasePlan(const ir::OperandIndex &ind)
 {
-  assert(_tensors->getManagedTensor(ind));
+  assert(_tensors->getITensor(ind));
 
   // This method is called only when a tensor has proper shape
-  assert(!_tensors->getManagedTensor(ind)->is_dynamic());
+  assert(!_tensors->getITensor(ind)->is_dynamic());
 
   if (!_as_constants[ind])
     _nonconst_mgr->releasePlan(ind);
@@ -108,8 +97,10 @@ void StaticTensorManager::iterate(const std::function<void(const ir::OperandInde
 {
   for (const auto &it : _tensors->managed_tensors())
     fn(it.first);
+  for (const auto &it : _tensors->external_tensors())
+    fn(it.first);
 }
 
-} // namespace cpu_common
+} // namespace cpu
 } // namespace backend
 } // namespace onert
