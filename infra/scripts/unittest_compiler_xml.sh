dcf46b93fd7980efb57c2892f345dec5f2447c18
Hyeongseok Oh <hseok82.oh@samsung.com>
2020-07-16 17:17:44 +0900

    [infra/script] Revise unittest_compiler_xml.sh (#3226)
    - Use NNCC_WORKSPACE setting
    - Add execution permission
    - Test only, not configure and build
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/scripts/unittest_compiler_xml.sh
+++ infra/scripts/unittest_compiler_xml.sh
@@ -6,8 +6,8 @@ set -eo pipefail
 
 CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_PATH="$CURRENT_PATH/../../"
-
-UNITTEST_REPORT_DIR=${ROOT_PATH}build/unittest_compiler_xml
+NNCC_WORKSPACE=${NNCC_WORKSPACE:-${ROOT_PATH}build}
+UNITTEST_REPORT_DIR=${NNCC_WORKSPACE}/unittest_compiler_xml
 
 for i in "$@"
 do
@@ -23,20 +23,7 @@ if [ ! -e "$UNITTEST_REPORT_DIR" ]; then
   mkdir -p $UNITTEST_REPORT_DIR
 fi
 
-source ${CURRENT_PATH}/compiler_modules.sh
-
-NNCC_CFG_OPTION=" -DCMAKE_BUILD_TYPE=Debug"
-NNCC_CFG_STRICT=" -DENABLE_STRICT_BUILD=ON"
-NNCC_CFG_MODULES=" -DBUILD_WHITELIST=$DEBUG_BUILD_ITEMS"
-
-if [ $# -ne 0 ]; then
-	echo "Additional cmake configuration: $@"
-fi
-
-./nncc configure $NNCC_CFG_OPTION $NNCC_CFG_STRICT $NNCC_CFG_MODULES
-./nncc build -j4
-
-for TEST_BIN in `find ${ROOT_PATH}build/compiler -type f -executable -name *_test`; do
+for TEST_BIN in `find ${NNCC_WORKSPACE}/compiler -type f -executable -name *_test`; do
   TEST_NAME="$(basename -- $TEST_BIN)"
   LUGI_LOG=999 $TEST_BIN --gtest_output="xml:$UNITTEST_REPORT_DIR/$TEST_NAME.xml"
 done
