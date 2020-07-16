81ad5dd703c7b0f7272e016523f90f5e9e926924
Hanjoung Lee <hanjoung.lee@samsung.com>
2020-07-16 11:23:48 +0900

    [onert] Optimization for model input copies
    Do not insert Permute operation when tensors are copied from controlflow
    to cpu(model input cases).
    
    ONE-DCO-1.0-Signed-off-by: Hanjoung Lee <hanjoung.lee@samsung.com>

--- runtime/onert/core/src/ir/pass/PermutationInsertionPass.cc
+++ runtime/onert/core/src/ir/pass/PermutationInsertionPass.cc
@@ -62,27 +62,26 @@ void PermutationInsertionPass::callback(const OperandIndex &index, Operand &obje
     auto insert_set = operand_li->use_factors() - operand_li->def_factors();
     auto def_factor = operand_li->def_factors().getOnlyElement();
 
-    auto compatible_backends = [](auto /* backend1 */, auto /* backend2 */) {
-      // TODO If other issues for Permute elimination are resolved, enable this
-      return false;
-      /*
+    auto compatible_backends = [](auto backend1, auto backend2) {
       // TODO This is a workaround for not inserting Permute between cpu and controlflow.
       //      To be general, we need another way of checking they are compatible.
       const auto cf = backend::controlflow::Config::ID;
       const auto cpu = "cpu";
       const auto id1 = backend1->config()->id();
       const auto id2 = backend2->config()->id();
-      return (id1 == cpu && id2 == cf) // Allows no-Permute for Model inputs
-          || (id1 == cf && id2 == cpu); // Allows no-Permute for Model outputs
-          */
+      // NOTE This is to skip Permute insertion for model inputs(controlflow -> cpu), but not
+      // outputs. This function currently assumes that backend1 is Def and backend2 is Use. However
+      // it is going to be fixed soon.
+      // TODO make both ways work
+      return (id1 == cpu && id2 == cf);
     };
 
     for (auto factor : insert_set)
     {
+      // Check exceptional cases that Permute ops are not inserted
       if (factor.layout() == def_factor.layout() &&
           compatible_backends(factor.backend(), def_factor.backend()))
       {
-        // For this factor we can just reuse existing operand - Permute is not added.
         VERBOSE(PermutationInsertionPass) << "Permutation Insertion is skipped for operand "
                                           << index << " / as the tensor is compatible with backend "
                                           << factor.backend()->config()->id() << std::endl;
