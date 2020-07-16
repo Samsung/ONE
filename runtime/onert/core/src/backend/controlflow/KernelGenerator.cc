b0910000829537940bbac2a0e79e56e005499f6e
ragmani <ragmani0216@gmail.com>
2020-07-17 08:17:12 +0900

    [onert] Fix no member named `DynamicInferer`
    This commit fixes no member named `DynamicIferer` in KernelGenerator of controlflow
    
    Signed-off-by: ragmani <ragmani0216@gmail.com>

--- runtime/onert/core/src/backend/controlflow/KernelGenerator.cc
+++ runtime/onert/core/src/backend/controlflow/KernelGenerator.cc
@@ -47,7 +47,7 @@ void KernelGenerator::visit(const ir::OpSequence &op_seq)
   assert(_tensor_builder->tensorRegistry());
 
   auto dyn_tensor_manager = _tensor_builder->dynamicTensorManager();
-  auto dyn_shape_inferer = std::make_unique<exec::DynamicInferer>(
+  auto dyn_shape_inferer = std::make_unique<exec::DynamicShapeInferer>(
       _graph.operands(), dyn_tensor_manager, _tensor_builder->tensorRegistry());
 
   // TODO Always returning FunctionSequenceForDynamicBackend may cause performance issue
