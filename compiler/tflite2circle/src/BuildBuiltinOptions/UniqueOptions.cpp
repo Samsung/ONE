c1051f0390515f7ccab02023393dd453f3ba782c
venkat.iyer <venkat.iyer@samsung.com>
2020-07-17 16:41:41 +0900

    [tflite2circle] Enable Unique operation
    This commit will add unique operation to tflit2circle
    
    ONE-DCO-1.0-Signed-off-by: venkat.iyer <venkat.iyer@samsung.com>
    
    Signed-off-by: venkat.iyer <venkat.iyer@samsung.com>

---
+++ compiler/tflite2circle/src/BuildBuiltinOptions/UniqueOptions.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "UniqueOptions.h"
+#include "DataLookup.h"
+
+#include <cassert>
+
+namespace tflite2circle
+{
+
+flatbuffers::Offset<circle::UniqueOptions>
+build_circle_UniqueOptions(flatbuffers::FlatBufferBuilder &fb, const tflite::Operator *op)
+{
+  auto tflite_builtin_options = op->builtin_options_as_UniqueOptions();
+  assert(tflite_builtin_options);
+  circle::UniqueOptionsBuilder builtin_options_builder{fb};
+  builtin_options_builder.add_idx_out_type(
+      get_circle_tensortype(tflite_builtin_options->idx_out_type()));
+  return builtin_options_builder.Finish();
+}
+
+} // namespace tflite2circle
