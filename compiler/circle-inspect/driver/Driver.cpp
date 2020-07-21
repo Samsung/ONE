21cca62ef0cc0b7f9df78d04af52b2b606aef04a
Seok Namkoong <seok9311@naver.com>
2020-07-21 12:50:37 +0900

    [circle-inspect] Introduce DumpOperatorVersion (#3288)
    * [circle-inspect] Introduce DumpOperatorVersion
    
    This commit will introduce DumpOperatorVersion, which dumps
    the version of circle operator.
    
    ONE-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>
    
    * remove deprecated code
    
    * fix typo

--- compiler/circle-inspect/driver/Driver.cpp
+++ compiler/circle-inspect/driver/Driver.cpp
@@ -34,6 +34,10 @@ int entry(int argc, char **argv)
   arser.add_argument("--conv2d_weight")
       .nargs(0)
       .help("Dump Conv2D series weight operators in circle file");
+  arser.add_argument("--op_version")
+      .nargs(1)
+      .type(arser::DataType::STR)
+      .help("Dump circle operator version");
   arser.add_argument("circle").type(arser::DataType::STR).help("Circle file to inspect");
 
   try
@@ -47,7 +51,7 @@ int entry(int argc, char **argv)
     return 255;
   }
 
-  if (!arser["--operators"] && !arser["--conv2d_weight"])
+  if (!arser["--operators"] && !arser["--conv2d_weight"] && !arser["--op_version"])
   {
     std::cout << "At least one option must be specified" << std::endl;
     std::cout << arser;
@@ -60,6 +64,8 @@ int entry(int argc, char **argv)
     dumps.push_back(std::make_unique<circleinspect::DumpOperators>());
   if (arser["--conv2d_weight"])
     dumps.push_back(std::make_unique<circleinspect::DumpConv2DWeight>());
+  if (arser["--op_version"])
+    dumps.push_back(std::make_unique<circleinspect::DumpOperatorVersion>());
 
   std::string model_file = arser.get<std::string>("circle");
 
