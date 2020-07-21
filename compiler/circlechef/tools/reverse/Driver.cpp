0b0e0b1b5d55aa7d04812fa443103179e4a5626b
seongwoo <mhs4670go@naver.com>
2020-07-21 18:49:10 +0900

    [circlechef] change return value
    This commit changes return value when an error is occurred
    
    ONE-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>

--- compiler/circlechef/tools/reverse/Driver.cpp
+++ compiler/circlechef/tools/reverse/Driver.cpp
@@ -38,7 +38,7 @@ int entry(int argc, char **argv)
   {
     std::cout << err.what() << std::endl;
     std::cout << arser;
-    return 0;
+    return 255;
   }
 
   std::string circle_path = arser.get<std::string>("circle");
