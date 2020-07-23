5c75ad673ba4c2501a8aacb7e2f52a7731fd78c9
Alexander Moiseev <a.moiseev@samsung.com>
2020-07-23 10:38:23 +0300

    [res] Add examples for NonMaxSuppressionV5 op
    Add examples for NonMaxSuppressionV5 op.
    
    ONE-DCO-1.0-Signed-off-by: Alexander Moiseev <a.moiseev@samsung.com>

---
+++ res/TensorFlowPythonExamples/examples/non_max_suppression_with_scores/__init__.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+max_output_size = tf.compat.v1.constant(4)
+
+in_boxes_ = tf.compat.v1.placeholder(dtype=tf.float32, shape=(8, 4), name="Hole")
+in_scores_ = tf.compat.v1.placeholder(dtype=tf.float32, shape=(8), name="Hole")
+non_max_suppression_with_scores_ = tf.compat.v1.image.non_max_suppression_with_scores(
+    in_boxes_, in_scores_, max_output_size)
