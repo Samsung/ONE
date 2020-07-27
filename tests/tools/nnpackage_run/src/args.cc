5244892194779c64bb1207bb678fcda2380a72cf
Hanjoung Lee <hanjoung.lee@samsung.com>
2020-07-27 18:49:02 +0900

    [nnpkgrun] Refine Args impl (#3472)
    Use `notifier` of `boost::program_options` to reduce branches in `Parse`
    method rather than `if` statement for each argument.
    
    This is for better score on CyclomaticComplexity.
    
    ONE-DCO-1.0-Signed-off-by: Hanjoung Lee <hanjoung.lee@samsung.com>

--- tests/tools/nnpackage_run/src/args.cc
+++ tests/tools/nnpackage_run/src/args.cc
@@ -16,6 +16,7 @@
 
 #include "args.h"
 
+#include <functional>
 #include <iostream>
 #include <json/json.h>
 
@@ -105,6 +106,75 @@ Args::Args(const int argc, char **argv)
 
 void Args::Initialize(void)
 {
+  auto process_nnpackage = [&](const std::string &package_filename) {
+    _package_filename = package_filename;
+
+    std::cerr << "Package Filename " << _package_filename << std::endl;
+    if (_package_filename.empty())
+    {
+      // TODO Print usage instead of the below message
+      std::cerr << "Please specify nnpackage file. Run with `--help` for usage."
+                << "\n";
+
+      exit(1);
+    }
+    else
+    {
+      if (access(_package_filename.c_str(), F_OK) == -1)
+      {
+        std::cerr << "nnpackage not found: " << _package_filename << "\n";
+      }
+    }
+  };
+
+  auto process_output_sizes = [&](const std::string &output_sizes_json_str) {
+    Json::Value root;
+    Json::Reader reader;
+    if (!reader.parse(output_sizes_json_str, root, false))
+    {
+      std::cerr << "Invalid JSON format for output_sizes \"" << output_sizes_json_str << "\"\n";
+      exit(1);
+    }
+
+    auto arg_map = argArrayToMap(root);
+    for (auto &pair : arg_map)
+    {
+      uint32_t key = pair.first;
+      Json::Value &val_json = pair.second;
+      if (!val_json.isUInt())
+      {
+        std::cerr << "All the values in `output_sizes` must be unsigned integers\n";
+        exit(1);
+      }
+      uint32_t val = val_json.asUInt();
+      _output_sizes[key] = val;
+    }
+  };
+
+  auto process_shape_prepare = [&](const std::string &shape_str) {
+    try
+    {
+      handleShapeParam(_shape_prepare, shape_str);
+    }
+    catch (const std::exception &e)
+    {
+      std::cerr << "error with '--shape_prepare' option: " << shape_str << std::endl;
+      exit(1);
+    }
+  };
+
+  auto process_shape_run = [&](const std::string &shape_str) {
+    try
+    {
+      handleShapeParam(_shape_run, shape_str);
+    }
+    catch (const std::exception &e)
+    {
+      std::cerr << "error with '--shape_run' option: " << shape_str << std::endl;
+      exit(1);
+    }
+  };
+
   // General options
   po::options_description general("General options", 100);
 
@@ -112,32 +182,33 @@ void Args::Initialize(void)
   general.add_options()
     ("help,h", "Print available options")
     ("version", "Print version and exit immediately")
-    ("nnpackage", po::value<std::string>()->required())
+    ("nnpackage", po::value<std::string>()->required()->notifier(process_nnpackage))
 #if defined(ONERT_HAVE_HDF5) && ONERT_HAVE_HDF5 == 1
-    ("dump,d", po::value<std::string>()->default_value(""), "Output filename")
-    ("load,l", po::value<std::string>()->default_value(""), "Input filename")
+    ("dump,d", po::value<std::string>()->default_value("")->notifier([&](const auto &v) { _dump_filename = v; }), "Output filename")
+    ("load,l", po::value<std::string>()->default_value("")->notifier([&](const auto &v) { _load_filename = v; }), "Input filename")
 #endif
-    ("output_sizes", po::value<std::string>(),
+    ("output_sizes", po::value<std::string>()->notifier(process_output_sizes),
         "The output buffer size in JSON 1D array\n"
         "If not given, the model's output sizes are used\n"
         "e.g. '[0, 40, 2, 80]' to set 0th tensor to 40 and 2nd tensor to 80.\n")
-    ("num_runs,r", po::value<int>()->default_value(1), "The number of runs")
-    ("warmup_runs,w", po::value<int>()->default_value(0), "The number of warmup runs")
-    ("run_delay,t", po::value<int>()->default_value(-1), "Delay time(ms) between runs (as default no delay")
-    ("gpumem_poll,g", po::value<bool>()->default_value(false), "Check gpu memory polling separately")
-    ("mem_poll,m", po::value<bool>()->default_value(false), "Check memory polling")
-    ("write_report,p", po::value<bool>()->default_value(false),
+    ("num_runs,r", po::value<int>()->default_value(1)->notifier([&](const auto &v) { _num_runs = v; }), "The number of runs")
+    ("warmup_runs,w", po::value<int>()->default_value(0)->notifier([&](const auto &v) { _warmup_runs = v; }), "The number of warmup runs")
+    ("run_delay,t", po::value<int>()->default_value(-1)->notifier([&](const auto &v) { _run_delay = v; }), "Delay time(ms) between runs (as default no delay")
+    ("gpumem_poll,g", po::value<bool>()->default_value(false)->notifier([&](const auto &v) { _gpumem_poll = v; }), "Check gpu memory polling separately")
+    ("mem_poll,m", po::value<bool>()->default_value(false)->notifier([&](const auto &v) { _mem_poll = v; }), "Check memory polling")
+    ("write_report,p", po::value<bool>()->default_value(false)->notifier([&](const auto &v) { _write_report = v; }),
          "Write report\n"
          "{exec}-{nnpkg}-{backend}.csv will be generated.\n"
          "e.g. nnpackage_run-UNIT_Add_000-acl_cl.csv.\n"
          "{nnpkg} name may be changed to realpath if you use symbolic-link.")
-    ("shape_prepare", po::value<std::string>()->default_value("[]"),
+    ("shape_prepare", po::value<std::string>()->default_value("[]")->notifier(process_shape_prepare),
          "set shape of specified tensor before compilation\n"
          "e.g. '[0, [1, 2], 2, []]' to set 0th tensor to [1, 2] and 2nd tensor to [].\n")
-    ("shape_run", po::value<std::string>()->default_value("[]"),
+    ("shape_run", po::value<std::string>()->default_value("[]")->notifier(process_shape_run),
          "set shape of specified tensor right before running\n"
          "e.g. '[1, [1, 2]]` to set 1st tensor to [1, 2].\n")
-    ("verbose_level,v", po::value<int>()->default_value(0), "Verbose level\n"
+    ("verbose_level,v", po::value<int>()->default_value(0)->notifier([&](const auto &v) { _verbose_level = v; }),
+         "Verbose level\n"
          "0: prints the only result. Messages btw run don't print\n"
          "1: prints result and message btw run\n"
          "2: prints all of messages to print\n")
@@ -180,158 +251,23 @@ void Args::Parse(const int argc, char **argv)
     return;
   }
 
-  po::notify(vm);
   try
   {
-#if defined(ONERT_HAVE_HDF5) && ONERT_HAVE_HDF5 == 1
-    if (vm.count("dump"))
-    {
-      _dump_filename = vm["dump"].as<std::string>();
-    }
-
-    if (vm.count("load"))
-    {
-      _load_filename = vm["load"].as<std::string>();
-    }
-#endif
-
-    if (vm.count("nnpackage"))
-    {
-      _package_filename = vm["nnpackage"].as<std::string>();
-
-      if (_package_filename.empty())
-      {
-        // TODO Print usage instead of the below message
-        std::cerr << "Please specify nnpackage file. Run with `--help` for usage."
-                  << "\n";
-
-        exit(1);
-      }
-      else
-      {
-        if (access(_package_filename.c_str(), F_OK) == -1)
-        {
-          std::cerr << "nnpackage not found: " << _package_filename << "\n";
-        }
-      }
-    }
-
-    if (vm.count("output_sizes"))
-    {
-      auto output_sizes_json_str = vm["output_sizes"].as<std::string>();
-
-      Json::Value root;
-      Json::Reader reader;
-      if (!reader.parse(output_sizes_json_str, root, false))
-      {
-        std::cerr << "Invalid JSON format for output_sizes \"" << output_sizes_json_str << "\"\n";
-        exit(1);
-      }
-
-      auto arg_map = argArrayToMap(root);
-      for (auto &pair : arg_map)
-      {
-        uint32_t key = pair.first;
-        Json::Value &val_json = pair.second;
-        if (!val_json.isUInt())
-        {
-          std::cerr << "All the values in `output_sizes` must be unsigned integers\n";
-          exit(1);
-        }
-        uint32_t val = val_json.asUInt();
-        _output_sizes[key] = val;
-      }
-    }
-
-    if (vm.count("num_runs"))
-    {
-      _num_runs = vm["num_runs"].as<int>();
-    }
-
-    if (vm.count("warmup_runs"))
-    {
-      _warmup_runs = vm["warmup_runs"].as<int>();
-    }
-
-    if (vm.count("run_delay"))
-    {
-      _run_delay = vm["run_delay"].as<int>();
-    }
-
-    if (vm.count("gpumem_poll"))
-    {
-      _gpumem_poll = vm["gpumem_poll"].as<bool>();
-    }
-
-    if (vm.count("mem_poll"))
-    {
-      _mem_poll = vm["mem_poll"].as<bool>();
-      // Instead of EXECUTE to avoid overhead, memory polling runs on WARMUP
-      if (_mem_poll && _warmup_runs == 0)
-      {
-        _warmup_runs = 1;
-      }
-    }
-
-    if (vm.count("write_report"))
-    {
-      _write_report = vm["write_report"].as<bool>();
-    }
-
-    if (vm.count("verbose_level"))
-    {
-      _verbose_level = vm["verbose_level"].as<int>();
-    }
+    po::notify(vm);
   }
   catch (const std::bad_cast &e)
   {
-    std::cerr << "error by bad cast" << e.what() << '\n';
+    std::cerr << "Bad cast error - " << e.what() << '\n';
     exit(1);
   }
 
-  if (vm.count("shape_prepare"))
+  // This must be run after `notify` as `_warm_up_runs` must have been processed before.
+  if (vm.count("mem_poll"))
   {
-    std::string shape_str;
-    try
+    // Instead of EXECUTE to avoid overhead, memory polling runs on WARMUP
+    if (_mem_poll && _warmup_runs == 0)
     {
-      shape_str = vm["shape_prepare"].as<std::string>();
-    }
-    catch (const std::bad_cast &e)
-    {
-      std::cerr << "error by bad cast with '--shape_prepare' option" << e.what() << '\n';
-      exit(1);
-    }
-    try
-    {
-      handleShapeParam(_shape_prepare, shape_str);
-    }
-    catch (const std::exception &e)
-    {
-      std::cerr << "error with '--shape_prepare' option: " << shape_str << std::endl;
-      exit(1);
-    }
-  }
-
-  if (vm.count("shape_run"))
-  {
-    std::string shape_str;
-    try
-    {
-      shape_str = vm["shape_run"].as<std::string>();
-    }
-    catch (const std::bad_cast &e)
-    {
-      std::cerr << "error by bad cast with '--shape_run' option" << e.what() << '\n';
-      exit(1);
-    }
-    try
-    {
-      handleShapeParam(_shape_run, shape_str);
-    }
-    catch (const std::exception &e)
-    {
-      std::cerr << "error with '--shape_run' option: " << shape_str << std::endl;
-      exit(1);
+      _warmup_runs = 1;
     }
   }
 }
