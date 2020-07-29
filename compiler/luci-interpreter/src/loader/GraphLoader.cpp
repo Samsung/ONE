9b763addb30261beb9a0f87b69213cb0f7dc18b9
seongwoo <mhs4670go@naver.com>
2020-07-29 16:23:45 +0900

    [luci-interpreter] Add CIRCLECONST CircleOpCode case
    This commit adds CIRCLECONST CircleOpCode case.
    
    `CONST` will be deprecated soon.
    
    ONE-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>

--- compiler/luci-interpreter/src/loader/GraphLoader.cpp
+++ compiler/luci-interpreter/src/loader/GraphLoader.cpp
@@ -70,6 +70,7 @@ bool isExecutableNode(const luci::CircleNode *node)
   {
     // These nodes denote inputs / outputs of a graph.
     case luci::CircleOpcode::CONST:
+    case luci::CircleOpcode::CIRCLECONST:
     case luci::CircleOpcode::CIRCLEINPUT:
     case luci::CircleOpcode::CIRCLEOUTPUT:
     // The following nodes denote outputs of multiple-output nodes.
