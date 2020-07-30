7c2d064777c20dc071fb0d43e2a872416956babb
seongwoo chae <mhs4670go@naver.com>
2020-07-30 11:06:28 +0900

    [luci] Replace CONST with CIRCLECONST (#3537)
    This commit replaces CONST CircleOpCode with CIRCLECONST.
    
    CONST will be deprecated when the replacement process is over.
    
    ONE-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>

--- compiler/luci/lang/include/luci/IR/Nodes/CircleConst.h
+++ compiler/luci/lang/include/luci/IR/Nodes/CircleConst.h
@@ -31,7 +31,7 @@ namespace luci
  * @brief Class to build tensor data
  * @note  This will not be exported as a specific op
  */
-class CircleConst final : public FixedArityNode<0, CircleNodeImpl<CircleOpcode::CONST>>
+class CircleConst final : public FixedArityNode<0, CircleNodeImpl<CircleOpcode::CIRCLECONST>>
 {
 public:
   CircleConst() = default;
