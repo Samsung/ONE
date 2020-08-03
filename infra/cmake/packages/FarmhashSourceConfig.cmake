2bd64685e2a300cb0164352e6454350a8903e40b
SaeHie Park <saehie.park@gmail.com>
2020-08-03 14:13:54 +0900

    [infra] Revise comment for farmhash
    Revise farmhash note for Tensorflow version to 2.3.0
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- infra/cmake/packages/FarmhashSourceConfig.cmake
+++ infra/cmake/packages/FarmhashSourceConfig.cmake
@@ -9,7 +9,7 @@ function(_FarmhashSource_import)
 
   # NOTE TensorFlow 1.12 downloads farmhash from the following URL
   #      TensorFlow 1.13.1 downloads farmhash from the following URL
-  #      TensorFlow 2.3-rc0 downloads farmhash from the following URL
+  #      TensorFlow 2.3.0 downloads farmhash from the following URL
   envoption(FARMHASH_1_12_URL https://github.com/google/farmhash/archive/816a4ae622e964763ca0862d9dbd19324a1eaf45.tar.gz)
 
   ExternalSource_Download(FARMHASH ${FARMHASH_1_12_URL})
