c885c4f557f1caf3246c9f2ee107130e3c8b77eb
underflow101 <ikarus125@gmail.com>
2020-08-05 17:59:16 +0900

    feat: Add Test Inputs, Quant/Dequant funcs, License
    - Added Test inputs and outputs
    	- ref: [Tensorflow URL](https://github.com/tensorflow/tensorflow/blob/v2.1.0/tensorflow/lite/kernels/activations_test.cc#L615-L670)
    - Added quantize, dequantize functions
    - Added License for Tensorflow Authors
    
    Signed-off-by: underflow101 <ikarus125@gmail.com>

--- compiler/luci-interpreter/src/kernels/Tanh.test.cpp
+++ compiler/luci-interpreter/src/kernels/Tanh.test.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ * Copyright 2017 The TensorFlow Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,13 +29,95 @@ using namespace testing;
 
 TEST(TanhTest, Float)
 {
-  // TODO fix input & output ref data.
+  Shape input_shape{1, 2, 4, 1};
+  std::vector<float> input_data{
+      0, -6, 2,  4, //
+      3, -2, 10, 1, //
+  };
+  Tensor input_tensor = makeInputTensor<DataType::FLOAT32>(input_shape, input_data);
+  Tensor output_tensor = makeOutputTensor(DataType::FLOAT32);
 
-  // TODO make a Shape checking of output_tensor.
+  Tanh kernel(&input_tensor, &output_tensor);
+  kernel.configure();
+  kernel.execute();
+
+  std::vector<float> ref_output_data{
+      0,          -0.9999877, 0.9640275, 0.999329,  //
+      0.99505475, -0.9640275, 1,         0.7615941, //
+  };
+  EXPECT_THAT(extractTensorData<float>(output_tensor),
+              ElementsAreArray(ArrayFloatNear(ref_output_data)));
 }
 
-// TODO Uint8
-// Need to Implement GetDequantizedOutput Function.
+TEST(TanhTest, Uint8)
+{
+  float scale = 1.f / 128;
+  int32_t zero_point = 0;
+  float kMin = -1;
+  // float kMin = 0;
+  float kMax = 127.f / 128.f;
+  // float kMax = 255;
+  float kTanhTolerance = 2 * (1. / 256);
+  std::pair<float, int32_t> quant_param = quantizationParams<uint8_t>(kMin, kMax);
+  std::vector<float> input_data{
+      0,  -6, 2, 4, //
+      -4, -2, 8, 1, //
+      0,  -6, 2, 4, //
+      -4, -2, 8, 1, //
+      0,  -6, 2, 4, //
+      -4, -2, 8, 1, //
+      0,  -6, 2, 4, //
+      -4, -2, 8, 1, //
+      0,  -6, 2, 4, //
+      -4, -2, 8, 1, //
+      0,  -6, 2, 4, //
+      -4, -2, 8, 1, //
+  };
+  Tensor input_tensor{
+      DataType::U8, {2, 6, 4, 1}, {{quant_param.first}, {quant_param.second}}, ""};
+  Tensor output_tensor = makeOutputTensor(DataType::U8, quant_param.first, quant_param.second);
+  std::vector<uint8_t> quantize_input =
+      quantize<uint8_t>(input_data, quant_param.first * 8, quant_param.second * 8);
+  input_tensor.writeData(quantize_input.data(), quantize_input.size() * sizeof(uint8_t));
+
+  Tanh kernel(&input_tensor, &output_tensor);
+  kernel.configure();
+  kernel.execute();
+
+  std::vector<float> output_data{
+      0.0,       -0.999987, 0.964027, 0.999329, //
+      -0.999329, -0.96402,  0.99999,  0.76159,  //
+      0.0,       -0.999987, 0.964027, 0.999329, //
+      -0.999329, -0.96402,  0.99999,  0.76159,  //
+      0.0,       -0.999987, 0.964027, 0.999329, //
+      -0.999329, -0.96402,  0.99999,  0.76159,  //
+      0.0,       -0.999987, 0.964027, 0.999329, //
+      -0.999329, -0.96402,  0.99999,  0.76159,  //
+      0.0,       -0.999987, 0.964027, 0.999329, //
+      -0.999329, -0.96402,  0.99999,  0.76159,  //
+      0.0,       -0.999987, 0.964027, 0.999329, //
+      -0.999329, -0.96402,  0.99999,  0.76159,  //
+  };
+  std::vector<uint8_t> ref_output_data{
+      128, 0, 251, 255, //
+      0,   5, 255, 225, //
+      128, 0, 251, 255, //
+      0,   5, 255, 225, //
+      128, 0, 251, 255, //
+      0,   5, 255, 225, //
+      128, 0, 251, 255, //
+      0,   5, 255, 225, //
+      128, 0, 251, 255, //
+      0,   5, 255, 225, //
+      128, 0, 251, 255, //
+      0,   5, 255, 225, //
+  };
+  std::vector<int32_t> ref_output_shape{2, 6, 4, 1};
+  EXPECT_THAT(dequantize<uint8_t>(extractTensorData<uint8_t>(output_tensor), output_tensor.scale(),
+                                  output_tensor.zero_point()),
+              ElementsAreArray(ArrayFloatNear(output_data, kTanhTolerance)));
+  EXPECT_THAT(extractTensorShape(output_tensor), ::testing::ElementsAreArray(ref_output_data));
+}
 
 } // namespace
 } // namespace kernels
