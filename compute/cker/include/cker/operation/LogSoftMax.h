12b2012899ecb74c543065fec7a13134a9154ef9
LeeHanSol <hansolu.lee@samsung.com>
2020-08-12 10:05:05 +0900

    [onert] Add log_softmax kernel to support quant8 (#3758)
    Add quant8 kernel and testcase for log_softmax
    
    ONE-DCO-1.0-Signed-off-by: Leehansol <hansolu.lee@samsung.com>

--- compute/cker/include/cker/operation/LogSoftMax.h
+++ compute/cker/include/cker/operation/LogSoftMax.h
@@ -77,6 +77,62 @@ inline void LogSoftmax(const SoftmaxParams &params, const Shape &input_shape,
   }
 }
 
+inline void LogSoftmax(const SoftmaxParams &params, float input_scale, const Shape &input_shape,
+                       const uint8_t *input_data, const Shape &output_shape, uint8_t *output_data)
+{
+  const int rank = input_shape.DimensionsCount();
+  const int axis = (params.axis < 0) ? params.axis + rank : params.axis;
+  const double beta = params.beta;
+  const int depth = MatchingDim(input_shape, axis, output_shape, axis);
+
+  const int32_t clamp_max = std::numeric_limits<uint8_t>::max();
+  const int32_t clamp_min = std::numeric_limits<uint8_t>::min();
+
+  int outer_size = 1;
+  for (int i = 0; i < axis; ++i)
+  {
+    outer_size *= input_shape.Dims(i);
+  }
+
+  int inner_size = 1;
+  for (int i = axis + 1; i < rank; ++i)
+  {
+    inner_size *= input_shape.Dims(i);
+  }
+
+  for (int i = 0; i < outer_size; ++i)
+  {
+    for (int j = 0; j < inner_size; ++j)
+    {
+      uint8_t max_val = std::numeric_limits<uint8_t>::min();
+      for (int c = 0; c < depth; ++c)
+      {
+        max_val = std::max(max_val, input_data[(i * depth + c) * inner_size]);
+      }
+
+      float sum_exp = 0.0f;
+      const int32_t max_uint8 = std::numeric_limits<uint8_t>::max();
+      const float *table_offset = &params.table[max_uint8 - max_val];
+      for (int c = 0; c < depth; ++c)
+      {
+        sum_exp += table_offset[input_data[(i * depth + c) * inner_size]];
+      }
+      const float log_sum_exp = std::log(sum_exp);
+
+      const float scale = input_scale / params.scale;
+      const float precomputed = (input_scale * max_val * beta + log_sum_exp) / params.scale;
+      for (int c = 0; c < depth; ++c)
+      {
+        const float log_prob =
+            scale * input_data[(i * depth + c) * inner_size] * beta - precomputed;
+        const int32_t prob_quantized = std::rint(log_prob) + params.zero_point;
+        output_data[(i * depth + c) * inner_size] =
+            static_cast<uint8_t>(std::max(std::min(clamp_max, prob_quantized), clamp_min));
+      }
+    }
+  }
+}
+
 } // namespace cker
 } // namespace nnfw
 
