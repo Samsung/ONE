7b58c7e223333433ddba1ec327f6e7d08c3a981f
struss <rrstrous@nate.com>
2020-08-14 11:53:36 +0900

    [luci-interpreter]Negative Tests for Concatenation kernel
    This commit add negative tests on `Concatenation` kernel on luci-interpreter.
    
    ONE-DCO-1.0-Signed-off-by: KiDeuk Bang <rrstrous@nate.com>

--- compiler/luci-interpreter/src/kernels/Concatenation.test.cpp
+++ compiler/luci-interpreter/src/kernels/Concatenation.test.cpp
@@ -78,6 +78,43 @@ TEST(ConcatenationTest, Float)
   }
 }
 
+TEST(ConcatenationTest, Unsupported_Configure_Type_NEG)
+{
+  std::vector<int8_t> input1_data{1, 2, 3, 4, 5, 6};
+  std::vector<int8_t> input2_data{7, 8, 9, 10, 11, 12};
+  Tensor input1_tensor = makeInputTensor<DataType::S8>({2, 3}, input1_data);
+  Tensor input2_tensor = makeInputTensor<DataType::S8>({2, 3}, input2_data);
+  Tensor output_tensor = makeOutputTensor(DataType::S8);
+  ConcatenationParams params{};
+
+  // Try different 'axis' and expect different results.
+  {
+    params.axis = -1;
+
+    Concatenation kernel({&input1_tensor, &input2_tensor}, &output_tensor, params);
+    EXPECT_ANY_THROW(kernel.configure());
+  }
+}
+
+TEST(ConcatenationTest, Unsupported_Execute_Type_NEG)
+{
+  std::vector<int16_t> input1_data{1, 2, 3, 4, 5, 6};
+  std::vector<int16_t> input2_data{7, 8, 9, 10, 11, 12};
+  Tensor input1_tensor = makeInputTensor<DataType::S16>({2, 3}, input1_data);
+  Tensor input2_tensor = makeInputTensor<DataType::S16>({2, 3}, input2_data);
+  Tensor output_tensor = makeOutputTensor(DataType::S16);
+  ConcatenationParams params{};
+
+  // Try different 'axis' and expect different results.
+  {
+    params.axis = -1;
+
+    Concatenation kernel({&input1_tensor, &input2_tensor}, &output_tensor, params);
+    kernel.configure();
+    EXPECT_ANY_THROW(kernel.execute());
+  }
+}
+
 } // namespace
 } // namespace kernels
 } // namespace luci_interpreter
