c1c5a032687454dd90c042fae305d4120e888aad
Sanggyu Lee <takepencil@naver.com>
2020-08-14 11:47:54 +0900

    [tool/update-version] change name and interface of update-version (#3824)
    - It uses similar syntax for onert version
    - ./onert-version.sh will show current version.
    - ./onert-version.sh -s 1.10.0 will set version information.
    - name is changed since it does more than update.
    
    ONE-DCO-1.0-Signed-off-by: Sanggyu Lee <sg5.lee@samsung.com>

---
+++ tools/onert_version/onert_version.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -eu
+
+progname=$(basename "${BASH_SOURCE[0]}")
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+nnfw_root="$( cd "${script_dir%*/*/*}" && pwd )"
+
+usage() {
+  echo "Usage: $progname version"
+  echo "Update or show onert version information"
+  echo ""
+  echo "Options:"
+  echo "    -h   show this help"
+  echo "    -s   set onert  version"
+  echo ""
+  echo "Examples:"
+  echo "    $progname           => show current onert version"
+  echo "    $progname -s 1.6.0  => set onert version info in all sources"
+  exit 1
+}
+
+show_version() {
+  version_line=$(cat ${nnfw_root}/packaging/nnfw.spec | grep "Version:")
+  echo ${version_line#"Version:"}
+
+  exit 0
+}
+
+set_version() {
+  version=$1
+  perl -pi -e "s/^release = .*/release = \'$version\'/" ${nnfw_root}/docs/conf.py
+  perl -pi -e "s/^Version: .*/Version: $version/" ${nnfw_root}/packaging/nnfw.spec
+
+  IFS=. read M m p <<< "$version"
+  hex=$(printf '0x%08x' $(( (($M << 24)) | (($m << 8)) | $p )))
+  perl -pi -e "s/^#define NNFW_VERSION.*/#define NNFW_VERSION $hex/" ${nnfw_root}/runtime/onert/api/include/nnfw_version.h
+
+  perl -pi -e "s/versionName .*$/versionName \"$version\"/" ${nnfw_root}/runtime/contrib/android/api/build.gradle
+}
+
+if [ $# -eq 0 ]; then
+  show_version
+fi
+
+while getopts "hs:" OPTION; do
+case "${OPTION}" in
+    h) usage;;
+    s) set_version "$OPTARG";;
+    ?) exit 1;;
+esac
+done
+
+shift $((OPTIND-1))
