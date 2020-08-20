3c9d92770ca097af566a104c4f15b0b5b620ac4b
seongwoo <mhs4670go@naver.com>
2020-08-20 15:47:25 +0900

    [circle-quantizer] verify circle file
    This commit enables circle-quantizer to verify circle file.
    
    ONE-DCO-1.0-Signed-off-by: seongwoo <mhs4670go@naver.com>

--- compiler/circle-quantizer/src/CircleQuantizer.cpp
+++ compiler/circle-quantizer/src/CircleQuantizer.cpp
@@ -128,6 +128,15 @@ int entry(int argc, char **argv)
   // Load model from the file
   foder::FileLoader file_loader{input_path};
   std::vector<char> model_data = file_loader.load();
+
+  // Verify flatbuffers
+  flatbuffers::Verifier verifier{reinterpret_cast<uint8_t *>(model_data.data()), model_data.size()};
+  if (!circle::VerifyModelBuffer(verifier))
+  {
+    std::cerr << "ERROR: Invalid input file '" << input_path << "'" << std::endl;
+    return EXIT_FAILURE;
+  }
+
   const circle::Model *circle_model = circle::GetModel(model_data.data());
   if (circle_model == nullptr)
   {
