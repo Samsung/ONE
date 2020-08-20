bf7806a9d235f340c0a7aacb035cccced2d1dd29
SaeHie Park <saehie.park@gmail.com>
2020-08-20 14:01:12 +0900

    [circle2circle] Fix segfault for invalid input (#3877)
    This will fix to handle segment fault error for invalid input file
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/circle2circle/src/Circle2Circle.cpp
+++ compiler/circle2circle/src/Circle2Circle.cpp
@@ -157,6 +157,14 @@ int entry(int argc, char **argv)
     std::cerr << err.what() << std::endl;
     return EXIT_FAILURE;
   }
+
+  flatbuffers::Verifier verifier{reinterpret_cast<uint8_t *>(model_data.data()), model_data.size()};
+  if (!circle::VerifyModelBuffer(verifier))
+  {
+    std::cerr << "ERROR: Invalid input file '" << input_path << "'" << std::endl;
+    return EXIT_FAILURE;
+  }
+
   const circle::Model *circle_model = circle::GetModel(model_data.data());
   if (circle_model == nullptr)
   {
