793a662ea16922557ffc95c97cde0a8296eaed6c
underflow101 <ikarus125@gmail.com>
2020-08-27 23:18:56 +0900

    feat: Add Tanh under kernels directory
    - Added 3 source codes under luci-interpreter/src/kernels directory
    	- LogSoftmax.h
    	- LogSoftmax.cpp
    	- LogSoftmax.test.cpp
    - Added LogSoftmax for CMakeLists
    	- Added LogSoftmax.cpp & LogSoftmax.h for SOURCES
    	- Added LogSoftmax.test.cpp for TEST SOURCES
    
    Signed-off-by: underflow101 <ikarus125@gmail.com>

---
+++ compiler/luci-interpreter/src/kernels/LogSoftmax.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "kernels/LogSoftmax.h"
+
+#include "kernels/Utils.h"
+
+#include <tensorflow/lite/kernels/internal/reference/reference_ops.h>
+
+#include <tensorflow/lite/kernels/internal/optimized/optimized_ops.h>
+
+namespace luci_interpreter
+{
+namespace kernels
+{
+
+LogSoftmax::LogSoftmax(const Tensor *input, Tensor *output) : Kernel({input}, {output}) {}
+
+void LogSoftmax::configure()
+{
+  assert(input()->element_type() == output()->element_type());
+  output()->resize(input()->shape());
+}
+
+void LogSoftmax::execute() const
+{
+  switch (input()->element_type())
+  {
+    case DataType::FLOAT32:
+      evalFloat();
+      break;
+    case DataType::U8:
+      evalQuantized();
+      break;
+    default:
+      throw std::runtime_error("Unsupported type.");
+  }
+}
+
+void LogSoftmax::evalFloat() const
+{
+  tflite::SoftmaxParams params{};
+  tflite::reference_ops::LogSoftmax(params, getTensorShape(input()), getTensorData<float>(input()),
+                                    getTensorShape(output()), getTensorData<float>(output()));
+}
+
+void LogSoftmax::evalQuantized() const
+{
+  const auto input_shape = getTensorShape(input());
+  const auto output_shape = getTensorShape(output());
+  const auto input_scale = input()->scale();
+  const int size = tflite::MatchingFlatSize(getTensorShape(input()), getTensorShape(output()));
+  uint8_t *output_data = getTensorData<uint8_t>(output());
+  const uint8_t *input_data = getTensorData<uint8_t>(input());
+
+  tflite::SoftmaxParams params{};
+
+  params.table = new float[256];
+  params.zero_point = output()->zero_point();
+  params.scale = output()->scale();
+  params.beta = 1.0;
+
+  tflite::optimized_ops::PopulateSoftmaxLookupTable(&params, input()->scale(), params.beta);
+  tflite::optimized_ops::LogSoftmax(params, input_scale, input_shape, input_data, output_shape, output_data);
+}
+
+} // namespace kernels
+} // namespace luci_interpreter
