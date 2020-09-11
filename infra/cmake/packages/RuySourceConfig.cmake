b7eca53f55f2c6094eccc943bc5b4907dc9c9fae
Hyeongseok Oh <hseok82.oh@samsung.com>
2020-09-11 13:17:11 +0900

    [infra] Move RuySource download cmake (#4215)
    Move RuySource download cmake into infra/cmake/packages
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/cmake/packages/RuySourceConfig.cmake
+++ infra/cmake/packages/RuySourceConfig.cmake
@@ -1,6 +1,6 @@
 function(_RuySource_import)
   if(NOT ${DOWNLOAD_RUY})
-    set(RuySource_DIR FALSE PARENT_SCOPE)
+    set(RuySource_FOUND FALSE PARENT_SCOPE)
     return()
   endif(NOT ${DOWNLOAD_RUY})
 
@@ -9,11 +9,13 @@ function(_RuySource_import)
 
   # NOTE Downloads source from latest ruy library (2020-04-10)
   envoption(EXTERNAL_DOWNLOAD_SERVER "https://github.com")
-  set(RUY_URL ${EXTERNAL_DOWNLOAD_SERVER}/google/ruy/archive/2e2658f964638ab7aa562d4b48b76007d44e38f0.tar.gz)
-  ExternalSource_Download("ruy" ${RUY_URL})
+  envoption(RUY_URL ${EXTERNAL_DOWNLOAD_SERVER}/google/ruy/archive/2e2658f964638ab7aa562d4b48b76007d44e38f0.tar.gz)
+  ExternalSource_Download(RUY
+    DIRNAME RUY
+    URL ${RUY_URL})
 
-  set(RuySource_DIR ${ruy_SOURCE_DIR} PARENT_SCOPE)
-  set(RuySource_FOUND ${ruy_SOURCE_GET} PARENT_SCOPE)
+  set(RuySource_DIR ${RUY_SOURCE_DIR} PARENT_SCOPE)
+  set(RuySource_FOUND TRUE PARENT_SCOPE)
 endfunction(_RuySource_import)
 
 _RuySource_import()
