bc5a04279ad87ffcc9b211a6b942b480b66d5261
Hyeongseok Oh <hseok82.oh@samsung.com>
2020-09-14 15:49:33 +0900

    [infra/cmake] Merge neon2sse source download (#4243)
    - Use neon2sse source package in infra/cmake/packages
    - Update comment
    - Update environment variable name
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/cmake/packages/NEON2SSESourceConfig.cmake
+++ infra/cmake/packages/NEON2SSESourceConfig.cmake
@@ -7,12 +7,13 @@ function(_NEON2SSESource_import)
   nnas_include(ExternalSourceTools)
   nnas_include(OptionTools)
 
-  # NOTE TensorFlow 1.12 downloads NEON2SSE from the following URL
   # NOTE TensorFlow 1.13.1 downloads NEON2SSE from the following URL
+  # NOTE TensorFlow 2.1 downloads NEON2SSE from the following URL
   # NOTE TensorFlow 2.2 downloads NEON2SSE from the following URL
-  envoption(NEON2SSE_1_12_URL https://github.com/intel/ARM_NEON_2_x86_SSE/archive/1200fe90bb174a6224a525ee60148671a786a71f.tar.gz)
+  # NOTE TensorFlow 2.3 downloads NEON2SSE from the following URL
+  envoption(NEON2SSE_URL https://github.com/intel/ARM_NEON_2_x86_SSE/archive/1200fe90bb174a6224a525ee60148671a786a71f.tar.gz)
 
-  ExternalSource_Download(NEON2SSE ${NEON2SSE_1_12_URL})
+  ExternalSource_Download(NEON2SSE ${NEON2SSE_URL})
 
   set(NEON2SSESource_DIR ${NEON2SSE_SOURCE_DIR} PARENT_SCOPE)
   set(NEON2SSESource_FOUND TRUE PARENT_SCOPE)
