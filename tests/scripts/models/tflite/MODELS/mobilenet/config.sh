0974b71cce7eccbd9bcea653e1aa992c1f4211ff
Hyeongseok Oh <hseok82.oh@samsung.com>
2020-09-16 14:04:02 +0900

    Rename MODELFILE_SERVER_PATH to MODELFILE_URL_BASE

--- tests/scripts/models/tflite/MODELS/mobilenet/config.sh
+++ tests/scripts/models/tflite/MODELS/mobilenet/config.sh
@@ -1,2 +1,2 @@
-MODELFILE_SERVER_PATH="https://storage.googleapis.com/download.tensorflow.org/models/tflite"
+MODELFILE_URL_BASE="https://storage.googleapis.com/download.tensorflow.org/models/tflite"
 MODELFILE_NAME="mobilenet_v1_0.25_128_float_2017_11_08.zip"
--- tests/scripts/models/tflite/MODELS/mobilenet_quant8/config.sh
+++ tests/scripts/models/tflite/MODELS/mobilenet_quant8/config.sh
@@ -1,2 +1,2 @@
-MODELFILE_SERVER_PATH="https://storage.googleapis.com/download.tensorflow.org/models/tflite"
+MODELFILE_URL_BASE="https://storage.googleapis.com/download.tensorflow.org/models/tflite"
 MODELFILE_NAME="mobilenet_v1_1.0_224_quant_and_labels.zip"
