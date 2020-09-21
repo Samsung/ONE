7e4f3782fd7e3b9244dfeed6ee323d0a577d785a
Seok Namkoong <seok9311@naver.com>
2020-09-21 19:31:12 +0900

    [luci] Ignore shape and dtype check for CircleOutputExclude (#4357)
    `CircleOutputExclude` is for not exporting graph output.
    Therefore shape and dtype check for `CircleOutputExclude` is not needed.
    
    ONE-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>

--- compiler/luci/service/src/Validate.cpp
+++ compiler/luci/service/src/Validate.cpp
@@ -75,6 +75,11 @@ bool validate_shape_dtype(loco::Graph *g)
     assert(circle_output != nullptr);
     assert(circle_output->from() != nullptr);
     auto circle_node = loco::must_cast<luci::CircleNode *>(circle_output->from());
+
+    // Shape and dtype validation for CiecleOutputExclude is not needed
+    if (dynamic_cast<luci::CircleOutputExclude *>(circle_node))
+      continue;
+
     assert(loco::shape_known(circle_node));
 
     // check if output node shape is same as graph output shape
