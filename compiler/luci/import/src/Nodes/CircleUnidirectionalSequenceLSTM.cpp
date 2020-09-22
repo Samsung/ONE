0b2f4ec011ecd9dad7e1f5fe5542a9cef1596a7f
struss <rrstrous@nate.com>
2020-09-22 15:34:23 +0900

    Update More.

--- compiler/luci/import/src/Nodes/CircleUnidirectionalSequenceLSTM.cpp
+++ compiler/luci/import/src/Nodes/CircleUnidirectionalSequenceLSTM.cpp
@@ -38,34 +38,82 @@ CircleNode *CircleUnidirectionalSequenceLSTMGraphBuilder::build_node(
   node->input(inputs.at(0));
 
   node->input_to_input_weights(inputs.at(1)); // Optional
+  if (auto input_to_input_weights = dynamic_cast<luci::CircleOutputExclude *>(node->input_to_input_weights()))
+  {
+    input_to_input_weights->dtype(loco::DataType::FLOAT32);
+  }
   node->input_to_cell_weights(inputs.at(2));
   node->input_to_forget_weights(inputs.at(3));
   node->input_to_output_weights(inputs.at(4));
 
   node->recurrent_to_input_weights(inputs.at(5)); // Optional
+  if (auto recurrent_to_input_weights = dynamic_cast<luci::CircleOutputExclude *>(node->recurrent_to_input_weights()))
+  {
+    recurrent_to_input_weights->dtype(loco::DataType::FLOAT32);
+  }
   node->recurrent_to_cell_weights(inputs.at(6));
   node->recurrent_to_forget_weights(inputs.at(7));
   node->recurrent_to_output_weights(inputs.at(8));
 
   node->cell_to_input_weights(inputs.at(9));   // Optional
+  if (auto cell_to_input_weights = dynamic_cast<luci::CircleOutputExclude *>(node->cell_to_input_weights()))
+  {
+    cell_to_input_weights->dtype(loco::DataType::FLOAT32);
+  }
   node->cell_to_forget_weights(inputs.at(10)); // Optional
+  if (auto cell_to_forget_weights = dynamic_cast<luci::CircleOutputExclude *>(node->cell_to_forget_weights()))
+  {
+    cell_to_forget_weights->dtype(loco::DataType::FLOAT32);
+  }
   node->cell_to_output_weights(inputs.at(11)); // Optional
+  if (auto cell_to_output_weights = dynamic_cast<luci::CircleOutputExclude *>(node->cell_to_output_weights()))
+  {
+    cell_to_output_weights->dtype(loco::DataType::FLOAT32);
+  }
 
   node->input_gate_bias(inputs.at(12)); // Optional
+  if (auto input_gate_bias = dynamic_cast<luci::CircleOutputExclude *>(node->input_gate_bias()))
+  {
+    input_gate_bias->dtype(loco::DataType::FLOAT32);
+  }
   node->forget_gate_bias(inputs.at(13));
   node->cell_gate_bias(inputs.at(14));
   node->output_gate_bias(inputs.at(15));
 
   node->projection_weights(inputs.at(16)); // Optional
+  if (auto projection_weights = dynamic_cast<luci::CircleOutputExclude *>(node->projection_weights()))
+  {
+    projection_weights->dtype(loco::DataType::FLOAT32);
+  }
   node->projection_bias(inputs.at(17));    // Optional
+  if (auto projection_bias = dynamic_cast<luci::CircleOutputExclude *>(node->projection_bias()))
+  {
+    projection_bias->dtype(loco::DataType::FLOAT32);
+  }
 
   node->activation_state(inputs.at(18));
   node->cell_state(inputs.at(19));
 
   node->input_layer_norm_coefficients(inputs.at(20));  // Optional
+  if (auto input_layer_norm_coefficients = dynamic_cast<luci::CircleOutputExclude *>(node->input_layer_norm_coefficients()))
+  {
+    input_layer_norm_coefficients->dtype(loco::DataType::FLOAT32);
+  }
   node->forget_layer_norm_coefficients(inputs.at(21)); // Optional
+  if (auto forget_layer_norm_coefficients = dynamic_cast<luci::CircleOutputExclude *>(node->forget_layer_norm_coefficients()))
+  {
+    forget_layer_norm_coefficients->dtype(loco::DataType::FLOAT32);
+  }
   node->cell_layer_norm_coefficients(inputs.at(22));   // Optional
+  if (auto cell_layer_norm_coefficients = dynamic_cast<luci::CircleOutputExclude *>(node->cell_layer_norm_coefficients()))
+  {
+    cell_layer_norm_coefficients->dtype(loco::DataType::FLOAT32);
+  }
   node->output_layer_norm_coefficients(inputs.at(23)); // Optional
+  if (auto output_layer_norm_coefficients = dynamic_cast<luci::CircleOutputExclude *>(node->output_layer_norm_coefficients()))
+  {
+    output_layer_norm_coefficients->dtype(loco::DataType::FLOAT32);
+  }
 
   const auto *options = op.builtin_options.AsUnidirectionalSequenceLSTMOptions();
   node->fusedActivationFunction(luci_actfunc(options->fused_activation_function));
