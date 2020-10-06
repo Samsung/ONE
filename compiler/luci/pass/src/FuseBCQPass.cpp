a4a7c6a9ada94a0ab371a578abb69c7e0fb6a38c
Seok Namkoong <seok9311@naver.com>
2020-10-06 16:02:38 +0900

    [luci] Check more validation for BCQ information
    This commit will add more validation for BCQ information
    
    ONE-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>

--- compiler/luci/pass/src/FuseBCQPass.cpp
+++ compiler/luci/pass/src/FuseBCQPass.cpp
@@ -267,12 +267,123 @@ private:
 
   bool is_bcqinfo_valid()
   {
-    // do_w_x should be int32 or bool type
+    std::set<int32_t> ref_prefix_set;
+
     for (auto n : _do_w_x)
     {
-      if (n.second->dtype() != loco::DataType::BOOL && n.second->dtype() != loco::DataType::S32)
+      // do_w_x should be BOOL type
+      if (n.second->dtype() != loco::DataType::BOOL)
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : do_w_x has wrong type" << std::endl;
         return false;
+      }
+
+      // Generate reference prefix set
+      ref_prefix_set.insert(n.first);
     }
+
+    for (auto n : _alpha)
+    {
+      // alpha should be FLOAT32 type
+      if (n.second->dtype() != loco::DataType::FLOAT32)
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : alpha has wrong type" << std::endl;
+        return false;
+      }
+
+      if (ref_prefix_set.find(n.first) == ref_prefix_set.end())
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : Invalid prefix is found" << std::endl;
+        return false;
+      }
+    }
+
+    for (auto n : _packed_binary_code)
+    {
+      // packed_binary_code should be INT32 type
+      if (n.second->dtype() != loco::DataType::S32)
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : packed_binary_code has wrong type" << std::endl;
+        return false;
+      }
+
+      if (ref_prefix_set.find(n.first) == ref_prefix_set.end())
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : Invalid prefix is found" << std::endl;
+        return false;
+      }
+    }
+
+    for (auto n : _number_of_clusters)
+    {
+      // number_of_clusters should be INT32 type
+      if (n.second->dtype() != loco::DataType::S32)
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : number_of_clusters has wrong type" << std::endl;
+        return false;
+      }
+
+      if (ref_prefix_set.find(n.first) == ref_prefix_set.end())
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : Invalid prefix is found" << std::endl;
+        return false;
+      }
+    }
+
+    for (auto n : _size_of_clusters)
+    {
+      // size_of_clusters should be INT32 type
+      if (n.second->dtype() != loco::DataType::S32)
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : size_of_clusters has wrong type" << std::endl;
+        return false;
+      }
+
+      if (ref_prefix_set.find(n.first) == ref_prefix_set.end())
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : Invalid prefix is found" << std::endl;
+        return false;
+      }
+    }
+
+    for (auto n : _qbits_of_clusters)
+    {
+      // qbits_of_clusters should be INT32 type
+      if (n.second->dtype() != loco::DataType::S32)
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : qbits_of_clusters has wrong type" << std::endl;
+        return false;
+      }
+
+      if (ref_prefix_set.find(n.first) == ref_prefix_set.end())
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : Invalid prefix is found" << std::endl;
+        return false;
+      }
+    }
+
+    for (auto n : _fusable_op)
+    {
+      if (ref_prefix_set.find(n.first) == ref_prefix_set.end())
+      {
+        LOGGER(l);
+        WARN(l) << "FuseBCQPass : Invalid prefix is found" << std::endl;
+        return false;
+      }
+    }
+
+    // As dequant_weight is not used for fusing, skip validation.
 
     return true;
   }
