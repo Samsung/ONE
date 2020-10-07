9df5111d2ac98da727d5d02b2a59c3ff0ac958da
SaeHie Park <saehie.park@gmail.com>
2020-10-07 17:22:09 +0900

    [res] More TF examples
    This will add TF examples that was missing: atrous_conv2d, flatten and instance_norm
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ res/TensorFlowPythonExamples/examples/instance_norm/__init__.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+sess = tf.Session()
+
+in_ = tf.compat.v1.placeholder(dtype=tf.float32, shape=(3, 3), name="Hole")
+norm_ = tf.contrib.layers.instance_norm(in_)
+
+# we need to save checkpoint to freeze dropped model
+init = tf.initialize_all_variables()
+sess.run(init)
+
+saver = tf.train.Saver()
+saver.save(sess, './ckpt/instance_norm.ckpt')
+
+# use below command to freeze this model after running tfpem.py
+'''
+freeze_graph --input_graph instance_norm.pbtxt \
+--input_binary=false \
+--input_checkpoint=./ckpt/instance_norm.ckpt \
+--output_node_names=InstanceNorm/instancenorm/add_1 \
+--output_graph instance_norm_fr.pbtxt
+'''
