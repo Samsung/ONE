4d7c67ec5cb79b625c69bfd80107b55864880ec9
Roman Maltsev <r.maltsev@partner.samsung.com>
2020-10-08 15:09:10 +0000

    [onert] Codestyle fix, PR #4538 follow-up
    Use snake_case variable names instead of camelCase
    
    ONE-DCO-1.0-Signed-off-by: Roman Maltsev <r.maltsev@partner.samsung.com>

--- runtime/onert/backend/cpu/ops/StridedSliceLayer.cc
+++ runtime/onert/backend/cpu/ops/StridedSliceLayer.cc
@@ -37,17 +37,17 @@ StridedSliceLayer::StridedSliceLayer()
 
 template <typename T> void StridedSliceLayer::stridedSliceImpl()
 {
-  const auto inputShape = getTensorShape(_input);
-  const auto outputShape = getTensorShape(_output);
+  const auto input_shape = getTensorShape(_input);
+  const auto output_shape = getTensorShape(_output);
   auto op_params = nnfw::cker::buildStridedSliceParams(
       reinterpret_cast<uint32_t *>(_begin->buffer()), reinterpret_cast<uint32_t *>(_end->buffer()),
       reinterpret_cast<uint32_t *>(_strides->buffer()), _begin_mask, _end_mask, _shrink_axis_mask,
-      inputShape.DimensionsCount());
+      input_shape.DimensionsCount());
 
-  nnfw::cker::checkOutputSize(op_params, inputShape, outputShape, inputShape.DimensionsCount());
+  nnfw::cker::checkOutputSize(op_params, input_shape, output_shape, input_shape.DimensionsCount());
 
-  nnfw::cker::StridedSlice(op_params, inputShape, reinterpret_cast<const T *>(_input->buffer()),
-                           outputShape, reinterpret_cast<T *>(_output->buffer()));
+  nnfw::cker::StridedSlice(op_params, input_shape, reinterpret_cast<const T *>(_input->buffer()),
+                           output_shape, reinterpret_cast<T *>(_output->buffer()));
 }
 
 void StridedSliceLayer::configure(const IPortableTensor *input, const IPortableTensor *begin,
