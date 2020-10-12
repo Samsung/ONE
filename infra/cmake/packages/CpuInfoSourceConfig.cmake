fcc4725b5768011965ce915c1c50249f70f85415
Hyeongseok Oh <hseok82.oh@samsung.com>
2020-10-12 12:35:46 +0900

    [infra] Update cpuinfo package
    Download patched upstream cpuinfo source
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/cmake/packages/CpuInfoSourceConfig.cmake
+++ infra/cmake/packages/CpuInfoSourceConfig.cmake
@@ -8,19 +8,11 @@ function(_CpuInfoSource_import)
   nnas_include(OptionTools)
 
   envoption(EXTERNAL_DOWNLOAD_SERVER "https://github.com")
-  envoption(CPUINFO_URL ${EXTERNAL_DOWNLOAD_SERVER}/pytorch/cpuinfo/archive/5cefcd6293e6881754c2c53f99e95b159d2d8aa5.tar.gz)
+  # CPUINFO commit including patch from tflite v2.3
+  envoption(CPUINFO_URL ${EXTERNAL_DOWNLOAD_SERVER}/pytorch/cpuinfo/archive/63b254577ed77a8004a9be6ac707f3dccc4e1fd9.tar.gz)
   ExternalSource_Download(CPUINFO
     DIRNAME CPUINFO
     URL ${CPUINFO_URL})
-
-  # Applying patch to cpuinfo (This patch comes from tflite v2.3)
-  execute_process(
-    COMMAND patch -p1 --forward --ignore-whitespace
-    WORKING_DIRECTORY ${CPUINFO_SOURCE_DIR}
-    INPUT_FILE "${CMAKE_CURRENT_LIST_DIR}/CpuInfo/cpuinfo.patch"
-    OUTPUT_VARIABLE OUTPUT
-    RESULT_VARIABLE RESULT
-  )
 
   set(CpuInfoSource_DIR ${CPUINFO_SOURCE_DIR} PARENT_SCOPE)
   set(CpuInfoSource_FOUND TRUE PARENT_SCOPE)
