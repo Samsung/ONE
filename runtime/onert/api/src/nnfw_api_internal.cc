Samsung/ONE

commit b168ea9b2da3ec35e0ea1f00a061f96cad0f574d

[onert] Do nothing when 'passed shape == model shape' while setting input shape (#3998)
This makes nnfw_set_input_tensorinfo() do nothing when 'passed shape == model shape'.

Signed-off-by: Hyun Sik Yoon <hyunsik.yoon.1024@gmail.com>

runtime/onert/api/src/nnfw_api_internal.cc
@@ -481,26 +481,27 @@ NNFW_STATUS nnfw_session::apply_tensorinfo(uint32_t index, nnfw_tensorinfo ti)
     }
   }
 
+  auto ind = primary_subgraph()->getInputs().at(index);
+  auto &input = primary_subgraph()->operands().at(ind);
+
+  onert::ir::Shape new_shape(ti.rank);
+  for (int32_t i = 0; i < ti.rank; i++)
+    new_shape.dim(i) = ti.dims[i];
+
+  // if passed shape is same with the shape of model, do nothing
+  if (input.info().shape() == new_shape)
+    return NNFW_STATUS_NO_ERROR;
+
   if (!isStatePreparedOrFinishedRun())
   {
     // In this case, if we apply input shape in primary_subgraph, it will propagate after
     // compilation and excution
-    auto ind = primary_subgraph()->getInputs().at(index);
-    auto &input = primary_subgraph()->operands().at(ind);
-
-    onert::ir::Shape new_shape(ti.rank);
-    for (int32_t i = 0; i < ti.rank; i++)
-      new_shape.dim(i) = ti.dims[i];
 
     // overwrite input shape with the shape from ti
     input.info().shape(new_shape);
   }
   else // when called after nnfw_session::prepare()
   {
-    onert::ir::Shape new_shape(ti.rank);
-    for (int32_t i = 0; i < ti.rank; i++)
-      new_shape.dim(i) = ti.dims[i];
-
     _execution->changeInputShape(onert::ir::IOIndex(index), new_shape);
   }
 

commit ae5e68568c533e1f2049ddee25241c2094d54be8

[onert] Bugfix: Compare with runtime shape rather than model default shape in the logic of avoiding dynamic input mode.
This is a fix of the bug introduced in PR #3998

ONE-DCO-1.0-Signed-off-by: Roman Maltsev <r.maltsev@partner.samsung.com>

runtime/onert/api/src/nnfw_api_internal.cc
@@ -492,28 +492,26 @@ NNFW_STATUS nnfw_session::apply_tensorinfo(uint32_t index, nnfw_tensorinfo ti)
     }
   }
 
-  auto ind = primary_subgraph()->getInputs().at(index);
-  auto &input = primary_subgraph()->operands().at(ind);
-
   onert::ir::Shape new_shape(ti.rank);
   for (int32_t i = 0; i < ti.rank; i++)
     new_shape.dim(i) = ti.dims[i];
-
-  // if passed shape is same with the shape of model, do nothing
-  if (input.info().shape() == new_shape)
-    return NNFW_STATUS_NO_ERROR;
 
   if (!isStatePreparedOrFinishedRun())
   {
     // In this case, if we apply input shape in primary_subgraph, it will propagate after
     // compilation and excution
-
+    auto ind = primary_subgraph()->getInputs().at(index);
+    auto &input = primary_subgraph()->operands().at(ind);
     // overwrite input shape with the shape from ti
     input.info().shape(new_shape);
   }
   else // when called after nnfw_session::prepare()
   {
-    _execution->changeInputShape(onert::ir::IOIndex(index), new_shape);
+    // If the passed shape is the same as before, do nothing. This will prevent activating dynamic
+    // shape inference when not yet active and new shape is still the same as the prepared model
+    // shape.
+    if (_execution->getInputShape(onert::ir::IOIndex{index}) != new_shape)
+      _execution->changeInputShape(onert::ir::IOIndex(index), new_shape);
   }
 
   return NNFW_STATUS_NO_ERROR;