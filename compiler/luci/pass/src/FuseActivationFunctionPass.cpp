777213757089110c1e04858b8c4a75811d441dc0
Hyukjin Jeong <hj1.jeong@samsung.com>
2020-11-02 15:54:14 +0900

    [luci] Fuse activation function pass (#4862)
    This adds FuseActivationFunctionPass to luci
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

---
+++ compiler/luci/pass/src/FuseActivationFunctionPass.cpp
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "luci/Pass/FuseActivationFunctionPass.h"
+
+#include <luci/IR/CircleNodes.h>
+#include <luci/IR/CircleOpcode.h>
+
+namespace luci
+{
+
+bool fuse_activation_function(luci::CircleNode *node)
+{
+  auto preds = loco::preds(node);
+  assert(preds.size() == 1);
+
+  auto pred_node = static_cast<luci::CircleNode *>(*preds.begin());
+  if (loco::succs(pred_node).size() != 1)
+    return false;
+
+  auto node_with_fused_act =
+      dynamic_cast<luci::LuciNodeMixin<luci::LuciNodeTrait::FusedActFunc> *>(pred_node);
+  if (node_with_fused_act == nullptr)
+    return false;
+
+  auto fused_act = node_with_fused_act->fusedActivationFunction();
+
+  luci::FusedActFunc target_func = luci::FusedActFunc::UNDEFINED;
+
+  auto opcode = node->opcode();
+  if (opcode == luci::CircleOpcode::RELU)
+  {
+    if (fused_act == luci::FusedActFunc::NONE || fused_act == luci::FusedActFunc::RELU)
+      target_func = luci::FusedActFunc::RELU;
+    else if (fused_act == luci::FusedActFunc::RELU6)
+      target_func = luci::FusedActFunc::RELU6;
+    else
+      return false;
+  }
+  else if (opcode == luci::CircleOpcode::RELU6)
+  {
+    if (fused_act == luci::FusedActFunc::NONE || fused_act == luci::FusedActFunc::RELU ||
+        fused_act == luci::FusedActFunc::RELU6)
+      target_func = luci::FusedActFunc::RELU6;
+    else
+      return false;
+  }
+  else if (opcode == luci::CircleOpcode::RELU_N1_TO_1)
+  {
+    if (fused_act == luci::FusedActFunc::NONE || fused_act == luci::FusedActFunc::RELU_N1_TO_1)
+      target_func = luci::FusedActFunc::RELU_N1_TO_1;
+    else
+      return false;
+  }
+  else if (opcode == luci::CircleOpcode::TANH)
+  {
+    if (fused_act == luci::FusedActFunc::NONE)
+      target_func = luci::FusedActFunc::TANH;
+    else
+      return false;
+  }
+  else
+    return false;
+
+  node_with_fused_act->fusedActivationFunction(target_func);
+  loco::replace(node).with(pred_node);
+
+  node->drop();
+
+  return true;
+}
+
+bool FuseActivationFunctionPass::run(loco::Graph *g)
+{
+  bool changed = false;
+  for (auto node : loco::active_nodes(loco::output_nodes(g)))
+  {
+    auto circle_node = static_cast<luci::CircleNode *>(node);
+    auto opcode = circle_node->opcode();
+    if (opcode == luci::CircleOpcode::RELU || opcode == luci::CircleOpcode::RELU6 ||
+        opcode == luci::CircleOpcode::RELU_N1_TO_1 || opcode == luci::CircleOpcode::TANH)
+    {
+      if (fuse_activation_function(circle_node))
+        changed = true;
+    }
+  }
+
+  return changed;
+}
+
+} // namespace luci
---
+++ compiler/luci/pass/src/FuseActivationFunctionPass.test.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "FuseActivationFunctionPassInternal.h"
+
+#include <luci/IR/CircleNodes.h>
+
+#include <gtest/gtest.h>
+
+namespace
+{
+
+/**
+ *  Simple graph for test
+ *
+ *  BEFORE
+ *
+ *         [Conv1]
+ *           |
+ *     [Activation func]
+ *           |
+ *         [Conv2]
+ *
+ *  AFTER
+ *
+ *   [Conv1 + Activation func]
+ *           |
+ *         [Conv2]
+ *
+ */
+class SimpleGraph
+{
+public:
+  SimpleGraph()
+  {
+    conv1 = g.nodes()->create<luci::CircleConv2D>();
+    conv2 = g.nodes()->create<luci::CircleConv2D>();
+    relu = g.nodes()->create<luci::CircleRelu>();
+
+    conv1->fusedActivationFunction(luci::FusedActFunc::NONE);
+
+    relu->features(conv1);
+    conv2->input(relu);
+  }
+
+public:
+  loco::Graph g;
+  luci::CircleConv2D *conv1;
+  luci::CircleConv2D *conv2;
+  luci::CircleRelu *relu;
+};
+
+} // namespace
+
+TEST(FusePreActivationBatchNorm, fuse_activation_function)
+{
+  SimpleGraph g;
+
+  EXPECT_TRUE(luci::fuse_activation_function(g.relu));
+
+  EXPECT_EQ(g.conv1, g.conv2->input());
+}
+
+TEST(FusePreActivationBatchNorm, fuse_activation_function_dup_relu)
+{
+  SimpleGraph g;
+  g.conv1->fusedActivationFunction(luci::FusedActFunc::RELU);
+
+  EXPECT_TRUE(luci::fuse_activation_function(g.relu));
+
+  EXPECT_EQ(g.conv1, g.conv2->input());
+}
+
+TEST(FusePreActivationBatchNorm, fuse_activation_function_NEG)
+{
+  SimpleGraph g;
+  g.conv2->input(g.conv1);
+
+  // Conv1 has multiple successors
+  EXPECT_FALSE(luci::fuse_activation_function(g.relu));
+
+  g.conv2->input(g.relu);
+  g.conv1->fusedActivationFunction(luci::FusedActFunc::TANH);
+
+  // Conv1 already has activation function
+  EXPECT_FALSE(luci::fuse_activation_function(g.relu));
+}
