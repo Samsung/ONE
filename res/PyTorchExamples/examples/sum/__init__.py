88c43d531e78027d7f3a93e8acc004b865baf21f
underflow101 <ikarus125@gmail.com>
2020-11-04 00:28:28 +0900

    feat: Add PyTorchExamples of sum
    - Added PyTorch Example of sum
    
    Signed-off-by: underflow101 <ikarus125@gmail.com>

---
+++ res/PyTorchExamples/examples/sum/__init__.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+
+# model
+class net_sum(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return torch.sum(input)
+
+
+_model_ = net_sum()
+
+# dummy input for onnx generation
+_dummy_ = torch.randn(1, 2, 3, 3)
