abe020ef4bbfc74d362a73edb0a318a423a12555
SaeHie Park <saehie.park@gmail.com>
2020-11-11 07:52:20 +0900

    [one-cmds] Fix actions for image preprocess
    This will fix action and condition to invoke preprocess_images script
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/one-cmds/tests/prepare_test_materials.sh
+++ compiler/one-cmds/tests/prepare_test_materials.sh
@@ -44,14 +44,12 @@ if [[ ! -s "bcq.pb" ]]; then
 fi
 
 if [[ ! -s "img_files" ]]; then
-    rm -rf img_files
+    rm -rf img_files.zip
     wget https://github.com/Samsung/ONE/files/5499172/img_files.zip
     unzip img_files.zip
 fi
 
-if [ ! -s "raw_files" ] && [ ! -s "datalist.txt" ]; then
-    rm -rf raw_files
-    rm -rf datalist.txt
+if [ ! -d "raw_files" ] || [ ! -s "datalist.txt" ]; then
     ../bin/venv/bin/python preprocess_images.py
 fi
 
