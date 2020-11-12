6584865d14e15a3e00d286baa5f1a02463f7fc0c
Seok Namkoong <seok9311@naver.com>
2020-11-12 18:45:19 +0900

    [luci] Introduce ShapeSignatureInferencePass in luci
    This commit will introduce `ShapeSigantureInferencePass` in `luci`
    
    ONE-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>

---
+++ compiler/luci/pass/src/ShapeSignatureInferencePass.cpp
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "luci/Pass/ShapeSignatureInferencePass.h"
+
+#include <luci/IR/CircleShapeSignature.h>
+#include <luci/Service/CircleShapeSignatureInferenceRule.h>
+
+#include <loco.h>
+
+namespace
+{
+
+bool is_same_signature(luci::ShapeSignature lhs, luci::ShapeSignature rhs)
+{
+  if (lhs.rank() != rhs.rank())
+    return false;
+
+  for (uint32_t i = 0; i < lhs.rank(); ++i)
+    if (lhs.dim(i) != rhs.dim(i))
+      return false;
+
+  return true;
+}
+
+} // namespace
+
+namespace luci
+{
+
+bool ShapeSignatureInferencePass::run(loco::Graph *g)
+{
+  luci::CircleShapeSignatureInferenceRule signature_inference_rule;
+  bool changed = false;
+
+  for (auto node : loco::postorder_traversal(loco::output_nodes(g)))
+  {
+    luci::ShapeSignature shape_signature;
+
+    auto circle_node = loco::must_cast<luci::CircleNode *>(node);
+    if (signature_inference_rule.infer(circle_node, shape_signature))
+    {
+      if (!is_same_signature(circle_node->shape_signature(), shape_signature))
+      {
+        circle_node->shape_signature(shape_signature);
+        changed = true;
+      }
+    }
+  }
+
+  return changed;
+}
+
+} // namespace luci
