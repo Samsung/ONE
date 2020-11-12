b4767768b6cae3c03bfeb0c2489ab876ca42541c
Hyeongseok Oh <hseok82.oh@samsung.com>
2020-11-12 17:20:00 +0900

    [onert/cpu] Merge DepthwiseConv uint8 kernel with float
    Merge optimized DepthwiseConv uint8 multithread kernel with float DepthwiseConv by using template
    Update uint8 kernels in DepthwiseConvUint8.h
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- compute/cker/include/cker/operation/DepthwiseConv.h
+++ compute/cker/include/cker/operation/DepthwiseConv.h
@@ -73,62 +73,6 @@ private:
   int thread_dim_;
 };
 
-inline void DepthwiseConv(const DepthwiseConvParams &params, const Shape &input_shape,
-                          const uint8_t *input_data, const Shape &filter_shape,
-                          const uint8_t *filter_data, const Shape &bias_shape,
-                          const int32_t *bias_data, const Shape &output_shape, uint8_t *output_data)
-{
-  const int depth_multiplier = params.depth_multiplier;
-  const int32_t output_activation_min = params.quantized_activation_min;
-  const int32_t output_activation_max = params.quantized_activation_max;
-  const int dilation_width_factor = params.dilation_width_factor;
-  const int dilation_height_factor = params.dilation_height_factor;
-  assert(dilation_width_factor >= 1);
-  assert(dilation_height_factor >= 1);
-  UNUSED_RELEASE(dilation_width_factor);
-  UNUSED_RELEASE(dilation_height_factor);
-  assert(input_shape.DimensionsCount() == 4);
-  assert(filter_shape.DimensionsCount() == 4);
-  assert(output_shape.DimensionsCount() == 4);
-  assert(output_activation_min <= output_activation_max);
-  UNUSED_RELEASE(output_activation_min);
-  UNUSED_RELEASE(output_activation_max);
-  const int output_depth = MatchingDim(filter_shape, 3, output_shape, 3);
-  const int input_depth = input_shape.Dims(3);
-  assert(output_depth == input_depth * depth_multiplier);
-  assert(bias_shape.FlatSize() == output_depth);
-  UNUSED_RELEASE(input_depth);
-  UNUSED_RELEASE(output_depth);
-  UNUSED_RELEASE(depth_multiplier);
-
-// Enable for arm64 except for the Nvidia Linux 4 Tegra (L4T) running on
-// Jetson TX-2. This compiler does not support the offsetof() macro.
-#if defined(__aarch64__)
-//  TODO Use below codes
-
-//  const int stride_width = params.stride_width;
-//  const int stride_height = params.stride_height;
-//  const int pad_width = params.padding_values.width;
-//  const int pad_height = params.padding_values.height;
-//  const int output_shift = params.output_shift;
-//
-//  // Call kernel optimized for depthwise convolutions using 3x3 filters if
-//  // parameters are supported.
-//  if (Fast3x3FilterKernelSupported(
-//          input_shape, filter_shape, stride_width, stride_height,
-//          dilation_width_factor, dilation_height_factor, pad_width, pad_height,
-//          depth_multiplier, output_shape, output_shift)) {
-//    DepthwiseConv3x3Filter(params, input_shape, input_data, filter_shape,
-//                           filter_data, bias_shape, bias_data, output_shape,
-//                           output_data);
-//    return;
-//  }
-#endif
-
-  optimized::DepthwiseConvGeneral(params, input_shape, input_data, filter_shape, filter_data,
-                                  bias_shape, bias_data, output_shape, output_data);
-}
-
 inline int HowManyConvThreads(const Shape &output_shape, const Shape &filter_shape)
 {
   // How many scalar multiplications are needed to make it worth using one
@@ -171,11 +115,11 @@ inline bool MultithreadAlongBatches(int thread_count, int batches)
   return ((batches % thread_count) == 0);
 }
 
-// TODO Support different type by template
+template <typename T, typename TS>
 inline void DepthwiseConv(const DepthwiseConvParams &params, const Shape &input_shape,
-                          const float *input_data, const Shape &filter_shape,
-                          const float *filter_data, const Shape &bias_shape, const float *bias_data,
-                          const Shape &output_shape, float *output_data, ruy::Context *ruy_context)
+                          const T *input_data, const Shape &filter_shape, const T *filter_data,
+                          const Shape &bias_shape, const TS *bias_data, const Shape &output_shape,
+                          T *output_data, ruy::Context *ruy_context)
 {
   assert(input_shape.DimensionsCount() == 4);
   assert(filter_shape.DimensionsCount() == 4);
@@ -190,7 +134,10 @@ inline void DepthwiseConv(const DepthwiseConvParams &params, const Shape &input_
   thread_count = std::max(1, std::min(thread_count, max_threads));
   // Cap the number of threads to 2 for float path to avoid regression in
   // performance (b/132294857).
-  thread_count = std::min(thread_count, 2);
+  if (std::is_floating_point<T>::value)
+  {
+    thread_count = std::min(thread_count, 2);
+  }
 
   const int output_batches = output_shape.Dims(0);
   const int output_height = output_shape.Dims(1);
@@ -215,7 +162,7 @@ inline void DepthwiseConv(const DepthwiseConvParams &params, const Shape &input_
     thread_dim_size = output_height;
   }
 
-  std::vector<DepthwiseConvWorkerTask<float, float>> tasks;
+  std::vector<DepthwiseConvWorkerTask<T, TS>> tasks;
   // TODO(b/131746020) don't create new heap allocations every time.
   // At least we make it a single heap allocation by using reserve().
   tasks.reserve(thread_count);
