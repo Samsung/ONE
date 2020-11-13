2a6146f32d353213717b68ac67f72ed4f5ddf6a8
Hyukjin Jeong <hj1.jeong@samsung.com>
2020-11-13 18:53:35 +0900

    [luci-interpreter] Enable INT16, INT64 data types (#5051)
    This enables INT16/INT64 data types to run INT16-quantized models
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

--- compiler/luci-interpreter/src/loader/GraphLoader.cpp
+++ compiler/luci-interpreter/src/loader/GraphLoader.cpp
@@ -57,8 +57,12 @@ const void *getNodeData(const luci::CircleConst *node, size_t *data_size)
       return getNodeDataImpl<DataType::U8>(node, data_size);
     case DataType::FLOAT32:
       return getNodeDataImpl<DataType::FLOAT32>(node, data_size);
+    case DataType::S16:
+      return getNodeDataImpl<DataType::S16>(node, data_size);
     case DataType::S32:
       return getNodeDataImpl<DataType::S32>(node, data_size);
+    case DataType::S64:
+      return getNodeDataImpl<DataType::S64>(node, data_size);
     default:
       throw std::runtime_error("Unsupported type.");
   }
