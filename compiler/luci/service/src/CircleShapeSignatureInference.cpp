7a0f8ab8894db38305a537a447457fa0ec2dfb05
Seok Namkoong <seok9311@naver.com>
2020-11-13 19:26:00 +0900

    [luci] Re-design shape signature inference service
    This commit will re-design `luci/Service` of `ShapeSignature` by
    - Introducing inner namespace
    - Renaming class/struct/file
    and apply those changes to `CircleShapeSignatureInferencePass`
    
    ONE-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>

--- compiler/luci/service/src/CircleShapeSignatureInference.cpp
+++ compiler/luci/service/src/CircleShapeSignatureInference.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#include "luci/Service/CircleShapeSignatureInferenceRule.h"
+#include "luci/Service/CircleShapeSignatureInference.h"
 
 #include <luci/Log.h>
 
@@ -39,14 +39,16 @@ std::ostream &operator<<(std::ostream &os, const luci::ShapeSignature &shape_sig
 namespace luci
 {
 
-bool CircleShapeSignatureInferenceRule::infer(const luci::CircleNode *circle_node,
-                                              ShapeSignature &shape_signature) const
+namespace ssinf
+{
+
+bool Rule::infer(const luci::CircleNode *circle_node, ShapeSignature &shape_signature) const
 {
   LOGGER(l);
 
   // There is nothing to check before ShapeSignatureInference.
 
-  ShapeSignatureInferenceAlgorithm alg;
+  Algorithm alg;
 
   shape_signature = circle_node->accept(&alg);
 
@@ -56,5 +58,7 @@ bool CircleShapeSignatureInferenceRule::infer(const luci::CircleNode *circle_nod
 
   return true;
 }
+
+} // namespace ssinf
 
 } // namespace luci
