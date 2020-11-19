848bdd76d572400308176b256d4e4e8327a86a85
Yongseop Kim <yons.kim@samsung.com>
2020-11-19 20:03:42 +0900

    skip building x86_64 for xnnpack
    ONE-DCO-1.0-Signed-off-by: Yongseop Kim <yons.kim@samsung.com>

--- infra/nnfw/cmake/options/options_x86_64-linux.cmake
+++ infra/nnfw/cmake/options/options_x86_64-linux.cmake
@@ -3,3 +3,4 @@
 #
 option(BUILD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
 option(DOWNLOAD_ARMCOMPUTE "Download ARM Compute source" OFF)
+option(BUILD_XNNPACK "Build XNNPACK" OFF)
