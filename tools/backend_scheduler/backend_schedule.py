feefea34bb6f00367280fe0388a475c07fcf3b79
JiHwan Yeo <jihwan.yeo@samsung.com>
2020-11-19 17:13:16 +0900

    Update backend_schedule.py to handle given backends
    ONE-DCO-1.0-Signed-off-by: JiHwan Yeo <jihwan.yeo@samsung.com>

--- tools/backend_scheduler/backend_schedule.py
+++ tools/backend_scheduler/backend_schedule.py
@@ -11,11 +11,13 @@ def main(args):
     root_path = dirname(dirname(dirname(script_path)))
     os.chdir(root_path)
 
+    backend_list = ["cpu", "ruy"]
+
     op_time = {}
     for trace_file in os.listdir('./tools/backend_scheduler/traces'):
         if trace_file.endswith('.json') or trace_file.endswith('.md'):
             continue
-        if args.nnpackage_name not in trace_file:
+        if args.trace_name not in trace_file.rpartition('_')[0]:
             continue
         with open('./tools/backend_scheduler/traces/' + trace_file, 'r') as f:
             data = json.load(f)
@@ -26,7 +28,7 @@ def main(args):
                 backend = entry
                 backend_data = execution_data[backend]
                 for op in backend_data:
-                    op_index = op.split(' ')[0][1:]
+                    op_index = int(op.split(' ')[0][1:])
                     op_type = op.split(' ')[1]
                     time = int(backend_data[op]["Avg_Time"])
                     if op_index not in op_time.keys():
@@ -34,34 +36,46 @@ def main(args):
                         op_time[op_index].update({"type": op_type})
                     else:
                         op_time[op_index].update({backend: time})
-            print(op_time)
 
     backend_mapping = {}
+    backend_count = {}
+    for backend in backend_list:
+        backend_count[backend] = 0
+
+    # Find fastest library for each operation
     for op_index, value in op_time.items():
         op_type = value['type']
         if op_type != 'Conv2D':
             continue
-        cpu_time = value['cpu']
-        ruy_time = value['ruy']
 
-        if cpu_time < ruy_time:
-            backend_mapping[op_index] = 'cpu'
-            print("Use cpu : " + op_index)
-        else:
-            backend_mapping[op_index] = 'ruy'
+        op_infer_time = 0
+        for backend in backend_list:
+            backend_time = value[backend]
+
+            if op_infer_time == 0 or backend_time < op_infer_time:
+                op_infer_time = backend_time
+                backend_mapping[op_index] = backend
+
+    # Count backends
+    for op_index, backend in backend_mapping.items():
+        backend_count[backend] += 1
+
+    # Find default backend for Conv2D
+    default_backend = max(backend_count, key=backend_count.get)
 
     backend_conf = ""
-    for op_index, backend in backend_mapping.items():
-        backend_conf += "{}={};".format(op_index, backend)
+    for op_index, backend in sorted(backend_mapping.items()):
+        if backend != default_backend:
+            backend_conf += "{}={};".format(op_index, backend)
 
-    print(backend_conf)
+    print("-------- Backend Scheduling --------")
+    print("OP_BACKEND_MAP='{}' OP_BACKEND_Conv2D={} BACKENDS='{}'".format(
+        backend_conf, default_backend, ';'.join(backend_list)))
 
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("nnpackage_name",
-                            type=str,
-                            help="nnpackage folder to profile")
+    arg_parser.add_argument("trace_name", type=str, help="Recorded trace file name")
     args = arg_parser.parse_args()
 
     main(args)
