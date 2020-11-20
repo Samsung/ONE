ec6f5858403576ab64c1197ad46d796ca36bcc40
Yongseop Kim <yons.kim@samsung.com>
2020-11-20 15:56:09 +0900

    Remove xnnpack on qint8 tests in DepthwiseConv
    ONE-DCO-1.0-Signed-off-by: Yongseop Kim <yons.kim@samsung.com>

--- tests/nnfw_api/src/one_op_tests/DepthwiseConv2D.cc
+++ tests/nnfw_api/src/one_op_tests/DepthwiseConv2D.cc
@@ -224,7 +224,7 @@ TEST_P(DepthwiseConv2DVariation, Test)
       param.stride, param.input_depth, param.depth_multiplier));
   std::vector<uint8_t> ref_input(input64.begin(), input64.begin() + param.input_depth * 4);
   _context->addTestCase(uniformTCD<uint8_t>({ref_input}, {param.ref_output}));
-  _context->setBackends({"acl_cl", "acl_neon", "cpu", "xnnpack"});
+  _context->setBackends({"acl_cl", "acl_neon", "cpu"});
 
   SUCCEED();
 }
