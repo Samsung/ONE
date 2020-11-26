6bda046744bc088c6e7f83e80fbc34c05f9399d4
struss <rrstrous@nate.com>
2020-11-26 11:05:15 +0900

    [circle2circle]Introduce RemoveRedundantTranspose pass in circle2circle
    This commit introduce `RemoveRedundantTranspose` pass in `circle2circle`
    
    ONE-DCO-1.0-Signed-off-by: KiDeuk Bang <rrstrous@nate.com>

--- compiler/circle2circle/src/Circle2Circle.cpp
+++ compiler/circle2circle/src/Circle2Circle.cpp
@@ -110,6 +110,12 @@ int entry(int argc, char **argv)
       .default_value(false)
       .help("This will fuse BatchNorm operators of pre-activations to Convolution operator");
 
+  arser.add_argument("--remove_redundant_transpose")
+      .nargs(0)
+      .required(false)
+      .default_value(false)
+      .help("This will fuse or remove subsequent Transpose operators");
+
   arser.add_argument("--resolve_customop_add")
       .nargs(0)
       .required(false)
@@ -220,6 +226,8 @@ int entry(int argc, char **argv)
     options->enable(Algorithms::MakeBatchNormGammaPositive);
   if (arser.get<bool>("--fuse_preactivation_batchnorm"))
     options->enable(Algorithms::FusePreActivationBatchNorm);
+  if (arser.get<bool>("--remove_redundant_transpose"))
+    options->enable(Algorithms::RemoveRedundantTranspose);
   if (arser.get<bool>("--resolve_customop_add"))
     options->enable(Algorithms::ResolveCustomOpAdd);
   if (arser.get<bool>("--resolve_customop_batchmatmul"))
