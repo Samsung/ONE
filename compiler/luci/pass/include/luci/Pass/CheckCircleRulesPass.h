42a5ce43f46e3e22f55b212dcc5d631269381154
Seok Namkoong <seok9311@naver.com>
2020-12-01 17:21:18 +0900

    introduce new pass for checking correctness of new circle rules

---
+++ compiler/luci/pass/include/luci/Pass/CheckCircleRulesPass.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __LUCI_CHECK_CIRCLE_RULES_PASS_H__
+#define __LUCI_CHECK_CIRCLE_RULES_PASS_H__
+
+#include <loco.h>
+
+#include <luci/ModulePass.h>
+
+namespace luci
+{
+
+/**
+ * @brief Pass to infer shape of circle nodes
+ */
+class CheckCircleRulesPass : public luci::Pass
+{
+public:
+  virtual const char *name(void) const { return "luci::CheckCircleRulesPass"; }
+
+public:
+  bool run(luci::Module *m);
+  bool run(loco::Graph *graph);
+};
+
+} // namespace luci
+
+#endif //__LUCI_CHECK_CIRCLE_RULES_PASS_H__
