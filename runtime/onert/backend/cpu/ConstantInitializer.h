34baae5edfbd9e88ed464f5f40d5c20b2155ef3f
dayo09 <dayg502@gmail.com>
2020-12-01 13:57:42 +0900

    [onert] update header guard name
    - edit constant initializer header guard from _COMPILER_ to _BACKEND_
    
    ONE-DCO-1.0-Signed-off-by: dayo09 <dayg502@gmail.com>

--- runtime/onert/backend/cpu/ConstantInitializer.h
+++ runtime/onert/backend/cpu/ConstantInitializer.h
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#ifndef __ONERT_COMPILER_CPU_CONSTANT_INITIALIZER_H__
-#define __ONERT_COMPILER_CPU_CONSTANT_INITIALIZER_H__
+#ifndef __ONERT_BACKEND_CPU_CONSTANT_INITIALIZER_H__
+#define __ONERT_BACKEND_CPU_CONSTANT_INITIALIZER_H__
 
 #include "backend/cpu_common/TensorRegistry.h"
 
@@ -60,4 +60,4 @@ private:
 } // namespace backend
 } // namespace onert
 
-#endif // __ONERT_COMPILER_CPU_CONSTANT_INITIALIZER_H__
+#endif // __ONERT_BACKEND_CPU_CONSTANT_INITIALIZER_H__
