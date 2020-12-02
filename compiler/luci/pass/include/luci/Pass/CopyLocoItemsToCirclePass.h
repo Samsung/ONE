d0c1b6ffaa030812a47daee5c463b07610d9c255
Seok Namkoong <seok9311@naver.com>
2020-12-02 15:33:47 +0900

    [luci] Introduce CopyLocoItemsToCirclePass in luci
    Until refactoring for removing dependency between `loco` shape and circle shape is finished,
    shape and dtype of `loco` should be copied to circle nodes to keep consistency.
    This commit will introduce `CopyLocoItemsToCirclePass` and this pass will do it.
    FYI, be careful that when copying `loco` shape to circle node, shape signature of the circle node
    is considered, but shape of `loco` is not changed.
    
    When the refactoring is finished, this pass will be removed.
    
    ONE-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>

---
+++ compiler/luci/pass/include/luci/Pass/CopyLocoItemsToCirclePass.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __LUCI_COPY_LOCO_ITEMS_TO_CIRCLE_PASS_H__
+#define __LUCI_COPY_LOCO_ITEMS_TO_CIRCLE_PASS_H__
+
+#include <loco.h>
+
+#include <luci/ModulePass.h>
+
+namespace luci
+{
+
+/**
+ * @brief Pass to copy shape/dtype of loco to circle node
+ *
+ * CAUTION : This pass will be removed after refactoring is finished
+ */
+class CopyLocoItemsToCirclePass : public luci::Pass
+{
+public:
+  virtual const char *name(void) const { return "luci::CopyLocoItemsToCirclePass"; }
+
+public:
+  bool run(luci::Module *m);
+  bool run(loco::Graph *graph);
+};
+
+} // namespace luci
+
+#endif //__LUCI_COPY_LOCO_ITEMS_TO_CIRCLE_PASS_H__
