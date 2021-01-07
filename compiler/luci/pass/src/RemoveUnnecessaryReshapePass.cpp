6906a9ad91f86f5078d77f3ccb955f6b0ed30672
struss <rrstrous@nate.com>
2021-01-07 18:58:04 +0900

    Fix logic for checking this reshape not needed.
    ONE-DCO-1.0-Signed-off-by: KiDeuk Bang <rrstrous@nate.com>

--- compiler/luci/pass/src/RemoveUnnecessaryReshapePass.cpp
+++ compiler/luci/pass/src/RemoveUnnecessaryReshapePass.cpp
@@ -27,43 +27,25 @@ bool remove_no_effect_reshape(luci::CircleNode *node)
   if (target_node == nullptr)
     return false;
 
-  std::vector<int32_t> shape_info;
   auto new_shape = dynamic_cast<luci::CircleConst *>(target_node->shape());
-  // If shape node is not a const node, get updated shape info from option.
   if (new_shape == nullptr)
-  {
-    auto dummy_shape = dynamic_cast<luci::CircleOutputDummy *>(target_node->shape());
-    if (dummy_shape == nullptr)
-      return false;
-
-    // Need to extract new_shape info on target_node->newShape()
-    shape_info.resize(target_node->newShape()->rank());
-    for (uint32_t i = 0; i < target_node->newShape()->rank(); i++)
-      shape_info.at(i) = target_node->newShape()->dim(i);
-  }
-  // If shape node is presented as const node, extract updated shape from const node.
-  else
-  {
-    shape_info.resize(new_shape->dim(0).value());
-    for (uint32_t i = 0; i < new_shape->dim(0).value(); i++)
-      shape_info.at(i) = new_shape->at<loco::DataType::S32>(i);
-  }
+    return false;
 
   // Compare updated shape and input shape.
   auto input_node = loco::must_cast<luci::CircleNode *>(target_node->tensor());
-  if (input_node->rank() != shape_info.size())
+  if (input_node->rank() != new_shape->dim(0).value())
     return false;
   for (uint32_t i = 0; i < input_node->rank(); i++)
   {
     // If update_shape is -1, don't care
     // TODO check updated shape has value -1 at most one.
-    if (shape_info.at(i) == -1)
+    if (new_shape->at<loco::DataType::S32>(i) == -1)
       continue;
     // If input_shape dynamic, can't remove this.
     if (!input_node->dim(i).known())
       return false;
     // If input_shape and updated shape differ, also can't remove.
-    if (input_node->dim(i).value() != static_cast<uint32_t>(shape_info.at(i)))
+    if (input_node->dim(i).value() != static_cast<uint32_t>(new_shape->at<loco::DataType::S32>(i)))
       return false;
   }
 
