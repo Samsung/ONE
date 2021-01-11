f85543ebbfe5a2328b6b2a37c2de82e99e1f1863
SlavikMIPT <slavikmipt@gmail.com>
2021-01-12 07:37:24 +0800

    pepper-str cmake imported

--- compiler/luci-micro/import_libs.py
+++ compiler/luci-micro/import_libs.py
@@ -2,3 +2,4 @@ import shutil
 
 shutil.copytree('../stdex', './lib/stdex', dirs_exist_ok=True)
 shutil.copytree('../oops', './lib/oops', dirs_exist_ok=True)
+shutil.copytree('../pepper-str', './lib/pepper-str', dirs_exist_ok=True)
