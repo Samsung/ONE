f319c0ba436332b54fcdb801aa22488e9729a8c8
SlavikMIPT <slavikmipt@gmail.com>
2021-01-12 10:14:24 +0800

    LUCI_MICRO env var

--- compiler/luci-micro/import_libs.py
+++ compiler/luci-micro/import_libs.py
@@ -10,17 +10,17 @@ shutil.copytree('../loco', './lib/loco', dirs_exist_ok=True)
 shutil.copytree('../locomotiv', './lib/locomotiv', dirs_exist_ok=True)
 shutil.copytree('../angkor', './lib/angkor', dirs_exist_ok=True)
 
-pattern  = r"target_link_libraries\(.+ (nncc_common|nncc_coverage)\)"
-for libname in ['locomotiv', 'angkor', 'loco']:
-	filename = f'./lib/{libname}/CMakeLists.txt'
-	new_file = []
-	with open(filename, 'r') as f:
-	   lines = f.readlines()
-	for line in lines:
-	    match = re.search(pattern, line)
-	    if not match:
-	        new_file.append(line)
+# pattern  = r"target_link_libraries\(.+ (nncc_common|nncc_coverage)\)"
+# for libname in ['locomotiv', 'angkor', 'loco']:
+# 	filename = f'./lib/{libname}/CMakeLists.txt'
+# 	new_file = []
+# 	with open(filename, 'r') as f:
+# 	   lines = f.readlines()
+# 	for line in lines:
+# 	    match = re.search(pattern, line)
+# 	    if not match:
+# 	        new_file.append(line)
 
-	with open(filename, 'w') as f:
-	     f.seek(0)
-	     f.writelines(new_file)
+# 	with open(filename, 'w') as f:
+# 	     f.seek(0)
+# 	     f.writelines(new_file)
