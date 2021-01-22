1ee59fdb3c8b94bebc160614a4c3bd92fd2d13b2
Anton Zhukov <anton.v.zhukov@gmail.com>
2021-01-22 11:06:41 +0300

    Update

--- compiler/luci/pass/src/RemoveUnnecessaryStridedSlicePass.cpp
+++ compiler/luci/pass/src/RemoveUnnecessaryStridedSlicePass.cpp
@@ -39,22 +39,19 @@ int64_t value_from_circle_const(const luci::CircleConst *node, uint32_t idx)
 
 bool remove_no_effect_strided_slice(luci::CircleStridedSlice *target_node)
 {
-  auto begin_const = loco::must_cast<luci::CircleConst *>(target_node->begin());
+  auto begin_const = dynamic_cast<luci::CircleConst *>(target_node->begin());
   if (begin_const == nullptr)
     return false;
 
-  auto strides_const = loco::must_cast<luci::CircleConst *>(target_node->strides());
+  auto strides_const = dynamic_cast<luci::CircleConst *>(target_node->strides());
   if (strides_const == nullptr)
     return false;
 
-  auto input_node = loco::must_cast<luci::CircleNode *>(target_node->input());
-  if (input_node == nullptr)
-    return false;
-
-  auto end_const = loco::must_cast<luci::CircleConst *>(target_node->end());
+  auto end_const = dynamic_cast<luci::CircleConst *>(target_node->end());
   if (end_const == nullptr)
     return false;
 
+  auto input_node = loco::must_cast<luci::CircleNode *>(target_node->input());
   for (uint32_t i = 0; i < input_node->rank(); i++)
   {
     if (value_from_circle_const(begin_const, i) != 0)
