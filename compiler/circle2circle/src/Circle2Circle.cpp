b3fe97ec8bc61fa4e1de73491820e7f8f9e3308b
SaeHie Park <saehie.park@gmail.com>
2021-01-27 16:39:38 +0900

    [circle2circle] Enable substitute_squeeze_to_reshape
    This will enable substitute_squeeze_to_reshape option.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/circle2circle/src/Circle2Circle.cpp
+++ compiler/circle2circle/src/Circle2Circle.cpp
@@ -207,6 +207,12 @@ int entry(int argc, char **argv)
     .default_value(false)
     .help("This will convert single input Pack to Reshape");
 
+  arser.add_argument("--substitute_squeeze_to_reshape")
+    .nargs(0)
+    .required(false)
+    .default_value(false)
+    .help("This will convert certain condition Squeeze to Reshape");
+
   arser.add_argument("--substitute_transpose_to_reshape")
     .nargs(0)
     .required(false)
@@ -357,6 +363,8 @@ int entry(int argc, char **argv)
     options->enable(Algorithms::ShuffleWeightTo16x1Float32);
   if (arser.get<bool>("--substitute_pack_to_reshape"))
     options->enable(Algorithms::SubstitutePackToReshape);
+  if (arser.get<bool>("--substitute_squeeze_to_reshape"))
+    options->enable(Algorithms::SubstituteSqueezeToReshape);
   if (arser.get<bool>("--substitute_transpose_to_reshape"))
     options->enable(Algorithms::SubstituteTransposeToReshape);
   if (arser.get<bool>("--transform_min_max_to_relu6"))
