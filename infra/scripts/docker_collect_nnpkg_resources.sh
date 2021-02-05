ee14647be90d7ab737ed91785170f02534f82cf2
Hyeongseok Oh <hseok82.oh@samsung.com>
2021-02-05 11:23:06 +0900

    [infra] Remove ubuntu 16.04 dockerfile (#5963)
    This commit remove ubuntu 16.04 (xenial) dockerfile and related comments
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/scripts/docker_collect_nnpkg_resources.sh
+++ infra/scripts/docker_collect_nnpkg_resources.sh
@@ -40,8 +40,8 @@ if [ -d $ONNXRUNTIME_PREFIX ]; then
 fi
 
 # docker image name
-# - for xenial, use DOCKER_IMAGE_NAME="nnfw/one-devtools:xenial"
 # - for bionic, use DOCKER_IMAGE_NAME="nnfw/one-devtools:bionic"
+# - for focal, use DOCKER_IMAGE_NAME="nnfw/one-devtools:focal"
 if [[ -z $DOCKER_IMAGE_NAME ]]; then
   echo "It will use default docker image name"
 fi
