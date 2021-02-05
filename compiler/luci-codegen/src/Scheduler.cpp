714862db9cc92f78245573c6e7cf0467158cfe1b
Alexander Efimov <a.efimov@samsung.com>
2021-02-05 20:47:11 +0300

    use default auto scheduler

--- compiler/luci-codegen/src/Scheduler.cpp
+++ compiler/luci-codegen/src/Scheduler.cpp
@@ -15,17 +15,43 @@
  */
 
 #include "Scheduler.h"
+//#include "loco/IR/Algorithm.h"
 
 namespace luci_codegen
 {
 
 void Scheduler::process()
 {
-  std::
-  for (auto output: _subgraph.get_outputs())
+  for (auto &input: _subgraph.get_inputs())
   {
-    output.second.vectorize(output.second.args()[0], Halide::Expr(4));
+    luci::CircleNode *node = input.first;
+    Halide::ImageParam input_param = input.second;
+    Halide::Region estimates;
+    uint32_t rank = node->rank();
+    for (int i = 0; i < rank; ++i)
+    {
+      estimates.emplace_back(0, static_cast<int>(node->dim(rank - 1 - i).value()));
+    }
+    input_param.set_estimates(std::move(estimates));
   }
+  std::vector<Halide::Func> halide_outputs;
+  for (auto &output : _subgraph.get_outputs())
+  {
+    luci::CircleNode *node = output.first;
+    Halide::Func output_func = output.second;
+    halide_outputs.push_back(output_func);
+    Halide::Region estimates;
+    uint32_t rank = node->rank();
+    for (int i = 0; i < rank; ++i)
+    {
+      estimates.emplace_back(0, static_cast<int>(node->dim(rank - 1 - i).value()));
+    }
+    output_func.set_estimates(std::move(estimates));
+  }
+  Halide::Pipeline pipeline(halide_outputs);
+  Halide::MachineParams params(1, 32*1024, 40);
+  // todo see GeneratorBase::build_module how to compile pipeline to module and use schedule
+  Halide::AutoSchedulerResults schedule = pipeline.auto_schedule(Halide::get_host_target(), params);
 }
 
 } // namespace luci_codegen
