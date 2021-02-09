5cb99bc42973f2a3668e8b99e04c871175aadb77
Anton Zhukov <anton.v.zhukov@gmail.com>
2021-02-09 20:06:14 +0300

    format

--- compiler/luci/pass/src/RemoveUnnecessaryStridedSlicePass.cpp
+++ compiler/luci/pass/src/RemoveUnnecessaryStridedSlicePass.cpp
@@ -74,8 +74,7 @@ bool remove_no_effect_strided_slice(luci::CircleStridedSlice *target_node)
    * We check additional attributes on zero after shapes
    * for skipping wrong StridedSlice operator.
    */
-  if (target_node->new_axis_mask() != 0 ||
-      target_node->shrink_axis_mask() != 0)
+  if (target_node->new_axis_mask() != 0 || target_node->shrink_axis_mask() != 0)
     return false;
 
   replace(target_node).with(input_node);
