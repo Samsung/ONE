5fb88e5fe3e6cd23490d800fbd4cc7ce17783462
chunseoklee <chunseoklee@naver.com>
2021-02-15 13:15:03 +0900

    ONE Runtime version bump to 1.14.0 (#6018)
    - Bump to 1.14.0
    - Note that ONE Runtime 1.13.0 omitted in release
    
    ONE-DCO-1.0-Signed-off-by: Chunseok Lee <chunseok.lee@samsung.com>

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, Samsung Research & contributors'
 author = 'Samsung Research & contributors'
 
 # The full version, including alpha/beta/rc tags
-release = '1.13.0'
+release = '1.14.0'
 
 # -- General configuration ---------------------------------------------------
 
