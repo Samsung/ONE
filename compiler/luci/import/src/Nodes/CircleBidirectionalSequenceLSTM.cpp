5958053bf06ecfe52ddfe7733a28cb59cc00e144
SaeHie Park <saehie.park@gmail.com>
2021-03-03 14:29:49 +0900

    [luci] Remove unused import CircleCustom and others (#6179)
    This will remove unused import codes in CircleCustom,
    CircleBidirectionalSequenceLSTM and CircleIf.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/import/src/Nodes/CircleBidirectionalSequenceLSTM.cpp
+++ compiler/luci/import/src/Nodes/CircleBidirectionalSequenceLSTM.cpp
@@ -99,66 +99,6 @@ CircleNode *CircleBidirectionalSequenceLSTMGraphBuilder::build_node(const BuildN
   return node;
 }
 
-#if 0
-CircleNode *CircleBidirectionalSequenceLSTMGraphBuilder::build(const circle::OperatorT &op,
-                                                               GraphBuilderContext *context) const
-{
-  assert(context != nullptr);
-
-  const std::vector<int32_t> &inputs = op.inputs;
-  const std::vector<int32_t> &outputs = op.outputs;
-  const auto &tensors = context->reader()->tensors();
-  auto tensors_ptr = context->reader()->tensors_ptr();
-  assert(tensors_ptr != nullptr);
-
-  std::vector<CircleNode *> input_nodes;
-  for (const int32_t input_tensor_index : inputs)
-  {
-    if (input_tensor_index >= 0)
-    {
-      auto input = context->nodefinder()->node(input_tensor_index);
-      if (input != nullptr)
-        input_nodes.push_back(input);
-    }
-    else
-    {
-      // If there is no tensor, insert CircleOutputExclude.
-      input_nodes.push_back(context->graph()->nodes()->create<luci::CircleOutputExclude>());
-    }
-  }
-
-  // Create CircleBidirectionalSequenceLSTM
-  CircleNode *node =
-    CircleBidirectionalSequenceLSTMGraphBuilder::build_node(op, input_nodes, context->graph());
-
-  assert(int32_t(outputs.size()) == 2);
-  // Let's use name of output 0 as BidirectionalSequenceLSTM name
-  const circle::TensorT &output_tensor = *tensors[outputs[0]];
-  node->name(tensor_name(output_tensor));
-
-  // Create virtual outputs of BidirectionalSequenceLSTM
-  for (int32_t n = 0; n < 2; ++n)
-  {
-    const circle::TensorT &output_tensor = *tensors[outputs[n]];
-
-    auto *nodeout = context->graph()->nodes()->create<CircleBidirectionalSequenceLSTMOut>();
-    copy_tensor_attributes(output_tensor, nodeout);
-    // mark shape_status
-    if (tensors_ptr->Get(outputs[n])->shape() == nullptr)
-      nodeout->shape_status(ShapeStatus::NOSHAPE);
-    else
-      nodeout->shape_status(ShapeStatus::VALID);
-
-    nodeout->input(node);
-    nodeout->index(n);
-
-    context->nodefinder()->enroll(outputs[n], nodeout);
-  }
-
-  return node;
-}
-#endif
-
 CircleNode *CircleBidirectionalSequenceLSTMGraphBuilder::build_out(const BuildOutArgs &boa) const
 {
   auto *nodeout = boa.node->graph()->nodes()->create<CircleBidirectionalSequenceLSTMOut>();
--- compiler/luci/import/src/Nodes/CircleCustom.cpp
+++ compiler/luci/import/src/Nodes/CircleCustom.cpp
@@ -27,68 +27,6 @@ bool CircleCustomGraphBuilder::validate(const ValidateArgs &) const
   return true;
 }
 
-#if 0
-CircleNode *CircleCustomGraphBuilder::build(const circle::OperatorT &op,
-                                            GraphBuilderContext *context) const
-{
-  assert(context != nullptr);
-
-  auto graph = context->graph();
-
-  const std::vector<int32_t> &inputs = op.inputs;
-  const std::vector<int32_t> &outputs = op.outputs;
-  const auto &tensors = context->reader()->tensors();
-  auto tensors_ptr = context->reader()->tensors_ptr();
-  assert(tensors_ptr != nullptr);
-
-  // Create CircleCustom
-  const auto &opcodes = context->reader()->opcodes();
-  const uint32_t opcode_index = op.opcode_index;
-  const circle::OperatorCodeT &opcode = *opcodes[opcode_index];
-
-  auto *node = graph->nodes()->create<CircleCustom>(inputs.size(), outputs.size());
-  uint32_t input_idx = 0;
-  for (const int32_t input_tensor_index : inputs)
-  {
-    node->inputs(input_idx++, context->nodefinder()->node(input_tensor_index));
-  }
-  node->custom_options(std::vector<uint8_t>{op.custom_options.begin(), op.custom_options.end()});
-  node->custom_code(opcode.custom_code);
-  // Operator version of custom is always 1, so do nothing
-
-  uint32_t output_count = outputs.size();
-
-  assert(output_count > 0);
-  {
-    // Let's use attributes from output 0 for this node
-    const circle::TensorT &output_tensor = *tensors[outputs[0]];
-    node->name(tensor_name(output_tensor));
-    node->dtype(luci_datatype(output_tensor.type));
-  }
-
-  // Create virtual outputs of Custom
-  for (uint32_t n = 0; n < output_count; ++n)
-  {
-    const circle::TensorT &output_tensor = *tensors[outputs[n]];
-
-    auto *nodeout = graph->nodes()->create<CircleCustomOut>();
-    copy_tensor_attributes(output_tensor, nodeout);
-    // mark shape_status
-    if (tensors_ptr->Get(outputs[n])->shape() == nullptr)
-      nodeout->shape_status(ShapeStatus::NOSHAPE);
-    else
-      nodeout->shape_status(ShapeStatus::VALID);
-
-    nodeout->input(node);
-    nodeout->index(n);
-
-    context->nodefinder()->enroll(outputs[n], nodeout);
-  }
-
-  return node;
-}
-#endif
-
 CircleNode *CircleCustomGraphBuilder::build_node(const BuildNodeArgs &bna) const
 {
   uint32_t input_count = bna.op.inputs.size();
