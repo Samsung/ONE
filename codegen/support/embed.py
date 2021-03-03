94330e8be5ba9ded6ba5512bb17ebcb0e6d86d61
binarman <efimov.alexander@gmail.com>
2021-03-04 01:28:21 +0300

    format fix

--- codegen/support/embed.py
+++ codegen/support/embed.py
@@ -19,25 +19,27 @@
 
 import sys
 
+
 def generate(intput_filename, output_filename, array_name):
-  file_contents = ""
-  with open(input_filename, 'r') as input_file:
-    line = input_file.readline()
-    while line:
-      if line.find("//") != 0:
-        file_contents += line
-        file_contents += "\n"
-      line = input_file.readline()
-  with open(output_filename, 'w') as output_file:
-    prelude = 'const char *' + array_name + ' = R"magic_9681(' # random number
-    finishing = ')magic_9681";\n';
-    output_file.write(prelude + file_contents + finishing)
+    file_contents = ""
+    with open(input_filename, 'r') as input_file:
+        line = input_file.readline()
+        while line:
+            if line.find("//") != 0:
+                file_contents += line
+                file_contents += "\n"
+            line = input_file.readline()
+    with open(output_filename, 'w') as output_file:
+        prelude = 'const char *' + array_name + ' = R"magic_9681('  # random number
+        finishing = ')magic_9681";\n'
+        output_file.write(prelude + file_contents + finishing)
+
 
 if __name__ == '__main__':
-  if len(sys.argv) != 4:
-    print('Usage: embed <input file> <output_file> <name of generated array>')
-    exit(1)
-  input_filename = sys.argv[1]
-  output_filename = sys.argv[2]
-  array_name = sys.argv[3]
-  generate(input_filename, output_filename, array_name)
+    if len(sys.argv) != 4:
+        print('Usage: embed <input file> <output_file> <name of generated array>')
+        exit(1)
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+    array_name = sys.argv[3]
+    generate(input_filename, output_filename, array_name)
