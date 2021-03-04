ff1570c576d2a115d2267e060f369f18334ea590
SaeHie Park <saehie.park@gmail.com>
2021-03-04 18:43:48 +0900

    [luci] Revise BatchNormPatternFinder test (#6206)
    This will revise BatchNormPatternFinder pass test with TestIOGraph.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/pass/src/BatchNormPatternFinder.test.cpp
+++ compiler/luci/pass/src/BatchNormPatternFinder.test.cpp
@@ -16,76 +16,16 @@
 
 #include "BatchNormPatternFinder.h"
 
+#include "test/TestIOGraph.h"
+
 #include <luci/IR/CircleNodes.h>
 
 #include <gtest/gtest.h>
-
-#include <initializer_list>
 
 namespace luci
 {
 namespace test
 {
-
-using uilist = std::initializer_list<uint32_t>;
-using ilist = std::initializer_list<int32_t>;
-
-/**
- * @brief Graph with Input and Output
- */
-class TestIOGraph
-{
-public:
-  TestIOGraph() = default;
-
-public:
-  void init(const uilist shape_in, const uilist shape_out)
-  {
-    _graph_input = _g.inputs()->create();
-    _graph_output = _g.outputs()->create();
-
-    _input = _g.nodes()->create<luci::CircleInput>();
-    _input->shape(shape_in);
-    _input->shape_status(luci::ShapeStatus::VALID);
-    _input->name("input");
-
-    _output = _g.nodes()->create<luci::CircleOutput>();
-    _output->shape(shape_out);
-    _output->shape_status(luci::ShapeStatus::VALID);
-    _output->name("output");
-
-    _input->index(_graph_input->index());
-    _output->index(_graph_output->index());
-
-    auto input_shape = std::make_unique<loco::TensorShape>();
-    set(input_shape.get(), shape_in);
-    _graph_input->shape(std::move(input_shape));
-
-    auto output_shape = std::make_unique<loco::TensorShape>();
-    set(output_shape.get(), shape_out);
-    _graph_output->shape(std::move(output_shape));
-  }
-
-protected:
-  void set(loco::TensorShape *shape, const uilist &values)
-  {
-    uint32_t r = 0;
-    shape->rank(values.size());
-    for (auto v : values)
-      shape->dim(r++).set(v);
-  }
-
-public:
-  loco::Graph *g(void) { return &_g; }
-  luci::CircleOutput *output(void) { return _output; }
-
-protected:
-  loco::Graph _g;
-  loco::GraphInput *_graph_input = nullptr;
-  loco::GraphOutput *_graph_output = nullptr;
-  luci::CircleInput *_input = nullptr;
-  luci::CircleOutput *_output = nullptr;
-};
 
 /**
  * @brief Graphlet with Add and Const as beta from BatchNorm
@@ -95,7 +35,7 @@ class AddBetaGraphlet
 public:
   AddBetaGraphlet() = default;
 
-  void init(loco::Graph *g, const uilist shape, luci::FusedActFunc actf)
+  void init(loco::Graph *g, const ShapeU32 shape, luci::FusedActFunc actf)
   {
     _add = g->nodes()->create<luci::CircleAdd>();
     _add_beta = g->nodes()->create<luci::CircleConst>();
@@ -135,7 +75,7 @@ class MulGammaGraphlet
 public:
   MulGammaGraphlet() = default;
 
-  void init(loco::Graph *g, const uilist shape, luci::FusedActFunc actf)
+  void init(loco::Graph *g, const ShapeU32 shape, luci::FusedActFunc actf)
   {
     _mul = g->nodes()->create<luci::CircleMul>();
     _mul_gamma = g->nodes()->create<luci::CircleConst>();
@@ -175,18 +115,18 @@ class MulAddGraph : public TestIOGraph, public AddBetaGraphlet, public MulGammaG
 public:
   MulAddGraph() = default;
 
-  void init(const uilist shape_in, const uilist shape_out)
+  void init(const ShapeU32 shape_in, const ShapeU32 shape_out)
   {
     TestIOGraph::init(shape_in, shape_out);
     MulGammaGraphlet::init(g(), shape_in, luci::FusedActFunc::NONE);
     AddBetaGraphlet::init(g(), shape_out, luci::FusedActFunc::RELU);
 
     // connect network
-    _mul->x(_input);
+    _mul->x(input());
     _mul->y(_mul_gamma);
     _add->x(_mul);
     _add->y(_add_beta);
-    _output->from(_add);
+    output()->from(_add);
   }
 };
 
@@ -198,69 +138,79 @@ class AddGraph : public TestIOGraph, public AddBetaGraphlet
 public:
   AddGraph() = default;
 
-  void init(const uilist shape_in, const uilist shape_out)
+  void init(const ShapeU32 shape_in, const ShapeU32 shape_out)
   {
     TestIOGraph::init(shape_in, shape_out);
     AddBetaGraphlet::init(g(), shape_in, luci::FusedActFunc::RELU);
 
     // connect network
-    _add->x(_input);
+    _add->x(input());
     _add->y(_add_beta);
-    _output->from(_add);
+    output()->from(_add);
   }
 };
 
 } // namespace test
 } // namespace luci
 
-TEST(BatchNormPatternFinderTest, is_batchnorm_add)
+class BatchNormPatternFinderMulAddTest : public ::testing::Test
 {
-  luci::test::MulAddGraph mag;
+public:
+  BatchNormPatternFinderMulAddTest() = default;
 
-  mag.init({1, 16, 16, 4}, {1, 16, 16, 4});
+protected:
+  luci::test::MulAddGraph _mag;
+};
+
+class BatchNormPatternFinderAddTest : public ::testing::Test
+{
+public:
+  BatchNormPatternFinderAddTest() = default;
+
+protected:
+  luci::test::AddGraph _ag;
+};
+
+TEST_F(BatchNormPatternFinderMulAddTest, is_batchnorm_add)
+{
+  _mag.init({1, 16, 16, 4}, {1, 16, 16, 4});
 
   luci::CircleMul *mul = nullptr;
   luci::CircleConst *beta = nullptr;
 
-  auto res = luci::is_batchnorm_add(mag.add(), mul, beta);
+  auto res = luci::is_batchnorm_add(_mag.add(), mul, beta);
   ASSERT_TRUE(res);
   ASSERT_NE(nullptr, mul);
   ASSERT_NE(nullptr, beta);
 }
 
-TEST(BatchNormPatternFinderTest, is_batchnorm_add2)
+TEST_F(BatchNormPatternFinderMulAddTest, is_batchnorm_add2)
 {
-  luci::test::MulAddGraph mag;
+  _mag.init({1, 16, 16, 4}, {1, 16, 16, 4});
 
-  mag.init({1, 16, 16, 4}, {1, 16, 16, 4});
-
-  auto res = luci::is_batchnorm_add(mag.add());
+  auto res = luci::is_batchnorm_add(_mag.add());
   ASSERT_TRUE(res);
 }
 
-TEST(BatchNormPatternFinderTest, is_batchnorm_add_NEG)
+TEST_F(BatchNormPatternFinderAddTest, is_batchnorm_add_NEG)
 {
-  luci::test::AddGraph ag;
-
-  ag.init({1, 16, 16, 4}, {1, 16, 16, 4});
+  _ag.init({1, 16, 16, 4}, {1, 16, 16, 4});
 
   luci::CircleMul *mul = nullptr;
   luci::CircleConst *beta = nullptr;
 
-  auto res = luci::is_batchnorm_add(ag.add(), mul, beta);
+  auto res = luci::is_batchnorm_add(_ag.add(), mul, beta);
   ASSERT_FALSE(res);
 }
 
-TEST(BatchNormPatternFinderTest, is_batchnorm_mul)
+TEST_F(BatchNormPatternFinderMulAddTest, is_batchnorm_mul)
 {
-  luci::test::MulAddGraph mag;
-
-  mag.init({1, 16, 16, 4}, {1, 16, 16, 4});
+  _mag.init({1, 16, 16, 4}, {1, 16, 16, 4});
 
   luci::CircleNode *pred = nullptr;
   luci::CircleConst *gamma = nullptr;
 
-  auto res = luci::is_batchnorm_mul(mag.mul(), pred, gamma);
+  auto res = luci::is_batchnorm_mul(_mag.mul(), pred, gamma);
   ASSERT_TRUE(res);
   ASSERT_NE(nullptr, pred);
   ASSERT_NE(nullptr, gamma);
