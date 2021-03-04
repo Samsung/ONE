f5c566bc065926a287e21457e9578d2ac505988d
SaeHie Park <saehie.park@gmail.com>
2021-03-04 17:02:08 +0900

    [luci] Revise FuseActivationFunctionPass test
    This will revise FuseActivationFunctionPass test with TestIOGraph.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/pass/src/FuseActivationFunctionPass.test.cpp
+++ compiler/luci/pass/src/FuseActivationFunctionPass.test.cpp
@@ -14,16 +14,18 @@
  * limitations under the License.
  */
 
-#include "FuseActivationFunctionPassInternal.h"
-
 #include "luci/Pass/FuseActivationFunctionPass.h"
 
 #include <luci/IR/CircleNodes.h>
+
+#include "test/TestIOGraph.h"
 
 #include <gtest/gtest.h>
 
 namespace
 {
+
+using namespace luci::test;
 
 /**
  *  Simple graph for test
@@ -43,31 +45,96 @@ namespace
  *         [Conv2]
  *
  */
-class SimpleGraph
+class ConvReluConvGraphlet
 {
 public:
-  SimpleGraph()
+  ConvReluConvGraphlet() = default;
+
+  void init(loco::Graph *g)
   {
-    conv1 = g.nodes()->create<luci::CircleConv2D>();
-    conv2 = g.nodes()->create<luci::CircleConv2D>();
-    relu = g.nodes()->create<luci::CircleRelu>();
+    _conv1 = g->nodes()->create<luci::CircleConv2D>();
+    _conv2 = g->nodes()->create<luci::CircleConv2D>();
+    _relu = g->nodes()->create<luci::CircleRelu>();
+    _conv1_f = g->nodes()->create<luci::CircleConst>();
+    _conv1_b = g->nodes()->create<luci::CircleConst>();
+    _conv2_f = g->nodes()->create<luci::CircleConst>();
+    _conv2_b = g->nodes()->create<luci::CircleConst>();
 
-    conv1->fusedActivationFunction(luci::FusedActFunc::NONE);
+    _conv1->fusedActivationFunction(luci::FusedActFunc::NONE);
 
-    relu->features(conv1);
-    conv2->input(relu);
-
-    conv1->name("conv1");
-    conv2->name("conv2");
-    relu->name("relu");
+    _conv1->name("conv1");
+    _conv2->name("conv2");
+    _relu->name("relu");
+    _conv1_f->name("conv1f");
+    _conv1_b->name("conv1b");
+    _conv2_f->name("conv2f");
+    _conv2_b->name("conv2b");
   }
 
 public:
-  loco::Graph g;
-  luci::CircleConv2D *conv1;
-  luci::CircleConv2D *conv2;
-  luci::CircleRelu *relu;
+  luci::CircleRelu *relu() { return _relu; }
+  luci::CircleConv2D *conv1() { return _conv1; }
+  luci::CircleConv2D *conv2() { return _conv2; }
+
+protected:
+  luci::CircleConv2D *_conv1 = nullptr;
+  luci::CircleConv2D *_conv2 = nullptr;
+  luci::CircleRelu *_relu = nullptr;
+  luci::CircleConst *_conv1_f = nullptr;
+  luci::CircleConst *_conv1_b = nullptr;
+  luci::CircleConst *_conv2_f = nullptr;
+  luci::CircleConst *_conv2_b = nullptr;
 };
+
+class FuseActTestGraph : public TestIOGraph, public ConvReluConvGraphlet
+{
+public:
+  FuseActTestGraph() = default;
+
+  void init(void)
+  {
+    TestIOGraph::init({1}, {1});
+    ConvReluConvGraphlet::init(g());
+
+    _conv1->input(input());
+    _conv1->filter(_conv1_f);
+    _conv1->bias(_conv1_b);
+
+    _relu->features(_conv1);
+
+    _conv2->input(_relu);
+    _conv2->filter(_conv2_f);
+    _conv2->bias(_conv2_b);
+
+    output()->from(_conv2);
+  }
+};
+
+class ConvHasMultiSuccGraph : public TestIOGraph, public ConvReluConvGraphlet
+{
+public:
+  ConvHasMultiSuccGraph() = default;
+
+  void init(void)
+  {
+    TestIOGraph::init({1}, {1});
+    ConvReluConvGraphlet::init(g());
+
+    _conv1->input(input());
+    _conv1->filter(_conv1_f);
+    _conv1->bias(_conv1_b);
+
+    _relu->features(_conv1);
+
+    _conv2->input(_conv1);
+    _conv2->filter(_conv2_f);
+    _conv2->bias(_conv2_b);
+
+    output()->from(_relu); // We need to check from relu
+  }
+};
+
+// TODO use ::testing::Test
 
 } // namespace
 
@@ -80,34 +147,46 @@ TEST(FuseActivationFunctionPassTest, name)
 
 TEST(FusePreActivationBatchNorm, fuse_activation_function)
 {
-  SimpleGraph g;
+  FuseActTestGraph g;
+  luci::FuseActivationFunctionPass pass;
 
-  EXPECT_TRUE(luci::fuse_activation_function(g.relu));
+  g.init();
 
-  EXPECT_EQ(g.conv1, g.conv2->input());
+  EXPECT_TRUE(pass.run(g.g()));
+  EXPECT_EQ(g.conv1(), g.conv2()->input());
 }
 
 TEST(FusePreActivationBatchNorm, fuse_activation_function_dup_relu)
 {
-  SimpleGraph g;
-  g.conv1->fusedActivationFunction(luci::FusedActFunc::RELU);
+  FuseActTestGraph g;
+  luci::FuseActivationFunctionPass pass;
 
-  EXPECT_TRUE(luci::fuse_activation_function(g.relu));
+  g.init();
+  g.conv1()->fusedActivationFunction(luci::FusedActFunc::RELU);
 
-  EXPECT_EQ(g.conv1, g.conv2->input());
+  EXPECT_TRUE(pass.run(g.g()));
+  EXPECT_EQ(g.conv1(), g.conv2()->input());
 }
 
-TEST(FusePreActivationBatchNorm, fuse_activation_function_NEG)
+TEST(FusePreActivationBatchNorm, fuse_activation_function_mulsucc_NEG)
 {
-  SimpleGraph g;
-  g.conv2->input(g.conv1);
+  ConvHasMultiSuccGraph g;
+  luci::FuseActivationFunctionPass pass;
 
-  // Conv1 has multiple successors
-  EXPECT_FALSE(luci::fuse_activation_function(g.relu));
+  g.init();
 
-  g.conv2->input(g.relu);
-  g.conv1->fusedActivationFunction(luci::FusedActFunc::TANH);
+  // Relu input Conv2D has multiple successors
+  EXPECT_FALSE(pass.run(g.g()));
+}
 
-  // Conv1 already has activation function
-  EXPECT_FALSE(luci::fuse_activation_function(g.relu));
+TEST(FusePreActivationBatchNorm, fuse_activation_function_tanh_NEG)
+{
+  FuseActTestGraph g;
+  luci::FuseActivationFunctionPass pass;
+
+  g.init();
+  g.conv1()->fusedActivationFunction(luci::FusedActFunc::TANH);
+
+  // Relu input Conv2D already has activation function
+  EXPECT_FALSE(pass.run(g.g()));
 }
