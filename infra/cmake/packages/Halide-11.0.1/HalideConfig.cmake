43046c823b7190dc7eb56d6e33d82b6b54e97192
binarman <efimov.alexander@gmail.com>
2021-03-10 00:41:20 +0300

    use hand written cmake for Halide to overcome minimal cmake version requirement

--- infra/cmake/packages/Halide-11.0.1/HalideConfig.cmake
+++ infra/cmake/packages/Halide-11.0.1/HalideConfig.cmake
@@ -1,9 +1,10 @@
 # assume for now that we build fox x86 linux
 set(Halide_11_0_1_URL "https://github.com/halide/Halide/releases/download/v11.0.1/Halide-11.0.1-x86-64-linux-85c1b91c47ce15aab0d9502d955e48615f3bcee0.tar.gz")
 
+# this is actually compiled binary download, not source code
 ExternalSource_Download(Halide DIRNAME Halide-11.0.1 "${Halide_11_0_1_URL}")
 
-find_library(Halide_LIB NAMES libHalide.so PATHS "${Halide_SOURCE_DIR}/lib")
+find_library(Halide_LIB NAMES Halide PATHS "${Halide_SOURCE_DIR}/lib")
 set(Halide_INCLUDE_DIR "${Halide_SOURCE_DIR}/include")
 
 add_library(Halide::Halide SHARED IMPORTED)
