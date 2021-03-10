bfdff9151298ebb1bd2507314ea8efd60108fecb
Seok Namkoong <seok9311@naver.com>
2021-03-10 14:26:57 +0900

    [luci] Remove deprecated codes
    This commit removes deprecated source codes which are not used.
    
    ONE-DCO-1.0-Signed-off-by: Seok NamKoong <seok9311@naver.com>

--- compiler/luci/service/src/CircleTypeInference.cpp
+++ compiler/luci/service/src/CircleTypeInference.cpp
@@ -20,59 +20,8 @@
 #include <luci/Log.h>
 
 #include <loco.h>
-#include <loco/Service/TypeInference.h>
-
-#include <mio/circle/schema_generated.h>
-#include <oops/InternalExn.h>
 
 #include <type_traits>
-
-namespace
-{
-
-circle::TensorType translateLocoTypeToCircle(loco::DataType dtype)
-{
-  switch (dtype)
-  {
-    case loco::DataType::U8:
-      return circle::TensorType_UINT8;
-    //  case loco::DataType::U16: unsupported
-    //  case loco::DataType::U32: unsupported
-    //  case loco::DataType::U64: unsupported
-    case loco::DataType::S8:
-      return circle::TensorType_INT8;
-    case loco::DataType::S16:
-      return circle::TensorType_INT16;
-    case loco::DataType::S32:
-      return circle::TensorType_INT32;
-    case loco::DataType::S64:
-      return circle::TensorType_INT64;
-    case loco::DataType::FLOAT16:
-      return circle::TensorType_FLOAT16;
-    case loco::DataType::FLOAT32:
-      return circle::TensorType_FLOAT32;
-    //  case loco::DataType::FLOAT64: unsupported
-    case loco::DataType::BOOL:
-      return circle::TensorType_BOOL;
-    default:
-      break;
-  }
-
-  INTERNAL_EXN_V("Invalid loco dtype", oops::to_uint32(dtype));
-}
-
-} // namespace
-
-namespace luci
-{
-
-circle::TensorType TypeInference::get(loco::Node *node)
-{
-  assert(luci::dtype_known(node));
-  return translateLocoTypeToCircle(luci::dtype_get(node));
-}
-
-} // namespace luci
 
 namespace
 {
