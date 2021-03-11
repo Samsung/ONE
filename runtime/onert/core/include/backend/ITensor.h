Samsung/ONE

commit 8c921264a29928493018605d85aa40520224c22e

[onert] Refactor BackendContext (#5926)
* [onert] Refactor BackendContext

Pass a partial graph when creating a BackendContext. "partial graph"
here means a subset of an original graph that contains
operations/operands used by a backend. Before this commit, each backend
had a reference to the whole graph which could be abused and error-prone.

- Remove `initializeBackendContext` - as a partial graph is passed for
  each graph, we do not need to build used operation/operand lists
  anymore
- Introduce `ContextData` which contains all the data for the backend
  - Make parameter of `Backend::newContext` simple - pass a single
    struct `ContextData` instead
  - Make `BackendContext` methods(`genTensors` and `genKernels`) not
    have any parameters, just use `ContextData`.

More Context : #5006

ONE-DCO-1.0-Signed-off-by: Hanjoung Lee <hanjoung.lee@samsung.com>

* Update

ONE-DCO-1.0-Signed-off-by: Hanjoung Lee <hanjoung.lee@samsung.com>

runtime/onert/core/include/backend/cpu_common/BackendContextHelpers.h
@@ -34,11 +34,10 @@ namespace cpu_common
 {
 
 // TODO Remove the template param BackendContext once unification of cpu backend context is done
-template <typename T_BackendContext>
-void planTensors(const T_BackendContext &ctx, const std::vector<onert::ir::OperationIndex> &order,
-                 const compiler::GraphLowerInfo &lower_info)
+template <typename T_BackendContext> void planTensors(const T_BackendContext &ctx)
 {
-  auto graph = ctx.graph();
+  const ir::Graph &graph = *ctx.graph();
+  const auto &order = ctx.data().op_order;
   auto tensor_builder = ctx.tensor_builder;
 
   ir::OperandIndexMap<uint32_t> uses_map;
@@ -46,24 +45,14 @@ void planTensors(const T_BackendContext &ctx, const std::vector<onert::ir::Opera
   ir::OperandIndexSequence constants;
 
   auto model_io =
-    (graph->getInputs() + graph->getOutputs()) | ir::Remove::UNDEFINED | ir::Remove::DUPLICATED;
+    (graph.getInputs() + graph.getOutputs()) | ir::Remove::UNDEFINED | ir::Remove::DUPLICATED;
 
   // Prepare scanning
-  for (auto ind : ctx.operand_list())
-  {
-    if (model_io.contains(ind))
-      continue;
-    const auto &obj = graph->operands().at(ind);
-    const auto &li = lower_info.operand.getRawPtr(ind);
-    if (li->def_factors().getOnlyElement().backend() != ctx.backend())
-      continue;
-
-    // Ignore unused tensor
-    if (li->def_factors().size() == 0 && li->use_factors().size() == 0)
-    {
-      VERBOSE_F() << "Operand " << ind << " will not be used. no more process." << std::endl;
+  graph.operands().iterate([&](const ir::OperandIndex &ind, const ir::Operand &obj) {
+    if (ctx.external_operands().contains(ind))
       return;
-    }
+
+    // TODO Check if we need to handle unused tensors
 
     uses_map[ind] = obj.getUses().size();
     def_map[ind] = obj.getDef().valid() ? 1 : 0;
@@ -71,16 +60,18 @@ void planTensors(const T_BackendContext &ctx, const std::vector<onert::ir::Opera
     if (obj.isConstant())
       constants.append(ind);
 
-    auto factor = li->def_factors().getOnlyElement();
     if (!tensor_builder->isRegistered(ind))
     {
       // These tensors do not exist in any  (No use and def)
       const auto info = obj.info();
-      const auto backend_layout = factor.layout();
-      // TODO Change tensor info to have permuted shape
-      tensor_builder->registerTensorInfo(ind, info, backend_layout);
+      // NOTE Currently we only support NHWC tensors for cpu-common tensors.
+      //      There is no way to get the layout info from the backend context for now.
+      //      When we support NCHW tensors as well, we also need to change tensor info to be
+      //      permuted shape.
+      assert(ctx.operand_layouts().at(ind) == ir::Layout::NHWC);
+      tensor_builder->registerTensorInfo(ind, info, ir::Layout::NHWC);
     }
-  }
+  });
 
   // Start scanning to do notify{First|Last}Use for each tensor
 
@@ -93,13 +84,28 @@ void planTensors(const T_BackendContext &ctx, const std::vector<onert::ir::Opera
     tensor_builder->notifyFirstUse(ind);
   }
 
+  for (auto &pair : def_map)
+  {
+    if (pair.second == 0)
+      tensor_builder->notifyFirstUse(pair.first);
+  }
+
+  // This is a workaround to keep the operands over the execution
+  // (the operands look like they are unused)
+  std::vector<ir::OperandIndex> operands_last_until_end;
+  for (auto &pair : uses_map)
+  {
+    if (pair.second == 0)
+      operands_last_until_end.push_back(pair.first);
+  }
+
   // At each operation,
   // 1. Scan DEF of outputs. If the DEF, allocate it
   // 2. Scan DEF of inputs. If variable tensor, allocate it
   // 3. Scan USE of inputs. Decrease the USE and deallocate if the USE is 0
   for (const auto op_ind : order)
   {
-    const auto &op = graph->operations().at(op_ind);
+    const auto &op = graph.operations().at(op_ind);
     // TODO Remove indentation
     {
       auto op_inputs = op.getInputs() | ir::Remove::DUPLICATED | ir::Remove::UNDEFINED;
@@ -108,7 +114,7 @@ void planTensors(const T_BackendContext &ctx, const std::vector<onert::ir::Opera
       // Define outputs
       for (const auto &ind : op_outputs)
       {
-        if (model_io.contains(ind))
+        if (ctx.external_operands().contains(ind))
           continue;
         if (!tensor_builder->isRegistered(ind))
           continue;
@@ -125,18 +131,16 @@ void planTensors(const T_BackendContext &ctx, const std::vector<onert::ir::Opera
       // non-constant because of less memory usage by memory planning in here
       for (const auto &ind : op_inputs)
       {
-        if (model_io.contains(ind))
+        if (ctx.external_operands().contains(ind))
           continue;
         if (!tensor_builder->isRegistered(ind))
           continue;
-        const auto &operand = graph->operands().at(ind);
+        const auto &operand = graph.operands().at(ind);
         if (operand.info().isVariable())
         {
           // The variable tensor with buffer is not supported yet
           assert(operand.data() == nullptr);
           assert(operand.getUses().size() == 1 && !operand.getDef().valid());
-          assert(lower_info.operand.at(ind).def_factors().size() == 1 &&
-                 lower_info.operand.at(ind).use_factors().size() == 1);
           assert(uses_map[ind] == 1 && def_map[ind] == 0);
           tensor_builder->notifyFirstUse(ind);
         }
@@ -144,7 +148,7 @@ void planTensors(const T_BackendContext &ctx, const std::vector<onert::ir::Opera
 
       for (const auto &ind : op_inputs)
       {
-        if (model_io.contains(ind))
+        if (ctx.external_operands().contains(ind))
           continue;
         if (!tensor_builder->isRegistered(ind))
           continue;
@@ -166,6 +170,11 @@ void planTensors(const T_BackendContext &ctx, const std::vector<onert::ir::Opera
     }
   }
 
+  for (auto ind : operands_last_until_end)
+  {
+    tensor_builder->notifyLastUse(ind);
+  }
+
   // Dispose and validate
   for (const auto &ind : constants)
   {
@@ -185,56 +194,36 @@ void planTensors(const T_BackendContext &ctx, const std::vector<onert::ir::Opera
                 [](std::pair<const ir::OperandIndex, uint32_t> it) { return it.second == 0; }));
 }
 
-template <typename T_BackendContext>
-ITensorRegistry *genTensors(T_BackendContext &ctx,
-                            const std::vector<onert::ir::OperationIndex> &order,
-                            const compiler::GraphLowerInfo &lower_info)
+template <typename T_BackendContext> ITensorRegistry *genTensors(T_BackendContext &ctx)
 {
-  auto graph = ctx.graph();
+  const ir::Graph &graph = *ctx.graph();
   auto tensor_builder = ctx.tensor_builder;
 
   auto model_io =
-    (graph->getInputs() + graph->getOutputs()) | ir::Remove::UNDEFINED | ir::Remove::DUPLICATED;
-  for (auto index : ctx.operand_list())
-  {
-    if (model_io.contains(index))
-      continue;
-    const auto &obj = graph->operands().at(index);
-    const auto frontend_layout = [&]() {
-      if (obj.getUses().size() == 0)
-        return ir::Layout::UNKNOWN;
-      auto use_op_ind = *obj.getUses().begin(); // FIXME What if it has two or more uses?
-      for (auto &operation_info : ctx.operation_list())
-      {
-        if (operation_info.index == use_op_ind)
-          return operation_info.layout;
-      }
-      return ir::Layout::UNKNOWN;
-    }();
-    const auto &permute_factor =
-      lower_info.operand.getRawPtr(index)->def_factors().getOnlyElement();
-    if (permute_factor.backend() != ctx.backend())
-      continue;
-    const auto backend_layout = permute_factor.layout();
-    ir::OperandInfo backend_info{permuteShape(obj.shape(), frontend_layout, backend_layout),
-                                 obj.typeInfo(), obj.info().memAllocType(), obj.isConstant()};
-    tensor_builder->registerTensorInfo(index, backend_info, backend_layout);
-  }
+    (graph.getInputs() + graph.getOutputs()) | ir::Remove::UNDEFINED | ir::Remove::DUPLICATED;
+  graph.operands().iterate([&](const ir::OperandIndex &ind, const ir::Operand &obj) {
+    if (ctx.external_operands().contains(ind))
+      return;
+    // NOTE Assuming there is no layout changes (Always assume NHWC or UNKNOWN)
+    assert(graph.layout() != ir::Layout::NCHW);
+    ir::OperandInfo backend_info{obj.shape(), obj.typeInfo(), obj.info().memAllocType(),
+                                 obj.isConstant()};
+    tensor_builder->registerTensorInfo(ind, backend_info, ir::Layout::NHWC);
+  });
 
   // TODO Get compiler options from compiler, and use it rather than getting it from Env
   if (util::getConfigString(util::config::EXECUTOR) == "Linear")
   {
-    cpu_common::planTensors(ctx, order, lower_info);
+    cpu_common::planTensors(ctx);
   }
   else
   {
     // For the executors that does not have fixed linear execution order:
     // To make tensors never be deallocated, this is a workaround to use static memory planner
-    for (auto ind : ctx.operand_list())
-    {
+    graph.operands().iterate([&](const ir::OperandIndex &ind, const ir::Operand &) {
       if (tensor_builder->isRegistered(ind))
         tensor_builder->notifyFirstUse(ind);
-    }
+    });
   }
 
   tensor_builder->allocate();
@@ -244,11 +233,9 @@ ITensorRegistry *genTensors(T_BackendContext &ctx,
 
 inline void initConsts(BackendContext &ctx)
 {
-  for (auto ind : ctx.operand_list())
-  {
-    const auto &operand = ctx.graph()->operands().at(ind);
-    if (!operand.isConstant())
-      continue;
+  ctx.graph()->operands().iterate([&](const ir::OperandIndex &ind, const ir::Operand &operand) {
+    if (ctx.external_operands().contains(ind) || !operand.isConstant())
+      return;
 
     auto tensor = ctx.tensor_registry->getNativeITensor(ind);
     assert(tensor != nullptr);
@@ -260,7 +247,7 @@ inline void initConsts(BackendContext &ctx)
     ExternalTensor *ext_tensor = dynamic_cast<ExternalTensor *>(tensor);
     assert(ext_tensor);
     ext_tensor->setData(data);
-  }
+  });
 }
 
 } // namespace cpu_common

runtime/onert/core/src/compiler/ExecutorFactory.cc
@@ -18,6 +18,7 @@
 
 #include <deque>
 #include <functional>
+#include "ir/OperationCloner.h"
 #include "exec/ExecutionObservers.h"
 #include "exec/LinearExecutor.h"
 #include "exec/DataflowExecutor.h"
@@ -96,31 +97,115 @@ void initializeSubgraphIOTensors(compiler::LoweredGraph &lowered_graph,
   }
 }
 
-backend::BackendContexts createBackendContexts(const ir::Graph &graph,
-                                               const compiler::CompilerOptions &options)
+backend::BackendContexts createBackendContexts(const compiler::LoweredGraph &lgraph,
+                                               bool linear_executor)
 {
   backend::BackendContexts contexts;
-  bool linear_executor = (options.executor == "Linear");
   auto &backend_manager = compiler::BackendManager::get();
 
-  // Always create Builtin backend context
-  auto builtin_backend = backend_manager.getBuiltin();
-  contexts.emplace(builtin_backend,
-                   builtin_backend->newContext(graph, graph.getKernelBuilder(), linear_executor));
+  std::unordered_map<const backend::Backend *, backend::ContextData> context_data_map;
 
-  // Create contexts for other backends
-  for (auto backend_str : options.backend_list)
+  // Generate partial graphs for each backend
+  for (auto backend : backend_manager.getAll())
   {
-    auto backend = backend_manager.get(backend_str);
-    if (!backend)
-    {
-      VERBOSE_F() << "Failed to create a context - backend '" << backend_str << "' was not loaded."
-                  << std::endl;
-      continue;
-    }
+    auto &data = context_data_map[backend];
+    auto graph = std::make_unique<ir::Graph>();
+    graph->setLayout(lgraph.graph().layout());
+    data.graph = std::move(graph);
+  }
 
-    contexts.emplace(backend,
-                     backend->newContext(graph, graph.getKernelBuilder(), linear_executor));
+  auto &whole_graph = lgraph.graph();
+  // Separate operands into partial graphs
+  whole_graph.operands().iterate(
+    [&](const ir::OperandIndex &operand_ind, const ir::Operand &operand) {
+      auto &operand_li = lgraph.lower_info().operand;
+      const auto &def_factors = operand_li.at(operand_ind).def_factors();
+      if (def_factors.size() == 0) // Ignore unused tensor
+        return;
+      const auto &def_factor = def_factors.getOnlyElement();
+      const auto backend = def_factor.backend();
+      auto &partial_graph = *context_data_map[backend].graph;
+      auto &operand_layouts = context_data_map[backend].operand_layouts;
+      assert(operand_layouts.find(operand_ind) == operand_layouts.end());
+      operand_layouts[operand_ind] = def_factor.layout();
+
+      // Copy the operand and insert it to the partial graph
+      auto new_operand = std::make_unique<ir::Operand>(operand);
+      // TODO Introduce a method for resetting use/def values of Operand
+      // NOTE Use/Def info is going to be filled in `Graph::finishBuilding`
+      const_cast<ir::OperationIndexSet &>(new_operand->getUses()).clear();
+      new_operand->unsetDef();
+      auto new_operand_ind = partial_graph.addOperand(operand_ind, std::move(new_operand));
+      UNUSED_RELEASE(new_operand_ind);
+      assert(new_operand_ind == operand_ind);
+    });
+  // Separate operations into partial graphs
+  whole_graph.operations().iterate(
+    [&](const ir::OperationIndex &op_ind, const ir::Operation &operation) {
+      auto &op_li = lgraph.lower_info().operation;
+      auto backend = op_li.at(op_ind).backend();
+      auto &partial_graph = *context_data_map[backend].graph;
+      auto &external_operands = context_data_map[backend].external_operands;
+      auto &operand_layouts = context_data_map[backend].operand_layouts;
+
+      {
+        // Add missing operands (externals)
+        auto io_list = (operation.getInputs() + operation.getOutputs()) | ir::Remove::DUPLICATED |
+                       ir::Remove::UNDEFINED;
+        for (auto operand_ind : io_list)
+        {
+          if (partial_graph.operands().exist(operand_ind))
+            continue;
+
+          // Copy the operand and insert it to the partial graph
+          const auto &operand = whole_graph.operands().at(operand_ind);
+          auto new_operand = std::make_unique<ir::Operand>(operand);
+          // TODO Introduce a method for resetting use/def values of Operand
+          // NOTE Use/Def info is going to be filled in `Graph::finishBuilding`
+          const_cast<ir::OperationIndexSet &>(new_operand->getUses()).clear();
+          new_operand->unsetDef();
+          auto new_operand_ind = partial_graph.addOperand(operand_ind, std::move(new_operand));
+          UNUSED_RELEASE(new_operand_ind);
+          assert(new_operand_ind == operand_ind);
+
+          auto layout =
+            lgraph.lower_info().operand.at(operand_ind).def_factors().getOnlyElement().layout();
+          assert(operand_layouts.find(operand_ind) == operand_layouts.end());
+          operand_layouts[operand_ind] = layout;
+          external_operands.add(operand_ind);
+        }
+
+        auto new_op_ind = partial_graph.addOperation(op_ind, clone(operation));
+        UNUSED_RELEASE(new_op_ind);
+        assert(new_op_ind == op_ind);
+      }
+    });
+
+  // Create contexts
+  auto whole_op_order = lgraph.graph().topolSortOperations();
+  for (auto &pair : context_data_map)
+  {
+    auto backend = pair.first;
+    auto &data = pair.second;
+    data.graph->finishBuilding();
+    // Handle graph input/outputs or external tensors
+    data.graph->operands().iterate([&](const ir::OperandIndex &ind, const ir::Operand &operand) {
+      if (whole_graph.getInputs().contains(ind) || whole_graph.getOutputs().contains(ind))
+        data.external_operands.add(ind);
+      // Inputs are either "graph input" or "no def op and non-constant"
+      if (whole_graph.getInputs().contains(ind) ||
+          (!operand.getDef().valid() && !operand.isConstant()))
+        // Outputs are either "graph output" or "no uses"
+        data.graph->addInput(ind);
+      if (whole_graph.getOutputs().contains(ind) || operand.getUses().size() == 0)
+        data.graph->addOutput(ind);
+    });
+
+    std::copy_if(whole_op_order.begin(), whole_op_order.end(), std::back_inserter(data.op_order),
+                 [&](const auto &ind) { return data.graph->operations().exist(ind); });
+    data.is_linear_executor = linear_executor;
+    data.custom_kernel_builder = lgraph.graph().getKernelBuilder();
+    contexts.emplace(backend, backend->newContext(std::move(data)));
   }
   return contexts;
 }
@@ -153,41 +238,6 @@ exec::IExecutor *ExecutorFactory::create(std::unique_ptr<compiler::LoweredGraph>
                                          const std::shared_ptr<exec::ExecutorMap> &executor_map)
 {
   return _map.at(options.executor)(std::move(lowered_graph), options, executor_map);
-}
-
-void ExecutorFactory::initializeBackendContext(compiler::LoweredGraph *lowered_graph,
-                                               const backend::BackendContexts &backend_contexts)
-{
-  struct Entry
-  {
-    std::vector<backend::BackendContext::OperationInfo> operation_list;
-    std::vector<ir::OperandIndex> operand_list;
-  };
-  std::unordered_map<const backend::Backend *, Entry> backend_assets;
-
-  lowered_graph->graph().operations().iterate(
-    [&](const ir::OperationIndex &op_ind, const ir::Operation &) {
-      auto &op_li = lowered_graph->lower_info().operation;
-      auto backend = op_li.getRawPtr(op_ind)->backend();
-      backend_assets[backend].operation_list.emplace_back(op_ind, lowered_graph->graph().layout());
-    });
-
-  // Build lists for operands
-  lowered_graph->graph().operands().iterate([&](const ir::OperandIndex &ind, const ir::Operand &) {
-    const auto lower_info = lowered_graph->lower_info().operand.getRawPtr(ind);
-    for (auto factor : lower_info->def_factors())
-    {
-      auto backend = factor.backend();
-      backend_assets[backend].operand_list.emplace_back(ind);
-    }
-  });
-
-  for (auto &pair : backend_assets)
-  {
-    auto backend = pair.first;
-    auto &arg = pair.second;
-    backend_contexts.at(backend)->initialize(arg.operation_list, arg.operand_list);
-  }
 }
 
 void ExecutorFactory::prepareMigrantTensors(compiler::LoweredGraph &lowered_graph,
@@ -223,8 +273,7 @@ ExecutorFactory::createLinearExecutor(std::unique_ptr<compiler::LoweredGraph> lo
                                       const std::shared_ptr<exec::ExecutorMap> &executor_map)
 {
   backend::BackendContexts backend_contexts =
-    createBackendContexts(lowered_graph->graph(), options);
-  initializeBackendContext(lowered_graph.get(), backend_contexts);
+    createBackendContexts(*lowered_graph, options.executor == "Linear");
 
   TensorRegistries tensor_regs{backend_contexts, true};
 
@@ -241,7 +290,7 @@ ExecutorFactory::createLinearExecutor(std::unique_ptr<compiler::LoweredGraph> lo
 
   for (auto &pair : backend_contexts)
   {
-    pair.second->genTensors(order, lowered_graph->lower_info());
+    pair.second->genTensors();
   }
 
   prepareMigrantTensors(*lowered_graph, backend_contexts);
@@ -277,7 +326,7 @@ ExecutorFactory::createLinearExecutor(std::unique_ptr<compiler::LoweredGraph> lo
   // Generate kernels
   for (auto &pair : ordered_contexts)
   {
-    auto codes = pair.second->genKernels(order);
+    auto codes = pair.second->genKernels();
     for (auto &pair : codes)
     {
       auto &op_ind = pair.first;
@@ -311,9 +360,7 @@ exec::IExecutor *ExecutorFactory::createDataflowExecutor(
   const std::shared_ptr<exec::ExecutorMap> &executor_map, bool parallel)
 {
   backend::BackendContexts backend_contexts =
-    createBackendContexts(lowered_graph->graph(), options);
-
-  initializeBackendContext(lowered_graph.get(), backend_contexts);
+    createBackendContexts(*lowered_graph, options.executor == "Linear");
 
   TensorRegistries tensor_regs{backend_contexts, true};
 
@@ -324,13 +371,9 @@ exec::IExecutor *ExecutorFactory::createDataflowExecutor(
     (lowered_graph->graph().getInputs() + lowered_graph->graph().getOutputs()) |
       ir::Remove::DUPLICATED | ir::Remove::UNDEFINED);
 
-  // linearize
-  // This order is just for giving topological order info to the backens
-  // TODO When we pass a partial graph to a backend, we can remove this
-  auto order = Linear::linearize(*lowered_graph);
   for (auto &pair : backend_contexts)
   {
-    pair.second->genTensors(order, lowered_graph->lower_info());
+    pair.second->genTensors();
   }
 
   prepareMigrantTensors(*lowered_graph, backend_contexts);
@@ -366,7 +409,7 @@ exec::IExecutor *ExecutorFactory::createDataflowExecutor(
   // Generate kernels
   for (auto &pair : ordered_contexts)
   {
-    auto codes = pair.second->genKernels(order);
+    auto codes = pair.second->genKernels();
     for (auto &pair : codes)
     {
       auto &op_ind = pair.first;

commit 04eac093de680ca14db257f2f4eaed570f4c4552

[onert] Fix deallocation bug
Fix deallocation bug that was introduced by #5926 .

ONE-DCO-1.0-Signed-off-by: Hanjoung Lee <hanjoung.lee@samsung.com>

runtime/onert/core/include/backend/ITensor.h
@@ -88,6 +88,12 @@ public:
     throw std::runtime_error("This backend does not support dynamic tensor");
   }
 
+  /// @brief Reset the buffer address
+  virtual void resetBuffer()
+  {
+    throw std::runtime_error("This backend does not support resetting buffer");
+  }
+
   /**
    * @brief Set the shape of tenser to new_shape
    * @note  Higer dimension will be placed on front.

runtime/onert/core/include/backend/cpu_common/BackendContextHelpers.h
@@ -157,12 +157,6 @@ template <typename T_BackendContext> void planTensors(const T_BackendContext &ct
       {
         // plan for deallocation of static tensornode
         tensor_builder->notifyLastUse(ind);
-
-        // plan for deallocation of dynamic tensor
-        auto dyn_tensor_manager = tensor_builder->dynamicTensorManager();
-        auto *tensor = ctx.tensor_registry->getITensor(ind);
-        assert(tensor);
-        dyn_tensor_manager->planDealloc(op_ind, tensor);
       }
     }
   }

runtime/onert/core/include/backend/cpu_common/Tensor.h
@@ -65,14 +65,9 @@ public:
   }
 
   /**
-   * @brief Mark this tensor does not have memory.
-   *        Real memory deallocation should be done by caller.
+   * @brief Reset the buffer and deallocate the allocation if it is managed by itself
    */
-  void resetBuffer()
-  {
-    _allocator.reset();
-    _buffer = nullptr;
-  }
+  void resetBuffer() override;
 
 public:
   uint8_t *buffer() const override { return _buffer; }

runtime/onert/core/src/backend/cpu_common/Tensor.cc
@@ -18,6 +18,7 @@
 
 #include "ir/DataType.h"
 #include "backend/cpu_common/MemoryManager.h"
+#include "util/logging.h"
 
 namespace onert
 {
@@ -83,6 +84,20 @@ bool Tensor::applyShape(const ir::Shape &new_shape)
 }
 
 ir::Shape Tensor::getShape() const { return _info.shape(); }
+
+void Tensor::resetBuffer()
+{
+  if (_allocator)
+  {
+    VERBOSE() << "DEALLOCATE!!!!" << std::endl;
+    _buffer = nullptr;
+    _allocator.reset();
+    if (_dynamic_mem_mgr)
+    {
+      _dynamic_mem_mgr->deallocate(this);
+    }
+  }
+}
 
 } // namespace cpu_common
 } // namespace backend

runtime/onert/core/src/compiler/ExecutorFactory.cc
@@ -67,6 +67,32 @@ private:
   std::shared_ptr<backend::IConfig> _config;
 };
 
+// XXX Remove OperandIndex in the entry (it was just for debugging)
+using DeallocList = std::vector<std::pair<backend::ITensor *, ir::OperandIndex>>;
+// Deallocation after execution of an operation used by Linear Executor
+class LinearDeallocFunction final : public exec::IFunction
+{
+public:
+  LinearDeallocFunction(const DeallocList &tensors) : _dealloc_list{tensors} {}
+
+  void run() override
+  {
+    for (auto pair : _dealloc_list)
+    {
+      auto tensor = pair.first;
+      VERBOSE(LinearDealloc) << "ISDYNAMIC " << tensor->is_dynamic() << std::endl;
+      if (!tensor->is_dynamic())
+        continue;
+
+      VERBOSE(LinearDealloc) << "Deallocating tensor " << pair.second << std::endl;
+      tensor->resetBuffer();
+    }
+  }
+
+private:
+  DeallocList _dealloc_list;
+};
+
 void initializeSubgraphIOTensors(compiler::LoweredGraph &lowered_graph,
                                  const backend::BackendContexts &backend_contexts,
                                  const ir::OperandIndexSequence &indices)
@@ -267,6 +293,8 @@ ExecutorFactory::createLinearExecutor(std::unique_ptr<compiler::LoweredGraph> lo
                                       const compiler::CompilerOptions &options,
                                       const std::shared_ptr<exec::ExecutorMap> &executor_map)
 {
+  auto graph = lowered_graph->graph();
+
   backend::BackendContexts backend_contexts =
     createBackendContexts(*lowered_graph, options.executor == "Linear");
 
@@ -316,10 +344,66 @@ ExecutorFactory::createLinearExecutor(std::unique_ptr<compiler::LoweredGraph> lo
       ordered_contexts.emplace_front(pair.first, pair.second.get());
   }
 
+  // Simulate the execution for deallocation of tensors
+  std::unordered_map<ir::OperationIndex, DeallocList> dealloc_list_map;
+  {
+    ir::OperandIndexMap<uint32_t> uses_map;
+    ir::OperandIndexSequence constants;
+
+    auto model_io =
+      (graph.getInputs() + graph.getOutputs()) | ir::Remove::UNDEFINED | ir::Remove::DUPLICATED;
+
+    // Prepare scanning
+    graph.operands().iterate([&](const ir::OperandIndex &ind, const ir::Operand &obj) {
+      uses_map[ind] = obj.getUses().size();
+
+      if (obj.isConstant())
+        constants.append(ind);
+    });
+
+    // A trick to consider constants as an execption
+    for (const auto &ind : constants)
+    {
+      uses_map[ind]++;
+    }
+
+    for (const auto op_ind : order)
+    {
+      const auto &op = graph.operations().at(op_ind);
+      auto op_inputs = op.getInputs() | ir::Remove::DUPLICATED | ir::Remove::UNDEFINED;
+      auto op_outputs = op.getOutputs() | ir::Remove::DUPLICATED | ir::Remove::UNDEFINED;
+
+      for (const auto &ind : op_inputs)
+      {
+        const auto &operand = graph.operands().at(ind);
+        assert(uses_map.find(ind) != uses_map.end());
+        assert(uses_map[ind] > 0);
+        uses_map[ind]--;
+        if (uses_map[ind] == 0 && !operand.info().isVariable() && !model_io.contains(ind))
+        {
+          VERBOSE(DEALLOC_LIST) << op_ind << " : " << ind << "   "
+                                << (void *)tensor_regs.getITensor(ind) << std::endl;
+          dealloc_list_map[op_ind].emplace_back(tensor_regs.getITensor(ind), ind);
+        }
+      }
+    }
+
+    // Dispose and validate
+    for (const auto &ind : constants)
+    {
+      --uses_map[ind];
+    }
+
+    assert(
+      std::all_of(uses_map.begin(), uses_map.end(),
+                  [](std::pair<const ir::OperandIndex, uint32_t> it) { return it.second == 0; }));
+  }
+
   // Generate kernels
   for (auto &pair : ordered_contexts)
   {
     auto codes = pair.second->genKernels();
+
     for (auto &pair : codes)
     {
       auto &op_ind = pair.first;
@@ -328,6 +412,8 @@ ExecutorFactory::createLinearExecutor(std::unique_ptr<compiler::LoweredGraph> lo
       auto lower_info = lowered_graph->lower_info().operation.getRawPtr(op_ind);
       if (options.he_profiling_mode)
         fn_seq->wrap<SyncFunction>(lower_info->backend()->config());
+      if (!dealloc_list_map[op_ind].empty())
+        fn_seq->append(std::make_unique<LinearDeallocFunction>(dealloc_list_map[op_ind]));
       builder.append(op_ind, {op_ind, &op, lower_info, std::move(fn_seq)});
     }
   }

runtime/onert/core/src/exec/DynamicShapeInferer.cc
@@ -48,11 +48,11 @@ void DynamicShapeInferer::handleBinaryArithmeticOp(const ir::Operation &op,
 
     So, only when all inputs are static, we can skip dynamic shape inference.
   */
-  if ((!lhs->is_dynamic()) && (!rhs->is_dynamic()))
-    return;
-
   auto output_idx = op.getOutputs().at(0);
   auto output = _tensor_registry->getITensor(output_idx);
+
+  if ((!lhs->is_dynamic()) && (!rhs->is_dynamic()) && !output->is_dynamic())
+    return;
 
   ir::Shape new_shape = shape_inference::inferEltwiseShape(lhs_shape, rhs_shape);
 

runtime/onert/core/src/exec/FunctionSequence.cc
@@ -53,9 +53,6 @@ void FunctionSequence::run()
       // run kernel
       function->run();
     }
-
-    // deallocate input tensors which are no longer used
-    _dynamic_tensor_ctx->dynamic_tensor_manager->deallocInput(op_ind);
   }
   else
   {

runtime/onert/core/src/exec/LinearExecutor.cc
@@ -37,6 +37,7 @@ void LinearExecutor::executeImpl()
     ruy::profiler::ScopeLabel label(code.op->name());
 #endif
     _subject.notifyJobBegin(this, profiling_subg_index, code.op_ind, backend);
+    VERBOSE(LinearExecutor) << "Executing " << code.op_ind << std::endl;
 
     auto &fn_seq = code.fn_seq;
 