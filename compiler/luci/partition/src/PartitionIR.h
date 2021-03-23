5c4d2f72a3837c4f3453746ac0ea4a87479a50e6
SaeHie Park <saehie.park@gmail.com>
2021-03-23 12:18:25 +0900

    [luci] Introduce partition IRs (#6313)
    * [luci] Introduce partition IRs
    
    This will introduce luci partition IRs for node and graph.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>
    
    * Update compiler/luci/partition/src/PartitionIR.h
    
    Co-authored-by: seongwoo chae <mhs4670go@naver.com>
    
    * Update compiler/luci/partition/src/PartitionIR.h
    
    Co-authored-by: Seok Namkoong <seok9311@naver.com>
    
    Co-authored-by: seongwoo chae <mhs4670go@naver.com>
    Co-authored-by: Seok Namkoong <seok9311@naver.com>

---
+++ compiler/luci/partition/src/PartitionIR.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __LUCI_PARTITION_IR_H__
+#define __LUCI_PARTITION_IR_H__
+
+#include <luci/IR/CircleNodes.h>
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace luci
+{
+
+struct PGraph;
+
+/**
+ * @brief Partition Node with CircleNode with group name
+ * @note  node just points to source luci::CircleNode, NOT the cloned node
+ *        CloneContext is used to find cloned node from source node
+ */
+struct PNode
+{
+  const luci::CircleNode *node = nullptr;
+  std::string group;
+
+  const PGraph *pgraph = nullptr;
+};
+
+/**
+ * @brief Partition Graph with Partition Nodes of same group and graph I/Os
+ */
+struct PGraph
+{
+  std::vector<std::unique_ptr<PNode>> pnodes;
+  std::string group;
+  uint32_t id = 0;
+
+  // I/O while partitioning
+  std::vector<luci::CircleNode *> inputs;
+  std::vector<luci::CircleNode *> outputs;
+};
+
+struct PGraphs
+{
+  std::vector<std::unique_ptr<PGraph>> pgraphs;
+
+  // node2group is to find group key from source node
+  std::map<const luci::CircleNode *, std::string> node2group;
+
+  // id2pgraph is to find *pgraph from pgraph id
+  std::map<uint32_t, PGraph *> id2pgraph;
+};
+
+} // namespace luci
+
+#endif // __LUCI_PARTITON_IR_H__
