94516c13bda26181818eae048609ed2a4cbbed35
dayo <dayg502@gmail.com>
2021-04-08 14:27:29 +0900

    [luci] Support quantization of floor (#6468)
    * [luci] Support quantization of floor
    
    This commit supports quantization of floor operation
    and the unit test.
    
    ONE-DCO-1.0-Signed-off-by: Dayoung Lee <dayg502@gmail.com>
    
    * format
    
    * Follow an alphabetical order

--- compiler/luci/pass/src/QuantizeWithMinMaxPass.cpp
+++ compiler/luci/pass/src/QuantizeWithMinMaxPass.cpp
@@ -929,6 +929,7 @@ void quantize_const_inputs(luci::CircleNode *node, loco::DataType output_type)
     case luci::CircleOpcode::ADD_N:
     case luci::CircleOpcode::DIV:
     case luci::CircleOpcode::EQUAL:
+    case luci::CircleOpcode::FLOOR:
     case luci::CircleOpcode::GREATER:
     case luci::CircleOpcode::GREATER_EQUAL:
     case luci::CircleOpcode::LESS:
