cc9af412905465eca7d1b40fd8e391be8f3e6f27
binarman <efimov.alexander@gmail.com>
2021-04-09 13:41:25 +0300

    [souschef] Unify gaussian generators
    This commit unifies float, int32_t and uint8_t gaussian generators.
    
    ONE-DCO-1.0-Signed-off-by: Alexander Efimov <a.efimov@samsung.com>

--- compiler/souschef/src/Gaussian.cpp
+++ compiler/souschef/src/Gaussian.cpp
@@ -26,76 +26,48 @@
 namespace souschef
 {
 
-std::vector<uint8_t> GaussianFloat32DataChef::generate(int32_t count) const
+template <typename T>
+static std::vector<uint8_t> generate_gaussian(int32_t count, float mean, float stddev)
 {
   // TODO Support seed value override
   auto seed = std::chrono::system_clock::now().time_since_epoch().count();
 
   std::minstd_rand rand{static_cast<std::minstd_rand::result_type>(seed)};
-  std::normal_distribution<float> dist{_mean, _stddev};
+  std::normal_distribution<float> dist{mean, stddev};
 
   std::vector<uint8_t> res;
 
+  constexpr float max_cap = std::numeric_limits<T>::max();
+  constexpr float min_cap = std::numeric_limits<T>::min();
   for (uint32_t n = 0; n < count; ++n)
   {
-    auto const value = dist(rand);
+    float raw_value = dist(rand);
+    const float capped_value = std::max(min_cap, std::min(max_cap, raw_value));
+    auto const value = static_cast<T>(capped_value);
     auto const arr = reinterpret_cast<const uint8_t *>(&value);
 
-    for (uint32_t b = 0; b < sizeof(float); ++b)
+    for (uint32_t b = 0; b < sizeof(T); ++b)
     {
       res.emplace_back(arr[b]);
     }
   }
 
   return res;
+}
+
+std::vector<uint8_t> GaussianFloat32DataChef::generate(int32_t count) const
+{
+  return generate_gaussian<float>(count, _mean, _stddev);
 }
 
 std::vector<uint8_t> GaussianInt32DataChef::generate(int32_t count) const
 {
-  // TODO Support seed value override
-  auto seed = std::chrono::system_clock::now().time_since_epoch().count();
-
-  std::minstd_rand rand{static_cast<std::minstd_rand::result_type>(seed)};
-  std::normal_distribution<float> dist{_mean, _stddev};
-
-  std::vector<uint8_t> res;
-
-  for (uint32_t n = 0; n < count; ++n)
-  {
-    auto const value = static_cast<int32_t>(dist(rand));
-    auto const arr = reinterpret_cast<const uint8_t *>(&value);
-
-    for (uint32_t b = 0; b < sizeof(int32_t); ++b)
-    {
-      res.emplace_back(arr[b]);
-    }
-  }
-
-  return res;
+  return generate_gaussian<int32_t>(count, _mean, _stddev);
 }
 
 std::vector<uint8_t> GaussianUint8DataChef::generate(int32_t count) const
 {
-  // TODO Support seed value override
-  auto seed = std::chrono::system_clock::now().time_since_epoch().count();
-
-  std::minstd_rand rand{static_cast<std::minstd_rand::result_type>(seed)};
-  std::normal_distribution<float> dist{_mean, _stddev};
-
-  std::vector<uint8_t> res;
-
-  for (uint32_t n = 0; n < count; ++n)
-  {
-    auto const value = static_cast<uint8_t>(dist(rand));        // uint8_t for data type
-    auto const arr = reinterpret_cast<const uint8_t *>(&value); // uint8_t for byte streaming
-
-    for (uint32_t b = 0; b < sizeof(uint8_t); ++b)
-    {
-      res.emplace_back(arr[b]);
-    }
-  }
-
-  return res;
+  return generate_gaussian<uint8_t>(count, _mean, _stddev);
 }
 
 std::unique_ptr<DataChef> GaussianFloat32DataChefFactory::create(const Arguments &args) const
