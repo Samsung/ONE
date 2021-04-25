8078724901d792bf99a362b7ff21e69cbd9bb18b
Sung-Jae Lee <sj925.lee@samsung.com>
2021-04-26 07:42:26 +0900

    Update TCM script (#6656)
    - Update tool package to `tca-standalone-1.0.0.jar`
    
    Signed-off-by: Sung-Jae Lee <sj925.lee@samsung.com>

--- infra/scripts/build-tcm.sh
+++ infra/scripts/build-tcm.sh
@@ -2,7 +2,7 @@
 #
 # STEP 1
 #   Download latest TCM tool from 
-#   https://github.sec.samsung.net/RS-TCM/tca-standalone/releases/download/v0.0.8/tca-standalone-0.0.8.jar
+#   https://github.sec.samsung.net/RS-TCM/tca-standalone/releases/download/v1.0.0/tca-standalone-1.0.0.jar
 #
 # STEP 2
 #   Create symbolic link `./src` for source directory to be analyzed which has `.ahub` configuration.
@@ -16,7 +16,7 @@
 
 echo ${PROJECT_DIR:=${PWD}}
 
-java -jar $PROJECT_DIR/tca-standalone-0.0.8.jar \
+java -jar $PROJECT_DIR/tca-standalone-1.0.0.jar \
   --outdir=$PROJECT_DIR/tcm-output \
   --config=$PROJECT_DIR/src/.ahub/tcchecker-tca/config.yaml \
   --local=$PROJECT_DIR/src \
