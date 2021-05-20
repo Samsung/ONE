e9d1f32e534df52f6faaec635e5c6ed6ab02ffee
Sung-Jae Lee <sj925.lee@samsung.com>
2021-05-20 17:36:22 +0900

    Update TCM build script. (#6865)
    - Update package information for v1.0.2
    - Add optional parameter [test_target] to support new TCM config for
      AHUB support.
    
    Signed-off-by: Sung-Jae Lee <sj925.lee@samsung.com>

--- infra/scripts/build-tcm.sh
+++ infra/scripts/build-tcm.sh
@@ -2,13 +2,16 @@
 #
 # STEP 1
 #   Download latest TCM tool from 
-#   https://github.sec.samsung.net/RS-TCM/tca-standalone/releases/download/v1.0.0/tca-standalone-1.0.0.jar
+#   https://github.sec.samsung.net/RS-TCM/tca-standalone/releases/download/1.0.2/tca-standalone-1.0.2.jar
 #
 # STEP 2
 #   Create symbolic link `./src` for source directory to be analyzed which has `.ahub` configuration.
 #
 # STEP 3
-#   run this `build-tcm.sh` script.
+#   run this script in `build-tcm.sh [test_target]` format.
+#   ex) $ build_tcm.sh                # to analyze both NN Runtime and NN Compiler
+#   ex) $ build_tcm.sh NN_Runtime     # to analyze NN Runtime only
+#   ex) $ build_tcm.sh NN_Compiler    # to analyze NN Compiler only
 #
 # See the following link for additional details.
 #   https://github.sec.samsung.net/RS-TCM/tca-standalone/wiki/Tutorials-CPP-Gtest
@@ -16,9 +19,10 @@
 
 echo ${PROJECT_DIR:=${PWD}}
 
-java -jar $PROJECT_DIR/tca-standalone-1.0.0.jar \
+java -jar $PROJECT_DIR/tca-standalone-1.0.2.jar \
   --outdir=$PROJECT_DIR/tcm-output \
   --config=$PROJECT_DIR/src/.ahub/tcchecker-tca/config.yaml \
   --local=$PROJECT_DIR/src \
   --logfile=$PROJECT_DIR/tcm-output/tcm.log \
   --debug
+  $@
