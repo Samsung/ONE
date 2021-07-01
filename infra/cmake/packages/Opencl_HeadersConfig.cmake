e93bcb128a564a7b7224d7756e2bca9ba7bf94ad
YiHyunJin <33678155+YiHyunJin@users.noreply.github.com>
2021-07-01 19:08:28 +0900

    [onert] Fix gbs build for nnfw_api_gtest (#7119)
    TEST_GPU_CL_BACKEND was not enable because Opencl_Headers was not found on gbs build.
    So, Fixed by adding nnas_find_package(Opencl_Headers QUIET).
    
    Signed-off-by: hj0412-yi <hj0412.yi@samsung.com>

--- infra/cmake/packages/Opencl_HeadersConfig.cmake
+++ infra/cmake/packages/Opencl_HeadersConfig.cmake
@@ -1,16 +1,21 @@
 function(_Opencl_Headers_import)
   nnas_find_package(Opencl_HeadersSource QUIET)
 
+  # NOTE This line prevents multiple definitions of target
+  if(TARGET Headers)
+    set(Opencl_HeadersSource_DIR ${Opencl_HeadersSource_DIR} PARENT_SCOPE)
+    set(Opencl_Headers_FOUND TRUE PARENT_SCOPE)
+    return()
+  endif(TARGET Headers)
+
   if(NOT Opencl_HeadersSource_FOUND)
+    message(STATUS "Opencl_Headers: Source not found")
     set(Opencl_Headers_FOUND FALSE PARENT_SCOPE)
     return()
   endif(NOT Opencl_HeadersSource_FOUND)
 
-  if(NOT TARGET opencl_headers)
-    nnas_include(ExternalProjectTools)
-    add_extdirectory("${Opencl_HeadersSource_DIR}" OPENCL_HEADERS)
-  endif(NOT TARGET opencl_headers)
-
+  add_extdirectory("${Opencl_HeadersSource_DIR}" OPENCL_HEADERS EXCLUDE_FROM_ALL)
+  set(Opencl_Headers_DIR ${Opencl_HeadersSource_DIR} PARENT_SCOPE)
   set(Opencl_Headers_FOUND TRUE PARENT_SCOPE)
 endfunction(_Opencl_Headers_import)
 
