1d245ee369aba09cf732ade145d8a8bf6c38bfbc
Chunseok Lee <chunseok.lee@samsung.com>
2021-07-12 18:18:15 +0900

    Disable gpu_cl backend on x86_64
    DO NOT use gpu_cl on x86_64 arch. not tested yet.
    
    ONE-DCO-1.0-Signed-off-by: Chunseok Lee <chunseok.lee@samsung.com>

--- infra/nnfw/cmake/options/options_x86_64-linux.cmake
+++ infra/nnfw/cmake/options/options_x86_64-linux.cmake
@@ -4,3 +4,5 @@
 option(BUILD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
 option(DOWNLOAD_ARMCOMPUTE "Download ARM Compute source" OFF)
 option(BUILD_XNNPACK "Build XNNPACK" OFF)
+option(DOWNLOAD_OPENCL_HEADERS "Download opencl headers" OFF)
+option(BUILD_GPU_CL "Build gpu_cl backend" OFF)
