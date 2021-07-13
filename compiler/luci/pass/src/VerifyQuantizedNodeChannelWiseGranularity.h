bd2f0797357c0bbabeb2f1fcef94510a030cccbe
Hyukjin Jeong <hj1.jeong@samsung.com>
2021-07-14 06:23:52 +0900

    [circle-quantizer] Verify optional bias in FC layer. (#7214)
    * [circle-quantizer] Verify optional bias in FC layer.
    
    This verifies optional bias in FC layer.
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>
    
    * Add comment

--- compiler/luci/pass/src/VerifyQuantizedNodeChannelWiseGranularity.h
+++ compiler/luci/pass/src/VerifyQuantizedNodeChannelWiseGranularity.h
@@ -179,7 +179,10 @@ private:
     RETURN_FALSE_UNLESS(is_lwq(node))
     RETURN_FALSE_UNLESS(is_lwq(node->input()))
     RETURN_FALSE_UNLESS(is_cwq_const(node->weights(), 0))
-    RETURN_FALSE_UNLESS(is_cwq_const(node->bias(), rank(node->bias()) - 1))
+    luci::CircleConst *bias = dynamic_cast<luci::CircleConst *>(node->bias());
+    // Bias is optional (it can be CircleOutputExclude)
+    if (bias != nullptr)
+      RETURN_FALSE_UNLESS(is_cwq_const(node->bias(), rank(node->bias()) - 1))
     return true;
   }
 
