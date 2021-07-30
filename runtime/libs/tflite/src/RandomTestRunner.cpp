2a4b0a3316e8f85bdc844e3bb1e09762fbb21d49
Hyeongseok Oh <hseok82.oh@samsung.com>
2021-07-30 17:10:36 +0900

    [lib/tflite] Remove NNAPI delegator extension (#7352)
    This commit remove NNAPI delegator extension library.
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- runtime/libs/tflite/src/RandomTestRunner.cpp
+++ runtime/libs/tflite/src/RandomTestRunner.cpp
@@ -20,7 +20,6 @@
 #include "tflite/RandomTestRunner.h"
 #include "tflite/Diff.h"
 #include "tflite/TensorLogger.h"
-#include "tflite/ext/nnapi_delegate.h"
 
 #include <misc/tensor/IndexIterator.h>
 #include <misc/tensor/Object.h>
@@ -66,8 +65,6 @@ int RandomTestRunner::run(size_t running_count)
   std::cout << "[NNAPI TEST] Run T/F Lite Interpreter without NNAPI" << std::endl;
   _tfl_interp->Invoke();
 
-  nnfw::tflite::NNAPIDelegate d;
-
   for (size_t i = 1; i <= running_count; ++i)
   {
     resetter.run(*(_nnapi.get()));
@@ -77,21 +74,9 @@ int RandomTestRunner::run(size_t running_count)
 
     std::cout << "[NNAPI TEST #" << i << "] Run T/F Lite Interpreter with NNAPI" << std::endl;
 
-    char *env = getenv("UPSTREAM_DELEGATE");
-
-    if (env && !std::string(env).compare("1"))
+    if (_nnapi->Invoke() != kTfLiteOk)
     {
-      _nnapi->Invoke();
-    }
-    else
-    {
-      // WARNING
-      // primary_subgraph: Experimental interface. Return 1st sugbraph
-      // Invoke() will call BuildGraph() internally
-      if (d.Invoke(&_nnapi.get()->primary_subgraph()))
-      {
-        throw std::runtime_error{"Failed to BuildGraph"};
-      }
+      throw std::runtime_error{"Failed to Run T/F Lite Interpreter with NNAPI"};
     }
 
     // Compare OFM
