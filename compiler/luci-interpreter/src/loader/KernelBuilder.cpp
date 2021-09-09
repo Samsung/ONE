62a2d9aefca84a256f25e46deb973faf5034f479
Maksim Bronnikov <max120199@gmail.com>
2021-09-09 10:54:09 +0300

    [DRAFT][luci-interpreter] Add SplitV support
    This draft adds support of SplitV to luci-interpreter.
    
    ONE-DCO-1.0-Signed-off-by: Maksim Bronnikov max120199@gmail.com

--- compiler/luci-interpreter/src/loader/KernelBuilder.cpp
+++ compiler/luci-interpreter/src/loader/KernelBuilder.cpp
@@ -74,6 +74,7 @@
 #include "kernels/SpaceToBatchND.h"
 #include "kernels/SpaceToDepth.h"
 #include "kernels/Split.h"
+#include "kernels/SplitV.h"
 #include "kernels/StridedSlice.h"
 #include "kernels/Sqrt.h"
 #include "kernels/Square.h"
@@ -1045,6 +1046,25 @@ std::unique_ptr<Kernel> build_kernel_CircleSplit(const luci::CircleNode *circle_
 
   // NOTE 'num_splits' attribute is ignored.
   return std::make_unique<kernels::Split>(axis, input, std::move(outputs));
+}
+
+std::unique_ptr<Kernel> build_kernel_CircleSplitV(const luci::CircleNode *circle_node,
+                                                  KernelBuilderHelper &helper)
+{
+  const auto *node = dynamic_cast<const luci::CircleSplitV *>(circle_node);
+  if (node == nullptr)
+    throw std::runtime_error("wrong builder for operation");
+  auto output_nodes = collectOutputNodes<luci::CircleSplitVOut>(node);
+  assert(node->arity() == 3);
+  assert(output_nodes.size() == static_cast<size_t>(node->num_split()));
+
+  const Tensor *input = helper.getInputTensor(node->input());
+  const Tensor *sizes_data = helper.getInputTensor(node->size_splits());
+  const Tensor *axis = helper.getInputTensor(node->split_dim());
+  std::vector<Tensor *> outputs = helper.getOutputTensors(output_nodes);
+
+  // NOTE 'num_splits' attribute is ignored.
+  return std::make_unique<kernels::SplitV>(input, sizes_data, axis, std::move(outputs));
 }
 
 std::unique_ptr<Kernel> build_kernel_CircleSqrt(const luci::CircleNode *circle_node,
