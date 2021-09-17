5117841d6673c353ac33972f403e429ae767c6a4
Alexander Efimov <a.efimov@samsung.com>
2021-09-17 19:57:34 +0300

    [luci-micro] Add basic cmake infrastructure
    This PR adds basic CMakeLists.txt to build luci-interpreter for arm microctrollers.
    
    ONE-DCO-1.0-Signed-off-by: Alexander Efimov <a.efimov@samsung.com>

--- compiler/luci-interpreter/pal/mcu/pal.cmake
+++ compiler/luci-interpreter/pal/mcu/pal.cmake
@@ -23,27 +23,29 @@ macro(initialize_pal)
         message(STATUS "Skipping luci-interpreter: Ruy not found")
         return()
     endif ()
-    #find_package(Threads REQUIRED)
 
     set(PAL_INITIALIZED TRUE)
 endmacro()
 
 macro(add_pal_to_target TGT)
     target_include_directories(${TGT} PRIVATE "${PAL}")
-    target_include_directories(${TGT} SYSTEM PRIVATE
-            "${TensorFlowRuySource_DIR}"
-            "${TensorFlowGEMMLowpSource_DIR}"
-            "${TensorFlowEigenSource_DIR}"
-            "${TensorFlowSource_DIR}")
+    target_include_directories(${TGT} PRIVATE
+      "${TensorFlowRuySource_DIR}"
+      "${TensorFlowGEMMLowpSource_DIR}"
+      "${TensorFlowEigenSource_DIR}"
+      "${TensorFlowSource_DIR}"
+    )
     target_include_directories(${TGT} PRIVATE ${LUCI_INTERPRETER_PAL_DIR})
 
-    # TODO put it back, I changed my mind.
-    # instead add sources with visitors in this library
     set(PAL_SOURCES ${TensorFlowSource_DIR}/tensorflow/lite/kernels/internal/quantization_util.cc)
     add_library(luci_interpreter_mcu_pal STATIC ${PAL_SOURCES})
     set_target_properties(luci_interpreter_mcu_pal PROPERTIES POSITION_INDEPENDENT_CODE ON)
-    target_include_directories(luci_interpreter_mcu_pal SYSTEM PRIVATE "${TensorFlowSource_DIR}")
+    target_include_directories(luci_interpreter_mcu_pal PRIVATE
+      "${TensorFlowRuySource_DIR}"
+      "${TensorFlowGEMMLowpSource_DIR}"
+      "${TensorFlowEigenSource_DIR}"
+      "${TensorFlowSource_DIR}"
+    )
 
     target_link_libraries(${TGT} PRIVATE luci_interpreter_mcu_pal)
-    #target_link_libraries(${TGT} PRIVATE Threads::Threads luci_interpreter_mcu_pal)
 endmacro()
---
+++ compiler/luci-micro/README.md
@@ -0,0 +1,17 @@
+# luci-micro
+
+`luci-micro` is MCU specialized build of luci-interpreter with several benchmark applications.
+
+## Contents
+
+Luci-micro contains cmake infrastructure to build:
+- stand-alone interpreter library
+- benchmark applications using luci interpreter on arm MCUs
+
+## How to build
+
+TBD
+
+## How to run
+
+TBD
