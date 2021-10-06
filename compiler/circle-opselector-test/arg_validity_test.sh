360a3a9108790237d2de4107b512199555dbd9cc
parkeunsang <dislive@naver.com>
2021-10-06 16:03:43 +0900

    remove circle-opselector-test folder for split pr
    ONE-DCO-1.0-Signed-off-by: parkeunsang <dislive@naver.com>

--- compiler/circle-opselector-test/arg_validity_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script test the validity of options
-# ex) --by_id "1,2" is valid but --by_id "a,b" is not valid because id parameters must be not negative integer. 
-#
-# HOW TO USE(Not defined definitely yet)
-#
-# ./arg_validity_test.sh
-exit 0
