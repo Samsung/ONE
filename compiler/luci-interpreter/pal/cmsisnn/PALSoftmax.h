44ece14e5971b13d1c7e463814fad10b6e9603fe
Vyacheslav Bazhenov <v.bazhenov@partner.samsung.com>
2021-10-08 14:00:10 +0300

    [DRAFT][WIP][luci-micro] CMSIS-NN build
    This commit introduces cmsis-nn optimized kernels build
    
    ONE-DCO-1.0-Signed-off-by: Vyacheslav Bazhenov <slavikmipt@gmail.com>

---
+++ compiler/luci-interpreter/pal/cmsisnn/PALSoftmax.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2021 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LUCI_INTERPRETER_PAL_SOFTMAX_H
+#define LUCI_INTERPRETER_PAL_SOFTMAX_H
+
+#include <arm_nnfunctions.h>
+#include <tensorflow/lite/kernels/internal/reference/softmax.h>
+
+namespace luci_interpreter_pal {
+    static inline void PopulateSoftmaxLookupTable(tflite::SoftmaxParams *data, float input_scale,
+                                                  float beta) {
+        // Do nothing for mcu
+        (void) data;
+        (void) input_scale;
+        (void) beta;
+    }
+
+    static inline void InitializeParams(tflite::SoftmaxParams *params, float input_scale, float beta) {
+        int32 input_beta_multiplier;
+        int input_beta_left_shift;
+        static const int kScaledDiffIntegerBits = 5;
+        tflite::PreprocessSoftmaxScaling(beta, input_scale, kScaledDiffIntegerBits,
+                                         &input_beta_multiplier, &input_beta_left_shift);
+
+        params->input_multiplier = input_beta_multiplier;
+        params->input_left_shift = input_beta_left_shift;
+        params->diff_min =
+                -tflite::CalculateInputRadius(kScaledDiffIntegerBits, params->input_left_shift);
+    }
+
+    template<typename T>
+    inline void Softmax(const tflite::SoftmaxParams &params,
+                        const tflite::RuntimeShape &input_shape, const T *input_data,
+                        const tflite::RuntimeShape &output_shape, T *output_data) {
+        // MARK: At this moment this operation doesn't support on mcu
+        assert(false && "Softmax NYI");
+        (void) params;
+        (void) input_shape;
+        (void) input_data;
+        (void) output_shape;
+        (void) output_data;
+    }
+
+    template<>
+    inline void Softmax<int8_t>(const tflite::SoftmaxParams &params,
+                                const tflite::RuntimeShape &input_shape, const int8_t *input_data,
+                                const tflite::RuntimeShape &output_shape, int8_t *output_data) {
+        const int32_t num_rows = 0;
+        const int32_t row_size = 0;
+        const int32_t mult = params.input_multiplier;
+        const int32_t shift = params.input_left_shift;
+        const int32_t diff_min = params.diff_min;
+
+        arm_softmax_s8(input_data, num_rows, row_size, mult, shift, diff_min, output_data);
+    }
+} // namespace luci_interpreter_pal
+
+#endif // LUCI_INTERPRETER_PAL_SOFTMAX_H
---
+++ compiler/luci-interpreter/pal/cmsisnn/pal.cmake
@@ -0,0 +1,68 @@
+macro(initialize_pal)
+    nnas_find_package(TensorFlowSource EXACT 2.6.0 QUIET)
+    nnas_find_package(TensorFlowGEMMLowpSource EXACT 2.6.0 QUIET)
+    nnas_find_package(TensorFlowEigenSource EXACT 2.6.0 QUIET)
+    nnas_find_package(TensorFlowRuySource EXACT 2.6.0 QUIET)
+    nnas_include(ExternalSourceTools)
+    nnas_include(OptionTools)
+
+    envoption(CMSIS_5_8_0_URL https://github.com/ARM-software/CMSIS_5/archive/refs/tags/5.8.0.tar.gz)
+
+    ExternalSource_Download(CMSIS DIRNAME CMSIS-5.8.0 ${CMSIS_5_8_0_URL})
+
+    set(CMSISSource_DIR ${CMSIS_SOURCE_DIR})
+    set(CMSISSource_FOUND TRUE)
+
+    if (NOT TensorFlowSource_FOUND)
+        message(STATUS "Skipping luci-interpreter: TensorFlow not found")
+        return()
+    endif ()
+
+    if (NOT TensorFlowGEMMLowpSource_FOUND)
+        message(STATUS "Skipping luci-interpreter: gemmlowp not found")
+        return()
+    endif ()
+
+    if (NOT TensorFlowEigenSource_FOUND)
+        message(STATUS "Skipping luci-interpreter: Eigen not found")
+        return()
+    endif ()
+
+    if (NOT TensorFlowRuySource_FOUND)
+        message(STATUS "Skipping luci-interpreter: Ruy not found")
+        return()
+    endif ()
+
+    if (NOT CMSISSource_FOUND)
+        message(STATUS "Skipping luci-interpreter: CMSIS not found")
+        return()
+    endif ()
+    #find_package(Threads REQUIRED)
+
+    set(PAL_INITIALIZED TRUE)
+endmacro()
+
+macro(add_pal_to_target TGT)
+    target_include_directories(${TGT} PRIVATE "${PAL}")
+    target_include_directories(${TGT} PRIVATE
+            "${TensorFlowRuySource_DIR}"
+            "${TensorFlowGEMMLowpSource_DIR}"
+            "${TensorFlowEigenSource_DIR}"
+            "${TensorFlowSource_DIR}")
+    target_include_directories(${TGT} PRIVATE ${LUCI_INTERPRETER_PAL_DIR})
+
+    # TODO put it back, I changed my mind.
+    # instead add sources with visitors in this library
+    set(PAL_SOURCES ${TensorFlowSource_DIR}/tensorflow/lite/kernels/internal/quantization_util.cc)
+    add_library(luci_interpreter_mcu_pal STATIC ${PAL_SOURCES})
+    set_target_properties(luci_interpreter_mcu_pal PROPERTIES POSITION_INDEPENDENT_CODE ON)
+    target_include_directories(luci_interpreter_mcu_pal PRIVATE
+            "${TensorFlowRuySource_DIR}"
+            "${TensorFlowGEMMLowpSource_DIR}"
+            "${TensorFlowEigenSource_DIR}"
+            "${TensorFlowSource_DIR}"
+    )
+
+    target_link_libraries(${TGT} PRIVATE luci_interpreter_mcu_pal)
+    #target_link_libraries(${TGT} PRIVATE Threads::Threads luci_interpreter_mcu_pal)
+endmacro()
