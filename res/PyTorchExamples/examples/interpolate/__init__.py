02c756f119d934eaa1fa0b74c81b39a43ab7f2d3
Alexander Efimov <efimov.alexander@gmail.com>
2021-11-16 11:48:16 +0300

    [res] interpolate PyTorch example (#7997)
    This PR adds interpolate PyTorch example with bilinear mode.
    
    ONE-DCO-1.0-Signed-off-by: Alexander Efimov <a.efimov@samsung.com>

---
+++ res/PyTorchExamples/examples/interpolate/__init__.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+
+# model
+#
+# Notes:
+# - This operation requires minimum 11 onnx opset version
+# - tf_onnx 1.9 fails to convert this model using opcode version 13+, because unsqueeze operation is not supported yet
+class net_interpolate(nn.Module):
+    def __init__(self, scale_factor):
+        super().__init__()
+        self.scale_factor = scale_factor
+
+    def forward(self, input):
+        return torch.nn.functional.interpolate(
+            input,
+            scale_factor=self.scale_factor,
+            mode='bilinear',
+            align_corners=True,
+            recompute_scale_factor=True)
+
+    def onnx_opset_version(self):
+        return 11
+
+
+_model_ = net_interpolate([2, 2])
+
+# dummy input for onnx generation
+_dummy_ = torch.randn(1, 2, 3, 3)
