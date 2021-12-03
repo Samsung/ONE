2d4698dafbe8fb2beb8aefe30bf753f24833aa6e
Alexander Efimov <a.efimov@samsung.com>
2021-12-03 21:02:07 +0300

    [res] Add more PyTorch RNN examples
    This PR adds more RNN examples in res/PyTorchExamples.
    
    ONE-DCO-1.0-Signed-off-by: Alexander Efimov <a.efimov@samsung.com>

---
+++ res/PyTorchExamples/examples/RNN-noinit/__init__.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+_input_size = 4
+_seq_len = 2
+_batch = 3
+_hidden_size = 3
+
+# model
+class net_RNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.op = nn.RNN(_input_size, _hidden_size, 1)
+
+    def forward(self, inputs):
+        return self.op(inputs[0])
+
+
+_model_ = net_RNN()
+
+# dummy input for onnx generation
+_dummy_ = [torch.randn(_seq_len, _batch, _input_size)]
