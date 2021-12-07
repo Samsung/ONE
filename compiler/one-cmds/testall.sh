15bf4279a1aaf3c6e520ce44fee722d14d8049f3
Alexander Efimov <a.efimov@samsung.com>
2021-12-08 02:48:31 +0300

    add LSTM tests to testall.sh

--- compiler/one-cmds/testall.sh
+++ compiler/one-cmds/testall.sh
@@ -31,4 +31,28 @@ echo RNN-bi
 ./legalize_onnx.py RNN-bi.onnx mod_RNN-bi.onnx
 ./compare.py RNN-bi.onnx mod_RNN-bi.onnx
 
+echo
+echo
+echo LSTM
+./legalize_onnx.py LSTM.onnx mod_LSTM.onnx
+./compare.py LSTM.onnx mod_LSTM.onnx
+
+echo
+echo
+echo LSTM-noinit
+./legalize_onnx.py LSTM-noinit.onnx mod_LSTM-noinit.onnx
+./compare.py LSTM-noinit.onnx mod_LSTM-noinit.onnx
+
+echo
+echo
+echo LSTM-nobias
+./legalize_onnx.py LSTM-nobias.onnx mod_LSTM-nobias.onnx
+./compare.py LSTM-nobias.onnx mod_LSTM-nobias.onnx
+
+echo
+echo
+echo LSTM-bi
+./legalize_onnx.py LSTM-bi.onnx mod_LSTM-bi.onnx
+./compare.py LSTM-bi.onnx mod_LSTM-bi.onnx
+
 echo SUCCESS
