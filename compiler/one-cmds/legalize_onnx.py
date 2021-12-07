00fbc0083276acafa73346ea53a629b073cda80e
Alexander Efimov <a.efimov@samsung.com>
2021-12-07 22:55:03 +0300

    add more comments and support forward/reverse LSTM

--- compiler/one-cmds/legalize_onnx.py
+++ compiler/one-cmds/legalize_onnx.py
@@ -50,6 +50,16 @@ class ModelTransformerHelper:
       return base_name + '0'
 
   def make_node(self, opcode, inputs, outputs, *p_args, **k_args):
+    """
+    Create node and it in graph.
+   
+    Parameters:
+        opcode (str): opcode name of desired operation
+        inputs (list of str): names of input tensors
+        outputs (list of str or int): names of output tensors or number of tensors that should be created
+        p_args: additional arguments for onnx make_node helper
+        k_args: attributes for onnx node
+    """
     if type(outputs) == int:
       outputs = [self.make_tensor_with_base_name('') for i in range(outputs)]
     assert(type(outputs) == list)
@@ -105,6 +115,19 @@ def dtype_to_np(dtype):
     raise NotImplementedError('unsupported data type')
 
 def generate_one_direction_RNN(transformer, X, W, R, B, initial_h, clip, activation):
+  """
+  This function generates subgraph that represents one direction of unrolled RNN layer
+
+  Parameters:
+      transformer (ModelTransformerHelper): helper for model generation
+      X (list of str): names of input tensors in sequence. Tensor shapes: [batch_size, input_size].
+      W (list of str): name of weight tensor
+      R (list of str): name of recurrence weight tensor
+      B (list of str): name of bias tensor
+      initial_h (str or None): name of tensor containing initial hidden state. Shape [batch_size, hidden_size]
+      clip (float or None): range which clips input of activations
+      act (str): activation function
+  """
   # one direction RNN:
   #
   # H = f(X*(W^T) + h*(R^T) + B)
@@ -277,6 +300,184 @@ def legalize_RNN(transformer, tensor_infos, node):
   transformer.mark_for_deletion(node)
 
 
+def generate_one_direction_LSTM(transformer, X, W, R, B, initial_h, initial_c, P, clip, act, dtype, hidden_size, batch_size):
+  """
+  This function generates subgraph that represents one direction of unrolled LSTM layer
+
+  Parameters:
+      transformer (ModelTransformerHelper): helper for model generation
+      tensor_infos (dict of Info): shapes and dtypes of tensors
+      X (list of str): names of input tensors in sequence. Tensor shapes: [batch_size, input_size]
+      W (list of str): name of concatenated weight tensor: [input, output, forget, cell]
+      R (list of str): name of concatenated recurrence weights tensor: [input, output, forget, cell]
+      B (list of str): name of concatenated bias tensor: [input, output, forget, cell]
+      initial_h (str or None): name of tensor containing initial hidden state. Shape [batch_size, hidden_size]
+      initial_c (str or None): name of tensor containing initial cell state. Shape [batch_size, hidden_size]
+      P (list of str): name of concatenated peephole tensor: [input, output, forget]
+      clip (float or None): range which clips input of activations
+      act (dict of str):  activation functions {'f': 'Sigmoid', 'g': 'Tanh', 'h': 'Tanh'}
+      hidden_size (int): hidden dimension
+      batch_size (int): batch dimension
+  """
+  # one direction LSTM:
+  # 
+  # From onnx Operators.onnx
+  #
+  # it = f(Xt*(Wi^T) + Ht-1*(Ri^T) + Pi (.) Ct-1 + Wbi + Rbi)
+  # ft = f(Xt*(Wf^T) + Ht-1*(Rf^T) + Pf (.) Ct-1 + Wbf + Rbf)
+  # ct = g(Xt*(Wc^T) + Ht-1*(Rc^T) + Wbc + Rbc)
+  # Ct = ft (.) Ct-1 + it (.) ct
+  # ot = f(Xt*(Wo^T) + Ht-1*(Ro^T) + Po (.) Ct + Wbo + Rbo)
+  # Ht = ot (.) h(Ct)
+  #
+  # X - input tensor
+  # i - input gate
+  # o - output gate
+  # f - forget gate
+  # c - cell gate
+  # t - time step (t-1 means previous time step)
+  # W[iofc] - W parameter weight matrix for input, output, forget, and cell gates
+  # R[iofc] - R recurrence weight matrix for input, output, forget, and cell gates
+  # Wb[iofc] - W bias vectors for input, output, forget, and cell gates
+  # Rb[iofc] - R bias vectors for input, output, forget, and cell gates
+  # P[iof] - P peephole weight vector for input, output, and forget gates
+  # WB[iofc] - W parameter weight matrix for backward input, output, forget, and cell gates
+  # RB[iofc] - R recurrence weight matrix for backward input, output, forget, and cell gates
+  # WBb[iofc] - W bias vectors for backward input, output, forget, and cell gates
+  # RBb[iofc] - R bias vectors for backward input, output, forget, and cell gates
+  # PB[iof] - P peephole weight vector for backward input, output, and forget gates
+  # H - Hidden state
+
+  seq_length = len(X)
+  state_h_tensors = []
+
+  w_tensors = transformer.make_node('Split', W, 4, axis=0, split=[hidden_size]*4)
+  W = {'i': w_tensors[0], 'o': w_tensors[1], 'f': w_tensors[2], 'c': w_tensors[3]}
+
+  r_tensors = transformer.make_node('Split', R, 4, axis=0, split=[hidden_size]*4)
+  R = {'i': r_tensors[0], 'o': r_tensors[1], 'f': r_tensors[2], 'c': r_tensors[3]}
+
+  if B is not None:
+    separate_b_tensors = transformer.make_node('Split', B, 8, axis=0, split=[hidden_size]*8)
+    b_tensors = []
+    for i in range(4):
+      b_tensors += transformer.make_node('Add', [separate_b_tensors[i*2], separate_b_tensors[i*2+1]], 1)
+  else:
+    b_tensors = transformer.make_constant_tensor(np.zeros((hidden_size), dtype=dtype), 'zero_b')
+  B = {'i': b_tensors[0], 'o': b_tensors[1], 'f': b_tensors[2], 'c': b_tensors[3]}
+
+  if initial_h is not None:
+    previous_h_state_tensor = initial_h
+  else:
+    previous_h_state_tensor = transformer.make_constant_tensor(np.zeros((batch_size, hidden_size), dtype=dtype), 'initial_h')[0]
+
+  if initial_c is not None:
+    previous_c_state_tensor = initial_c
+  else:
+    previous_c_state_tensor = transformer.make_constant_tensor(np.zeros((batch_size, hidden_size), dtype=dtype), 'initial_c')[0]
+
+  if P is not None:
+    p_tensors = transformer.make_node('Split', P, 3, axis = 0, split=[hidden_size] * 3)
+    P = {'i': p_tensors[0], 'o': p_tensors[1], 'f': p_tensors[2]}
+  else:
+    zero = transformer.make_constant_tensor(np.zeros((hidden_size), dtype=dtype), 'zero_peephole')[0]
+    P = {'i': zero, 'o': zero, 'f': zero}
+
+  for i in range(seq_length):
+    # it = f(Xt*(Wi^T) + Ht-1*(Ri^T) + Pi (.) Ct-1 + Wbi + Rbi)
+    it = transformer.make_node('Gemm', [X[i], W['i'], B['i']], 1, transB=1)
+    it = transformer.make_node('Gemm', [previous_h_state_tensor, R['i'], it[0]], 1, transB=1)
+    peephole_it = transformer.make_node('Mul', [P['i'], previous_c_state_tensor], 1)
+    it = transformer.make_node('Add', it + peephole_it, 1)
+    if clip is not None:
+      it = transformer.make_node('Clip', it, 1, min=-clip, max=clip)
+    it = transformer.make_node(act['f'], it, 1)
+
+    # ft = f(Xt*(Wf^T) + Ht-1*(Rf^T) + Pf (.) Ct-1 + Wbf + Rbf)
+    ft = transformer.make_node('Gemm', [X[i], W['f'], B['f']], 1, transB=1)
+    ft = transformer.make_node('Gemm', [previous_h_state_tensor, R['f'], ft[0]], 1, transB=1)
+    peephole_ft = transformer.make_node('Mul', [P['f'], previous_c_state_tensor], 1)
+    ft = transformer.make_node('Add', ft + peephole_ft, 1)
+    if clip is not None:
+      ft = transformer.make_node('Clip', ft, 1, min=-clip, max=clip)
+    ft = transformer.make_node(act['f'], ft, 1)
+
+    # ct = g(Xt*(Wc^T) + Ht-1*(Rc^T) + Wbc + Rbc)
+    ct = transformer.make_node('Gemm', [X[i], W['c'], B['c']], 1, transB=1)
+    ct = transformer.make_node('Gemm', [previous_h_state_tensor, R['c'], ct[0]], 1, transB=1)
+    if clip is not None:
+      ct = transformer.make_node('Clip', ct, 1, min=-clip, max=clip)
+    ct = transformer.make_node(act['g'], ct, 1)
+
+    # Ct = ft (.) Ct-1 + it (.) ct
+    ft_Ct = transformer.make_node('Mul', ft + [previous_c_state_tensor], 1)
+    it_ct = transformer.make_node('Mul', it + ct, 1)
+    Ct = transformer.make_node('Add', ft_Ct + it_ct, 1)
+    previous_c_state_tensor = Ct[0]
+
+    # ot = f(Xt*(Wo^T) + Ht-1*(Ro^T) + Po (.) Ct + Wbo + Rbo)
+    ot = transformer.make_node('Gemm', [X[i], W['o'], B['o']], 1, transB=1)
+    ot = transformer.make_node('Gemm', [previous_h_state_tensor, R['o'], ot[0]], 1, transB=1)
+    peephole_ot = transformer.make_node('Mul', [P['o'], Ct[0]], 1)
+    ot = transformer.make_node('Add', ot + peephole_ot, 1)
+    if clip is not None:
+      ot = transformer.make_node('Clip', ot, 1, min=-clip, max=clip)
+    ot = transformer.make_node(act['f'], ot, 1)
+
+    # Ht = ot (.) h(Ct)
+    Ht = transformer.make_node(act['h'], Ct, 1)
+    Ht = transformer.make_node('Mul', ot + Ht, 1)
+    previous_h_state_tensor = Ht[0]
+    state_h_tensors += Ht
+
+  return (state_h_tensors, previous_c_state_tensor)
+
+
+def transform_unidirectional_LSTM(transformer, original_node, x, tensor_infos, activations, clip, direction, hidden_size, layout):
+  inputs = original_node.input
+  outputs = original_node.output
+  if direction == 'reverse':
+    x.reverse()
+  w = transformer.make_node('Squeeze', [inputs[1]], 1, axes=[0])
+  r = transformer.make_node('Squeeze', [inputs[2]], 1, axes=[0])
+
+  b = None
+  if len(inputs) > 3 and inputs[3] != '':
+    b = transformer.make_node('Squeeze', [inputs[3]], 1, axes=[0])
+
+  initial_h = None
+  if len(inputs) > 5 and inputs[5] != '':
+    direction_dim = layout
+    initial_h = transformer.make_node('Squeeze', [inputs[5]], 1, axes=[direction_dim])[0]
+
+  initial_c = None
+  if len(inputs) > 6 and inputs[6] != '':
+    direction_dim = layout
+    initial_c = transformer.make_node('Squeeze', [inputs[6]], 1, axes=[direction_dim])[0]
+
+  p = None
+  if len(inputs) > 7 and inputs[7] != '':
+    p = transformer.make_node('Squeeze', [inputs[7]], 1, axes=[0])
+
+  dtype = dtype_to_np(tensor_infos[inputs[0]].dtype)
+  batch_size = tensor_infos[inputs[0]].shape[1-layout]
+
+  act = {'f': activations[0], 'g': activations[1], 'h': activations[2]}
+ 
+  state_h_tensors, state_c_tensor = generate_one_direction_LSTM(transformer, x, w, r, b, initial_h, initial_c, p, clip, act, dtype, hidden_size, batch_size)
+
+  y_direction_dim = layout + 1
+  y_h_direction_dim = layout
+  state_layout_tensors = []
+  seq_length_dim = layout
+  for h_state in state_h_tensors:
+    state_layout_tensors += transformer.make_node("Unsqueeze", [h_state], 1, axes=[seq_length_dim, y_direction_dim])
+
+  Y_h = transformer.make_node('Unsqueeze', [state_h_tensors[-1]], [outputs[1]], axes=[y_h_direction_dim])
+  Y_c = transformer.make_node('Unsqueeze', [state_c_tensor], [outputs[2]], axes=[y_h_direction_dim])
+  Y = transformer.make_node('Concat', state_layout_tensors, [outputs[0]], axis=seq_length_dim)
+
+
 def legalize_LSTM(transformer, tensor_infos, node):
   inputs = node.input
   outputs = node.output
@@ -286,7 +487,7 @@ def legalize_LSTM(transformer, tensor_infos, node):
   # attributes
   activation_alpha = []
   activation_beta = []
-  activations = ['Sigmoid', 'Tanh', 'Tanh']
+  activations = ['Sigmoid', 'Tanh', 'Tanh'] * 2
   clip = None
   direction = 'forward'
   hidden_size = 0
