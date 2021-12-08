c568981b0463a18446a4067f9949ff3dde571e2e
SaeHie Park <saehie.park@gmail.com>
2021-12-08 14:22:36 +0900

    [luci] Introduce copy_QuantParam (#8115)
    This will introduce copy_QuantParam method.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/lang/include/luci/IR/CircleQuantParam.h
+++ compiler/luci/lang/include/luci/IR/CircleQuantParam.h
@@ -32,6 +32,11 @@ struct CircleQuantParam
   int32_t quantized_dimension{0};
 };
 
+struct CircleNode;
+
+// TODO rename to copy_quantparam
+void copy_QuantParam(const luci::CircleNode *src, luci::CircleNode *dst);
+
 } // namespace luci
 
 #endif // __LUCI_IR_CIRCLEQUANTPARAM_H__
