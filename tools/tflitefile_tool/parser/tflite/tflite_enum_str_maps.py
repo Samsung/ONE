e4d55851932892bd9520a34ea6ec243ad017056a
Yongseop Kim <yons.kim@samsung.com>
2021-12-22 19:59:58 +0900

    Remove unnecessary files and comments

--- tools/tflitefile_tool/parser/tflite/tflite_enum_str_maps.py
+++ tools/tflitefile_tool/parser/tflite/tflite_enum_str_maps.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# TODO: Use this module
+
 import tflite.BuiltinOperator
 import tflite.ActivationFunctionType
 import tflite.BuiltinOptions
