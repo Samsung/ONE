d862fdcda8ef223a200daea5fb11d3fc8224a807
chunseoklee <chunseoklee@naver.com>
2021-12-30 09:06:19 +0900

    Support armv7hl arch in tizen (#8210)
    * Support armv7hl arch in tizen
    
    - armv7hl in cmake config files
    - armv7hl in spec file
    
    Change-Id: If9ce0012703948b2b76e17c753e29af94939b9fd
    Signed-off-by: Chunseok Lee <chunseok.lee@samsung.com>
    
    * Add GPU_CL backend build

---
+++ infra/nnfw/cmake/options/options_armv7hl-tizen.cmake
@@ -0,0 +1,15 @@
+#
+# armv7hl tizen cmake options
+#
+option(BUILD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
+option(BUILD_TENSORFLOW_LITE "Build TensorFlow Lite from the downloaded source" OFF)
+option(DOWNLOAD_NEON2SSE "Download NEON2SSE library source" OFF)
+
+option(BUILD_LOGGING "Build logging runtime" OFF)
+option(GENERATE_RUNTIME_NNAPI_TESTS "Generate NNAPI operation gtest" OFF)
+option(ENVVAR_ONERT_CONFIG "Use environment variable for onert configuration" OFF)
+
+option(DOWNLOAD_OPENCL_HEADERS "Download Opencl_headers source" ON)
+option(DOWNLOAD_TENSORFLOW_GPU "Download Tensorflow GPU delegate source" ON)
+option(BUILD_GPU_CL "Build gpu_cl backend" ON)
+option(BUILD_TENSORFLOW_LITE_GPU "Build TensorFlow Lite GPU delegate from the downloaded source" ON)
