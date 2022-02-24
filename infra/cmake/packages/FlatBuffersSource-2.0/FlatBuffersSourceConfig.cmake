ac963aa157c69234b442fb8e53aed6c1795da160
Hyeongseok Oh <hseok82.oh@samsung.com>
2022-02-24 13:49:22 +0900

    [infra] Use FlatBuffers 2.0 for runtime (#8513)
    This commit updates cmake to use flatbuffers 2.0 for runtime modules.
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

---
+++ infra/cmake/packages/FlatBuffersSource-2.0/FlatBuffersSourceConfig.cmake
@@ -0,0 +1,21 @@
+function(_FlatBuffersSource_import)
+  if(NOT DOWNLOAD_FLATBUFFERS)
+    set(FlatBuffersSource_FOUND FALSE PARENT_SCOPE)
+    return()
+  endif(NOT DOWNLOAD_FLATBUFFERS)
+
+  nnas_include(ExternalSourceTools)
+  nnas_include(OptionTools)
+
+  envoption(FLATBUFFERS_2_0_URL https://github.com/google/flatbuffers/archive/v2.0.0.tar.gz)
+  ExternalSource_Download(FLATBUFFERS
+    DIRNAME FLATBUFFERS-2.0
+    CHECKSUM MD5=a27992324c3cbf86dd888268a23d17bd
+    URL ${FLATBUFFERS_2_0_URL}
+  )
+
+  set(FlatBuffersSource_DIR ${FLATBUFFERS_SOURCE_DIR} PARENT_SCOPE)
+  set(FlatBuffersSource_FOUND TRUE PARENT_SCOPE)
+endfunction(_FlatBuffersSource_import)
+
+_FlatBuffersSource_import()
