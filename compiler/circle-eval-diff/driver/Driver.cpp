a7f89ff6b58358e46e70917cad727df5cb9f7402
Hyukjin Jeong <hj1.jeong@samsung.com>
2022-04-01 11:57:35 +0900

    [circle-eval-diff] Use CircleEvalDiff in driver (#8764)
    This uses CircleEvalDiff in driver.
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

--- compiler/circle-eval-diff/driver/Driver.cpp
+++ compiler/circle-eval-diff/driver/Driver.cpp
@@ -14,11 +14,37 @@
  * limitations under the License.
  */
 
+#include "CircleEvalDiff.h"
+
 #include <arser/arser.h>
 #include <vconone/vconone.h>
 
+using namespace circle_eval_diff;
+
 namespace
 {
+
+std::string to_lower_case(std::string s)
+{
+  std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) { return std::tolower(c); });
+  return s;
+}
+
+Metric to_metric(const std::string &str)
+{
+  if (to_lower_case(str).compare("mae") == 0)
+    return Metric::MAE;
+
+  throw std::runtime_error("Unsupported metric.");
+}
+
+InputFormat to_input_format(const std::string &str)
+{
+  if (to_lower_case(str).compare("h5") == 0)
+    return InputFormat::H5;
+
+  throw std::runtime_error("Unsupported input format.");
+}
 
 void print_version(void)
 {
@@ -69,11 +95,14 @@ int entry(const int argc, char **argv)
     .nargs(1)
     .type(arser::DataType::STR)
     .required(false)
+    .default_value("MAE")
     .help("Metric for comparison (default: MAE)");
 
   arser.add_argument("--input_data_format")
     .nargs(1)
     .type(arser::DataType::STR)
+    .required(false)
+    .default_value("h5")
     .help("Input data format. h5/hdf5 (default) or directory");
 
   try
@@ -87,7 +116,41 @@ int entry(const int argc, char **argv)
     return 255;
   }
 
-  // TODO Run CircleEvalDiff
+  const auto first_model_path = arser.get<std::string>("--first_model");
+  const auto second_model_path = arser.get<std::string>("--second_model");
+
+  // Default values
+  std::string first_input_data_path;
+  std::string second_input_data_path;
+  std::string metric;
+  std::string input_data_format;
+
+  if (arser["--first_input_data"])
+    first_input_data_path = arser.get<std::string>("--first_input_data");
+
+  if (arser["--second_input_data"])
+    second_input_data_path = arser.get<std::string>("--second_input_data");
+
+  if (arser["--first_input_data"] != arser["--second_input_data"])
+    throw std::runtime_error("Input data path should be given for both first_model and "
+                             "second_model, or neither must be given.");
+
+  metric = arser.get<std::string>("--metric");
+  input_data_format = arser.get<std::string>("--input_data_format");
+
+  auto ctx = std::make_unique<CircleEvalDiff::Context>();
+  {
+    ctx->first_model_path = first_model_path;
+    ctx->second_model_path = second_model_path;
+    ctx->metric = to_metric(metric);
+    ctx->input_format = to_input_format(input_data_format);
+  }
+
+  CircleEvalDiff ced(std::move(ctx));
+
+  ced.init();
+
+  ced.evalDiff(first_input_data_path, second_input_data_path);
 
   return EXIT_SUCCESS;
 }
