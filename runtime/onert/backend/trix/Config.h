4b56a32fb6249d1de285a579f68764e221b1ffa1
Jiyoung Giuliana Yun <wldudyun10@gmail.com>
2022-04-05 21:02:29 +0900

    Update runtime/onert/backend/trix/Config.h
    Co-authored-by: Jang Jiseob <ragmani0216@gmail.com>

--- runtime/onert/backend/trix/Config.h
+++ runtime/onert/backend/trix/Config.h
@@ -35,7 +35,7 @@ public:
   bool initialize() override;
   ir::Layout supportLayout(const ir::Operation &node, ir::Layout frontend_layout) override;
   bool supportPermutation() override { return true; }
-  bool supportDynamicTensor() override { return true; }
+  bool supportDynamicTensor() override { return false; }
   bool supportFP16() override { return false; }
 
   std::unique_ptr<util::ITimer> timer() override { return std::make_unique<util::CPUTimer>(); }
