1249fde8b22af3c2948d00bfe95e05f7bb6ae3c3
Hyeongseok Oh <hseok82.oh@samsung.com>
2022-04-12 18:49:51 +0900

    [infra/script] Pass build option (#8845)
    This commit introduce BUILD_OPTIONS for CI script to pass build option. (ex. TRIX_ENGINE_PREFIX)
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/scripts/docker_build_test_x64.sh
+++ infra/scripts/docker_build_test_x64.sh
@@ -31,8 +31,9 @@ pushd $ROOT_PATH > /dev/null
 
 export DOCKER_ENV_VARS
 export DOCKER_VOLUMES
+export BUILD_OPTIONS
 # Disable nnpackage_run build: mismatch between buildtool for CI and installed hdf5
-CMD="export OPTIONS='-DBUILD_NNPACKAGE_RUN=OFF' && \
+CMD="export OPTIONS='-DBUILD_NNPACKAGE_RUN=OFF $BUILD_OPTIONS' && \
      export BUILD_TYPE=Release && \
      cp -nv Makefile.template Makefile && \
      make all install build_test_suite"
