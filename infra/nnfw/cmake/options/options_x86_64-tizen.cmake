e6226360fa9aa91635c83e88fec18a9b1fad5281
chunseoklee <chunseok.lee@samsung.com>
2022-04-17 14:28:44 +0900

    [onert] Fix tizen x86_64 build fail (#8928)
    - Disable tensorflow-lite build on tizen x86_64
    
    ONE-DCO-1.0-Signed-off-by: Chunseok Lee <chunseok.lee@samsung.com>

--- infra/nnfw/cmake/options/options_x86_64-tizen.cmake
+++ infra/nnfw/cmake/options/options_x86_64-tizen.cmake
@@ -2,6 +2,7 @@
 # x86_64 linux cmake options
 #
 option(BUILD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
+option(BUILD_TENSORFLOW_LITE "Build TensorFlow Lite from the downloaded source" OFF)
 option(DOWNLOAD_ARMCOMPUTE "Download ARM Compute source" OFF)
 option(DOWNLOAD_GTEST "Download Google Test source and build Google Test" OFF)
 
