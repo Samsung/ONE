e46e618488f613fee60fe35a4c9e3395d8553bfb
SaeHie Park <saehie.park@gmail.com>
2022-04-21 16:35:40 +0900

    [luci-interpreter] Use must_cast (M~P)
    This will revise to use loco::must_cast to reduce duplicate codes.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci-interpreter/src/loader/nodes/Pad.cpp
+++ compiler/luci-interpreter/src/loader/nodes/Pad.cpp
@@ -24,9 +24,7 @@ namespace luci_interpreter
 std::unique_ptr<Kernel> build_kernel_CirclePad(const luci::CircleNode *circle_node,
                                                KernelBuilderHelper &helper)
 {
-  const auto *node = dynamic_cast<const luci::CirclePad *>(circle_node);
-  if (node == nullptr)
-    throw std::runtime_error("wrong builder for operation");
+  const auto *node = loco::must_cast<const luci::CirclePad *>(circle_node);
   assert(node->arity() == 2);
 
   const Tensor *input = helper.getInputTensor(node->input());
