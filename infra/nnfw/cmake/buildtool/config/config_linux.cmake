e4502e2646a3131072e77d0244f753029755ff3c
Hyeongseok Oh <hseok82.oh@samsung.com>
2022-04-28 16:07:33 +0900

    [infra] Revisit runtime build compiler version check
    This commit moves GNU compiler version check to root cmake
    and remove redundant version checking on runtime build.
    
    If NEON2SSE is used and GNU compiler version is under 9,
    tensorflow lite build uses release build option (-O3).
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/buildtool/config/config_linux.cmake
+++ infra/nnfw/cmake/buildtool/config/config_linux.cmake
@@ -2,15 +2,9 @@
 # linux common compile options
 #
 
-# remove warning from arm cl
+# Remove warning: ignoring attributes on template argument (ACL, Eigen, etc)
 # https://github.com/ARM-software/ComputeLibrary/issues/330
-set(GCC_VERSION_DISABLE_WARNING 6.0)
-if(CMAKE_CXX_COMPILER_VERSION VERSION_GREATER GCC_VERSION_DISABLE_WARNING)
-  message(STATUS "GCC version higher than ${GCC_VERSION_DISABLE_WARNING}")
-  set(FLAGS_CXXONLY ${FLAGS_CXXONLY}
-      "-Wno-ignored-attributes"
-      )
-endif()
+set(FLAGS_CXXONLY ${FLAGS_CXXONLY} "-Wno-ignored-attributes")
 
 # Disable annoying ABI compatibility warning.
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 7.0)
