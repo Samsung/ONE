7e16f7f6a2112d34bd8f1b54ed82aae60b49d00e
Hyukjin Jeong <hj1.jeong@samsung.com>
2022-05-02 10:12:39 +0900

    [luci] Support more ops in fake quantization.
    This supports more ops in fake quantization.
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

--- compiler/luci/pass/src/ConvertToFakeQuantizedModelPass.cpp
+++ compiler/luci/pass/src/ConvertToFakeQuantizedModelPass.cpp
@@ -212,6 +212,8 @@ struct FakeQuantize final : public luci::CircleNodeMutableVisitor<void>
   void visit(luci::CircleConcatenation *) {}
   void visit(luci::CircleSlice *) {}
   void visit(luci::CircleReshape *) {}
+  void visit(luci::CircleSplit *) {}
+  void visit(luci::CircleSplitOut *) {}
 
   // Virtual node
   void visit(luci::CircleOutputExclude *) {}
@@ -222,6 +224,8 @@ struct FakeQuantize final : public luci::CircleNodeMutableVisitor<void>
 
     insert_dequantize(node);
   }
+
+  void visit(luci::CircleDequantize *) {}
 };
 
 #undef RETURN_UNLESS
