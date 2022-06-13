69dc4a6f685017dac3e43e4f994c76f879ea73c7
hj0412.yi <hj0412.yi@samsung.com>
2022-06-13 09:38:18 +0900

    [infra] Introduce cmake to build gpu_cl
    I added config file to download source code to build Tensorflow
    lite 2.8.0 GPU CL. To build TF lite 2.8, we need an Egl header, an
    Opengl header and a VulkanSource.
    
    ONE-DCO-1.0-Signed-off-by: HyunJin Yi <hj0412.yi@samsung.com>

---
+++ infra/cmake/packages/VulkanSourceConfig.cmake
@@ -0,0 +1,21 @@
+function(_VulkanSource_import)
+  if(NOT ${DOWNLOAD_VULKAN})
+    set(VulkanSource_FOUND FALSE PARENT_SCOPE)
+    return()
+  endif(NOT ${DOWNLOAD_VULKAN})
+
+  nnas_include(ExternalSourceTools)
+  nnas_include(OptionTools)
+
+  envoption(EXTERNAL_DOWNLOAD_SERVER "https://github.com")
+  # fp16 commit in xnnpack 8b283aa30a31
+  envoption(VULKAN_URL ${EXTERNAL_DOWNLOAD_SERVER}/KhronosGroup/Vulkan-Headers/archive/ec2db85225ab410bc6829251bef6c578aaed5868.tar.gz)
+  ExternalSource_Download(VULKAN
+    DIRNAME VULKAN
+    URL ${VULKAN_URL})
+
+  set(VulkanSource_DIR ${VULKAN_SOURCE_DIR} PARENT_SCOPE)
+  set(VulkanSource_FOUND TRUE PARENT_SCOPE)
+endfunction(_VulkanSource_import)
+
+_VulkanSource_import()
---
+++ infra/nnfw/cmake/packages/TensorFlowGpuConfig.cmake
@@ -0,0 +1,46 @@
+# TensorFlowGpuConfig.cmake
+
+function(_Build_TfliteGpuDelagate_)
+  nnas_find_package(TensorFlowSource EXACT 2.7.0 QUIET)
+  return_unless(TensorFlowSource_FOUND)
+
+  nnas_find_package(TensorFlowGEMMLowpSource EXACT 2.7.0 QUIET)
+  return_unless(TensorFlowGEMMLowpSource_FOUND)
+
+  nnas_find_package(TensorFlowEigenSource EXACT 2.7.0 QUIET)
+  return_unless(TensorFlowEigenSource_FOUND)
+
+  nnas_find_package(AbseilSource REQUIRED)
+  return_unless(AbseilSource_FOUND)
+
+  nnas_find_package(Farmhash REQUIRED)
+  return_unless(Farmhash_FOUND)
+
+  nnas_find_package(FlatBuffers REQUIRED)
+
+  nnas_find_package(Fp16Source REQUIRED)
+  return_unless(Fp16Source_FOUND)
+
+  nnas_find_package(VulkanSource QUIET)
+  return_unless(VulkanSource_FOUND)
+
+  nnas_find_package(Opengl_HeadersSource QUIET)
+  return_unless(Opengl_HeadersSource_FOUND)
+
+  nnas_find_package(Egl_HeadersSource QUIET)
+  return_unless(Egl_HeadersSource_FOUND)
+
+  if(NOT TARGET TensorFlowGpu)
+    nnas_include(ExternalProjectTools)
+    add_extdirectory("${CMAKE_CURRENT_LIST_DIR}/TensorFlowLiteGpu" TensorFlowLiteGpu)
+  endif()
+  set(TensorFlowSource_DIR ${TensorFlowSource_DIR} PARENT_SCOPE)
+  set(TensorFlowGpu_DIR ${TensorFlowGpu_DIR} PARENT_SCOPE)
+endfunction(_Build_TfliteGpuDelagate_)
+
+if(BUILD_TENSORFLOW_LITE_GPU)
+  _Build_TfliteGpuDelagate_()
+  set(TensorFlowGpu_FOUND TRUE PARENT_SCOPE)
+else(BUILD_TENSORFLOW_LITE_GPU)
+  set(TensorFlowGpu_FOUND FALSE PARENT_SCOPE)
+endif(BUILD_TENSORFLOW_LITE_GPU)
