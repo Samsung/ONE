e5ad55d017fcb1186409d632e0e02bfe8f0cb0d1
hj0412.yi <hj0412.yi@samsung.com>
2022-06-14 11:01:38 +0900

    Rebase Tensorflow version 2.8.0

--- infra/cmake/packages/TensorFlowEigenSource-2.8.0/TensorFlowEigenSourceConfig.cmake
+++ infra/cmake/packages/TensorFlowEigenSource-2.8.0/TensorFlowEigenSourceConfig.cmake
@@ -7,12 +7,12 @@ function(_TensorFlowEigenSource_import)
   nnas_include(ExternalSourceTools)
   nnas_include(OptionTools)
 
-  # Exact version used by TensorFlow v2.7.0.
+  # Exact version used by TensorFlow v2.8.0.
   # See tensorflow/third_party/eigen3/workspace.bzl.
   envoption(EXTERNAL_DOWNLOAD_SERVER "https://gitlab.com")
-  envoption(TENSORFLOW_2_7_0_EIGEN_URL ${EXTERNAL_DOWNLOAD_SERVER}/libeigen/eigen/-/archive/7792b1e909a98703181aecb8810b4b654004c25d/eigen-7792b1e909a98703181aecb8810b4b654004c25d.tar.gz)
+  envoption(TENSORFLOW_2_8_0_EIGEN_URL ${EXTERNAL_DOWNLOAD_SERVER}/libeigen/eigen/-/archive/008ff3483a8c5604639e1c4d204eae30ad737af6/eigen-e1dd31ce174c3d26fbe38388f64b09d2adbd7557a59e90e6f545a288cc1755fc.tar.gz)
 
-  ExternalSource_Download(EIGEN DIRNAME TENSORFLOW-2.7.0-EIGEN ${TENSORFLOW_2_7_0_EIGEN_URL})
+  ExternalSource_Download(EIGEN DIRNAME TENSORFLOW-2.8.0-EIGEN ${TENSORFLOW_2_8_0_EIGEN_URL})
 
   set(TensorFlowEigenSource_DIR ${EIGEN_SOURCE_DIR} PARENT_SCOPE)
   set(TensorFlowEigenSource_FOUND TRUE PARENT_SCOPE)
