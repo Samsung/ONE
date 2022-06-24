128f8e329a3649161ba63f58fa9248bb33b7f494
Sanggyu Lee <takepencil@naver.com>
2022-06-24 21:36:52 +0900

    (api) nnfw_session::coptions is std::vector

--- runtime/onert/api/src/nnfw_api_internal.h
+++ runtime/onert/api/src/nnfw_api_internal.h
@@ -176,7 +176,7 @@ private:
 private:
   State _state{State::INITIALIZED};
   std::shared_ptr<onert::ir::Model> _model;
-  std::unique_ptr<onert::compiler::CompilerOptions> _coptions;
+  std::vector<std::unique_ptr<onert::compiler::CompilerOptions>> _coptions;
   std::shared_ptr<onert::compiler::CompilerArtifact> _compiler_artifact;
   std::unique_ptr<onert::exec::Execution> _execution;
   std::shared_ptr<onert::api::CustomKernelRegistry> _kernel_registry;
