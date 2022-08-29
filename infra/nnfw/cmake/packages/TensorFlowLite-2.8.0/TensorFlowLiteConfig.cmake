baf33d17042e34b76527220c1a80087d3ba15578
Hyeongseok Oh <hseok82.oh@samsung.com>
2022-08-29 18:04:33 +0900

    [infra] Enable TFLite 2.8 build and use (#9639)
    This commit update tflite_vanlla_run and TFLite 2.x build infra to use TFLite 2.8.0.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/packages/TensorFlowLite-2.8.0/TensorFlowLiteConfig.cmake
+++ infra/nnfw/cmake/packages/TensorFlowLite-2.8.0/TensorFlowLiteConfig.cmake
@@ -1,8 +1,8 @@
-if(BUILD_TENSORFLOW_LITE_2_3_0)
+if(BUILD_TENSORFLOW_LITE_2_8_0)
   macro(return_unless VAR)
   if(NOT ${VAR})
-    message("TFLiteVanillaRun: ${VAR} NOT TRUE")
-    set(TensorFlowLite_2_3_0_FOUND FALSE PARENT_SCOPE)
+    message("TFLite 2.8: ${VAR} NOT TRUE")
+    set(TensorFlowLite_2_8_0_FOUND FALSE PARENT_SCOPE)
     return()
   endif(NOT ${VAR})
   endmacro(return_unless)
@@ -10,7 +10,7 @@ if(BUILD_TENSORFLOW_LITE_2_3_0)
   nnas_include(ExternalSourceTools)
   nnas_include(OptionTools)
 
-  nnas_find_package(TensorFlowSource EXACT 2.3.0 QUIET)
+  nnas_find_package(TensorFlowSource EXACT 2.8.0 QUIET)
   return_unless(TensorFlowSource_FOUND)
 
   # Below urls come from https://github.com/tensorflow/tensorflow/blob/v2.3.0/tensorflow/tensorflow/workspace.bzl
@@ -22,23 +22,29 @@ if(BUILD_TENSORFLOW_LITE_2_3_0)
   return_unless(Farmhash_FOUND)
   nnfw_find_package(FlatBuffers QUIET)
   return_unless(FlatBuffers_FOUND)
-  nnas_find_package(TensorFlowGEMMLowpSource EXACT 2.3.0 QUIET)
+  nnas_find_package(TensorFlowGEMMLowpSource EXACT 2.8.0 QUIET)
   return_unless(TensorFlowGEMMLowpSource_FOUND)
   nnas_find_package(OouraFFTSource QUIET)
   return_unless(OouraFFTSource_FOUND)
-  nnfw_find_package(TensorFlowRuy QUIET)
-  return_unless(TensorFlowRuy_FOUND)
+  nnfw_find_package(Ruy QUIET)
+  return_unless(Ruy_FOUND)
 
   # TensorFlow Lite requires FP16 library's header only
   nnas_find_package(Fp16Source QUIET)
   return_unless(Fp16Source_FOUND)
 
+  # TensorFlow Lite requires Pybind11 library's header only
+  # But Pybind11 requires python3-dev package
+  # TODO Enable below by installing package on build system
+  #nnas_find_package(Pybind11Source QUIET)
+  #return_unless(Pybind11Source_FOUND)
+
   # Optional packages
   nnas_find_package(NEON2SSESource QUIET)
 
   nnas_include(ExternalProjectTools)
-  add_extdirectory("${CMAKE_CURRENT_LIST_DIR}/TensorFlowLite" tflite-2.3.0)
+  add_extdirectory("${CMAKE_CURRENT_LIST_DIR}/TensorFlowLite" tflite-2.8.0)
 
-  set(TensorFlowLite_2_3_0_FOUND TRUE)
+  set(TensorFlowLite_2_8_0_FOUND TRUE)
   return()
 endif()
