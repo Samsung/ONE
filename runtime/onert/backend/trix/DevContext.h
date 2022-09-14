b23eeb1459691e7801a89a9fce1386ad8eadf4c4
ragmani <ragmani0216@gmail.com>
2022-09-14 14:46:44 +0900

    [onert] Support batch-parallel execution of trix models
    This commit suppors batch-parallel execution of trix models.
      - Introduce BatchThreadPool
      - Gather things related to device handle in DevContext
      - Change ir::Shape::MAX_RANK to ir::Shape::ONERT_MAX_RANK
    
    ONE-DCO-1.0-Signed-off-by: ragmani <ragmani0216@gmail.com>

--- runtime/onert/backend/trix/DevContext.h
+++ runtime/onert/backend/trix/DevContext.h
@@ -17,7 +17,12 @@
 #ifndef __ONERT_BACKEND_TRIX_DEV_CONTEXT_H__
 #define __ONERT_BACKEND_TRIX_DEV_CONTEXT_H__
 
+#include "BatchThreadPool.h"
+
 #include <libnpuhost.h>
+#include <memory>
+#include <string>
+#include <unordered_map>
 
 namespace onert
 {
@@ -26,103 +31,111 @@ namespace backend
 namespace trix
 {
 
+using ModelID = uint32_t;
+
+/**
+ * @brief NPU device context of trix backend
+ *
+ */
 class DevContext
 {
 public:
-  DevContext()
-  {
-    auto device_count = getnumNPUdeviceByType(NPUCOND_TRIV2_CONN_SOCIP);
-    // TODO: x64 platform has 3 cores. We do not support more that 2 cores for now.
-    if (device_count > 2)
-    {
-      device_count = 2;
-    }
+  /**
+   * @brief Construct a new device Context object
+   *
+   */
+  DevContext();
 
-    if (device_count <= 0)
-    {
-      throw std::runtime_error("Unable to find TRIX NPU device");
-    }
+  /**
+   * @brief Destroy the device Context object
+   *
+   */
+  ~DevContext();
 
-    for (int i = 0; i < device_count; i++)
-    {
-      npudev_h h;
-      if (getNPUdeviceByType(&h, NPUCOND_TRIV2_CONN_SOCIP, i) < 0)
-      {
-        throw std::runtime_error("Failed to get TRIX NPU device handle");
-      }
-      _dev_handles.push_back(h);
-    }
-  }
+  DevContext(const DevContext &) = delete;
+  DevContext &operator=(const DevContext &) = delete;
 
-  ~DevContext()
-  {
-    for (auto h : _dev_handles)
-    {
-      if (h != nullptr)
-      {
-        unregisterNPUmodel_all(h);
-        putNPUdevice(h);
-      }
-    }
-  }
+  /**
+   * @brief Register a trix model for all NPU devices
+   *
+   * @param model_file_path File path of a trix model
+   * @return ModelID Internal ID of the trix model
+   */
+  ModelID registerModel(const std::string &model_file_path);
 
-  npudev_h getDev(int i) { return _dev_handles[i]; }
-  int getDevSize() { return _dev_handles.size(); }
+  /**
+   * @brief Unregister a trix model
+   *
+   * @param model_id Internal ID of the trix model to be unregistered
+   */
+  void unRegisterModel(ModelID model_id);
 
-  template <typename T> void setDataInfo(tensors_data_info *info, std::vector<T *> &tensors)
-  {
-    info->num_info = static_cast<uint32_t>(tensors.size());
-
-    for (uint32_t idx = 0; idx < info->num_info; ++idx)
-    {
-      info->info[idx].layout = convertDataLayout(tensors[idx]->layout());
-      info->info[idx].type = convertDataType(tensors[idx]->data_type());
-    }
-  }
-
-  template <typename T>
-  void setBuffer(generic_buffers *buf, std::vector<T *> &tensors, int batch_size, int batch_index)
-  {
-    buf->num_buffers = static_cast<uint32_t>(tensors.size());
-
-    for (uint32_t idx = 0; idx < buf->num_buffers; ++idx)
-    {
-      buf->bufs[idx].size = static_cast<uint64_t>(tensors[idx]->total_size() / batch_size);
-      buf->bufs[idx].addr = tensors[idx]->buffer() + (batch_index * buf->bufs[idx].size);
-      buf->bufs[idx].type = BUFFER_MAPPED;
-    }
-  }
+  /**
+   * @brief Request a trix model to be run on NPU
+   *
+   * @param model_id    Internal ID of a trix model
+   * @param input_bufs  Buffer data of inputs
+   * @param in_info     Data info of inputs
+   * @param output_bufs Buffer data of outputs
+   * @param out_info    data info of outputs
+   * @param batch_size  Batch size
+   */
+  void requestRun(ModelID model_id, input_buffers *input_bufs, tensors_data_info *in_info,
+                  output_buffers *output_bufs, tensors_data_info *out_info, size_t batch_size);
 
 private:
-  data_layout convertDataLayout(const ir::Layout layout)
-  {
-    switch (layout)
-    {
-      case ir::Layout::NCHW:
-        return DATA_LAYOUT_NCHW;
-      case ir::Layout::NHWC:
-        return DATA_LAYOUT_NHWC;
-      default:
-        throw std::runtime_error("Unknown Layout");
-    }
-  }
+  /**
+   * @brief Rquest one batch of a trix model to be run on a device of NPU
+   *
+   * @param dev_num     Device number
+   * @param model_id    Internal ID of a trix model
+   * @param input_bufs  Buffer data of inputs
+   * @param in_info     Data info of inputs
+   * @param output_bufs Buffer data of outputs
+   * @param out_info    data info of outputs
+   */
+  void runOneBatch(uint32_t dev_num, ModelID model_id, input_buffers *input_bufs,
+                   tensors_data_info *in_info, output_buffers *output_bufs,
+                   tensors_data_info *out_info);
 
-  data_type convertDataType(const ir::DataType type)
-  {
-    switch (type)
-    {
-      case ir::DataType::QUANT_UINT8_ASYMM:
-        return DATA_TYPE_QASYMM8;
-      case ir::DataType::QUANT_INT16_SYMM:
-        return DATA_TYPE_QSYMM16;
-      default:
-        throw std::runtime_error("Unsupported data type");
-    }
-  }
+  /**
+   * @brief Set the buffer object by batch
+   *
+   * @param origin_buf   Buffer object that has all batches
+   * @param batch_num    Batch number
+   * @param batch_offset Size of a batch
+   * @param batch_buf    One batch buffer object to be set
+   */
+  void setBufferByBatch(const generic_buffer &origin_buf, uint32_t batch_num, uint64_t batch_offset,
+                        generic_buffer *batch_buf);
 
 private:
-  // NPU device handles
+  /**
+   * @brief NPU device handles
+   *
+   */
   std::vector<npudev_h> _dev_handles;
+
+  /**
+   * @brief Threadpool for batch-by-batch multi-threading
+   *
+   */
+  std::unique_ptr<BatchThreadPool> _batch_thread_pool;
+
+  // TODO Change key to internal trix model context(?) if it is needed
+  /**
+   * @brief Map for ID of models
+   *        Internal Model ID : Model ID array for each device
+   *
+   */
+  std::unordered_map<ModelID, std::vector<uint32_t>> _model_ids;
+
+  /**
+   * @brief Map for meta data
+   *        Model ID at each device : meta data
+   *
+   */
+  std::unordered_map<uint32_t, std::shared_ptr<npubin_meta>> _meta_map;
 };
 
 } // namespace trix
