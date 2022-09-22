3372cdf2a401545b83e49659ff304f3e2ee67034
Hyukjin Jeong <hj1.jeong@samsung.com>
2022-09-22 10:11:33 +0900

    Apply comments

--- compiler/dalgona/tests/GenH5RandomInputs.py
+++ compiler/dalgona/tests/GenH5RandomInputs.py
@@ -18,8 +18,9 @@
 import h5py as h5
 import numpy as np
 from circle.Model import Model
-from circle.TensorType import TensorType
 import argparse
+
+from RandomDataGenerator import RandomDataGenerator
 
 #
 # This script generates a pack of random input data (.h5) expected by the input circle model
@@ -52,34 +53,14 @@ h5_file = h5.File(output_path, 'w')
 group = h5_file.create_group("value")
 group.attrs['desc'] = "Input data for " + model
 
-
-def toNumpyType(circle_type):
-    if circle_type == TensorType.UINT8:
-        return np.uint8
-    if circle_type == TensorType.FLOAT32:
-        return np.float32
-    if circle_type == TensorType.INT16:
-        return np.int16
-
-
 # Generate random data
 for i in range(num_data):
     sample = group.create_group(str(i))
     for j in range(len(inputs)):
         input_index = inputs[j]
         tensor = graph.Tensors(input_index)
-        np_type = toNumpyType(tensor.Type())
-        if np_type == np.uint8:
-            input_data = np.random.randint(
-                0, high=256, size=tensor.ShapeAsNumpy(), dtype=np_type)
-        elif np_type == np.float32:
-            input_data = np.array(10 * np.random.random_sample(tensor.ShapeAsNumpy()) - 5,
-                                  np_type)
-        elif np_type == np.int16:
-            input_data = np.random.randint(
-                -32767, high=32768, size=tensor.ShapeAsNumpy(), dtype=np_type)
-        else:
-            raise SystemExit('Unsupported data type')
+        g = RandomDataGenerator(tensor.ShapeAsNumpy())
+        input_data = g.gen(tensor.Type())
         sample.create_dataset(str(j), data=input_data)
 
 h5_file.close()
--- compiler/dalgona/tests/SingleOperatorTest.py
+++ compiler/dalgona/tests/SingleOperatorTest.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright 2022 Samsung Electronics Co., Ltd. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
