c90d6967f37efa9c9d761ea0a384af340a233e78
Hyukjin Jeong <hj1.jeong@samsung.com>
2022-09-23 15:30:00 +0900

    [dalgona] Support more operators (#9781)
    This supports more operators.
    - DConv, Add, FC, TConv, Instnorm
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

--- compiler/dalgona/tests/SingleOperatorTest.py
+++ compiler/dalgona/tests/SingleOperatorTest.py
@@ -22,6 +22,11 @@ from circle import Model
 from circle import BuiltinOptions
 from circle import BuiltinOperator
 from circle import Conv2DOptions
+from circle import DepthwiseConv2DOptions
+from circle import AddOptions
+from circle import FullyConnectedOptions
+from circle import TransposeConvOptions
+from circle import InstanceNormOptions
 
 
 class SingleOperatorTest(object):
@@ -67,3 +72,116 @@ class SingleOperatorTest(object):
     def Conv2DPost(self, name, input, filter, bias, padding, stride, dilation, output,
                    fused_act):
         self.testConv2D(padding, stride, dilation, fused_act)
+
+    def testAdd(self, fused_act):
+        # Check opcode
+        opcode = self._model.OperatorCodes(self._op.OpcodeIndex())
+        checkOpcode(opcode.BuiltinCode(), BuiltinOperator.BuiltinOperator.ADD)
+
+        # Check option
+        checkBuiltinOptionType(self._op.BuiltinOptionsType(),
+                               BuiltinOptions.BuiltinOptions.AddOptions)
+
+        self._opt = self._op.BuiltinOptions()
+        opt = AddOptions.AddOptions()
+        opt.Init(self._opt.Bytes, self._opt.Pos)
+        checkActivation(fused_act, opt.FusedActivationFunction())
+
+    def AddPre(self, name, x, y, fused_act):
+        self.testAdd(fused_act)
+
+    def AddPost(self, name, x, y, output, fused_act):
+        self.testAdd(fused_act)
+
+    def testDepthwiseConv2D(self, padding, stride, depth_multiplier, dilation, fused_act):
+        # Check opcode
+        opcode = self._model.OperatorCodes(self._op.OpcodeIndex())
+        checkOpcode(opcode.BuiltinCode(),
+                    BuiltinOperator.BuiltinOperator.DEPTHWISE_CONV_2D)
+
+        # Check option
+        checkBuiltinOptionType(self._op.BuiltinOptionsType(),
+                               BuiltinOptions.BuiltinOptions.DepthwiseConv2DOptions)
+
+        self._opt = self._op.BuiltinOptions()
+        opt = DepthwiseConv2DOptions.DepthwiseConv2DOptions()
+        opt.Init(self._opt.Bytes, self._opt.Pos)
+        checkPadding(padding, opt.Padding())
+        assertTrue(opt.StrideW() == stride['w'], "Stride_w mismatches")
+        assertTrue(opt.StrideH() == stride['h'], "Stride_h mismatches")
+        assertTrue(opt.DepthMultiplier() == depth_multiplier,
+                   "Depth multiplier mismatches")
+        assertTrue(opt.DilationWFactor() == dilation['w'], "Dilation_w mismatches")
+        assertTrue(opt.DilationHFactor() == dilation['h'], "Dilation_w mismatches")
+        checkActivation(fused_act, opt.FusedActivationFunction())
+
+    def DepthwiseConv2DPre(self, name, input, filter, bias, padding, stride,
+                           depth_multiplier, dilation, fused_act):
+        self.testDepthwiseConv2D(padding, stride, depth_multiplier, dilation, fused_act)
+
+    def DepthwiseConv2DPost(self, name, input, filter, bias, padding, stride,
+                            depth_multiplier, dilation, output, fused_act):
+        self.testDepthwiseConv2D(padding, stride, depth_multiplier, dilation, fused_act)
+
+    def testFullyConnected(self, fused_act):
+        # Check opcode
+        opcode = self._model.OperatorCodes(self._op.OpcodeIndex())
+        checkOpcode(opcode.BuiltinCode(), BuiltinOperator.BuiltinOperator.FULLY_CONNECTED)
+
+        # Check option
+        checkBuiltinOptionType(self._op.BuiltinOptionsType(),
+                               BuiltinOptions.BuiltinOptions.FullyConnectedOptions)
+
+        self._opt = self._op.BuiltinOptions()
+        opt = FullyConnectedOptions.FullyConnectedOptions()
+        opt.Init(self._opt.Bytes, self._opt.Pos)
+        checkActivation(fused_act, opt.FusedActivationFunction())
+
+    def FullyConnectedPre(self, name, input, weights, bias, fused_act):
+        self.testFullyConnected(fused_act)
+
+    def FullyConnectedPost(self, name, input, weights, bias, output, fused_act):
+        self.testFullyConnected(fused_act)
+
+    def testTransposeConv(self, padding, stride):
+        # Check opcode
+        opcode = self._model.OperatorCodes(self._op.OpcodeIndex())
+        checkOpcode(opcode.BuiltinCode(), BuiltinOperator.BuiltinOperator.TRANSPOSE_CONV)
+
+        # Check option
+        checkBuiltinOptionType(self._op.BuiltinOptionsType(),
+                               BuiltinOptions.BuiltinOptions.TransposeConvOptions)
+
+        self._opt = self._op.BuiltinOptions()
+        opt = TransposeConvOptions.TransposeConvOptions()
+        opt.Init(self._opt.Bytes, self._opt.Pos)
+        checkPadding(padding, opt.Padding())
+        assertTrue(opt.StrideW() == stride['w'], "Stride_w mismatches")
+        assertTrue(opt.StrideH() == stride['h'], "Stride_h mismatches")
+
+    def TransposeConvPre(self, name, input, filter, output_shape, bias, padding, stride):
+        self.testTransposeConv(padding, stride)
+
+    def TransposeConvPost(self, name, input, filter, output_shape, bias, padding, stride,
+                          output):
+        self.testTransposeConv(padding, stride)
+
+    def testInstanceNorm(self, epsilon):
+        # Check opcode
+        opcode = self._model.OperatorCodes(self._op.OpcodeIndex())
+        checkOpcode(opcode.BuiltinCode(), BuiltinOperator.BuiltinOperator.INSTANCE_NORM)
+
+        # Check option
+        checkBuiltinOptionType(self._op.BuiltinOptionsType(),
+                               BuiltinOptions.BuiltinOptions.InstanceNormOptions)
+
+        self._opt = self._op.BuiltinOptions()
+        opt = InstanceNormOptions.InstanceNormOptions()
+        opt.Init(self._opt.Bytes, self._opt.Pos)
+        assertTrue(opt.Epsilon() == epsilon, "epsilon mismatches")
+
+    def InstanceNormPre(self, name, input, gamma, beta, epsilon):
+        self.testInstanceNorm(epsilon)
+
+    def InstanceNormPost(self, name, input, gamma, beta, epsilon, output):
+        self.testInstanceNorm(epsilon)
