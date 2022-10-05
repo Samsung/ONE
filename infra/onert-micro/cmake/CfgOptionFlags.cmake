66f3b14910bdb62ac4a4fdf2c4ba315ea6a4eed3
Artem Balyshev <a.balyshev@partner.samsung.com>
2022-10-05 20:18:03 +0300

    fix naming and remove redundant parts

--- infra/onert-micro/cmake/CfgOptionFlags.cmake
+++ infra/onert-micro/cmake/CfgOptionFlags.cmake
@@ -7,14 +7,6 @@ include("cmake/options/options_${TARGET_PLATFORM}.cmake")
 ###
 ### Configuration
 ###
-option(DOWNLOAD_PROTOBUF "Download Protocol Buffer source" ON)
-option(BUILD_PROTOBUF "Locally build Protocol Buffer from the downloaded source" ON)
-option(DOWNLOAD_EIGEN "Download Eigen source" ON)
-option(DOWNLOAD_FARMHASH "Download farmhash source" ON)
-option(DOWNLOAD_GEMMLOWP "Download GEMM low precesion library source" ON)
-option(DOWNLOAD_RUY "Download ruy source" ON)
-option(DOWNLOAD_NEON2SSE "Download NEON2SSE library source" ON)
-option(DOWNLOAD_GFLAGS "Download GFlags source" OFF)
 option(DOWNLOAD_FLATBUFFERS "Download FlatBuffers source" ON)
 option(BUILD_FLATBUFFERS "Locally build Flatbuffers from the downloaded source" ON)
 option(DOWNLOAD_TENSORFLOW "Download TensorFlow source" ON)
