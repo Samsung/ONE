860c17e26225772bf65d9d6d6a9f4af1e36e0bda
Artem Balyshev <a.balyshev@partner.samsung.com>
2022-10-07 13:13:47 +0300

    [Draft][onert-micro] Introduce utils.cmake
    This PR introduce utils.cmake file to support common functions.
    
    ONE-DCO-1.0-Signed-off-by: Artem Balyshev a.balyshev@partner.samsung.com

--- infra/onert-micro/cmake/CfgOptionFlags.cmake
+++ infra/onert-micro/cmake/CfgOptionFlags.cmake
@@ -2,11 +2,14 @@
 # note: this should be placed before default setting for option setting priority
 #       (platform specific setting have higher priority)
 #
-include("cmake/options/options_${TARGET_PLATFORM}.cmake")
+include("${NNAS_PROJECT_SOURCE_DIR}/infra/onert-micro/cmake/options/options_${TARGET_PLATFORM}.cmake")
 
 ###
 ### Configuration
 ###
+option(DOWNLOAD_RUY "Download ruy source" ON)
+option(DOWNLOAD_EIGEN "Download Eigen source" ON)
+option(DOWNLOAD_GEMMLOWP "Download GEMM low precesion library source" ON)
 option(DOWNLOAD_FLATBUFFERS "Download FlatBuffers source" ON)
 option(BUILD_FLATBUFFERS "Locally build Flatbuffers from the downloaded source" ON)
 option(DOWNLOAD_TENSORFLOW "Download TensorFlow source" ON)
