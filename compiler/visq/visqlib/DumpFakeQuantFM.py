0a481e3ffa6c2a26878a07046b6db0a022b4f1e2
Hyukjin Jeong <hj1.jeong@samsung.com>
2022-10-17 10:01:41 +0900

    DRAFT: Introduce visq module
    On going draft to introduce visq module.
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

---
+++ compiler/visq/visqlib/DumpFakeQuantFM.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Script that dumps dequantized FM
+# NOTE This script runs on dalgona
+
+import numpy as np
+
+from pathlib import Path
+from Util import to_filename
+
+# Fake-quantized Op has the postfix of fq_postfix
+# TODO Remove coupling with fake quantization codes
+fq_postfix = '_FQ_Quantize_FQ_Dequantize'
+
+
+# Return the original name before fake quantization
+# Return None if name is not from fake quantization (Dequantize Op in original model)
+# TODO Handle the case when the original node's name contains fq_postfix
+def _name_before_fq(name):
+    if not name.endswith(fq_postfix):
+        return None
+
+    return name[0:name.find(fq_postfix)]
+
+
+# Dump fake-quantized model's intermediate FM data according to tensors.txt
+#
+# Before
+# self._dir/
+#  tensors.txt
+#
+# After
+# self._dir/
+#  tensors.txt
+#  <TENSOR_NAME>.npy
+# NOTE TENSOR_NAME is transformed by to_filename
+class DumpFakeQuantFM(object):
+    def StartAnalysis(self, args):
+        self._dir = Path(args)
+        self._num_data = 0
+        with open(self._dir / 'tensors.txt') as f:
+            self._target_tensors = set([line.rstrip() for line in f])
+
+    def EndNetworkExecution(self, outputs: list):
+        self._num_data += 1
+
+    def DefaultOpPost(self, name, opcode, inputs, output):
+        if opcode == 'Dequantize':
+            orig_name = _name_before_fq(name)
+            if orig_name in self._target_tensors:
+                data_path = self._dir / str(self._num_data)
+                data_path.mkdir(parents=False, exist_ok=True)
+                np.save(str(data_path / to_filename(orig_name)), output['data'])
---
+++ compiler/visq/visqlib/QErrorComputer.py
@@ -0,0 +1,183 @@
+# Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import glob
+import numpy as np
+
+from circle_schema import circle
+from pathlib import Path
+from visqlib.Util import to_filename
+
+
+class QErrorComputer:
+    def __init__(self, fp32_dir, fq_dir):
+        self._fp32_dir = fp32_dir
+        self._fq_dir = fq_dir
+        # Assumption: FM data are saved as follows
+        #
+        # fp32_dir/
+        #   tensors.txt
+        #   <DATA_INDEX>/
+        #     <TENSOR_NAME>.npy
+        #
+        # fq_dir/
+        #   tensors.txt
+        #   <DATA_INDEX>/
+        #     <TENSOR_NAME>.npy
+        self._num_data = len(list(filter(os.path.isdir, glob.glob(fp32_dir + '/*'))))
+        if self._num_data != len(list(filter(os.path.isdir, glob.glob(fq_dir + '/*')))):
+            raise RuntimeError("Number of data mistmatches")
+
+        self._filename_to_tensor = dict()
+        with open(Path(fp32_dir) / 'tensors.txt') as f:
+            tensors = set([line.rstrip() for line in f])
+            for tensor in tensors:
+                # Check if filename is unique
+                # Fix name finding logic unless
+                assert to_filename(tensor) not in self._filename_to_tensor
+                self._filename_to_tensor[to_filename(tensor)] = tensor
+
+    def run(self):
+        '''Return qerror map (dict: tensor_name(string) -> qerror(float)).'''
+        raise NotImplementedError  # Child must implement this
+
+
+class MPEIRComputer(QErrorComputer):
+    def __init__(self, fp32_dir, fq_dir):
+        super().__init__(fp32_dir, fq_dir)
+
+    def run(self):
+        qerror_map = dict()
+        for data_idx in range(self._num_data):
+            fp32_results = glob.glob(self._fp32_dir + '/' + str(data_idx) + '/*.npy')
+            for fp32_data_path in fp32_results:
+                fp32_data = np.load(fp32_data_path)
+                p = Path(fp32_data_path)
+                fq_data_path = self._fq_dir + '/' + str(data_idx) + '/' + p.with_suffix(
+                    '.npy').name
+                fq_data = np.load(fq_data_path)
+
+                diff = np.absolute(fp32_data - fq_data).reshape(-1)
+
+                fp32_min = np.min(fp32_data.reshape(-1))
+                fp32_max = np.max(fp32_data.reshape(-1))
+
+                # Peak Error-to-Interval Ratio (PEIR)
+                #
+                # DESIGN NOTE: PEIR is an analogue of PSNR
+                PEAK_ERROR = np.max(diff)
+                INTERVAL = fp32_max - fp32_min
+
+                # If INTERVAL is 0, PEIR becomes NaN.
+                # To prevent this, relaxed PEIR with epsilon(10^(-6)) is used.
+                rPEIR = PEAK_ERROR / (INTERVAL + 0.000001)
+
+                filename = p.with_suffix('').name
+                tensor_name = self._filename_to_tensor[filename]
+                if tensor_name in qerror_map:
+                    qerror_map[tensor_name] += rPEIR
+                else:
+                    qerror_map[tensor_name] = rPEIR
+
+        for tensor_name, acc in qerror_map.items():
+            qerror_map[tensor_name] = acc / self._num_data
+
+        return qerror_map
+
+
+class MSEComputer(QErrorComputer):
+    def __init__(self, fp32_dir, fq_dir):
+        super().__init__(fp32_dir, fq_dir)
+
+    def run(self):
+        qerror_map = dict()
+        qerror_min = float('inf')
+        qerror_max = -qerror_min
+        for data_idx in range(self._num_data):
+            fp32_results = glob.glob(self._fp32_dir + '/' + str(data_idx) + '/*.npy')
+            for fp32_data_path in fp32_results:
+                fp32_data = np.load(fp32_data_path)
+                p = Path(fp32_data_path)
+                fq_data_path = self._fq_dir + '/' + str(data_idx) + '/' + p.with_suffix(
+                    '.npy').name
+                fq_data = np.load(fq_data_path)
+
+                MSE = np.square(fp32_data - fq_data).mean()
+
+                filename = p.with_suffix('').name
+                tensor_name = self._filename_to_tensor[filename]
+                if tensor_name in qerror_map:
+                    qerror_map[tensor_name] += MSE
+                else:
+                    qerror_map[tensor_name] = MSE
+
+                qerror_min = min(MSE, qerror_min)
+                qerror_max = max(MSE, qerror_max)
+
+        for tensor_name, acc in qerror_map.items():
+            qerror_map[tensor_name] = acc / self._num_data
+
+        return qerror_map, qerror_min, qerror_max
+
+
+class MPESRComputer(QErrorComputer):
+    def __init__(self, fp32_dir, fq_dir, q_circle_path):
+        super().__init__(fp32_dir, fq_dir)
+        with open(q_circle_path, 'rb') as f:
+            self._model = circle.Model.GetRootAsModel(f.read(), 0)
+
+    def run(self):
+        qerror_map = dict()
+        qerror_min = float('inf')
+        qerror_max = -qerror_min
+        for data_idx in range(self._num_data):
+            fp32_results = glob.glob(self._fp32_dir + '/' + str(data_idx) + '/*.npy')
+            for fp32_data_path in fp32_results:
+                fp32_data = np.load(fp32_data_path)
+                p = Path(fp32_data_path)
+                fq_data_path = self._fq_dir + '/' + str(data_idx) + '/' + p.with_suffix(
+                    '.npy').name
+                fq_data = np.load(fq_data_path)
+
+                diff = np.absolute(fp32_data - fq_data).reshape(-1)
+
+                # Peak Error-to-Scale Ratio (PESR)
+                PEAK_ERROR = np.max(diff)
+
+                filename = p.with_suffix('').name
+                tensor_name = self._filename_to_tensor[filename]
+                graph = self._model.Subgraphs(0)
+                for i in range(graph.TensorsLength()):
+                    tensor = graph.Tensors(i)
+                    name = tensor.Name().decode('utf-8')
+                    if name == tensor_name:
+                        assert tensor.Quantization().ScaleLength() == 1
+                        SCALE = tensor.Quantization().Scale(0)
+                        break
+
+                PESR = PEAK_ERROR / SCALE
+
+                if tensor_name in qerror_map:
+                    qerror_map[tensor_name] += PESR
+                else:
+                    qerror_map[tensor_name] = PESR
+
+                qerror_min = min(PESR, qerror_min)
+                qerror_max = max(PESR, qerror_max)
+
+        for tensor_name, acc in qerror_map.items():
+            qerror_map[tensor_name] = acc / self._num_data
+
+        return qerror_map, qerror_min, qerror_max
