4f05b8036a262366f9b09891e2ea72b10c170b64
SaeHie Park <saehie.park@gmail.com>
2022-10-24 14:36:54 +0900

    [res] Remove pbtxt option for TensorFlowPythonExamples
    This will remove pbtxt mode option for TensorFlowPythonExamples.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- res/TensorFlowPythonExamples/tfpem.py
+++ res/TensorFlowPythonExamples/tfpem.py
@@ -8,7 +8,6 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Process TensorFlow Python Examples')
 
-parser.add_argument('--mode', metavar='MODE', choices=['pbtxt'], default='pbtxt')
 parser.add_argument('examples', metavar='EXAMPLES', nargs='+')
 
 args = parser.parse_args()
@@ -17,15 +16,14 @@ output_folder = "./output/"
 
 Path(output_folder).mkdir(parents=True, exist_ok=True)
 
-if args.mode == 'pbtxt':
-    for example in args.examples:
-        print("Generate '" + example + ".pbtxt'")
+for example in args.examples:
+    print("Generate '" + example + ".pbtxt'")
 
-        tf.compat.v1.reset_default_graph()
-        # https://stackoverflow.com/questions/37808866/proper-way-to-dynamically-import-a-module-with-relative-imports
-        importlib.import_module("examples." + example)
+    tf.compat.v1.reset_default_graph()
+    # https://stackoverflow.com/questions/37808866/proper-way-to-dynamically-import-a-module-with-relative-imports
+    importlib.import_module("examples." + example)
 
-        with open(output_folder + example + ".pbtxt", "w") as f:
-            f.write(str(tf.compat.v1.get_default_graph().as_graph_def(add_shapes=True)))
+    with open(output_folder + example + ".pbtxt", "w") as f:
+        f.write(str(tf.compat.v1.get_default_graph().as_graph_def(add_shapes=True)))
 
-        print("Generate '" + example + ".pbtxt' - Done")
+    print("Generate '" + example + ".pbtxt' - Done")
