f32cb10c09833a448e6fd82946d2303085af9afd
SaeHie Park <saehie.park@gmail.com>
2022-10-24 13:35:04 +0900

    [res] Revise TF examples to output folder (#9894)
    This will revise TensorFlowPythonExamples to generate to output folder
    and ignore from git.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- res/TensorFlowPythonExamples/tfpem.py
+++ res/TensorFlowPythonExamples/tfpem.py
@@ -13,6 +13,10 @@ parser.add_argument('examples', metavar='EXAMPLES', nargs='+')
 
 args = parser.parse_args()
 
+output_folder = "./output/"
+
+Path(output_folder).mkdir(parents=True, exist_ok=True)
+
 if args.mode == 'pbtxt':
     for example in args.examples:
         print("Generate '" + example + ".pbtxt'")
@@ -21,7 +25,7 @@ if args.mode == 'pbtxt':
         # https://stackoverflow.com/questions/37808866/proper-way-to-dynamically-import-a-module-with-relative-imports
         importlib.import_module("examples." + example)
 
-        with open(example + ".pbtxt", "w") as f:
+        with open(output_folder + example + ".pbtxt", "w") as f:
             f.write(str(tf.compat.v1.get_default_graph().as_graph_def(add_shapes=True)))
 
         print("Generate '" + example + ".pbtxt' - Done")
