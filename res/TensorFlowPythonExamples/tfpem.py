52519ea3c0b4690ccf57359b71421b2fc9d78277
SaeHie Park <saehie.park@gmail.com>
2022-10-24 17:13:45 +0900

    [res] Save keras and tflite models
    This will revise TensorFlowPythonExamples to save keras and tflite model
    if possible.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- res/TensorFlowPythonExamples/tfpem.py
+++ res/TensorFlowPythonExamples/tfpem.py
@@ -7,6 +7,7 @@ import importlib
 import argparse
 
 from pathlib import Path
+from tensorflow import keras
 
 parser = argparse.ArgumentParser(description='Process TensorFlow Python Examples')
 
@@ -23,9 +24,27 @@ for example in args.examples:
 
     tf.compat.v1.reset_default_graph()
     # https://stackoverflow.com/questions/37808866/proper-way-to-dynamically-import-a-module-with-relative-imports
-    importlib.import_module("examples." + example)
+    m = importlib.import_module("examples." + example)
 
     with open(output_folder + example + ".pbtxt", "w") as f:
         f.write(str(tf.compat.v1.get_default_graph().as_graph_def(add_shapes=True)))
 
     print("Generate '" + example + ".pbtxt' - Done")
+
+    # keras sequential?
+    if hasattr(m, 'model') and isinstance(m.model, keras.Sequential):
+        print("Generate '" + example + ".h5'")
+        m.model.save(output_folder + example + ".h5")
+        print("Generate '" + example + ".h5' - Done")
+
+        # tflite export for experiments
+        converter = tf.lite.TFLiteConverter.from_keras_model(m.model)
+        converter.allow_custom_ops = True
+        converter.experimental_new_converter = True
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
+        converter._experimental_lower_tensor_list_ops = False
+
+        tflite_model = converter.convert()
+        with open(output_folder + example + ".tflite", "wb") as f:
+            f.write(tflite_model)
+        print("Generate '" + example + ".tflite' - Done")
