a7243f8fb6fcfb204b3be2234b74242b664f7c00
SaeHie Park <saehie.park@gmail.com>
2022-10-25 11:53:16 +0900

    [res] Revise LSTM_unroll (#9920)
    This will revise LSTM_unroll script
    - remove unused import
    - add refer URL
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- res/TensorFlowPythonExamples/examples/LSTM_unroll/__init__.py
+++ res/TensorFlowPythonExamples/examples/LSTM_unroll/__init__.py
@@ -1,7 +1,4 @@
 # NOTE tested with TF 2.8.0
-import tensorflow as tf
-import numpy as np
-
 from tensorflow import keras
 
 model = keras.Sequential()
@@ -9,3 +6,5 @@ shape = (4, 4)
 
 model.add(keras.layers.InputLayer(input_shape=shape, batch_size=1))
 model.add(keras.layers.LSTM(2, input_shape=shape, unroll=True))
+
+# NOTE refer https://github.com/Samsung/ONE/issues/9895#issuecomment-1288436802
