b91dd0d31fece96833055ad61c11970c5cf7c248
Jiyoung Yun <jy910.yun@samsung.com>
2022-11-10 11:37:13 +0900

    [npud] Add trix backend
    This commit implements trix backend.
    
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

---
+++ runtime/service/npud/backend/trix/TrixBackend.cc
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "TrixBackend.h"
+
+#if defined(__linux__)
+extern "C" {
+using namespace ::npud::backend::trix;
+
+TrixBackend *allocate() { return new TrixBackend(); }
+
+void deallocate(TrixBackend *trix) { delete trix; }
+}
+#endif
+
+namespace npud
+{
+namespace backend
+{
+namespace trix
+{
+
+TrixBackend::TrixBackend() : _devType(NPUCOND_TRIV2_CONN_SOCIP)
+{
+  auto coreNum = getnumNPUdeviceByType(_devType);
+  if (coreNum <= 0)
+  {
+    return;
+  }
+
+  std::vector<npudev_h> handles;
+  for (int i = 0; i < coreNum; ++i)
+  {
+    npudev_h handle;
+    if (getNPUdeviceByType(&handle, _devType, i) < 0)
+    {
+      // Note Run for all cores.
+      continue;
+    }
+    handles.emplace_back(handle);
+  }
+
+  if (handles.size() == 0)
+  {
+    return;
+  }
+
+  _dev = std::make_unique<TrixDevice>();
+  _dev->handles = std::move(handles);
+}
+
+TrixBackend::~TrixBackend()
+{
+  for (const auto &handle : _dev->handles)
+  {
+    unregisterNPUmodel_all(handle);
+    putNPUdevice(handle);
+  }
+}
+
+NpuStatus TrixBackend::getVersion(std::string &version)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::createContext(int device_fd, int priority, NpuContext **ctx)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::destroyContext(NpuContext *ctx)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::createBuffer(NpuContext *ctx, GenericBuffer *buffer)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::destroyBuffer(NpuContext *ctx, GenericBuffer *buffer)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::registerModel(NpuContext *ctx, const std::string &modelPath,
+                                     ModelID *modelId)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::unregisterModel(NpuContext *ctx, ModelID modelId)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::createRequest(NpuContext *ctx, ModelID modelId, RequestID *requestId)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::destroyRequest(NpuContext *ctx, RequestID requestId)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::setRequestData(NpuContext *ctx, RequestID requestId, InputBuffers *inputBufs,
+                                      TensorDataInfos *inputInfos, OutputBuffers *outputBufs,
+                                      TensorDataInfos *outputInfos)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+NpuStatus TrixBackend::submitRequest(NpuContext *ctx, RequestID requestId)
+{
+  // TODO Implement details
+  return NPU_STATUS_ERROR_NOT_SUPPORTED;
+}
+
+} // namespace trix
+} // namespace backend
+} // namespace npud
