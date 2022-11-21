0c1de98729f1be749bebbdba5a12f3310d4853a6
Hyeongseok Oh <hseok82.oh@samsung.com>
2022-11-21 14:54:45 +0900

    [infra/npud] Enable tizen aarch64 npud build
    This commit enables npud build on aarch64 tizen build, like armv7l, armv7hl and x86_64.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/options/options_aarch64-tizen.cmake
+++ infra/nnfw/cmake/options/options_aarch64-tizen.cmake
@@ -9,3 +9,7 @@ option(DOWNLOAD_GTEST "Download Google Test source and build Google Test" OFF)
 option(BUILD_LOGGING "Build logging runtime" OFF)
 option(GENERATE_RUNTIME_NNAPI_TESTS "Generate NNAPI operation gtest" OFF)
 option(ENVVAR_ONERT_CONFIG "Use environment variable for onert configuration" OFF)
+
+option(BUILD_NPUD "Build NPU daemon" ON)
+# Do not allow to use CONFIG option on Tizen
+option(ENVVAR_NPUD_CONFIG "Use environment variable for npud configuration" OFF)
