8008f4fcc09e44c2331e552f065d63309b2cc2b5
SaeHie Park <saehie.park@gmail.com>
2022-11-22 02:20:13 +0000

    [infra/nncc] Relocate tree-vectorize
    This will move '-ftree-vectorize' option into NEON block.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- infra/nncc/cmake/buildtool/config/config_armv7l-linux.cmake
+++ infra/nncc/cmake/buildtool/config/config_armv7l-linux.cmake
@@ -11,13 +11,13 @@ include("${CMAKE_CURRENT_LIST_DIR}/config_linux.cmake")
 set(FLAGS_COMMON ${FLAGS_COMMON}
     "-mcpu=cortex-a7"
     "-mfloat-abi=hard"
-    "-ftree-vectorize"
     "-mfp16-format=ieee"
     )
 
 if(BUILD_ARM32_NEON)
   set(FLAGS_COMMON ${FLAGS_COMMON}
       "-mfpu=neon-vfpv4"
+      "-ftree-vectorize"
       )
 else(BUILD_ARM32_NEON)
   message(STATUS "ARMv7l: NEON is disabled")
