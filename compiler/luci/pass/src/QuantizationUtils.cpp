f3bfd3e52f3752e43a161b5158f7b4e03cb5bc0e
Hyukjin Jeong <hj1.jeong@samsung.com>
2022-11-23 14:46:05 +0900

    [luci] Update minimum scale of int16 quantization (#10111)
    This updates minimum scale of int16 quantization.
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

--- compiler/luci/pass/src/QuantizationUtils.cpp
+++ compiler/luci/pass/src/QuantizationUtils.cpp
@@ -126,8 +126,8 @@ void compute_sym_scale_zp(float min, float max, float &scaling_factor, int64_t &
                      : scale_factor_from_max_side;
 
   // protect scale from being very low to avoid overflow/underflow
-  if (scaling_factor < 1e-9)
-    scaling_factor = 1e-9;
+  if (scaling_factor < 1e-8)
+    scaling_factor = 1e-8;
 
   zp = 0;
   nudged_min = static_cast<float>(qmin_double * scaling_factor);
