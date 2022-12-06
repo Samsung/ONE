7b8dfe322c15c94cef2f80543bd6b8ea714317fd
Hyeongseok Oh <hseok82.oh@samsung.com>
2022-12-06 11:07:23 +0900

    [infra/npud] Disable default build on ubuntu x64 (#10163)
    This commit disables `npud` build on ubuntu x64 default setting.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/options/options_x86_64-linux.cmake
+++ infra/nnfw/cmake/options/options_x86_64-linux.cmake
@@ -4,6 +4,3 @@
 option(BUILD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
 option(BUILD_XNNPACK "Build XNNPACK" OFF)
 option(DOWNLOAD_ARMCOMPUTE "Download ARM Compute source" OFF)
-
-option(BUILD_NPUD "Build NPU daemon" ON)
-option(ENVVAR_NPUD_CONFIG "Use environment variable for npud configuration" ON)
