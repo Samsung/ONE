d4dd6b5f37b3f8bfcf0e2dd0f8f1a81fd3231ef3
stamalakhov <112689352+stamalakhov@users.noreply.github.com>
2022-12-23 05:35:46 +0300

    [mpqsolver] Introduce Evaluator (#10234)
    This commit introduces Evaluator for the task of automatic mixed precision quantization.
    
    Signed-off-by: s.malakhov <s.malakhov@partner.samsung.com>
    
    Signed-off-by: s.malakhov <s.malakhov@partner.samsung.com>

---
+++ compiler/circle-mpqsolver/src/bisection/Evaluator.cpp
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Evaluator.h"
+
+#include <luci_interpreter/Interpreter.h>
+
+#include <dio_hdf5/HDF5Importer.h>
+
+using namespace mpqsolver::bisection;
+
+using Shape = std::vector<loco::Dimension>;
+
+namespace
+{
+
+using namespace luci;
+
+template <typename NodeT> size_t get_tensor_size(const NodeT *node)
+{
+  uint32_t tensor_size = loco::size(node->dtype());
+  for (uint32_t i = 0; i < node->rank(); ++i)
+    tensor_size *= node->dim(i).value();
+  return tensor_size;
+}
+
+WholeOutput compute_outputs(const luci::Module *module, const std::string &h5file)
+{
+  dio::hdf5::HDF5Importer importer{h5file};
+  importer.importGroup("value");
+
+  bool is_raw_data = importer.isRawData();
+
+  const auto num_records = importer.numData();
+  if (num_records == 0)
+    throw std::runtime_error("The input data file does not contain any record.");
+  const auto input_nodes = loco::input_nodes(module->graph());
+  const auto num_inputs = input_nodes.size();
+
+  WholeOutput dataset_output;
+
+  // Create interpreter.
+  luci_interpreter::Interpreter interpreter(module);
+  for (int32_t record_idx = 0; record_idx < num_records; record_idx++)
+  {
+    if (num_inputs != importer.numInputs(record_idx))
+      throw std::runtime_error("Wrong number of inputs.");
+    for (int32_t input_idx = 0; input_idx < num_inputs; input_idx++)
+    {
+      const auto *input_node = loco::must_cast<const luci::CircleInput *>(input_nodes[input_idx]);
+      assert(input_node->index() == input_idx);
+
+      std::vector<char> input_data(get_tensor_size(input_node));
+
+      if (!is_raw_data)
+      {
+        loco::DataType dtype;
+        Shape shape;
+        importer.readTensor(record_idx, input_idx, &dtype, &shape, input_data.data());
+      }
+      else
+      {
+        // Skip type/shape check for raw data
+        importer.readTensor(record_idx, input_idx, input_data.data());
+      }
+
+      interpreter.writeInputTensor(input_node, input_data.data(), input_data.size());
+    }
+
+    interpreter.interpret();
+
+    Output nn_output;
+
+    // Get output.
+    const auto output_nodes = loco::output_nodes(module->graph());
+    for (int i = 0; i < module->graph()->outputs()->size(); i++)
+    {
+      const auto *output_node = loco::must_cast<const luci::CircleOutput *>(output_nodes[i]);
+      Buffer output_data(get_tensor_size(output_node));
+      interpreter.readOutputTensor(output_node, output_data.data(), output_data.size());
+      // output
+      nn_output.push_back(output_data);
+    }
+    dataset_output.push_back(nn_output);
+  }
+
+  return dataset_output;
+}
+
+} // namespace
+
+DatasetEvaluator::DatasetEvaluator(const luci::Module *ref_module, const std::string &h5file,
+                                   const ErrorMetric &metric)
+  : _ref_module(ref_module), _h5file(h5file), _metric(&metric)
+{
+  _ref_output = compute_outputs(_ref_module, _h5file);
+}
+
+void DatasetEvaluator::validate(const luci::Module *trgt_fq_module) const
+{
+  const auto output_nodes = loco::output_nodes(trgt_fq_module->graph());
+  for (size_t out_index = 0; out_index < output_nodes.size(); ++out_index)
+  {
+    const auto *output_node = loco::must_cast<const luci::CircleOutput *>(output_nodes[out_index]);
+    loco::DataType out_dtype = output_node->dtype();
+    if (out_dtype != loco::DataType::FLOAT32)
+      throw std::runtime_error("Unsupported output dtype " + output_node->name());
+  }
+}
+
+float DatasetEvaluator::evaluate(const luci::Module *trgt_fq_module) const
+{
+  if (trgt_fq_module == nullptr)
+    throw std::runtime_error("Invalid target module");
+
+  if (_metric == nullptr)
+    throw std::runtime_error("Invalid metric");
+
+  validate(trgt_fq_module);
+
+  const WholeOutput &cur_output = compute_outputs(trgt_fq_module, _h5file);
+  float error = _metric->compute(_ref_output, cur_output);
+  return error;
+}
---
+++ compiler/circle-mpqsolver/src/bisection/Evaluator.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MPQSOLVER_BISECTION_EVALUATOR_H__
+#define __MPQSOLVER_BISECTION_EVALUATOR_H__
+
+#include "ErrorMetric.h"
+
+#include <luci/IR/Module.h>
+#include <luci/CircleQuantizer.h>
+
+#include <string>
+#include <vector>
+
+namespace mpqsolver
+{
+namespace bisection
+{
+
+class DatasetEvaluator final
+{
+public:
+  /**
+   * @brief create Evaluator for comparing output of ref_module on h5file
+   */
+  DatasetEvaluator(const luci::Module *ref_module, const std::string &h5file,
+                   const ErrorMetric &metric);
+  DatasetEvaluator() = delete;
+  ~DatasetEvaluator() = default;
+
+  /**
+   * @brief evaluate trgt_fq_module (fake-quantized)
+   * returns error-metric
+   */
+  float evaluate(const luci::Module *trgt_fq_module) const;
+
+private:
+  /**
+   * @brief throws if there is something wrong with the module
+   */
+  void validate(const luci::Module *module) const;
+
+private:
+  const luci::Module *_ref_module = nullptr;
+  std::string _h5file;
+  WholeOutput _ref_output;
+  const ErrorMetric *_metric = nullptr;
+};
+
+} // namespace bisection
+} // namespace mpqsolver
+
+#endif //__MPQSOLVER_BISECTION_EVALUATOR_H__
