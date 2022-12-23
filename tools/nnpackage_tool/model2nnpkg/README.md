2bd9fc4d2541e6c088c7c0edfc8d2d9be97df3fb
Jang Jiseob <ragmani0216@gmail.com>
2022-12-23 13:47:22 +0900

    [tools/model2nnpkg] Introduce python script (#10221)
    This commit introduces a python script to create nnpackge from models.
    
    ONE-DCO-1.0-Signed-off-by: ragmani <ragmani0216@gmail.com>

--- tools/nnpackage_tool/model2nnpkg/README.md
+++ tools/nnpackage_tool/model2nnpkg/README.md
@@ -7,17 +7,46 @@ It takes `modelfile` as input and generates `nnpackage`.
 ## Usage
 
 ```
-Usage: model2nnpkg.sh [options] modelfile
-Convert modelfile to nnpackage.
+usage:  model2nnpkg.py [options]
+  Examples:
+      model2nnpkg.py -m add.tflite                           => create nnpkg "add" in current directory
+      model2nnpkg.py -o out -m add.tflite                    => create nnpkg "add" in out/
+      model2nnpkg.py -o out -p addpkg -m add.tflite          => create nnpkg "addpkg" in out/
+      model2nnpkg.py -c add.cfg -m add.tflite                => create nnpkg "add" with add.cfg
+      model2nnpkg.py -o out -p addpkg -m a1.tflite a2.tflite => create nnpkg "addpkg" with models a1.tflite and a2.tflite in out/
+
+
+Convert model files (tflite, circle or tvn) to nnpkg.
+
+options:
+  -h, --help            show this help message and exit
+  -o output_directory, --outdir output_directory
+                        set nnpkg output directory
+  -p nnpkg_name, --nnpkg-name nnpkg_name
+                        set nnpkg output name (default=[1st modelfile name])
+  -c conf [conf ...], --config conf [conf ...]
+                        provide configuration files
+  -m model [model ...], --models model [model ...]
+                        provide model files
+```
+
+## Usage (To be deprecated)
+```
+Usage: model2nnpkg.sh [options]
+Convert modelfile (tflite, circle or tvn) to nnpackage.
 
 Options:
     -h   show this help
     -o   set nnpackage output directory (default=.)
-    -p   set nnpackage output name (default=[modelfile name])
+    -p   set nnpackage output name (default=[1st modelfile name])
+    -c   provide configuration files
+    -m   provide model files
 
 Examples:
-    model2nnpkg.sh add.tflite                  => create nnpackage 'add' in ./
-    model2nnpkg.sh -o out add.tflite           => create nnpackage 'add' in out/
-    model2nnpkg.sh -o out -p addpkg add.tflite => create nnpackage 'addpkg' in out/
+    model2nnpkg.sh -m add.tflite                                              => create nnpackage 'add' in ./
+    model2nnpkg.sh -o out -m add.tflite                                       => create nnpackage 'add' in out/
+    model2nnpkg.sh -o out -p addpkg -m add.tflite                             => create nnpackage 'addpkg' in out/
+    model2nnpkg.sh -c add.cfg -m add.tflite                                   => create nnpackage 'add' with add.cfg
+    model2nnpkg.py -o out -p addpkg -m a1.tflite a2.tflite => create nnpackage "addpkg" with models a1.tflite and a2.tflite in out/
 
 ```
---
+++ tools/nnpackage_tool/model2nnpkg/model2nnpkg.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import json
+import os
+import shutil
+
+
+def _verify_args(args):
+    if args.config and len(args.config) != len(args.models):
+        raise Exception(
+            'error: when config file is provided, # of config file should be same with modelfile\n'
+            +
+            "Please provide config file for each model file, or don't provide config file."
+        )
+
+    for i in range(len(args.models)):
+        model_path = args.models[i]
+        if not os.path.isfile(model_path):
+            raise Exception('error: modelfile does not have extension.')
+
+        modelfile = os.path.basename(model_path)
+        if len(modelfile.split('.')) == 1:
+            raise Exception(
+                'error: modelfile does not have extension.\n' +
+                "Please provide extension so that $progname can identify what type of model you use."
+            )
+
+        if args.config:
+            config_path = os.path.basename(args.config[i])
+            if not os.path.isfile(config_path):
+                raise Exception(f'error: {config_path} does not exist.')
+
+
+def _get_args():
+    parser = argparse.ArgumentParser(
+        description='Convert model files (tflite, circle or tvn) to nnpkg.',
+        usage=''' %(prog)s [options]
+  Examples:
+      %(prog)s -m add.tflite                           => create nnpkg "add" in current directory
+      %(prog)s -o out -m add.tflite                    => create nnpkg "add" in out/
+      %(prog)s -o out -p addpkg -m add.tflite          => create nnpkg "addpkg" in out/
+      %(prog)s -c add.cfg -m add.tflite                => create nnpkg "add" with add.cfg
+      %(prog)s -o out -p addpkg -m a1.tflite a2.tflite => create nnpkg "addpkg" with models a1.tflite and a2.tflite in out/
+  ''')
+    parser.add_argument(
+        '-o',
+        '--outdir',
+        type=str,
+        default=os.getcwd(),
+        metavar='output_directory',
+        help='set nnpkg output directory')
+    parser.add_argument(
+        '-p',
+        '--nnpkg-name',
+        type=str,
+        metavar='nnpkg_name',
+        help='set nnpkg output name (default=[1st modelfile name])')
+    parser.add_argument(
+        '-c',
+        '--config',
+        type=str,
+        nargs='+',
+        default='',
+        metavar='conf',
+        help='provide configuration files')
+    parser.add_argument(
+        '-m',
+        '--models',
+        type=str,
+        nargs='+',
+        metavar='model',
+        help='provide model files')
+
+    args = parser.parse_args()
+
+    _verify_args(args)
+
+    if not args.nnpkg_name:
+        first_model_name = os.path.basename(args.models[0]).rsplit('.', 1)[0]
+        args.nnpkg_name = first_model_name
+
+    args.prog = parser.prog
+
+    return args
+
+
+def _generate_manifest(args):
+    config_list = ''
+    if args.config:
+        config_list = [os.path.basename(e) for e in args.config]
+    models_list = [os.path.basename(e) for e in args.models]
+    types_list = [os.path.basename(e).rsplit('.', 1)[1] for e in args.models]
+
+    manifest = {}
+    manifest["major-version"] = "1"
+    manifest["minor-version"] = "2"
+    manifest["patch-version"] = "0"
+    manifest["configs"] = config_list
+    manifest["models"] = models_list
+    manifest["model-types"] = types_list
+
+    return manifest
+
+
+def main():
+    # parse arguments
+    args = _get_args()
+
+    print(f'{args.prog}: Generating nnpkg {args.nnpkg_name} in {args.outdir}')
+    # mkdir nnpkg directory
+    nnpkg_path = os.path.join(args.outdir, args.nnpkg_name)
+    os.makedirs(os.path.join(nnpkg_path, 'metadata'), exist_ok=True)
+
+    # dump manifest file
+    manifest = _generate_manifest(args)
+    manifest_path = os.path.join(nnpkg_path, 'metadata', 'MANIFEST')
+    with open(manifest_path, "w") as json_file:
+        json.dump(manifest, json_file, indent=2)
+
+    # copy models and configurations
+    for i in range(len(args.models)):
+        shutil.copy2(args.models[i], nnpkg_path)
+        if args.config:
+            shutil.copy2(args.config[i], os.path.join(nnpkg_path, 'metadata'))
+
+
+if __name__ == "__main__":
+    main()
