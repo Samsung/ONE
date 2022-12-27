09f67ed36da209a0c5f916200b9ed933f99ffb47
stamalakhov <112689352+stamalakhov@users.noreply.github.com>
2022-12-28 00:31:00 +0300

    [mpqsolver] Introduce BisectionSolver (#10239)
    This commit introduces BisectionSolver for the task of automatic mixed precision quantization.
    
    Signed-off-by: s.malakhov <s.malakhov@partner.samsung.com>

---
+++ compiler/circle-mpqsolver/src/MPQSolver.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MPQSOLVER_MPQSOLEVR_SOLVER_H__
+#define __MPQSOLVER_MPQSOLEVR_SOLVER_H__
+
+#include <luci/IR/Module.h>
+
+#include <memory>
+#include <string>
+
+namespace mpqsolver
+{
+
+class MPQSolver
+{
+public:
+  /**
+   * @brief construct Solver using input_data_path for .h5 file,
+   * qerror_ratio to set target qerror, and input_quantization/output_quantization to set
+   * quantization type at input/output respectively
+   */
+  MPQSolver(const std::string &input_data_path, float qerror_ratio,
+            const std::string &input_quantization, const std::string &output_quantization);
+  virtual ~MPQSolver() = default;
+
+  /**
+   * @brief run solver for recorded float module at module_path
+   */
+  virtual std::unique_ptr<luci::Module> run(const std::string &module_path) = 0;
+
+protected:
+  std::string _input_data_path;
+  std::string _input_quantization;
+  std::string _output_quantization;
+  float _qerror_ratio = 0.f; // quantization error ratio
+};
+
+} // namespace mpqsolver
+
+#endif //__MPQSOLVER_MPQSOLEVR_SOLVER_H__
---
+++ compiler/circle-mpqsolver/src/bisection/BisectionSolver.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MPQSOLVER_BISECTION_SOLVER_H__
+#define __MPQSOLVER_BISECTION_SOLVER_H__
+
+#include "Quantizer.h"
+#include "Evaluator.h"
+#include <MPQSolver.h>
+
+#include <luci/IR/Module.h>
+
+#include <memory>
+#include <string>
+
+namespace mpqsolver
+{
+namespace bisection
+{
+
+class BisectionSolver final : public MPQSolver
+{
+public:
+  /**
+   * @brief Algorithm options for running bisection algorithm
+   */
+  enum Algorithm
+  {
+    Auto, // NYI
+    ForceQ16Front,
+    ForceQ16Back,
+  };
+
+public:
+  /**
+   * @brief construct Solver using input_data_path for .h5 file,
+   * qerror_ratio to set target qerror, and input_quantization/output_quantization to set
+   * quantization type at input/output respectively
+   */
+  BisectionSolver(const std::string &input_data_path, float qerror_ratio,
+                  const std::string &input_quantization, const std::string &output_quantization);
+  BisectionSolver() = delete;
+
+  /**
+   * @brief run bisection for recorded float module at module_path
+   */
+  std::unique_ptr<luci::Module> run(const std::string &module_path) override;
+
+  /**
+   * @brief set used algorithm
+   */
+  void algorithm(Algorithm algorithm);
+
+private:
+  float evaluate(const DatasetEvaluator &evaluator, const std::string &module_path,
+                 const std::string &def_quant, LayerParams &layers);
+
+private:
+  float _qerror = 0.f; // quantization error
+  Algorithm _algorithm = Algorithm::ForceQ16Front;
+  std::unique_ptr<Quantizer> _quantizer;
+};
+
+} // namespace bisection
+} // namespace mpqsolver
+
+#endif //__MPQSOLVER_BISECTION_SOLVER_H__
