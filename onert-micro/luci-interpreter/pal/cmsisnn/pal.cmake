9e7ec8384654dfb70511dead14419d72820ca767
Balyshev Artem <43214667+BalyshevArtem@users.noreply.github.com>
2023-02-15 14:15:23 +0300

    [onert-micro] Add CMSIS-NN UnidirectionalSequenceLSTM (#10460)
    This PR adds CMSIS-NN UnidirectionalSequenceLSTM support to onert-micro.
    
    ONE-DCO-1.0-Signed-off-by: Artem Balyshev <a.balyshev@samsung.com>
    
    Co-authored-by: Artem Balyshev <a.balyshev@samsung.com>

--- onert-micro/luci-interpreter/pal/cmsisnn/pal.cmake
+++ onert-micro/luci-interpreter/pal/cmsisnn/pal.cmake
@@ -1,9 +1,9 @@
 macro(initialize_pal)
-    nnas_find_package(TensorFlowSource EXACT 2.6.0 QUIET)
-    nnas_find_package(TensorFlowGEMMLowpSource EXACT 2.6.0 QUIET)
-    nnas_find_package(TensorFlowEigenSource EXACT 2.6.0 QUIET)
-    nnas_find_package(TensorFlowRuySource EXACT 2.6.0 QUIET)
-    nnas_find_package(CMSISSource EXACT 5.8.0 QUIET)
+    nnas_find_package(TensorFlowSource EXACT 2.6.0 REQUIRED)
+    nnas_find_package(TensorFlowGEMMLowpSource EXACT 2.6.0 REQUIRED)
+    nnas_find_package(TensorFlowEigenSource EXACT 2.6.0 REQUIRED)
+    nnas_find_package(TensorFlowRuySource EXACT 2.6.0 REQUIRED)
+    nnas_find_package(CMSIS-NN EXACT 4.0.0 REQUIRED)
 
     if (NOT TensorFlowSource_FOUND)
         message(STATUS "Skipping luci-interpreter: TensorFlow not found")
@@ -30,6 +30,11 @@ macro(initialize_pal)
         return()
     endif ()
 
+    if (NOT CMSIS_NNSource_FOUND)
+        message(STATUS "Skipping luci-interpreter: CMSIS-NN not found")
+        return()
+    endif ()
+
     set(PAL_INITIALIZED TRUE)
 endmacro()
 
@@ -42,7 +47,17 @@ macro(add_pal_to_target TGT)
             "${TensorFlowSource_DIR}")
     target_include_directories(${TGT} PRIVATE ${LUCI_INTERPRETER_PAL_DIR})
 
-    file(GLOB_RECURSE PAL_SOURCES "${CMSISSource_DIR}/CMSIS/NN/Source/*.c")
+    file(GLOB_RECURSE PAL_SOURCES "${CMSIS_NNSource_DIR}/Source/ActivationFunctions/*.c"
+            "${CMSIS_NNSource_DIR}/Source/BasicMathFunctions/*.c"
+            "${CMSIS_NNSource_DIR}/Source/ConcatenationFunctions/*.c"
+            "${CMSIS_NNSource_DIR}/Source/ConvolutionFunctions/*.c"
+            "${CMSIS_NNSource_DIR}/Source/FullyConnectedFunctions/*.c"
+            "${CMSIS_NNSource_DIR}/Source/LSTMFunctions/*.c"
+            "${CMSIS_NNSource_DIR}/Source/NNSupportFunctions/*.c"
+            "${CMSIS_NNSource_DIR}/Source/PoolingFunctions/*.c"
+            "${CMSIS_NNSource_DIR}/Source/ReshapeFunctions/*.c"
+            "${CMSIS_NNSource_DIR}/Source/SoftmaxFunctions/*.c")
+
     list(APPEND PAL_SOURCES ${TensorFlowSource_DIR}/tensorflow/lite/kernels/internal/quantization_util.cc
             ${TensorFlowSource_DIR}/tensorflow/lite/kernels/internal/tensor_utils.cc
             ${TensorFlowSource_DIR}/tensorflow/lite/kernels/internal/reference/portable_tensor_utils.cc)
@@ -53,13 +68,16 @@ macro(add_pal_to_target TGT)
             "${TensorFlowGEMMLowpSource_DIR}"
             "${TensorFlowEigenSource_DIR}"
             "${TensorFlowSource_DIR}"
+            "${CMSIS_NNSource_DIR}"
     )
 
-    add_subdirectory(${CMSISSource_DIR}/CMSIS/NN ${CMAKE_CURRENT_BINARY_DIR}/CMSISNN)
+    set(CMSIS_PATH ${CMSISSource_DIR} CACHE INTERNAL "CMSIS_PATH")
+    add_subdirectory(${CMSIS_NNSource_DIR} ${CMAKE_CURRENT_BINARY_DIR}/CMSISNN)
+
     target_include_directories(luci_interpreter_cmsisnn_pal PUBLIC
-            "${CMSISSource_DIR}/CMSIS/NN/Include"
             "${CMSISSource_DIR}/CMSIS/DSP/Include"
-            "${CMSISSource_DIR}/CMSIS/Core/Include")
+            "${CMSISSource_DIR}/CMSIS/Core/Include"
+            "${CMSIS_NNSource_DIR}/Include")
 
     target_link_libraries(${TGT} PRIVATE luci_interpreter_cmsisnn_pal)
 endmacro()
