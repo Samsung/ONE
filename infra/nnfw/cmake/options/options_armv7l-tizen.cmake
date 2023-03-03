d7456a5f73ad753d5bd09bec7d42d09cac4558a1
Hyeongseok Oh <hseok82.oh@samsung.com>
2023-03-03 10:30:18 +0900

    [gbs] Disable armcl backend on tizen
    This commit disables armcl (neon, cl) backend on tizen.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/options/options_armv7l-tizen.cmake
+++ infra/nnfw/cmake/options/options_armv7l-tizen.cmake
@@ -3,6 +3,7 @@
 #
 option(BUILD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
 option(BUILD_TENSORFLOW_LITE "Build TensorFlow Lite from the downloaded source" OFF)
+option(DOWNLOAD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
 option(DOWNLOAD_NEON2SSE "Download NEON2SSE library source" OFF)
 option(DOWNLOAD_GTEST "Download Google Test source and build Google Test" OFF)
 
