631e56b07a3aaffa9a7e027131f87f05bd01a480
Sanggyu Lee <takepencil@naver.com>
2023-04-12 17:13:31 +0900

    [onert] Introduce MinMaxMap (#10623)
    It introduces MinMaxMap for recording maxmax.
    It maps {subg_idx,op_idx} to {min,max}.
    
    ONE-DCO-1.0-Signed-off-by: Sanggyu Lee <sg5.lee@samsung.com>

---
+++ runtime/onert/core/include/util/MinMaxMap.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ONERT_UTIL_MINMAX_MAP_H_
+#define __ONERT_UTIL_MINMAX_MAP_H_
+
+#include <unordered_map>
+#include <utility>
+
+namespace onert
+{
+namespace util
+{
+
+template <typename N, typename Hash = std::hash<N>> class MinMaxMap
+{
+  struct MinMaxPair
+  {
+    float data[2]; // [0] = min, [1] = max
+  };
+
+public:
+  void append(N node, float min, float max) { _minmax_map[node] = {min, max}; }
+  auto begin() const { return _minmax_map.begin(); }
+  auto end() const { return _minmax_map.end(); }
+
+private:
+  std::unordered_map<N, MinMaxPair, Hash> _minmax_map;
+};
+
+} // namespace util
+} // namespace onert
+
+#endif // __ONERT_UTIL_MINMAX_MAP_H_
