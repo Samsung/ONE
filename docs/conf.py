d60f6edd34e91878fb021ea0a571fcea1192fc6c
Hyeongseok Oh <hseok82.oh@samsung.com>
2023-04-14 17:21:10 +0900

    [onert] Update version to 1.22.1 (#10631)
    Use version 1.22.1 on runtime.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, Samsung Research & contributors'
 author = 'Samsung Research & contributors'
 
 # The full version, including alpha/beta/rc tags
-release = '1.22.0'
+release = '1.22.1'
 
 # -- General configuration ---------------------------------------------------
 
