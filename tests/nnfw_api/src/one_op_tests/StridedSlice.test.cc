b107da9a37d1e1028de0976a4d346354bca4ade7
Sanggyu Lee <takepencil@naver.com>
2023-04-14 09:20:14 +0900

    [onert] Add negative TC for StridedSlice
    It adds different type of input and output as negative tc.
    
    ONE-DCO-1.0-Signed-off-by: Sanggyu Lee <sg5.lee@samsung.com>

--- tests/nnfw_api/src/one_op_tests/StridedSlice.test.cc
+++ tests/nnfw_api/src/one_op_tests/StridedSlice.test.cc
@@ -41,3 +41,28 @@ TEST_F(GenModelTest, OneOp_StridedSlice_LastDim)
 
   SUCCEED();
 }
+
+TEST_F(GenModelTest, neg_OneOp_StridedSlice_DifferentType)
+{
+  CircleGen cgen;
+  std::vector<int32_t> begin_data{0, 3};
+  std::vector<int32_t> end_data{0, 6};
+  std::vector<int32_t> strides_data{1, 1};
+  uint32_t begin_buf = cgen.addBuffer(begin_data);
+  uint32_t end_buf = cgen.addBuffer(end_data);
+  uint32_t strides_buf = cgen.addBuffer(strides_data);
+  int input = cgen.addTensor({{1, 6}, circle::TensorType::TensorType_FLOAT32});
+  int begin = cgen.addTensor({{2}, circle::TensorType::TensorType_INT32, begin_buf});
+  int end = cgen.addTensor({{2}, circle::TensorType::TensorType_INT32, end_buf});
+  int strides = cgen.addTensor({{2}, circle::TensorType::TensorType_INT32, strides_buf});
+  int out = cgen.addTensor({{1, 3}, circle::TensorType::TensorType_INT32});
+  cgen.addOperatorStridedSlice({{input, begin, end, strides}, {out}}, 1, 1);
+  cgen.setInputsAndOutputs({input}, {out});
+
+  _context = std::make_unique<GenModelTestContext>(cgen.finish());
+  _context->addTestCase(uniformTCD<float>({{1, 2, 3, 4, 5, 6}}, {{4, 5, 6}}));
+  _context->setBackends({"acl_cl", "acl_neon", "cpu"});
+  _context->expectFailModelLoad();
+
+  SUCCEED();
+}
