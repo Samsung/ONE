2499009020542e8c3b8c5ed260522c062c994bf0
Balyshev Artem <43214667+BalyshevArtem@users.noreply.github.com>
2023-04-17 17:13:35 +0300

    [onert-micro] Support ReduceProd kernel (#10653)
    This pr supports ReduceProd kernel and adds test for this kernel.
    
    ONE-DCO-1.0-Signed-off-by: Artem Balyshev <a.balyshev@samsung.com>
    
    Co-authored-by: Artem Balyshev <a.balyshev@samsung.com>

---
+++ onert-micro/luci-interpreter/src/kernels/ReduceCommon.test.cpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "kernels/TestUtils.h"
+#include "luci_interpreter/test_models/reduce_common/ReduceProdKernel.h"
+
+#include "loader/ModuleLoader.h"
+
+namespace luci_interpreter
+{
+namespace
+{
+
+using namespace testing;
+
+class ReduceCommonTest : public ::testing::Test
+{
+  // Do nothing
+};
+
+template <typename T>
+std::vector<T> checkReduceCommonKernel(test_kernel::TestDataBase<T> *test_data_base)
+{
+  MemoryManager memory_manager{};
+  RuntimeModule runtime_module{};
+  bool dealloc_input = true;
+
+  // Load model with single op
+  auto *model_data_raw = reinterpret_cast<const char *>(test_data_base->get_model_ptr());
+  ModuleLoader::load(&runtime_module, &memory_manager, model_data_raw, dealloc_input);
+
+  auto *main_runtime_graph = runtime_module.getMainGraph();
+  assert(main_runtime_graph->getNumOfInputTensors() == 1);
+
+  // Set input data
+  {
+    auto *input_tensor_data = reinterpret_cast<T *>(main_runtime_graph->configureGraphInput(0));
+    std::copy(test_data_base->get_input_data_by_index(0).begin(),
+              test_data_base->get_input_data_by_index(0).end(), input_tensor_data);
+  }
+
+  runtime_module.execute();
+
+  assert(main_runtime_graph->getNumOfOutputTensors() == 1);
+
+  T *output_data = reinterpret_cast<T *>(main_runtime_graph->getOutputDataByIndex(0));
+  const size_t num_elements = (main_runtime_graph->getOutputDataSizeByIndex(0) / sizeof(T));
+  std::vector<T> output_data_vector(output_data, output_data + num_elements);
+  return output_data_vector;
+}
+
+TEST_F(ReduceCommonTest, Reduce_Prod_Float_P)
+{
+  test_kernel::TestDataFloatReduceProd test_data_float_reduce_prod;
+  std::vector<float> output_data_vector = checkReduceCommonKernel(&test_data_float_reduce_prod);
+  EXPECT_THAT(output_data_vector,
+              kernels::testing::FloatArrayNear(
+                test_data_float_reduce_prod.get_output_data_by_index(0), 0.0001f));
+}
+
+TEST_F(ReduceCommonTest, Reduce_Prod_Int_P)
+{
+  test_kernel::TestDataIntReduceProd test_data_int_reduce_prod;
+  std::vector<int32_t> output_data_vector = checkReduceCommonKernel(&test_data_int_reduce_prod);
+  EXPECT_THAT(output_data_vector, test_data_int_reduce_prod.get_output_data_by_index(0));
+}
+
+// TODO: add negative tests?
+
+} // namespace
+} // namespace luci_interpreter
