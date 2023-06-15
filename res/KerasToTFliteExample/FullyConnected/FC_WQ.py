ab036ee559dac42be457d908303e20f959de104b
Chunseok Lee <chunseok.lee@samsung.com>
2023-06-15 17:06:09 +0300

    Add resource for generate tflite from keras
    - add an example to genreate model with 1 FC layer(weight quantized)
    
    ONE-DCO-1.0-Signed-off-by: Chunseok Lee <chunseok.lee@samsung.com>

---
+++ res/KerasToTFliteExample/FullyConnected/FC_WQ.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow import keras
+
+model = tf.keras.models.Sequential([
+    tf.keras.Input(shape=(16, 32)),
+    tf.keras.layers.Dense(32),
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001))
+
+model.summary()
+
+run_model = tf.function(lambda x: model(x))
+
+# This is important, let's fix the input size.
+BATCH_SIZE = 1
+X = 16
+Y = 32
+concrete_func = run_model.get_concrete_function(
+    tf.TensorSpec([BATCH_SIZE, X, Y], model.inputs[0].dtype))
+
+# model directory.
+MODEL_DIR = "keras_fc"
+model.save(MODEL_DIR, save_format="tf", signatures=concrete_func)
+
+converter = tf.lite.TFLiteConverter.from_saved_model(MODEL_DIR)
+converter.experimental_new_converter = True
+converter.optimizations = [tf.lite.Optimize.DEFAULT] # For Weight Quantization(a.k.a Hybrid Quantization)
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
+                                       tf.lite.OpsSet.SELECT_TF_OPS]
+converted_model = converter.convert()
+save_to = "fc_wq.tflite"
+if save_to is not None:
+    with open(save_to, 'wb') as tf_lite_file:
+        tf_lite_file.write(converted_model)
