8979cd1b05447db9718116b04b6b4787b5ed200b
ragmani <ragmani0216@gmail.com>
2023-06-26 14:13:09 +0900

    [onert] Introduce TrainableBackendContext of buildin backend
    This commit introduces TrainableBackendContext of buildin backend.
    
    ONE-DCO-1.0-Signed-off-by: ragmani <ragmani0216@gmail.com>

---
+++ runtime/onert/core/src/backend/builtin/train/TrainableBackendContext.cc
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "TrainableBackendContext.h"
+
+#include "backend/basic/BackendContextHelpers.h"
+#include "exec/FunctionSequence.h"
+
+namespace onert
+{
+namespace backend
+{
+namespace builtin
+{
+namespace train
+{
+
+ITensorRegistry *TrainableBackendContext::genTensors()
+{
+  return basic::genTensors(*this, _tensor_builder);
+}
+
+ITensorRegistry *TrainableBackendContext::genTrainingTensors()
+{
+  genGradTensors();
+
+  // TODO Generate training-related tensors except for gradient
+
+  return grad_tensor_registry().get();
+}
+
+void TrainableBackendContext::genGradTensors()
+{
+  const ir::train::TrainableGraph &tgraph = *trainable_graph();
+  auto tensor_builder = _grad_tensor_builder;
+  auto tensor_reg = grad_tensor_registry();
+
+  tgraph.operands().iterate([&](const ir::OperandIndex &ind, const ir::Operand &) {
+    if (external_operands().contains(ind))
+      return;
+    // NOTE Assuming there is no layout changes (Always assume NHWC or UNKNOWN)
+    assert(tgraph.layout() != ir::Layout::NCHW);
+
+    // TODO Register TensorInfo that has gradient's shape
+    // ir::OperandInfo backend_info{obj.shape(), obj.typeInfo(), obj.info().memAllocType(),
+    //                              obj.isConstant()};
+    // tensor_builder->registerTensorInfo(ind, backend_info, ir::Layout::NHWC);
+  });
+
+  // TODO Plan tensor builds to reduce peak memory usage
+  tgraph.operands().iterate([&](const ir::OperandIndex &ind, const ir::Operand &) {
+    if (tensor_builder->isRegistered(ind))
+      tensor_builder->notifyFirstUse(ind);
+  });
+
+  // TODO Allocate tensors
+  // tensor_builder->allocate();
+}
+
+backend::train::FunctionMap TrainableBackendContext::genKernels()
+{
+  backend::train::FunctionMap ret;
+
+  for (auto op_ind : _tdata->op_order)
+  {
+    auto tn_seq = kernel_gen->generate(op_ind);
+    ret.emplace_back(op_ind, std::move(tn_seq));
+  }
+
+  basic::initConsts(*this);
+
+  // TODO Enable prepare()
+  // for (auto &&it : ret)
+  // {
+  //   auto &fn_seq = it.second;
+  //   fn_seq->iterate([&](exec::IFunction &ifunc) { ifunc.prepare(); });
+  // }
+
+  return ret;
+}
+
+} // namespace train
+} // namespace builtin
+} // namespace backend
+} // namespace onert
---
+++ runtime/onert/core/src/backend/builtin/train/TrainableBackendContext.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ONERT_BACKEND_BUILTIN_TRAIN_BACKEND_CONTEXT_H__
+#define __ONERT_BACKEND_BUILTIN_TRAIN_BACKEND_CONTEXT_H__
+
+#include <backend/train/TrainableBackendContext.h>
+#include "KernelGenerator.h"
+#include "../ExternalContext.h"
+#include "../TensorBuilder.h"
+
+namespace onert
+{
+namespace backend
+{
+namespace builtin
+{
+namespace train
+{
+
+class TrainableBackendContext : public backend::train::TrainableBackendContext
+{
+public:
+  TrainableBackendContext(const backend::train::ITrainableBackend *backend,
+                          std::unique_ptr<backend::train::TrainableContextData> &&data,
+                          std::shared_ptr<ITensorRegistry> tensor_registry = nullptr,
+                          std::shared_ptr<TensorBuilder> tensor_builder = nullptr,
+                          std::shared_ptr<ITensorRegistry> grad_tensor_registry = nullptr,
+                          std::shared_ptr<TensorBuilder> grad_tensor_builder = nullptr,
+                          std::shared_ptr<KernelGenerator> kernel_gen = nullptr)
+    : backend::train::TrainableBackendContext(backend, std::move(data), tensor_registry,
+                                              grad_tensor_registry),
+      kernel_gen{kernel_gen}, _external_context(new ExternalContext),
+      _tensor_builder{tensor_builder}, _grad_tensor_builder{grad_tensor_builder}
+  {
+  }
+
+  ITensorRegistry *genTensors() override;
+  ITensorRegistry *genTrainingTensors() override;
+
+private:
+  void genGradTensors();
+
+public:
+  backend::train::FunctionMap genKernels() override;
+
+  std::shared_ptr<ExternalContext> external_context() { return _external_context; }
+
+public:
+  // TODO Make it private
+  std::shared_ptr<KernelGenerator> kernel_gen;
+
+private:
+  // NOTE ruy context has a thread pool, and when multiple ruy contexts are created,
+  //      the thread pool is also created in duplicate
+  // TODO Create one ruy context for session
+  std::shared_ptr<ExternalContext> _external_context;
+
+private:
+  std::shared_ptr<TensorBuilder> _tensor_builder;
+  std::shared_ptr<TensorBuilder> _grad_tensor_builder;
+};
+
+} // namespace train
+} // namespace builtin
+} // namespace backend
+} // namespace onert
+
+#endif // __ONERT_BACKEND_BUILTIN_TRAIN_BACKEND_CONTEXT_H__
