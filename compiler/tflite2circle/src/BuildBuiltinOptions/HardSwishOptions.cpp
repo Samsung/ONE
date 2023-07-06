1da4c85e17b0aaf20c3b68c708f19c012477fa1b
Seungho Henry Park <shs.park@samsung.com>
2023-07-06 22:34:07 +0900

    [tflite2circle] Add the HardSwish operator to tflite2circle
    Signed-off-by: Seungho Henry Park <shs.park@samsung.com>

---
+++ compiler/tflite2circle/src/BuildBuiltinOptions/HardSwishOptions.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "HardSwishOptions.h"
+#include "DataLookup.h"
+
+#include <cassert>
+
+namespace tflite2circle
+{
+
+flatbuffers::Offset<circle::HardSwishOptions>
+build_circle_HardSwishOptions(flatbuffers::FlatBufferBuilder &fb, const tflite::Operator *)
+{
+  circle::HardSwishOptionsBuilder builtin_options_builder{fb};
+  return builtin_options_builder.Finish();
+}
+
+} // namespace tflite2circle
