abfa86d556768621cb9dbf8f34a81c2623381d55
Jiyoung Yun <jy910.yun@samsung.com>
2023-07-10 18:29:50 +0900

    Fix errors
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

--- tests/tools/onert_train/test/rawdataloader.test.cc
+++ tests/tools/onert_train/test/rawdataloader.test.cc
@@ -53,29 +53,35 @@ public:
     }
   }
 
-  template <typename T> const std::string &generateInputData(const std::vector<T> &data)
+  template <typename T>
+  const std::string &generateInputData(const std::vector<std::vector<T>> &data)
   {
     generateData(_input_file, data);
     return _input_file;
   }
 
-  template <typename T> const std::string &generateExpectedData(const std::vector<T> &data)
+  template <typename T>
+  const std::string &generateExpectedData(const std::vector<std::vector<T>> &data)
   {
     generateData(_expected_file, data);
     return _expected_file;
   }
 
 private:
-  template <typename T> void generateData(const std::string &name, const std::vector<T> &data)
+  template <typename T>
+  void generateData(const std::string &name, const std::vector<std::vector<T>> &data)
   {
     try
     {
       std::ofstream file(name, std::ios::binary);
-      for (uint32_t i = 0; i < _data_length; ++i)
+      for (uint32_t i = 0; i < data.size(); ++i)
       {
-        for (uint32_t j = 0; j < data.size(); ++j)
+        for (uint32_t j = 0; j < _data_length; ++j)
         {
-          file.write(reinterpret_cast<const char *>(&data[j]), sizeof(data[j]));
+          for (uint32_t k = 0; k < data[i].size(); ++k)
+          {
+            file.write(reinterpret_cast<const char *>(&data[i][k]), sizeof(data[i][k]));
+          }
         }
       }
     }
@@ -123,17 +129,29 @@ TEST_F(RawDataLoaderTest, loadDatas_1)
   };
   std::vector<nnfw_tensorinfo> expected_infos{expected_info};
 
+  EXPECT_EQ(in_infos.size(), num_input);
+  EXPECT_EQ(expected_infos.size(), num_expected);
+
   // Generate test data
-  std::vector<uint32_t> in(num_input * num_elems(&in_info));
-  std::generate(in.begin(), in.end(), [] {
-    static uint32_t i = 0;
-    return i++;
-  });
-  std::vector<uint32_t> expected(num_expected * num_elems(&expected_info));
-  std::generate(expected.begin(), expected.end(), [in] {
-    auto sum = std::accumulate(in.begin(), in.end(), 0);
-    return sum;
-  });
+  std::vector<std::vector<uint32_t>> in(num_input);
+  for (uint32_t i = 0; i < num_input; ++i)
+  {
+    in[i].resize(num_elems(&in_infos[i]));
+    std::generate(in[i].begin(), in[i].end(), [this] {
+      static uint32_t i = 0;
+      return i++;
+    });
+  }
+
+  std::vector<std::vector<uint32_t>> expected(num_expected);
+  for (uint32_t i = 0; i < num_expected; ++i)
+  {
+    expected[i].resize(num_elems(&expected_infos[i]));
+    std::generate(expected[i].begin(), expected[i].end(), [in, i] {
+      auto sum = std::accumulate(in[i].begin(), in[i].end(), 0);
+      return sum;
+    });
+  }
 
   // Generate test data file
   DataFileGenerator file_gen(data_length);
@@ -147,14 +165,14 @@ TEST_F(RawDataLoaderTest, loadDatas_1)
   {
     for (uint32_t j = 0; j < batch_size; ++j)
     {
-      expected_in[i].insert(expected_in[i].end(), in.begin(), in.end());
+      expected_in[i].insert(expected_in[i].end(), in[i].begin(), in[i].end());
     }
   }
   for (uint32_t i = 0; i < num_expected; ++i)
   {
     for (uint32_t j = 0; j < batch_size; ++j)
     {
-      expected_ex[i].insert(expected_ex[i].end(), expected.begin(), expected.end());
+      expected_ex[i].insert(expected_ex[i].end(), expected[i].begin(), expected[i].end());
     }
   }
 
@@ -196,7 +214,7 @@ TEST_F(RawDataLoaderTest, loadDatas_1)
       {
         for (uint32_t k = 0; k < num_elem; ++k)
         {
-          auto inbufs = reinterpret_cast<uint32_t *>(inputs[j].data()) + k;
+          auto inbufs = reinterpret_cast<uint32_t *>(inputs[h * batch_size + j].data()) + k;
           gen_in[h].emplace_back(*inbufs);
         }
       }
@@ -209,7 +227,7 @@ TEST_F(RawDataLoaderTest, loadDatas_1)
       {
         for (uint32_t k = 0; k < num_elem; ++k)
         {
-          auto exbufs = reinterpret_cast<uint32_t *>(expecteds[j].data()) + k;
+          auto exbufs = reinterpret_cast<uint32_t *>(expecteds[h * batch_size + j].data()) + k;
           gen_ex[h].emplace_back(*exbufs);
         }
       }
@@ -243,17 +261,29 @@ TEST_F(RawDataLoaderTest, loadDatas_2)
   };
   std::vector<nnfw_tensorinfo> expected_infos{expected_info};
 
+  EXPECT_EQ(in_infos.size(), num_input);
+  EXPECT_EQ(expected_infos.size(), num_expected);
+
   // Generate test data
-  std::vector<float> in(num_input * num_elems(&in_info));
-  std::generate(in.begin(), in.end(), [] {
-    static float i = 0.f;
-    return i += 1.1f;
-  });
-  std::vector<float> expected(num_expected * num_elems(&expected_info));
-  std::generate(expected.begin(), expected.end(), [in] {
-    auto sum = std::accumulate(in.begin(), in.end(), 0.f);
-    return sum;
-  });
+  std::vector<std::vector<float>> in(num_input);
+  for (uint32_t i = 0; i < num_input; ++i)
+  {
+    in[i].resize(num_elems(&in_infos[i]));
+    std::generate(in[i].begin(), in[i].end(), [this] {
+      static float i = 0.f;
+      return i += 1.1f;
+    });
+  }
+
+  std::vector<std::vector<float>> expected(num_expected);
+  for (uint32_t i = 0; i < num_expected; ++i)
+  {
+    expected[i].resize(num_elems(&expected_infos[i]));
+    std::generate(expected[i].begin(), expected[i].end(), [in, i] {
+      auto sum = std::accumulate(in[i].begin(), in[i].end(), 0.f);
+      return sum;
+    });
+  }
 
   // Generate test data file
   DataFileGenerator file_gen(data_length);
@@ -267,14 +297,14 @@ TEST_F(RawDataLoaderTest, loadDatas_2)
   {
     for (uint32_t j = 0; j < batch_size; ++j)
     {
-      expected_in[i].insert(expected_in[i].end(), in.begin(), in.end());
+      expected_in[i].insert(expected_in[i].end(), in[i].begin(), in[i].end());
     }
   }
   for (uint32_t i = 0; i < num_expected; ++i)
   {
     for (uint32_t j = 0; j < batch_size; ++j)
     {
-      expected_ex[i].insert(expected_ex[i].end(), expected.begin(), expected.end());
+      expected_ex[i].insert(expected_ex[i].end(), expected[i].begin(), expected[i].end());
     }
   }
 
@@ -316,7 +346,7 @@ TEST_F(RawDataLoaderTest, loadDatas_2)
       {
         for (uint32_t k = 0; k < num_elem; ++k)
         {
-          auto inbufs = reinterpret_cast<float *>(inputs[j].data()) + k;
+          auto inbufs = reinterpret_cast<float *>(inputs[h * batch_size + j].data()) + k;
           gen_in[h].emplace_back(*inbufs);
         }
       }
@@ -329,7 +359,7 @@ TEST_F(RawDataLoaderTest, loadDatas_2)
       {
         for (uint32_t k = 0; k < num_elem; ++k)
         {
-          auto exbufs = reinterpret_cast<float *>(expecteds[j].data()) + k;
+          auto exbufs = reinterpret_cast<float *>(expecteds[h * batch_size + j].data()) + k;
           gen_ex[h].emplace_back(*exbufs);
         }
       }
@@ -340,7 +370,7 @@ TEST_F(RawDataLoaderTest, loadDatas_2)
   }
 }
 
-// multiple input and output test case
+// multiple input test case
 TEST_F(RawDataLoaderTest, loadDatas_3)
 {
   const uint32_t data_length = 100;
@@ -354,26 +384,47 @@ TEST_F(RawDataLoaderTest, loadDatas_3)
     .rank = 4,
     .dims = {1, 2, 2, 2},
   };
-  std::vector<nnfw_tensorinfo> in_infos{in_info};
+  std::vector<nnfw_tensorinfo> in_infos;
+  for (uint32_t i = 0; i < num_input; ++i)
+  {
+    in_infos.emplace_back(in_info);
+  }
 
   nnfw_tensorinfo expected_info = {
     .dtype = NNFW_TYPE_TENSOR_INT32,
     .rank = 4,
     .dims = {1, 1, 1, 1},
   };
-  std::vector<nnfw_tensorinfo> expected_infos{expected_info};
+  std::vector<nnfw_tensorinfo> expected_infos;
+  for (uint32_t i = 0; i < num_expected; ++i)
+  {
+    expected_infos.emplace_back(expected_info);
+  }
+
+  EXPECT_EQ(in_infos.size(), num_input);
+  EXPECT_EQ(expected_infos.size(), num_expected);
 
   // Generate test data
-  std::vector<uint32_t> in(num_input * num_elems(&in_info));
-  std::generate(in.begin(), in.end(), [] {
-    static uint32_t i = 0;
-    return i++;
-  });
-  std::vector<uint32_t> expected(num_expected * num_elems(&expected_info));
-  std::generate(expected.begin(), expected.end(), [in] {
-    auto sum = std::accumulate(in.begin(), in.end(), 0);
-    return sum;
-  });
+  std::vector<std::vector<uint32_t>> in(num_input);
+  for (uint32_t i = 0; i < num_input; ++i)
+  {
+    in[i].resize(num_elems(&in_infos[i]));
+    std::generate(in[i].begin(), in[i].end(), [this] {
+      static uint32_t i = 0;
+      return i++;
+    });
+  }
+
+  std::vector<std::vector<uint32_t>> expected(num_expected);
+  for (uint32_t i = 0; i < num_expected; ++i)
+  {
+    expected[i].resize(num_elems(&expected_infos[i]));
+    std::generate(expected[i].begin(), expected[i].end(), [in] {
+      auto sum = std::accumulate(in[0].begin(), in[0].end(), 0);
+      sum = std::accumulate(in[1].begin(), in[1].end(), sum);
+      return sum;
+    });
+  }
 
   // Generate test data file
   DataFileGenerator file_gen(data_length);
@@ -387,14 +438,14 @@ TEST_F(RawDataLoaderTest, loadDatas_3)
   {
     for (uint32_t j = 0; j < batch_size; ++j)
     {
-      expected_in[i].insert(expected_in[i].end(), in.begin(), in.end());
+      expected_in[i].insert(expected_in[i].end(), in[i].begin(), in[i].end());
     }
   }
   for (uint32_t i = 0; i < num_expected; ++i)
   {
     for (uint32_t j = 0; j < batch_size; ++j)
     {
-      expected_ex[i].insert(expected_ex[i].end(), expected.begin(), expected.end());
+      expected_ex[i].insert(expected_ex[i].end(), expected[i].begin(), expected[i].end());
     }
   }
 
@@ -436,7 +487,7 @@ TEST_F(RawDataLoaderTest, loadDatas_3)
       {
         for (uint32_t k = 0; k < num_elem; ++k)
         {
-          auto inbufs = reinterpret_cast<uint32_t *>(inputs[j].data()) + k;
+          auto inbufs = reinterpret_cast<uint32_t *>(inputs[h * batch_size + j].data()) + k;
           gen_in[h].emplace_back(*inbufs);
         }
       }
@@ -449,7 +500,7 @@ TEST_F(RawDataLoaderTest, loadDatas_3)
       {
         for (uint32_t k = 0; k < num_elem; ++k)
         {
-          auto exbufs = reinterpret_cast<uint32_t *>(expecteds[j].data()) + k;
+          auto exbufs = reinterpret_cast<uint32_t *>(expecteds[h * batch_size + j].data()) + k;
           gen_ex[h].emplace_back(*exbufs);
         }
       }
@@ -460,6 +511,144 @@ TEST_F(RawDataLoaderTest, loadDatas_3)
   }
 }
 
+// multiple input and output test case
+TEST_F(RawDataLoaderTest, loadDatas_4)
+{
+  const uint32_t data_length = 100;
+  const uint32_t num_input = 2;
+  const uint32_t num_expected = 2;
+  const uint32_t batch_size = 16;
 
+  // Set data tensor info
+  nnfw_tensorinfo in_info = {
+    .dtype = NNFW_TYPE_TENSOR_INT32,
+    .rank = 4,
+    .dims = {1, 2, 2, 2},
+  };
+  std::vector<nnfw_tensorinfo> in_infos;
+  for (uint32_t i = 0; i < num_input; ++i)
+  {
+    in_infos.emplace_back(in_info);
+  }
+
+  nnfw_tensorinfo expected_info = {
+    .dtype = NNFW_TYPE_TENSOR_INT32,
+    .rank = 4,
+    .dims = {1, 1, 1, 1},
+  };
+  std::vector<nnfw_tensorinfo> expected_infos;
+  for (uint32_t i = 0; i < num_expected; ++i)
+  {
+    expected_infos.emplace_back(expected_info);
+  }
+
+  EXPECT_EQ(in_infos.size(), num_input);
+  EXPECT_EQ(expected_infos.size(), num_expected);
+
+  // Generate test data
+  std::vector<std::vector<uint32_t>> in(num_input);
+  for (uint32_t i = 0; i < num_input; ++i)
+  {
+    in[i].resize(num_elems(&in_infos[i]));
+    std::generate(in[i].begin(), in[i].end(), [this] {
+      static uint32_t i = 0;
+      return i++;
+    });
+  }
+
+  std::vector<std::vector<uint32_t>> expected(num_expected);
+  for (uint32_t i = 0; i < num_expected; ++i)
+  {
+    expected[i].resize(num_elems(&expected_infos[i]));
+    std::generate(expected[i].begin(), expected[i].end(), [in, i] {
+      auto sum = std::accumulate(in[i].begin(), in[i].end(), 0);
+      return sum;
+    });
+  }
+
+  // Generate test data file
+  DataFileGenerator file_gen(data_length);
+  auto &input_file = file_gen.generateInputData<uint32_t>(in);
+  auto &expected_file = file_gen.generateExpectedData<uint32_t>(expected);
+
+  // Set expected datas
+  std::vector<std::vector<uint32_t>> expected_in(num_input);
+  std::vector<std::vector<uint32_t>> expected_ex(num_expected);
+  for (uint32_t i = 0; i < num_input; ++i)
+  {
+    for (uint32_t j = 0; j < batch_size; ++j)
+    {
+      expected_in[i].insert(expected_in[i].end(), in[i].begin(), in[i].end());
+    }
+  }
+  for (uint32_t i = 0; i < num_expected; ++i)
+  {
+    for (uint32_t j = 0; j < batch_size; ++j)
+    {
+      expected_ex[i].insert(expected_ex[i].end(), expected[i].begin(), expected[i].end());
+    }
+  }
+
+  // Load test datas
+  RawDataLoader loader;
+  Generator generator =
+    loader.loadData(input_file, expected_file, in_infos, expected_infos, data_length, batch_size);
+
+  // Allocate inputs and expecteds data memory
+  std::vector<Allocation> inputs(num_input * batch_size);
+  for (uint32_t i = 0; i < num_input; ++i)
+  {
+    auto bufsz = bufsize_for(&in_infos[i]);
+    for (uint32_t j = 0; j < batch_size; ++j)
+    {
+      inputs[i * batch_size + j].alloc(bufsz);
+    }
+  }
+  std::vector<Allocation> expecteds(num_expected * batch_size);
+  for (uint32_t i = 0; i < num_expected; ++i)
+  {
+    auto bufsz = bufsize_for(&expected_infos[i]);
+    for (uint32_t j = 0; j < batch_size; ++j)
+    {
+      expecteds[i * batch_size + j].alloc(bufsz);
+    }
+  }
+
+  uint32_t num_sample = data_length / batch_size;
+  for (uint32_t i = 0; i < num_sample; ++i)
+  {
+    auto data = generator(i, inputs, expecteds);
+
+    std::vector<std::vector<uint32_t>> gen_in(num_input);
+    for (uint32_t h = 0; h < num_input; ++h)
+    {
+      auto num_elem = num_elems(&in_infos[h]);
+      for (uint32_t j = 0; j < batch_size; ++j)
+      {
+        for (uint32_t k = 0; k < num_elem; ++k)
+        {
+          auto inbufs = reinterpret_cast<uint32_t *>(inputs[h * batch_size + j].data()) + k;
+          gen_in[h].emplace_back(*inbufs);
+        }
+      }
+    }
+    std::vector<std::vector<uint32_t>> gen_ex(num_expected);
+    for (uint32_t h = 0; h < num_expected; ++h)
+    {
+      auto num_elem = num_elems(&expected_infos[h]);
+      for (uint32_t j = 0; j < batch_size; ++j)
+      {
+        for (uint32_t k = 0; k < num_elem; ++k)
+        {
+          auto exbufs = reinterpret_cast<uint32_t *>(expecteds[h * batch_size + j].data()) + k;
+          gen_ex[h].emplace_back(*exbufs);
+        }
+      }
+    }
+
+    EXPECT_EQ(gen_in, expected_in);
+    EXPECT_EQ(gen_ex, expected_ex);
+  }
+}
 
 } // namespace
