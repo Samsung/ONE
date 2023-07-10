7f95af65a77f668caf8db30a5d6f26ac0d8a90c8
Artem Balyshev <a.balyshev@samsung.com>
2023-07-10 10:13:10 +0300

    [luci/pass] Introduce GeluPatternBase
    This PR introduces GeluPatternBase class.
    
    ONE-DCO-1.0-Signed-off-by: Artem Balyshev <a.balyshev@samsung.com>

--- compiler/luci/pass/src/FuseGeluPass.cpp
+++ compiler/luci/pass/src/FuseGeluPass.cpp
@@ -33,6 +33,33 @@ namespace
 // Float comparison
 bool same(float a, float b) { return fabs(a - b) < 1e-5; }
 
+class GeluPatternBase
+{
+public:
+  GeluPatternBase(luci::CircleMul *candidate) { _pattern_last_node = candidate; }
+
+  virtual ~GeluPatternBase() = default;
+
+public:
+  virtual bool matched() = 0;
+
+protected:
+  bool matched_base();
+
+public:
+  luci::CircleNode *_ifm = nullptr;
+  luci::CircleMul *_mul_sqrt = nullptr;
+  luci::CircleCustom *_erf = nullptr;
+  luci::CircleCustomOut *_erf_out = nullptr;
+  luci::CircleAdd *_add_one = nullptr;
+  luci::CircleMul *_mul = nullptr;
+  luci::CircleMul *_mul_half = nullptr;
+  luci::CircleConst *_const_sqrt = nullptr;
+  luci::CircleConst *_const_one = nullptr;
+  luci::CircleConst *_const_half = nullptr;
+  luci::CircleMul *_pattern_last_node = nullptr;
+};
+
 /**
  * Below diagram shows Gelu pattern to fuse.
  * - Gelu(x) = 0.5 * x * (1.0 + erf(x / sqrt(2.0)))
@@ -90,6 +117,51 @@ public:
 #define CHECK_OR_FALSE(condition) \
   if (not(condition))             \
     return false;
+
+bool GeluPatternBase::matched_base()
+{
+  if (auto erf = dynamic_cast<luci::CircleCustom *>(_erf_out->input()))
+    _erf = erf;
+
+  CHECK_OR_FALSE(_erf != nullptr);
+
+  // Check erf
+  CHECK_OR_FALSE(_erf->custom_code() == "Erf");
+  CHECK_OR_FALSE(_erf->numInputs() == 1);
+  CHECK_OR_FALSE(_erf->numOutputs() == 1);
+
+  if (auto mul_sqrt = dynamic_cast<luci::CircleMul *>(_erf->inputs(0)))
+    _mul_sqrt = mul_sqrt;
+
+  CHECK_OR_FALSE(_mul_sqrt != nullptr);
+
+  CHECK_OR_FALSE(luci::fill(&_ifm, &_const_sqrt).with_commutative_args_of(_mul_sqrt));
+
+  CHECK_OR_FALSE(_mul_sqrt->x() == _ifm);
+
+  // Check Activation to be NONE
+  CHECK_OR_FALSE(_mul_sqrt->fusedActivationFunction() == luci::FusedActFunc::NONE);
+  CHECK_OR_FALSE(_add_one->fusedActivationFunction() == luci::FusedActFunc::NONE);
+  CHECK_OR_FALSE(_mul->fusedActivationFunction() == luci::FusedActFunc::NONE);
+  CHECK_OR_FALSE(_mul_half->fusedActivationFunction() == luci::FusedActFunc::NONE);
+
+  // check _const_sqrt condition
+  CHECK_OR_FALSE(_const_sqrt->dtype() == loco::DataType::FLOAT32);
+  CHECK_OR_FALSE(_const_sqrt->size<loco::DataType::FLOAT32>() == 1);
+  CHECK_OR_FALSE(::same(_const_sqrt->at<loco::DataType::FLOAT32>(0), sqrtf(0.5f)));
+
+  // check if _const_half is 0.5 (fp32)
+  CHECK_OR_FALSE(_const_half->dtype() == loco::DataType::FLOAT32);
+  CHECK_OR_FALSE(_const_half->size<loco::DataType::FLOAT32>() == 1);
+  CHECK_OR_FALSE(_const_half->at<loco::DataType::FLOAT32>(0) == 0.5);
+
+  // check _const_one condition
+  CHECK_OR_FALSE(_const_one->dtype() == loco::DataType::FLOAT32);
+  CHECK_OR_FALSE(_const_one->size<loco::DataType::FLOAT32>() == 1);
+  CHECK_OR_FALSE(_const_one->at<loco::DataType::FLOAT32>(0) == 1);
+
+  return true;
+}
 
 bool GeluPattern::matched()
 {
