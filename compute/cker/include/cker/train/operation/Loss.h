33a7f8c76acc23c360f3267604746da1e5bf8a83
Jiyoung Yun <jy910.yun@samsung.com>
2023-08-08 17:43:22 +0900

    [onert] Implement Loss MSE function for training
    This commit implement Loss MSE function for training.
    And it introduces Loss cker and implement MSE function.
    
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

---
+++ compute/cker/include/cker/train/operation/Loss.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __NNFW_CKER_TRAIN_OPERATION_LOSS_H__
+#define __NNFW_CKER_TRAIN_OPERATION_LOSS_H__
+
+#include "cker/Shape.h"
+#include "cker/eigen/Utils.h"
+
+namespace nnfw
+{
+namespace cker
+{
+namespace train
+{
+
+template <typename T>
+inline void MSE(const Shape &y_pred_shape, const T *y_pred_data, const Shape &y_true_shape,
+                const T *y_true_data, const Shape &output_shape, T *output_data)
+{
+  // TODO Consider Reduction
+  if (output_shape != Shape{1})
+    throw std::runtime_error("cker::MSE: output_shape != Shape{1}");
+  if (y_pred_shape != y_true_shape)
+    throw std::runtime_error("cker::MSE: y_pred_shape != y_true_shape");
+
+  const auto y_pred = MapAsMatrixWithLastDimAsRows(y_pred_data, y_pred_shape);
+  const auto y_true = MapAsMatrixWithLastDimAsRows(y_true_data, y_true_shape);
+
+  double squared_sum = 0.0f;
+  for (size_t c = 0; c < (size_t)y_pred.cols(); ++c)
+  {
+    for (size_t r = 0; r < (size_t)y_pred.rows(); ++r)
+    {
+      double error = y_pred.coeff(r, c) - y_true.coeff(r, c);
+      squared_sum += (error * error);
+    }
+  }
+
+  auto size = y_pred.cols() * y_pred.rows();
+  output_data[0] = (T)(squared_sum / size);
+}
+
+} // namespace train
+} // namespace cker
+} // namespace nnfw
+
+#endif // __NNFW_CKER_TRAIN_OPERATION_LOSS_H__
