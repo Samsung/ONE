acf6fad5ecf9c29439785e23c1015a58d6a84a6d
Seungho Henry Park <136296779+shs-park@users.noreply.github.com>
2023-08-11 07:59:25 +0900

    [luci/pass] Support HardSwish decomposing pass (#11230)
    Decompose the HardSwish operation to Add, Mul and Relu6 operations.
    
    ONE-DCO-1.0-Signed-off-by: Seungho Henry Park <shs.park@samsung.com>

---
+++ compiler/luci/pass/src/DecomposeHardSwishPass.cpp
@@ -0,0 +1,147 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "luci/Pass/DecomposeHardSwishPass.h"
+
+#include "helpers/NodeFiller.h"
+#include "helpers/TypeMapper.h"
+
+#include <luci/IR/CircleNodes.h>
+#include <luci/Profile/CircleNodeOrigin.h>
+
+namespace
+{
+/**
+ *  BEFORE
+ *        [CircleNode]
+ *              |
+ *              |
+ *      [CircleHardSwish]
+ *              |
+ *              |
+ *        [CircleNode]
+ *
+ *
+ *  AFTER
+ *
+ *      [CircleNode]  [CircleConst]
+ *          |    \       /
+ *          |     \     /
+ *          |   [CircleAdd]
+ *          |        |
+ *          |        |
+ *          \  [CircleRelu6] [CircleConst]
+ *           \        \        /
+ *            \        \      /
+ *             \      [CircleMul]
+ *              \       /
+ *               \     /
+ *             [CircleMul]
+ *                  |
+ *                  |
+ *             [CircleNode]
+ *
+ */
+bool decompose_hardswish(luci::CircleHardSwish *hardswish)
+{
+  if (not hardswish)
+    return false;
+
+  if (hardswish->dtype() != loco::DataType::FLOAT32)
+    return false;
+
+  auto g = hardswish->graph();
+
+  auto name = hardswish->name();
+  assert(name.length() > 0);
+
+  // Create a const for CircleAdd operation
+  auto add_const = g->nodes()->create<luci::CircleConst>();
+  add_const->shape({}); // scalar
+  add_const->dtype(loco::DataType::FLOAT32);
+  add_const->rank(0);
+  add_const->size<loco::DataType::FLOAT32>(1);
+  add_const->at<loco::DataType::FLOAT32>(0) = 3.;
+  add_const->name(name + "/Add/const");
+  luci::add_origin(add_const, luci::get_origin(hardswish));
+
+  // Create an Add operation
+  auto add = g->nodes()->create<luci::CircleAdd>();
+  add->fusedActivationFunction(luci::FusedActFunc::NONE);
+  add->x(hardswish->features());
+  add->y(add_const);
+  add->name(name + "/Add");
+  luci::add_origin(add, luci::get_origin(hardswish));
+
+  // Create a Relu6 operation
+  auto relu6 = g->nodes()->create<luci::CircleRelu6>();
+  relu6->features(add);
+  relu6->name(name + "/Relu6");
+  luci::add_origin(relu6, luci::get_origin(hardswish));
+
+  // Create a const for CircleMul operation
+  auto mul_const = g->nodes()->create<luci::CircleConst>();
+  mul_const->shape({}); // scalar
+  mul_const->dtype(loco::DataType::FLOAT32);
+  mul_const->rank(0);
+  mul_const->size<loco::DataType::FLOAT32>(1);
+  mul_const->at<loco::DataType::FLOAT32>(0) = 1. / 6.;
+  mul_const->name(name + "/Mul/const");
+  luci::add_origin(mul_const, luci::get_origin(hardswish));
+
+  // Create first Mul operation
+  auto mul1 = g->nodes()->create<luci::CircleMul>();
+  mul1->fusedActivationFunction(luci::FusedActFunc::NONE);
+  mul1->x(relu6);
+  mul1->y(mul_const);
+  mul1->name(name + "/Mul1");
+  luci::add_origin(mul1, luci::get_origin(hardswish));
+
+  // Create second Mul operation
+  auto mul2 = g->nodes()->create<luci::CircleMul>();
+  mul2->fusedActivationFunction(luci::FusedActFunc::NONE);
+  mul2->x(hardswish->features());
+  mul2->y(mul1);
+  mul2->name(name + "/Mul2");
+  luci::add_origin(mul2, luci::get_origin(hardswish));
+
+  replace(hardswish).with(mul2);
+
+  return true;
+}
+
+} // namespace
+
+namespace luci
+{
+
+bool DecomposeHardSwishPass::run(loco::Graph *g)
+{
+  bool changed = false;
+
+  for (auto node : loco::active_nodes(loco::output_nodes(g)))
+  {
+    if (auto hardswish = dynamic_cast<luci::CircleHardSwish *>(node))
+    {
+      if (decompose_hardswish(hardswish))
+        changed = true;
+    }
+  }
+
+  return changed;
+}
+
+} // namespace luci
---
+++ compiler/luci/pass/src/DecomposeHardSwishPass.test.cpp
@@ -0,0 +1,205 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "luci/Pass/DecomposeHardSwishPass.h"
+
+#include <luci/IR/CircleNodes.h>
+
+#include <gtest/gtest.h>
+
+namespace
+{
+
+/**
+ *  HardSwish graph
+ *
+ *        [CircleInput]
+ *              |
+ *              |
+ *      [CircleHardSwish]
+ *              |
+ *              |
+ *        [CircleOutput]
+ */
+struct HardSwishGraph
+{
+  loco::Graph _g;
+  luci::CircleInput *_input = nullptr;
+  luci::CircleHardSwish *_hardswish = nullptr;
+  luci::CircleOutput *_output = nullptr;
+};
+
+class DecomposeHardSwishPass : public ::testing::Test
+{
+protected:
+  void MakeGraph()
+  {
+    const int N = 1;
+    const int H = 4;
+    const int W = 4;
+    const int C = 3;
+
+    // graph input and output
+    auto graph_input = _hardswish_g._g.inputs()->create();
+    auto graph_output = _hardswish_g._g.outputs()->create();
+
+    // CircleInput
+    _hardswish_g._input = _hardswish_g._g.nodes()->create<luci::CircleInput>();
+    _hardswish_g._input->index(graph_input->index());
+    _hardswish_g._input->shape({N, H, W, C});
+    _hardswish_g._input->dtype(loco::DataType::FLOAT32);
+    _hardswish_g._input->name("input");
+
+    // CircleHardSwish
+    _hardswish_g._hardswish = _hardswish_g._g.nodes()->create<luci::CircleHardSwish>();
+    _hardswish_g._hardswish->features(_hardswish_g._input);
+    _hardswish_g._hardswish->shape({N, H, W, C});
+    _hardswish_g._hardswish->dtype(loco::DataType::FLOAT32);
+    _hardswish_g._hardswish->name("hardswish");
+
+    // CircleOutput
+    _hardswish_g._output = _hardswish_g._g.nodes()->create<luci::CircleOutput>();
+    _hardswish_g._output->index(graph_output->index());
+    _hardswish_g._output->from(_hardswish_g._hardswish);
+    _hardswish_g._output->shape({N, H, W, C});
+    _hardswish_g._output->dtype(loco::DataType::FLOAT32);
+    _hardswish_g._output->name("output");
+  }
+
+  void MakeInt32Graph()
+  {
+    const int N = 1;
+    const int H = 4;
+    const int W = 4;
+    const int C = 3;
+
+    // graph input and output
+    auto graph_input = _hardswish_int32_g._g.inputs()->create();
+    auto graph_output = _hardswish_int32_g._g.outputs()->create();
+
+    // CircleInput
+    _hardswish_int32_g._input = _hardswish_int32_g._g.nodes()->create<luci::CircleInput>();
+    _hardswish_int32_g._input->index(graph_input->index());
+    _hardswish_int32_g._input->shape({N, H, W, C});
+    _hardswish_int32_g._input->dtype(loco::DataType::S32);
+    _hardswish_int32_g._input->name("input");
+
+    // CircleHardSwish
+    _hardswish_int32_g._hardswish = _hardswish_int32_g._g.nodes()->create<luci::CircleHardSwish>();
+    _hardswish_int32_g._hardswish->features(_hardswish_int32_g._input);
+    _hardswish_int32_g._hardswish->shape({N, H, W, C});
+    _hardswish_int32_g._hardswish->dtype(loco::DataType::S32);
+    _hardswish_int32_g._hardswish->name("hardswish");
+
+    // CircleOutput
+    _hardswish_int32_g._output = _hardswish_int32_g._g.nodes()->create<luci::CircleOutput>();
+    _hardswish_int32_g._output->index(graph_output->index());
+    _hardswish_int32_g._output->from(_hardswish_int32_g._hardswish);
+    _hardswish_int32_g._output->shape({N, H, W, C});
+    _hardswish_int32_g._output->dtype(loco::DataType::S32);
+    _hardswish_int32_g._output->name("output");
+  }
+
+  virtual void SetUp()
+  {
+    MakeGraph();
+    MakeInt32Graph();
+  }
+
+protected:
+  luci::DecomposeHardSwishPass _pass;
+  HardSwishGraph _hardswish_g;
+  HardSwishGraph _hardswish_int32_g;
+};
+
+} // namespace
+
+TEST_F(DecomposeHardSwishPass, name)
+{
+  auto const name = _pass.name();
+  ASSERT_NE(nullptr, name);
+}
+
+/**
+ *  Decomposed graph looks like below.
+ *
+ *      [CircleInput]  [CircleConst]
+ *          |    \       /
+ *          |     \     /
+ *          |   [CircleAdd]
+ *          |        |
+ *          |        |
+ *          \  [CircleRelu6] [CircleConst]
+ *           \        \        /
+ *            \        \      /
+ *             \      [CircleMul]
+ *              \       /
+ *               \     /
+ *             [CircleMul]
+ *                  |
+ *                  |
+ *             [CircleOutput]
+ *
+ */
+TEST_F(DecomposeHardSwishPass, simple_test)
+{
+  auto ret = _pass.run(&_hardswish_g._g);
+  EXPECT_TRUE(ret);
+
+  auto mul2 = dynamic_cast<luci::CircleMul *>(_hardswish_g._output->from());
+  EXPECT_NE(nullptr, mul2);
+
+  auto input2 = dynamic_cast<luci::CircleInput *>(mul2->x());
+  EXPECT_NE(nullptr, input2);
+
+  auto mul1 = dynamic_cast<luci::CircleMul *>(mul2->y());
+  EXPECT_NE(nullptr, mul1);
+
+  auto relu6 = dynamic_cast<luci::CircleRelu6 *>(mul1->x());
+  EXPECT_NE(nullptr, relu6);
+
+  auto mul_const = dynamic_cast<luci::CircleConst *>(mul1->y());
+  EXPECT_NE(nullptr, mul_const);
+  EXPECT_FLOAT_EQ(1. / 6., mul_const->at<loco::DataType::FLOAT32>(0));
+
+  auto add = dynamic_cast<luci::CircleAdd *>(relu6->features());
+  EXPECT_NE(nullptr, add);
+
+  auto input1 = dynamic_cast<luci::CircleInput *>(add->x());
+  EXPECT_NE(nullptr, input1);
+
+  auto add_const = dynamic_cast<luci::CircleConst *>(add->y());
+  EXPECT_NE(nullptr, add_const);
+  EXPECT_FLOAT_EQ(3., add_const->at<loco::DataType::FLOAT32>(0));
+}
+
+TEST_F(DecomposeHardSwishPass, check_last_node)
+{
+  auto ret = _pass.run(&_hardswish_g._g);
+  EXPECT_TRUE(ret);
+
+  auto hardswish = dynamic_cast<luci::CircleHardSwish *>(_hardswish_g._output->from());
+  EXPECT_EQ(nullptr, hardswish);
+}
+
+TEST_F(DecomposeHardSwishPass, wrong_condition_NEG)
+{
+  auto ret = _pass.run(&_hardswish_int32_g._g);
+  EXPECT_FALSE(ret);
+
+  auto hardswish = dynamic_cast<luci::CircleHardSwish *>(_hardswish_g._output->from());
+  EXPECT_NE(nullptr, hardswish);
+}
