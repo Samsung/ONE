940eaec91ec845fdd0624bc7b420753a2ac375cc
blue4683 <an4683@gmail.com>
2023-09-18 16:29:16 +0900

    [oenrt] bind nnfw.h with python
    This commit binds nnfw.h with python
    Added source code and header file related to nnfw_api_pybind
    
    ONE-DCO-1.0-Signed-off-by: blue4683 an4683@gmail.com

--- infra/nnfw/cmake/packages/Pybind11Config.cmake
+++ infra/nnfw/cmake/packages/Pybind11Config.cmake
@@ -17,30 +17,3 @@ if(${DOWNLOAD_PYBIND11})
   set(Pybind11_FOUND TRUE)
   return()
 endif(${DOWNLOAD_PYBIND11})
-### Find and use pre-installed Pybind11
-# if(NOT Pybind11_FOUND)
-#   # Reset package config directory cache to prevent recursive find
-#   unset(Pybind11_DIR CACHE)
-#   find_package(Pybind11)
-# endif(NOT Pybind11_FOUND)
-
-# if(${Pybind11_FOUND})
-#   if(NOT TARGET pybind11)
-#     add_library(pybind11 INTERFACE)
-#     target_include_directories(pybind11 INTERFACE ${Pybind11_INCLUDE_DIRS})
-#   endif(NOT TARGET pybind11)
-
-#   # Additional setup or libraries can be added if necessary
-
-#   set(Pybind11_FOUND TRUE)
-# else(${Pybind11_FOUND})
-#   find_path(Pybind11_INCLUDE_DIR NAMES pybind11.h)
-#   if(Pybind11_INCLUDE_DIR)
-#     if(NOT TARGET pybind11)
-#       add_library(pybind11 INTERFACE)
-#       target_include_directories(pybind11 INTERFACE ${Pybind11_INCLUDE_DIRS})
-#     endif(NOT TARGET pybind11)
-
-#     set(Pybind11_FOUND TRUE)
-#   endif(Pybind11_INCLUDE_DIR)
-# endif(${Pybind11_FOUND})
