fdbdbd1a39c56911f5e8f7bfc9d45a833e6c2a96
KangInyeong <kiyoog02@gmail.com>
2023-09-21 14:17:45 -0700

    feat: packaging for nnfw api modules

---
+++ infra/nnfw/python/nnfwapi/libnnfw_api_pybind.py
@@ -0,0 +1,22 @@
+import platform
+import os
+import shutil
+
+#__all__ = ['libnnfw_api_pybind']
+# check each architecture
+architecture = platform.machine()
+
+if architecture == 'x86_64':
+    from .x86_64 import libnnfw_api_pybind
+elif architecture == 'armv7l':
+    from .armv7l import libnnfw_api_pybind
+elif architecture == 'aarch64':
+    from .aarch64 import libnnfw_api_pybind
+else:
+    raise ImportError(f"Unsupported architecture: {architecture}")
+
+def nnfw_session():
+    return libnnfw_api_pybind.nnfw_session
+
+def tensorinfo():
+    return libnnfw_api_pybind.nnfw_tensorinfo()
