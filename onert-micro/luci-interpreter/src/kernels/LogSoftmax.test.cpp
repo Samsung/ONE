9eddf162c05abfe72d61d22e3f5c21f28d8196cf
Artem Balyshev <a.balyshev@samsung.com>
2023-09-29 12:20:04 +0300

    [onert-micro] Add float LogSoftmax kernels
    This commit adds float LogSoftmax kernels for onert-micro.
    
    ONE-DCO-1.0-Signed-off-by: Artem Balyshev <a.balyshev@samsung.com>

--- onert-micro/luci-interpreter/src/kernels/LogSoftmax.test.cpp
+++ onert-micro/luci-interpreter/src/kernels/LogSoftmax.test.cpp
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2020 Samsung Electronics Co., Ltd. All Rights Reserved
- * Copyright 2017 The TensorFlow Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,13 +14,13 @@
  * limitations under the License.
  */
 
-#include "kernels/LogSoftmax.h"
 #include "kernels/TestUtils.h"
-#include "luci_interpreter/TestMemoryManager.h"
+#include "luci_interpreter/test_models/log_softmax/FloatLogSoftmaxKernel.h"
+#include "luci_interpreter/test_models/log_softmax/NegLogSoftmaxKernel.h"
+
+#include "loader/ModuleLoader.h"
 
 namespace luci_interpreter
-{
-namespace kernels
 {
 namespace
 {
@@ -30,95 +29,59 @@ using namespace testing;
 
 class LogSoftmaxTest : public ::testing::Test
 {
-protected:
-  void SetUp() override { _memory_manager = std::make_unique<TestMemoryManager>(); }
-
-  std::unique_ptr<IMemoryManager> _memory_manager;
+  // Do nothing
 };
 
-TEST_F(LogSoftmaxTest, Float)
+template <typename T>
+std::vector<T> checkLogSoftmaxKernel(test_kernel::TestDataBase<T> *test_data_base)
 {
-  Shape input_shape{2, 4};
-  std::vector<float> input_data{
-    0, -6, 2,  4, //
-    3, -2, 10, 1, //
-  };
-  Tensor input_tensor =
-    makeInputTensor<DataType::FLOAT32>(input_shape, input_data, _memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::FLOAT32);
+  MemoryManager memory_manager{};
+  RuntimeModule runtime_module{};
+  bool dealloc_input = true;
 
-  LogSoftmax kernel(&input_tensor, &output_tensor);
-  kernel.configure();
-  _memory_manager->allocate_memory(output_tensor);
-  kernel.execute();
+  // Load model with single op
+  auto *model_data_raw = reinterpret_cast<const char *>(test_data_base->get_model_ptr());
+  ModuleLoader::load(&runtime_module, &memory_manager, model_data_raw, dealloc_input);
 
-  std::vector<float> ref_output_data{
-    -4.14297, -10.14297, -2.14297,   -.142971, //
-    -7.00104, -12.00104, -.00104087, -9.00104, //
-  };
-  EXPECT_THAT(extractTensorData<float>(output_tensor), FloatArrayNear(ref_output_data));
+  auto *main_runtime_graph = runtime_module.getMainGraph();
+  assert(main_runtime_graph->getNumOfInputTensors() == 1);
+
+  // Set input data
+  {
+    auto *input_tensor_data = reinterpret_cast<T *>(main_runtime_graph->configureGraphInput(0));
+    std::copy(test_data_base->get_input_data_by_index(0).begin(),
+              test_data_base->get_input_data_by_index(0).end(), input_tensor_data);
+  }
+
+  runtime_module.execute();
+
+  assert(main_runtime_graph->getNumOfOutputTensors() == 1);
+
+  T *output_data = reinterpret_cast<T *>(main_runtime_graph->getOutputDataByIndex(0));
+  const size_t num_elements = (main_runtime_graph->getOutputDataSizeByIndex(0) / sizeof(T));
+  std::vector<T> output_data_vector(output_data, output_data + num_elements);
+  return output_data_vector;
 }
 
-TEST_F(LogSoftmaxTest, Uint8)
+TEST_F(LogSoftmaxTest, Float_P)
 {
-  float kMin = -10;
-  float kMax = 10;
-  float kLogSoftmaxQuantizedTolerance = 16. / 256;
-  std::pair<float, int32_t> quant_param = quantizationParams<uint8_t>(kMin, kMax);
-  std::vector<float> input_data{
-    0, -6, 2,  4, //
-    3, -2, 10, 1, //
-  };
-  Tensor input_tensor = makeInputTensor<DataType::U8>({2, 4}, quant_param.first, quant_param.second,
-                                                      input_data, _memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::U8, 16. / 256, 255);
-
-  LogSoftmax kernel(&input_tensor, &output_tensor);
-  kernel.configure();
-  _memory_manager->allocate_memory(output_tensor);
-  kernel.execute();
-
-  std::vector<float> ref_output_data{
-    -4.14297, -10.14297, -2.14297,   -.142971, //
-    -7.00104, -12.00104, -.00104087, -9.00104, //
-  };
-  std::vector<int32_t> ref_output_shape{2, 4};
-  EXPECT_THAT(dequantizeTensorData(output_tensor),
-              FloatArrayNear(ref_output_data, kLogSoftmaxQuantizedTolerance));
-  EXPECT_THAT(extractTensorShape(output_tensor), ::testing::ElementsAreArray(ref_output_shape));
-  EXPECT_THAT(extractTensorData<uint8_t>(output_tensor),
-              ::testing::ElementsAreArray({189, 93, 221, 253, 142, 63, 255, 111}));
+  test_kernel::TestDataFloatLogSoftmax test_data_kernel;
+  std::vector<float> output_data_vector = checkLogSoftmaxKernel(&test_data_kernel);
+  EXPECT_THAT(output_data_vector, test_data_kernel.get_output_data_by_index(0));
 }
 
-TEST_F(LogSoftmaxTest, InvalidInputOutputType_NEG)
+TEST_F(LogSoftmaxTest, Input_output_type_mismatch_NEG)
 {
-  std::vector<float> input_data{
-    0, -6, 2,  4, //
-    3, -2, 10, 1, //
-  };
-  Tensor input_tensor =
-    makeInputTensor<DataType::FLOAT32>({2, 4}, input_data, _memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::U8, 16. / 256, 255);
+  test_kernel::NegTestDataInputOutputTypeMismatchLogSoftmaxKernel test_data_kernel;
 
-  LogSoftmax kernel(&input_tensor, &output_tensor);
-  EXPECT_ANY_THROW(kernel.configure());
-}
-
-TEST_F(LogSoftmaxTest, InvalidOutputQuantParam_NEG)
-{
-  std::pair<float, int32_t> quant_param = quantizationParams<uint8_t>(-10, 10);
-  std::vector<float> input_data{
-    0, -6, 2,  4, //
-    3, -2, 10, 1, //
-  };
-  Tensor input_tensor = makeInputTensor<DataType::U8>({2, 4}, quant_param.first, quant_param.second,
-                                                      input_data, _memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::U8, 20. / 256, 255);
-
-  LogSoftmax kernel(&input_tensor, &output_tensor);
-  EXPECT_ANY_THROW(kernel.configure());
+  MemoryManager memory_manager{};
+  RuntimeModule runtime_module{};
+  bool dealloc_input = true;
+  // Load model with single op
+  auto *model_data_raw = reinterpret_cast<const char *>(test_data_kernel.get_model_ptr());
+  EXPECT_DEATH(ModuleLoader::load(&runtime_module, &memory_manager, model_data_raw, dealloc_input),
+               "");
 }
 
 } // namespace
-} // namespace kernels
 } // namespace luci_interpreter
