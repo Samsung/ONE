230c3300f5fdeaa6aa74149da3ee377d9a34a6ce
Balyshev Artem <43214667+BalyshevArtem@users.noreply.github.com>
2023-10-03 12:55:01 +0300

    [onert-micro] Add float Cast kernels (#11632)
    This commit adds float Cast kernels for onert-micro.
    
    ONE-DCO-1.0-Signed-off-by: Artem Balyshev <a.balyshev@samsung.com>
    
    Co-authored-by: Artem Balyshev <a.balyshev@samsung.com>

--- onert-micro/luci-interpreter/src/kernels/Cast.test.cpp
+++ onert-micro/luci-interpreter/src/kernels/Cast.test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd. All Rights Reserved
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,228 +14,73 @@
  * limitations under the License.
  */
 
-#include "kernels/Cast.h"
 #include "kernels/TestUtils.h"
-#include "luci_interpreter/TestMemoryManager.h"
+#include "luci_interpreter/test_models/cast/FloatCastKernel.h"
+#include "luci_interpreter/test_models/cast/NegCastKernel.h"
+
+#include "loader/ModuleLoader.h"
 
 namespace luci_interpreter
-{
-namespace kernels
 {
 namespace
 {
 
 using namespace testing;
 
-template <typename T1, typename T2>
-void Check(std::initializer_list<int32_t> shape, std::initializer_list<T1> input_data,
-           std::initializer_list<T2> output_data)
+class CastTest : public ::testing::Test
 {
-  std::unique_ptr<IMemoryManager> memory_manager = std::make_unique<TestMemoryManager>();
-  constexpr DataType input_type = getElementType<T1>();
-  constexpr DataType output_type = getElementType<T2>();
-
-  Tensor input_tensor = makeInputTensor<input_type>(shape, input_data, memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(output_type);
-
-  Cast kernel(&input_tensor, &output_tensor);
-  kernel.configure();
-  memory_manager->allocate_memory(output_tensor);
-  kernel.execute();
-
-  EXPECT_THAT(extractTensorData<T2>(output_tensor), ::testing::ElementsAreArray(output_data));
-  EXPECT_THAT(extractTensorShape(output_tensor), shape);
-}
-
-template <typename T>
-void CheckBoolTo(std::initializer_list<int32_t> shape, std::initializer_list<bool> input_data,
-                 std::initializer_list<T> output_data)
-{
-  std::unique_ptr<IMemoryManager> memory_manager = std::make_unique<TestMemoryManager>();
-  constexpr DataType input_type = loco::DataType::BOOL;
-  constexpr DataType output_type = getElementType<T>();
-  std::vector<typename DataTypeImpl<input_type>::Type> input_data_converted;
-  for (auto elem : input_data)
-  {
-    input_data_converted.push_back(elem);
-  }
-
-  Tensor input_tensor =
-    makeInputTensor<input_type>(shape, input_data_converted, memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(output_type);
-
-  Cast kernel(&input_tensor, &output_tensor);
-  kernel.configure();
-  memory_manager->allocate_memory(output_tensor);
-  kernel.execute();
-
-  EXPECT_THAT(extractTensorData<T>(output_tensor), ::testing::ElementsAreArray(output_data));
-  EXPECT_THAT(extractTensorShape(output_tensor), shape);
-}
-
-template <typename T> class CastTest : public ::testing::Test
-{
+  // Do nothing
 };
 
-using IntDataTypes =
-  ::testing::Types<uint8_t, uint16_t, uint32_t, uint64_t, int8_t, int16_t, int32_t, int64_t>;
-TYPED_TEST_SUITE(CastTest, IntDataTypes);
-
-TYPED_TEST(CastTest, FloatToInt)
+template <typename T, typename U>
+std::vector<U> checkCastKernel(test_kernel::TestDataBase<T, U> *test_data_base)
 {
-  Check<float, TypeParam>(/*shape=*/{1, 1, 1, 4},
-                          /*input_data=*/
-                          {
-                            1.0f, 9.0f, 7.0f, 3.0f, //
-                          },
-                          /*output_data=*/
-                          {
-                            1, 9, 7, 3, //
-                          });
-  SUCCEED();
+  MemoryManager memory_manager{};
+  RuntimeModule runtime_module{};
+  bool dealloc_input = true;
+
+  // Load model with single op
+  auto *model_data_raw = reinterpret_cast<const char *>(test_data_base->get_model_ptr());
+  ModuleLoader::load(&runtime_module, &memory_manager, model_data_raw, dealloc_input);
+
+  auto *main_runtime_graph = runtime_module.getMainGraph();
+  assert(main_runtime_graph->getNumOfInputTensors() == 1);
+
+  // Set input data
+  {
+    auto *input_tensor_data = reinterpret_cast<T *>(main_runtime_graph->configureGraphInput(0));
+    std::copy(test_data_base->get_input_data_by_index(0).begin(),
+              test_data_base->get_input_data_by_index(0).end(), input_tensor_data);
+  }
+
+  runtime_module.execute();
+
+  assert(main_runtime_graph->getNumOfOutputTensors() == 1);
+
+  U *output_data = reinterpret_cast<U *>(main_runtime_graph->getOutputDataByIndex(0));
+  const size_t num_elements = (main_runtime_graph->getOutputDataSizeByIndex(0) / sizeof(U));
+  std::vector<U> output_data_vector(output_data, output_data + num_elements);
+  return output_data_vector;
 }
 
-TYPED_TEST(CastTest, IntToFloat)
+TEST_F(CastTest, Float_P)
 {
-  Check<TypeParam, float>(/*shape=*/{1, 1, 1, 4},
-                          /*input_data=*/
-                          {
-                            1, 9, 7, 3, //
-                          },
-                          /*output_data=*/
-                          {
-                            1.0f, 9.0f, 7.0f, 3.0f, //
-                          });
-  SUCCEED();
+  test_kernel::TestDataFloatCast test_data_kernel;
+  std::vector<int32_t> output_data_vector = checkCastKernel(&test_data_kernel);
+  EXPECT_THAT(output_data_vector, test_data_kernel.get_output_data_by_index(0));
 }
 
-template <typename T1, typename T2> void check_int()
+TEST_F(CastTest, Input_output_type_mismatch_NEG)
 {
-  Check<T1, T2>(/*shape=*/{1, 1, 1, 4},
-                /*input_data=*/
-                {
-                  1, 9, 7, 3, //
-                },
-                /*output_data=*/
-                {
-                  1, 9, 7, 3, //
-                });
-  SUCCEED();
-}
-
-TYPED_TEST(CastTest, IntToInt)
-{
-  check_int<TypeParam, uint8_t>();
-  check_int<TypeParam, uint16_t>();
-  check_int<TypeParam, uint32_t>();
-  check_int<TypeParam, uint64_t>();
-  check_int<TypeParam, int8_t>();
-  check_int<TypeParam, int16_t>();
-  check_int<TypeParam, int32_t>();
-  check_int<TypeParam, int64_t>();
-  SUCCEED();
-}
-
-TYPED_TEST(CastTest, IntToBool)
-{
-  Check<TypeParam, bool>(/*shape=*/{1, 1, 1, 4},
-                         /*input_data=*/
-                         {
-                           1, 0, 7, 0, //
-                         },
-                         /*output_data=*/
-                         {
-                           true, false, true, false, //
-                         });
-  SUCCEED();
-}
-
-TYPED_TEST(CastTest, BoolToInt)
-{
-  CheckBoolTo<TypeParam>(/*shape=*/{1, 1, 1, 4},
-                         /*input_data=*/
-                         {
-                           true, false, false, true, //
-                         },
-                         /*output_data=*/
-                         {
-                           1, 0, 0, 1, //
-                         });
-  SUCCEED();
-}
-
-TEST(CastTest, FloatToBool)
-{
-  Check<float, bool>(/*shape=*/{1, 1, 1, 4},
-                     /*input_data=*/
-                     {
-                       1.0f, 0.0f, 7.0f, 0.0f, //
-                     },
-                     /*output_data=*/
-                     {
-                       true, false, true, false, //
-                     });
-  SUCCEED();
-}
-
-TEST(CastTest, BoolToFloat)
-{
-  CheckBoolTo<float>(/*shape=*/{1, 1, 1, 4},
-                     /*input_data=*/
-                     {
-                       true, false, false, true, //
-                     },
-                     /*output_data=*/
-                     {
-                       1.0f, 0.0f, 0.0f, 1.0f, //
-                     });
-  SUCCEED();
-}
-
-TEST(CastTest, FloatToFloat)
-{
-  Check<float, float>(/*shape=*/{1, 1, 1, 4},
-                      /*input_data=*/
-                      {
-                        1.0f, 0.0f, 7.0f, 0.0f, //
-                      },
-                      /*output_data=*/
-                      {
-                        1.0f, 0.0f, 7.0f, 0.0f, //
-                      });
-  SUCCEED();
-}
-
-TEST(CastTest, BoolToBool)
-{
-  CheckBoolTo<bool>(/*shape=*/{1, 1, 1, 4},
-                    /*input_data=*/
-                    {
-                      true, true, false, false, //
-                    },
-                    /*output_data=*/
-                    {
-                      true, true, false, false, //
-                    });
-  SUCCEED();
-}
-
-TEST(CastTest, UnsupportedType_NEG)
-{
-  std::unique_ptr<IMemoryManager> memory_manager = std::make_unique<TestMemoryManager>();
-  Tensor input_tensor = makeInputTensor<DataType::FLOAT32>({1, 1, 2, 4},
-                                                           {
-                                                             1, 2, 7, 8, //
-                                                             1, 9, 7, 3, //
-                                                           },
-                                                           memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::Unknown);
-
-  Cast kernel(&input_tensor, &output_tensor);
-  EXPECT_ANY_THROW(kernel.configure());
-  SUCCEED();
+  test_kernel::NegTestDataInputOutputShapeMismatchCeilKernel test_data_kernel;
+  MemoryManager memory_manager{};
+  RuntimeModule runtime_module{};
+  bool dealloc_input = true;
+  // Load model with single op
+  auto *model_data_raw = reinterpret_cast<const char *>(test_data_kernel.get_model_ptr());
+  EXPECT_DEATH(ModuleLoader::load(&runtime_module, &memory_manager, model_data_raw, dealloc_input),
+               "");
 }
 
 } // namespace
-} // namespace kernels
 } // namespace luci_interpreter
