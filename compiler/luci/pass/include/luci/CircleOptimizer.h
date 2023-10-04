65640c05d9c9e0815fa8d29baf7654ee6e635044
Hyukjin Jeong <hj1.jeong@samsung.com>
2023-10-04 17:31:35 +0900

    [luci] Add ReplaceWithFCGeluFC algorithm (#11640)
    This adds ReplaceWithFCGeluFC algorithm to CircleOptimizer.
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

--- compiler/luci/pass/include/luci/CircleOptimizer.h
+++ compiler/luci/pass/include/luci/CircleOptimizer.h
@@ -69,6 +69,7 @@ public:
       ReplaceMulAddWithDepthwiseConv,
       ReplaceNonConstFCWithBatchMatMul,
       ReplaceSubWithAdd,
+      ReplaceWithFCGeluFC,
       SubstitutePackToReshape,
       SubstitutePadV2ToPad,
       SubstituteSplitVToSplit,
