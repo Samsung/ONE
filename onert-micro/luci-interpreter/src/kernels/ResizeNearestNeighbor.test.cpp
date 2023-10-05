a7188e0555e923e27b7bbd103976dd9b5f38a4f5
Artem Balyshev <a.balyshev@samsung.com>
2023-10-05 14:46:58 +0300

    [onert-micro] Add float ResizeNearestNeighbor kernels
    This commit adds float ResizeNearestNeighbor kernels for onert-micro.
    
    ONE-DCO-1.0-Signed-off-by: Artem Balyshev <a.balyshev@samsung.com>

--- onert-micro/luci-interpreter/src/kernels/ResizeNearestNeighbor.test.cpp
+++ onert-micro/luci-interpreter/src/kernels/ResizeNearestNeighbor.test.cpp
@@ -15,217 +15,74 @@
  * limitations under the License.
  */
 
-#include "kernels/ResizeNearestNeighbor.h"
 #include "kernels/TestUtils.h"
-#include "luci_interpreter/TestMemoryManager.h"
+#include "luci_interpreter/test_models/resize_nearest_neighbor/FloatResizeNearestNeighborKernel.h"
+#include "luci_interpreter/test_models/resize_nearest_neighbor/NegResizeNearestNeighborKernel.h"
+
+#include "loader/ModuleLoader.h"
 
 namespace luci_interpreter
-{
-namespace kernels
 {
 namespace
 {
 
 using namespace testing;
 
-template <typename T>
-void Check(std::initializer_list<int32_t> input_shape, std::initializer_list<int32_t> size_shape,
-           std::initializer_list<int32_t> output_shape, std::initializer_list<float> input_data,
-           std::initializer_list<int32_t> size_data, std::initializer_list<float> output_data,
-           bool align_corners, bool half_pixel_centers)
+class ResizeNearestNeighborTest : public ::testing::Test
 {
-  std::unique_ptr<IMemoryManager> memory_manager = std::make_unique<TestMemoryManager>();
-
-  Tensor input_tensor =
-    makeInputTensor<DataType::FLOAT32>(input_shape, input_data, memory_manager.get());
-  Tensor size_tensor = makeInputTensor<DataType::S32>(size_shape, size_data, memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::FLOAT32);
-
-  ResizeNearestNeighborParams params{};
-  params.align_corners = align_corners;
-  params.half_pixel_centers = half_pixel_centers;
-
-  ResizeNearestNeighbor kernel(&input_tensor, &size_tensor, &output_tensor, params);
-  kernel.configure();
-  memory_manager->allocate_memory(output_tensor);
-  kernel.execute();
-
-  EXPECT_THAT(extractTensorShape(output_tensor), ::testing::ElementsAreArray(output_shape));
-  EXPECT_THAT(extractTensorData<T>(output_tensor), FloatArrayNear(output_data));
-}
-
-template <>
-void Check<uint8_t>(std::initializer_list<int32_t> input_shape,
-                    std::initializer_list<int32_t> size_shape,
-                    std::initializer_list<int32_t> output_shape,
-                    std::initializer_list<float> input_data,
-                    std::initializer_list<int32_t> size_data,
-                    std::initializer_list<float> output_data, bool align_corners,
-                    bool half_pixel_centers)
-{
-  std::unique_ptr<IMemoryManager> memory_manager = std::make_unique<TestMemoryManager>();
-
-  std::pair<float, int32_t> quant_param =
-    quantizationParams<uint8_t>(std::min(input_data) < 0 ? std::min(input_data) : 0.f,
-                                std::max(input_data) > 0 ? std::max(input_data) : 0.f);
-  Tensor input_tensor = makeInputTensor<DataType::U8>(
-    input_shape, quant_param.first, quant_param.second, input_data, memory_manager.get());
-  Tensor size_tensor = makeInputTensor<DataType::S32>(size_shape, size_data, memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::U8, quant_param.first, quant_param.first);
-
-  ResizeNearestNeighborParams params{};
-  params.align_corners = align_corners;
-  params.half_pixel_centers = half_pixel_centers;
-
-  ResizeNearestNeighbor kernel(&input_tensor, &size_tensor, &output_tensor, params);
-  kernel.configure();
-  memory_manager->allocate_memory(output_tensor);
-  kernel.execute();
-
-  EXPECT_THAT(extractTensorShape(output_tensor), ::testing::ElementsAreArray(output_shape));
-  EXPECT_THAT(dequantizeTensorData(output_tensor),
-              FloatArrayNear(output_data, output_tensor.scale()));
-}
-
-template <typename T> class ResizeNearestNeighborTest : public ::testing::Test
-{
+  // Do nothing
 };
 
-using DataTypes = ::testing::Types<float, uint8_t>;
-TYPED_TEST_SUITE(ResizeNearestNeighborTest, DataTypes);
-
-TYPED_TEST(ResizeNearestNeighborTest, SimpleTest)
+template <typename T>
+std::vector<T> checkResizeNearestNeighborKernel(test_kernel::TestDataBase<T> *test_data_base)
 {
-  Check<TypeParam>({2, 2, 2, 1}, {2}, {2, 3, 3, 1},
-                   {
-                     3, 6,  //
-                     9, 12, //
-                     4, 10, //
-                     10, 16 //
-                   },
-                   {3, 3},
-                   {
-                     3, 3, 6,    //
-                     3, 3, 6,    //
-                     9, 9, 12,   //
-                     4, 4, 10,   //
-                     4, 4, 10,   //
-                     10, 10, 16, //
-                   },
-                   false, false);
+  MemoryManager memory_manager{};
+  RuntimeModule runtime_module{};
+  bool dealloc_input = true;
+
+  // Load model with single op
+  auto *model_data_raw = reinterpret_cast<const char *>(test_data_base->get_model_ptr());
+  ModuleLoader::load(&runtime_module, &memory_manager, model_data_raw, dealloc_input);
+
+  auto *main_runtime_graph = runtime_module.getMainGraph();
+  assert(main_runtime_graph->getNumOfInputTensors() == 1);
+
+  // Set input data
+  {
+    auto *input_tensor_data = reinterpret_cast<T *>(main_runtime_graph->configureGraphInput(0));
+    std::copy(test_data_base->get_input_data_by_index(0).begin(),
+              test_data_base->get_input_data_by_index(0).end(), input_tensor_data);
+  }
+
+  runtime_module.execute();
+
+  assert(main_runtime_graph->getNumOfOutputTensors() == 1);
+
+  T *output_data = reinterpret_cast<T *>(main_runtime_graph->getOutputDataByIndex(0));
+  const size_t num_elements = (main_runtime_graph->getOutputDataSizeByIndex(0) / sizeof(T));
+  std::vector<T> output_data_vector(output_data, output_data + num_elements);
+  return output_data_vector;
 }
 
-TYPED_TEST(ResizeNearestNeighborTest, AlignCenterTest)
+TEST_F(ResizeNearestNeighborTest, Float_P)
 {
-  Check<TypeParam>({2, 2, 2, 1}, {2}, {2, 3, 3, 1},
-                   {
-                     3, 6,  //
-                     9, 12, //
-                     4, 10, //
-                     10, 16 //
-                   },
-                   {3, 3},
-                   {
-                     3, 6, 6,    //
-                     9, 12, 12,  //
-                     9, 12, 12,  //
-                     4, 10, 10,  //
-                     10, 16, 16, //
-                     10, 16, 16, //
-                   },
-                   true, false);
+  test_kernel::TestDataFloatResizeNearestNeighbor test_data_kernel;
+  std::vector<float> output_data_vector = checkResizeNearestNeighborKernel(&test_data_kernel);
+  EXPECT_THAT(output_data_vector, kernels::testing::FloatArrayNear(
+                                    test_data_kernel.get_output_data_by_index(0), 0.0001f));
 }
 
-TYPED_TEST(ResizeNearestNeighborTest, HalfPixelCenterTest)
+TEST_F(ResizeNearestNeighborTest, Input_output_type_mismatch_NEG)
 {
-  Check<TypeParam>({2, 2, 2, 1}, {2}, {2, 3, 3, 1},
-                   {
-                     3, 6,  //
-                     9, 12, //
-                     4, 10, //
-                     10, 16 //
-                   },
-                   {3, 3},
-                   {
-                     3, 6, 6,    //
-                     9, 12, 12,  //
-                     9, 12, 12,  //
-                     4, 10, 10,  //
-                     10, 16, 16, //
-                     10, 16, 16, //
-                   },
-                   false, true);
-}
-
-TEST(ResizeNearestNeighborTest, InputShapeInvalid_NEG)
-{
-  std::unique_ptr<IMemoryManager> memory_manager = std::make_unique<TestMemoryManager>();
-
-  Tensor input_tensor = makeInputTensor<DataType::FLOAT32>({2, 2, 2},
-                                                           {
-                                                             3, 6,  //
-                                                             9, 12, //
-                                                             4, 10, //
-                                                             10, 16 //
-                                                           },
-                                                           memory_manager.get());
-  Tensor size_tensor = makeInputTensor<DataType::S32>({2}, {3, 3}, memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::FLOAT32);
-
-  ResizeNearestNeighborParams params{};
-  params.align_corners = false;
-  params.half_pixel_centers = false;
-
-  ResizeNearestNeighbor kernel(&input_tensor, &size_tensor, &output_tensor, params);
-  EXPECT_ANY_THROW(kernel.configure());
-}
-
-TEST(ResizeNearestNeighborTest, SizeShapeInvalid_NEG)
-{
-  std::unique_ptr<IMemoryManager> memory_manager = std::make_unique<TestMemoryManager>();
-
-  Tensor input_tensor = makeInputTensor<DataType::FLOAT32>({2, 2, 2, 1},
-                                                           {
-                                                             3, 6,  //
-                                                             9, 12, //
-                                                             4, 10, //
-                                                             10, 16 //
-                                                           },
-                                                           memory_manager.get());
-  Tensor size_tensor = makeInputTensor<DataType::S32>({2, 1}, {3, 3}, memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::FLOAT32);
-
-  ResizeNearestNeighborParams params{};
-  params.align_corners = false;
-  params.half_pixel_centers = false;
-
-  ResizeNearestNeighbor kernel(&input_tensor, &size_tensor, &output_tensor, params);
-  EXPECT_ANY_THROW(kernel.configure());
-}
-
-TEST(ResizeNearestNeighborTest, SizeDimInvalid_NEG)
-{
-  std::unique_ptr<IMemoryManager> memory_manager = std::make_unique<TestMemoryManager>();
-
-  Tensor input_tensor = makeInputTensor<DataType::FLOAT32>({2, 2, 2, 1},
-                                                           {
-                                                             3, 6,  //
-                                                             9, 12, //
-                                                             4, 10, //
-                                                             10, 16 //
-                                                           },
-                                                           memory_manager.get());
-  Tensor size_tensor = makeInputTensor<DataType::S32>({3}, {3, 3, 1}, memory_manager.get());
-  Tensor output_tensor = makeOutputTensor(DataType::FLOAT32);
-
-  ResizeNearestNeighborParams params{};
-  params.align_corners = false;
-  params.half_pixel_centers = false;
-
-  ResizeNearestNeighbor kernel(&input_tensor, &size_tensor, &output_tensor, params);
-  EXPECT_ANY_THROW(kernel.configure());
+  test_kernel::NegTestDataInputOutputTypeMismatchResizeNearestNeighborKernel test_data_kernel;
+  MemoryManager memory_manager{};
+  RuntimeModule runtime_module{};
+  bool dealloc_input = true;
+  // Load model with single op
+  auto *model_data_raw = reinterpret_cast<const char *>(test_data_kernel.get_model_ptr());
+  EXPECT_DEATH(ModuleLoader::load(&runtime_module, &memory_manager, model_data_raw, dealloc_input),
+               "");
 }
 
 } // namespace
-} // namespace kernels
 } // namespace luci_interpreter
