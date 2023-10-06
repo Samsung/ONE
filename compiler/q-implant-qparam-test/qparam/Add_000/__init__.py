52269cf75027089cd6edb98910e3f48516ef4baa
FantBlog <115213236+FantBlog@users.noreply.github.com>
2023-10-06 11:14:51 +0900

    [q-implant-qparam-test] Introduce test cases (#11679)
    This introduces test cases for q-implant-qparam-test.
    
    ONE-DCO-1.0-Signed-off-by: Seonguk Park tjddnr011012@gmail.com

---
+++ compiler/q-implant-qparam-test/qparam/Add_000/__init__.py
@@ -0,0 +1,38 @@
+from test_utils import TestCase
+from test_utils import gen_random_tensor
+
+
+class Add_000_Q8(TestCase):
+    def __init__(self):
+        self.name = _name_
+
+    def generate(self) -> dict:
+        json_content = dict()
+
+        # Generate ifm1
+        json_content['ifm1'] = gen_random_tensor(
+            "uint8",  # dtype_str
+            (1),  # scale_shape
+            (1),  # zerop_shape
+            0)  # quantized_dimension
+
+        # Generate ifm2
+        json_content['ifm2'] = gen_random_tensor(
+            "uint8",  #dtype_str
+            (1),  # scale_shape
+            (1),  # zerop_shape
+            0)  # quantized_dimension
+
+        # Generate ofm
+        json_content['ofm'] = gen_random_tensor(
+            "uint8",  # dtype_str
+            (1),  # scale_shape
+            (1),  # zerop_shape
+            0)  # quantized_dimension
+
+        return json_content
+
+
+_name_ = 'Add_000_Q8'
+
+_test_case_ = Add_000_Q8()
---
+++ compiler/q-implant-qparam-test/qparam/Reshape_000/__init__.py
@@ -0,0 +1,31 @@
+from test_utils import TestCase
+from test_utils import gen_random_tensor
+
+
+class Reshape_000_Q8(TestCase):
+    def __init__(self):
+        self.name = _name_
+
+    def generate(self) -> dict:
+        json_content = dict()
+
+        # Generate ifm
+        json_content['ifm'] = gen_random_tensor(
+            "uint8",  # dtype_str
+            (1),  # scale_shape
+            (1),  # zerop_shape
+            0)  # quantized_dimension
+
+        # Generate ofm
+        json_content['ofm'] = gen_random_tensor(
+            "uint8",  # dtype_str
+            (1),  # scale_shape
+            (1),  # zerop_shape
+            0)  # quantized_dimension
+
+        return json_content
+
+
+_name_ = 'Reshape_000_Q8'
+
+_test_case_ = Reshape_000_Q8()
