ab0e6a7122ce13b43fe586007bb6dd62d493f21b
s.malakhov <s.malakhov@partner.samsung.com>
2023-10-11 15:50:16 +0300

    [luci/pass] Implement Softmax decomposing pass
    This commit implements decomposition of the Softmax operation
    to Max, Sub, Exp, Sum, Div and possibly Mul  operations.
    
    ONE-DCO-1.0-Signed-off-by: s.malakhov <s.malakhov@partner.samsung.com>

--- compiler/luci/pass/include/luci/CircleOptimizer.h
+++ compiler/luci/pass/include/luci/CircleOptimizer.h
@@ -83,6 +83,7 @@ public:
       TransformMinMaxToRelu6Pass,
       TransformMinReluToRelu6Pass,
       DecomposeHardSwishPass,
+      DecomposeSoftmaxPass,
       SubstituteStridedSliceToReshape,
       SubstituteTransposeToReshape,
       RemoveRedundantQuantize,
