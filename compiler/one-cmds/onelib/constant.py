d58eec1406114b187404e4a555fd23038801a350
stamalakhov <112689352+stamalakhov@users.noreply.github.com>
2023-10-30 02:36:25 +0300

    [one-cmds] Add fuse_slice_with_tconv to O1 group (#11808)
    This commit adds fuse_slice_with_tconv option to O1 group.
    
    ONE-DCO-1.0-Signed-off-by: s.malakhov <s.malakhov@partner.samsung.com>

--- compiler/one-cmds/onelib/constant.py
+++ compiler/one-cmds/onelib/constant.py
@@ -43,6 +43,7 @@ class CONSTANT:
         'fuse_gelu',
         'fuse_mean_with_mean',
         'fuse_transpose_with_mean',
+        'fuse_slice_with_tconv',
         'transform_min_max_to_relu6',
         'transform_min_relu_to_relu6',
 
