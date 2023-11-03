1b3a8a7909a77ba6c3768fbaeaf0858389d89488
Jonghwa Lee <jonghwa3.lee@samsung.com>
2023-11-03 09:25:50 +0900

    [luci/export] Support CumSum operation
    This adds support for CumSum operation in luci export.
    
    ONE-DCO-1.0-Signed-off-by: Jonghwa Lee <jonghwa3.lee@samsung.com>

--- compiler/luci/export/src/CircleBuiltinTypesExtractor.h
+++ compiler/luci/export/src/CircleBuiltinTypesExtractor.h
@@ -118,6 +118,10 @@ public:
     return circle::CreateCosOptions(_builder).Union();
   }
   flatbuffers::Offset<void> visit(luci::CircleCustom *) { return _no_option; }
+  flatbuffers::Offset<void> visit(luci::CircleCumSum *node)
+  {
+    return circle::CreateCumsumOptions(_builder, node->exclusive(), node->reverse()).Union();
+  }
   flatbuffers::Offset<void> visit(luci::CircleDensify *)
   {
     return circle::CreateDensifyOptions(_builder).Union();
