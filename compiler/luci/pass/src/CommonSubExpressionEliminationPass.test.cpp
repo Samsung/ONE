207fd2d21b559d88a0b3c4dbf72fdf7623b2334b
Hyukjin Jeong <hj1.jeong@samsung.com>
2023-11-06 15:43:21 +0900

    [luci] Support CSE on Tranpose Op
    This supports CSE on Transpose Op.
    
    ONE-DCO-1.0-Signed-off-by: Hyukjin Jeong <hj1.jeong@samsung.com>

--- compiler/luci/pass/src/CommonSubExpressionEliminationPass.test.cpp
+++ compiler/luci/pass/src/CommonSubExpressionEliminationPass.test.cpp
@@ -91,6 +91,43 @@ public:
   }
 };
 
+class CSE_TransposeTestGraph : public CommonSubExpressionEliminationTestGraph
+{
+public:
+  std::vector<luci::CircleTranspose *> ops;
+
+protected:
+  virtual loco::Node *createExpression(luci::CircleNode *ifm, const std::string &name) override
+  {
+    auto perm = g()->nodes()->create<luci::CircleConst>();
+    perm->name(name + "_perm");
+    perm->dtype(loco::DataType::S32);
+    perm->shape({4});
+    perm->size<loco::DataType::S32>(4);
+    perm->at<loco::DataType::S32>(0) = 0;
+    perm->at<loco::DataType::S32>(1) = 3;
+    perm->at<loco::DataType::S32>(2) = 1;
+    perm->at<loco::DataType::S32>(3) = 2;
+
+    auto expr = g()->nodes()->create<luci::CircleTranspose>();
+    expr->name(name + "_transpose");
+    expr->dtype(loco::DataType::FLOAT32);
+    expr->shape({1, 32, 8, 8});
+    expr->a(ifm);
+    expr->perm(perm);
+
+    ops.emplace_back(expr);
+
+    return expr;
+  };
+
+public:
+  void init(void)
+  {
+    CommonSubExpressionEliminationTestGraph::init({{1, 8, 8, 32}}, {{1, 32, 8, 8}, {1, 32, 8, 8}});
+  }
+};
+
 } // namespace
 
 TEST(CommonSubExpressionEliminationTest, Quantize)
@@ -113,6 +150,30 @@ TEST(CommonSubExpressionEliminationTest, Quantize_NEG)
 
   // Different pattern
   g.ops[0]->input(g.ops[2]);
+
+  EXPECT_FALSE(pass.run(g.g()));
+}
+
+TEST(CommonSubExpressionEliminationTest, Transpose)
+{
+  CSE_TransposeTestGraph g;
+  luci::CommonSubExpressionEliminationPass pass;
+
+  g.init();
+
+  EXPECT_TRUE(pass.run(g.g()));
+}
+
+TEST(CommonSubExpressionEliminationTest, Transpose_NEG)
+{
+  CSE_TransposeTestGraph g;
+
+  luci::CommonSubExpressionEliminationPass pass;
+
+  g.init();
+
+  // Different pattern
+  g.ops[0]->a(g.ops[2]);
 
   EXPECT_FALSE(pass.run(g.g()));
 }
