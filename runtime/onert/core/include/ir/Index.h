137c601fe520b0a10501c458fc4e3d4df0e01059
Sanggyu <takepencil@naver.com>
2023-11-12 18:30:23 +0900

    [onert] Introduce OriginIndex in IR
    It introduces OriginIndex in IR.
    It will be used for exporting circle after training.
    
    ONE-DCO-1.0-Signed-off-by: Sanggyu Lee <sg5.lee@samsung.com>

--- runtime/onert/core/include/ir/Index.h
+++ runtime/onert/core/include/ir/Index.h
@@ -41,6 +41,9 @@ using SubgraphIndex = ::onert::util::Index<uint16_t, SubgraphIndexTag>;
 struct ModelIndexTag;
 using ModelIndex = ::onert::util::Index<uint16_t, ModelIndexTag>;
 
+struct OriginIndexTag;
+using OriginIndex = ::onert::util::Index<uint32_t, OriginIndexTag>;
+
 template <typename IndexType>
 std::ostream &_index_print_impl(std::ostream &o, const std::string &prefix, IndexType index)
 {
@@ -75,6 +78,10 @@ inline std::ostream &operator<<(std::ostream &o, const ModelIndex &i)
   return _index_print_impl(o, "MODEL", i);
 }
 
+inline std::ostream &operator<<(std::ostream &o, const OriginIndex &i)
+{
+  return _index_print_impl(o, "", i);
+}
 } // namespace ir
 } // namespace onert
 
