763acc416a2d327098b32f8ce11b27b087fd3310
Jiyoung Yun <jy910.yun@samsung.com>
2023-11-13 15:49:08 +0900

    [onert] Introduce CategoricalCrossEntropy kernel
    This commit introduces Categorical Cross Entropy forward and backward
    kernels.
    
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

--- compute/cker/include/cker/train/operation/Loss.h
+++ compute/cker/include/cker/train/operation/Loss.h
@@ -17,6 +17,8 @@
 #ifndef __NNFW_CKER_TRAIN_OPERATION_LOSS_H__
 #define __NNFW_CKER_TRAIN_OPERATION_LOSS_H__
 
+#include <numeric>
+
 #include "cker/Shape.h"
 #include "cker/eigen/Utils.h"
 
@@ -67,6 +69,64 @@ inline void MSEGrad(const Shape &y_pred_shape, const T *y_pred_data, const Shape
   for (int i = 0; i < size; ++i)
   {
     grad_data[i] = static_cast<T>(-2 * (y_true_data[i] - y_pred_data[i]) / size);
+  }
+}
+
+template <typename T> bool checkValue(const T *data, int size, T min, T max)
+{
+  for (int i = 0; i < size; ++i)
+  {
+    if (data[i] > max || data[i] < min)
+      return false;
+  }
+  return true;
+}
+
+template <typename T>
+inline void CategoricalCrossEntropy(const T *y_pred_data, const T *y_true_data, T *output_data,
+                                    const int batch_size, const int input_size)
+{
+  const T *y_prob_data = y_pred_data;
+
+  if (!checkValue(y_pred_data, input_size * batch_size, static_cast<T>(0), static_cast<T>(1)))
+  {
+    throw std::runtime_error("cker::CategoricalCrossEntropy: y_pred data is not logit data.");
+  }
+
+  std::vector<T> sum(batch_size, 0.f);
+  for (int b = 0; b < batch_size; ++b)
+  {
+    int b_offset = b * input_size;
+    for (int i = 0; i < input_size; ++i)
+    {
+      if (y_true_data[b_offset + i] != 0)
+      {
+        sum[b] += -std::log(std::max(y_prob_data[b_offset + i], static_cast<float>(1.0e-20))) *
+                  y_true_data[b_offset + i];
+      }
+    }
+  }
+
+  output_data[0] = std::accumulate(sum.begin(), sum.end(), 0.f) / static_cast<float>(batch_size);
+}
+
+template <typename T>
+inline void CategoricalCrossEntropyGrad(const T *y_pred_data, const T *y_true_data, T *grad_data,
+                                        const int batch_size, const int input_size)
+{
+  if (!checkValue(y_pred_data, input_size * batch_size, static_cast<T>(0), static_cast<T>(1)))
+  {
+    throw std::runtime_error("cker::CategoricalCrossEntropyGrad: y_pred data is not logit data.");
+  }
+
+  for (int b = 0; b < batch_size; ++b)
+  {
+    int b_offset = b * input_size;
+    for (int i = 0; i < input_size; ++i)
+    {
+      grad_data[b_offset + i] = -(y_true_data[b_offset + i] /
+                                  std::max(y_pred_data[b_offset + i], static_cast<float>(1e-20)));
+    }
   }
 }
 
