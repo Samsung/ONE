9cfb51eb44e62d35843a9f7a6385da3301ceb005
SeungHui Lee <shsh1004.lee@samsung.com>
2023-12-04 07:56:51 -0500

    [Draft][one-cmds] one-create-quant-dataset tool
    On going draft to land one-create-quant-dataset tool
    
    ONE-DCO-1.0-Signed-off-by: SeungHui Lee <shsh1004.lee@samsung.com>

---
+++ compiler/one-cmds/tests/preprocess_images_numpy.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os, shutil, PIL.Image, numpy as np
+
+input_dir = 'img_files'
+output_dir = 'numpy_files'
+list_file = 'datalist_numpy.txt'
+
+if os.path.exists(output_dir):
+    shutil.rmtree(output_dir, ignore_errors=True)
+os.makedirs(output_dir)
+
+for (root, _, files) in os.walk(input_dir):
+    datalist = open(list_file, 'w')
+    for f in files:
+        with PIL.Image.open(root + '/' + f) as image:
+            # To handle ANTIALIAS deprecation
+            ANTIALIAS = PIL.Image.Resampling.LANCZOS if hasattr(
+                PIL.Image, "Resampling") else PIL.Image.ANTIALIAS
+
+            img = np.array(image.resize((299, 299), ANTIALIAS)).astype(np.float32)
+            img = ((img / 255) - 0.5) * 2.0
+            output_file = output_dir + '/' + f.replace('jpg', 'npy')
+            np.save('./' + output_file, img)
+            datalist.writelines(os.path.abspath(output_file) + '\n')
+    datalist.close()
