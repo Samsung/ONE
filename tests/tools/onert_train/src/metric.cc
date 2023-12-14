32ae6d934ada2410ef20de40b2b4c2970a7a475d
Jiyoung Yun <jy910.yun@samsung.com>
2023-12-14 20:34:03 +0900

    [tools/onert_train] Introduce Metric class
    This commit introduces Metric class.
    Metric class calculates the metric value using the output and expected data.
    Only categorical accuracy is supported now.
    
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

---
+++ tests/tools/onert_train/src/metric.cc
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "metric.h"
+#include "nnfw_util.h"
+
+#include <algorithm>
+#include <stdexcept>
+
+namespace onert_train
+{
+
+Metric::Metric(const std::vector<Allocation> &output, const std::vector<Allocation> &expected,
+               const std::vector<nnfw_tensorinfo> &infos)
+  : _output{output}, _expected{expected}, _infos{infos}
+{
+  // DO NOTHING
+}
+
+template <typename T>
+float Metric::categoricalAccuracy(const T *output, const T *expected, uint32_t batch, uint64_t size)
+{
+  int correct = 0;
+  for (int b = 0; b < batch; ++b)
+  {
+    int b_offset = b * size;
+    std::vector<T> boutput(output + b_offset, output + b_offset + size);
+    std::vector<T> bexpected(expected + b_offset, expected + b_offset + size);
+    auto output_idx =
+      std::distance(boutput.begin(), std::max_element(boutput.begin(), boutput.end()));
+    auto expected_idx =
+      std::distance(bexpected.begin(), std::max_element(bexpected.begin(), bexpected.end()));
+    if (output_idx == expected_idx)
+      correct++;
+  }
+  return static_cast<float>(correct) / batch;
+}
+
+float Metric::categoricalAccuracy(int32_t index)
+{
+  auto batch = _infos[index].dims[0];
+  auto size = num_elems(&_infos[index]) / batch;
+  switch (_infos[index].dtype)
+  {
+    case NNFW_TYPE_TENSOR_FLOAT32:
+      return categoricalAccuracy(static_cast<const float *>(_output[index].data()),
+                                 static_cast<const float *>(_expected[index].data()), batch, size);
+    default:
+      throw std::runtime_error("Not supported tensor type in calculateAccuracy");
+  }
+}
+
+} // namespace onert_train
---
+++ tests/tools/onert_train/src/metric.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ONERT_TRAIN_METRIC_H__
+#define __ONERT_TRAIN_METRIC_H__
+
+#include "allocation.h"
+#include "nnfw.h"
+#include <vector>
+
+namespace onert_train
+{
+class Metric
+{
+public:
+  Metric(const std::vector<Allocation> &output, const std::vector<Allocation> &expected,
+         const std::vector<nnfw_tensorinfo> &infos);
+
+private:
+  template <typename T>
+  float categoricalAccuracy(const T *output, const T *expected, uint32_t batch, uint64_t size);
+
+public:
+  float categoricalAccuracy(int32_t index);
+
+private:
+  const std::vector<Allocation> &_output;
+  const std::vector<Allocation> &_expected;
+  const std::vector<nnfw_tensorinfo> &_infos;
+};
+
+} // namespace onert_train
+
+#endif // __ONERT_TRAIN_METRIC_H__
