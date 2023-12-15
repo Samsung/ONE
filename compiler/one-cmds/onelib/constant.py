4af08452b048fc48b93aed7328bab97c8c68012d
Artem Balyshev <a.balyshev@samsung.com>
2023-12-15 12:18:33 +0300

    add into O1

--- compiler/one-cmds/onelib/constant.py
+++ compiler/one-cmds/onelib/constant.py
@@ -43,6 +43,7 @@ class CONSTANT:
         'fuse_gelu',
         'fuse_mean_with_mean',
         'fuse_mul_with_conv',
+        'fuse_mul_div',
         'fuse_transpose_with_mean',
         'fuse_slice_with_tconv',
         'fuse_horizontal_fc_layers',
