68496e10b1131147f119f6ae5a21071c7b6ae20f
Jiyoung Giuliana Yun <jy910.yun@samsung.com>
2023-12-26 10:02:23 +0900

    [onert] Introduce Reduce trainable opeation (#12359)
    This commit introduces Reduce trainable operation.
    
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

--- runtime/onert/core/src/ir/train/operation/Reduce.cc
+++ runtime/onert/core/src/ir/train/operation/Reduce.cc
@@ -14,17 +14,36 @@
  * limitations under the License.
  */
 
-#ifndef __ONERT_IR_TRAIN_OPERATIONS_OPERATION_INCLUDE_H__
-#define __ONERT_IR_TRAIN_OPERATIONS_OPERATION_INCLUDE_H__
-
-#include "ir/train/operation/Conv2D.h"
-#include "ir/train/operation/ElementwiseActivation.h"
-#include "ir/train/operation/FullyConnected.h"
-#include "ir/train/operation/Loss.h"
-#include "ir/train/operation/Permute.h"
-#include "ir/train/operation/Pool2D.h"
 #include "ir/train/operation/Reduce.h"
-#include "ir/train/operation/Reshape.h"
-#include "ir/train/operation/Softmax.h"
 
-#endif // __ONERT_IR_TRAIN_OPERATIONS_OPERATION_INCLUDE_H__
+#include "ir/OperationVisitor.h"
+#include "ir/train/TrainableOperationVisitor.h"
+
+namespace onert
+{
+namespace ir
+{
+namespace train
+{
+namespace operation
+{
+
+std::unique_ptr<ITrainableOperation> Reduce::clone() const
+{
+  return std::make_unique<Reduce>(*this);
+}
+
+void Reduce::accept(OperationVisitor &v) const { v.visit(*this); }
+
+void Reduce::accept(TrainableOperationVisitor &v) const { v.visit(*this); }
+
+Reduce::Reduce(const OperationType &operation)
+  : OperationType{operation.getInputs(), operation.getOutputs(), operation.param()}
+{
+  // DO NOTHING
+}
+
+} // namespace operation
+} // namespace train
+} // namespace ir
+} // namespace onert
