2069a85f6190ed9bc0654d802f1fb92245428a7b
Jiyoung Giuliana Yun <jy910.yun@samsung.com>
2024-01-04 18:41:49 +0900

    [onert] Merge _training_step into TrainingInfo (#12390)
    This commit merges _training_step into TrainingInfo.
    It removes duplicated variable.
    
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

--- runtime/onert/api/src/nnfw_api_internal.cc
+++ runtime/onert/api/src/nnfw_api_internal.cc
@@ -1349,6 +1349,9 @@ NNFW_STATUS nnfw_session::train_prepare()
     if (not _train_info->isValid())
       throw std::runtime_error{"training info is not valid"};
 
+    // initialize trainingStep count
+    _train_info->trainingStep() = 0;
+
     auto compiler =
       onert::compiler::CompilerFactory::get().create(_nnpkg, _coptions, _train_info.get());
     _nnpkg.reset();
@@ -1534,7 +1537,8 @@ NNFW_STATUS nnfw_session::train_run(bool update_weights)
   {
     if (update_weights)
     {
-      _execution->train(_training_step++);
+      auto &training_step = _train_info->trainingStep();
+      _execution->train(training_step++);
     }
     else
       _execution->execute();
