cf94c8c74f23090ea462cd6a9445945ed290d3df
Jiyoung Yun <jy910.yun@samsung.com>
2024-01-30 11:52:07 +0900

    [onert] Introduce CodegenManager class
    This commit introduces CodegenManager class.
    This class runs codegen() function to load target backend library
    and call the codegen function.
    
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>
    Co-authored-by: Sanggyu Lee <sg5.lee@samsung.com>

---
+++ runtime/onert/core/src/odc/CodegenManager.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2024 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "CodegenLoader.h"
+#include "odc/CodegenManager.h"
+#include "util/Utils.h"
+
+#include <fstream>
+#include <mutex>
+
+namespace onert
+{
+namespace odc
+{
+
+bool CodegenManager::codegen(const char *target, CodegenPreference pref)
+{
+  // codegen function is thread-unsafe
+  static std::mutex lock;
+  std::lock_guard<std::mutex> guard(lock);
+
+  if (_export_model_path.empty())
+    throw std::runtime_error("Export model path is not set");
+
+  std::ifstream file(_model_path);
+  if (!file.good())
+    throw std::runtime_error("Model path does not exist");
+
+  auto &codegen_loader = CodegenLoader::instance();
+  codegen_loader.loadLibrary(target);
+  const auto code_generator = codegen_loader.get();
+  // TODO Use compile preference
+  UNUSED_RELEASE(pref);
+  const auto result = code_generator->codegen(_model_path.c_str(), _export_model_path.c_str());
+  codegen_loader.unloadLibrary();
+
+  return (result == 0);
+}
+
+} // namespace odc
+} // namespace onert
