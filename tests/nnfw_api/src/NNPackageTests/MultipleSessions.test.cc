6bbf9726d7062fecd2a81e131172fe4c557a3276
Yongseop Kim <yons.kim@samsung.com>
2024-01-31 14:05:41 +0900

    [tests/nnfw_api] Revisit nnfw_api tests directory
    Let's revisit nnfw_api tests directory
    - https://github.com/Samsung/ONE/issues/12501
    
    ONE-DCO-1.0-Signed-off-by: Yongseop Kim <yons.kim@samsung.com>

--- tests/nnfw_api/src/NNPackageTests/MultipleSessions.test.cc
+++ tests/nnfw_api/src/NNPackageTests/MultipleSessions.test.cc
@@ -15,7 +15,7 @@
  */
 
 #include "fixtures.h"
-#include "one_op_tests/WhileTestModel.h"
+#include "GenModelTests/one_op_tests/WhileTestModel.h"
 
 TEST_F(ValidationTestTwoSessions, neg_two_sessions_create)
 {
