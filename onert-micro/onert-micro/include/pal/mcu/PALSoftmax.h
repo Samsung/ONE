41247b0934a00380377b7920ef988aac8d8d6684
Artem Balyshev <a.balyshev@samsung.com>
2024-02-02 13:00:35 +0300

    change file data

--- onert-micro/onert-micro/include/pal/mcu/PALSoftmax.h
+++ onert-micro/onert-micro/include/pal/mcu/PALSoftmax.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Samsung Electronics Co., Ltd. All Rights Reserved
+ * Copyright (c) 2024 Samsung Electronics Co., Ltd. All Rights Reserved
  * Copyright 2017 The TensorFlow Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
