36f50afef7c98ce5a9987fc9d1767a9ba6939f92
SaeHie Park <saehie.park@gmail.com>
2024-03-15 10:02:39 +0900

    [infra] Introduce cmake config for TF2.12.1 (#12752)
    This will introduce cmake config files for TF2.12.1.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ infra/cmake/packages/TensorFlowGEMMLowpSource-2.12.1/TensorFlowGEMMLowpSourceConfig.cmake
@@ -0,0 +1,21 @@
+function(_TensorFlowGEMMLowpSource_import)
+  if(NOT DOWNLOAD_GEMMLOWP)
+    set(TensorFlowGEMMLowpSource_FOUND FALSE PARENT_SCOPE)
+    return()
+  endif(NOT DOWNLOAD_GEMMLOWP)
+
+  nnas_include(ExternalSourceTools)
+  nnas_include(OptionTools)
+
+  # Exact version used by TensorFlow v2.12.1.
+  # See tensorflow/third_party/gemmlowp/workspace.bzl.
+  envoption(EXTERNAL_DOWNLOAD_SERVER "https://github.com")
+  envoption(TENSORFLOW_2_12_1_GEMMLOWP_URL ${EXTERNAL_DOWNLOAD_SERVER}/google/gemmlowp/archive/e844ffd17118c1e17d94e1ba4354c075a4577b88.zip)
+
+  ExternalSource_Download(GEMMLOWP DIRNAME TENSORFLOW-2.12.1-GEMMLOWP ${TENSORFLOW_2_12_1_GEMMLOWP_URL})
+
+  set(TensorFlowGEMMLowpSource_DIR ${GEMMLOWP_SOURCE_DIR} PARENT_SCOPE)
+  set(TensorFlowGEMMLowpSource_FOUND TRUE PARENT_SCOPE)
+endfunction(_TensorFlowGEMMLowpSource_import)
+
+_TensorFlowGEMMLowpSource_import()
