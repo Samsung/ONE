2d1f7031c122814a6fb860a259be583d27f3d1f1
Vyacheslav Bazhenov <v.bazhenov@partner.samsung.com>
2024-03-27 15:58:29 +0300

    [luci-interpreter] Support Hybrid UINT4 weights FullyConnected kernel
    This commit adds support of UINT4 quantized weights in FullyConnected kernel.
    
    ONE-DCO-1.0-Signed-off-by: Vyacheslav Bazhenov <slavikmipt@gmail.com>

--- compiler/luci-interpreter/src/kernels/FullyConnected.cpp
+++ compiler/luci-interpreter/src/kernels/FullyConnected.cpp
@@ -61,6 +61,13 @@ void FullyConnected::configure()
     LUCI_INTERPRETER_CHECK(output()->element_type() == DataType::FLOAT32);
     LUCI_INTERPRETER_CHECK(!bias() || bias()->element_type() == DataType::FLOAT32)
   }
+  else if (weights()->element_type() == DataType::U4)
+  {
+    // TODO support other combinations when needed
+    LUCI_INTERPRETER_CHECK(input()->element_type() == DataType::FLOAT32);
+    LUCI_INTERPRETER_CHECK(output()->element_type() == DataType::FLOAT32);
+    LUCI_INTERPRETER_CHECK(!bias() || bias()->element_type() == DataType::FLOAT32)
+  }
   else
   {
     throw std::runtime_error("luci-intp FullyConnected(1) Unsupported type.");
@@ -97,12 +104,23 @@ void FullyConnected::configure()
 void FullyConnected::execute() const
 {
   const bool is_hybrid =
-    (input()->element_type() == DataType::FLOAT32 && weights()->element_type() == DataType::S4 &&
+    (input()->element_type() == DataType::FLOAT32 &&
+     (weights()->element_type() == DataType::S4 || weights()->element_type() == DataType::U4) &&
      output()->element_type() == DataType::FLOAT32 &&
      (!bias() || bias()->element_type() == DataType::FLOAT32));
   if (is_hybrid)
   {
-    evalHybridWI4AF32();
+    switch (weights()->element_type())
+    {
+      case DataType::S4:
+        evalHybridWI4AF32();
+        break;
+      case DataType::U4:
+        evalHybridWU4AF32();
+        break;
+      default:
+        throw std::runtime_error("luci-intp FullyConnected(3) Unsupported type.");
+    }
   }
   else
   {
@@ -225,6 +243,70 @@ void FullyConnected::evalHybridWI4AF32() const
     // 1bit for sign, 3bit for value
     weights_float[i] = weights()->scale() * weights_int4[i];
   }
+  tflite::reference_ops::FullyConnected(
+    params, getTensorShape(input()), getTensorData<float>(input()), getTensorShape(scratch()),
+    getTensorData<float>(scratch()), getTensorShape(bias()), getTensorData<float>(bias()),
+    getTensorShape(output()), getTensorData<float>(output()));
+}
+
+void FullyConnected::evalHybridWU4AF32() const
+{
+  float activation_min{};
+  float activation_max{};
+  calculateActivationRange(_params.activation, &activation_min, &activation_max);
+
+  tflite::FullyConnectedParams params{};
+  params.float_activation_min = activation_min;
+  params.float_activation_max = activation_max;
+  params.weights_format = tflite::FullyConnectedWeightsFormat::kDefault;
+
+  const auto *weights_uint4 = getTensorData<uint8_t>(weights());
+  auto *weights_float = getTensorData<float>(scratch());
+  const Shape &weights_shape = weights()->shape();
+  const auto weights_scales = weights()->scales();
+  const auto weights_zero_points = weights()->zero_points();
+  const auto weights_quantized_dimension = weights()->quantized_dimension();
+  LUCI_INTERPRETER_CHECK(weights_quantized_dimension == 0);
+  if (weights_scales.size() == 1)
+  {
+    // Per tensor
+    const auto scale = weights()->scale();
+    const auto zero_point = weights()->zero_point();
+    LUCI_INTERPRETER_CHECK(zero_point >= 0 and zero_point <= 15);
+    for (int32_t i = 0; i < weights_shape.num_elements(); ++i)
+    {
+      weights_float[i] =
+        scale * static_cast<float>(static_cast<int32_t>(weights_uint4[i]) - zero_point);
+    }
+  }
+  else
+  {
+    // Per channel
+    const int32_t quant_dim_size = weights_shape.dim(weights_quantized_dimension);
+
+    size_t outer_dims_size = 1;
+    size_t inner_dims_size = 1;
+    for (int i = 0; i < weights_quantized_dimension; ++i)
+      outer_dims_size *= weights_shape.dim(i);
+    for (int i = weights_quantized_dimension + 1; i < weights_shape.num_dims(); ++i)
+      inner_dims_size *= weights_shape.dim(i);
+
+    for (size_t outer_it = 0; outer_it < outer_dims_size; ++outer_it)
+      for (int32_t channel = 0; channel < quant_dim_size; ++channel)
+      {
+        int32_t zero_point = weights_zero_points[channel];
+        LUCI_INTERPRETER_CHECK(zero_point >= 0 and zero_point <= 15);
+        float scale = weights_scales[channel];
+        size_t offset = inner_dims_size * (quant_dim_size * outer_it + channel);
+        for (size_t inner_it = 0; inner_it < inner_dims_size; ++inner_it)
+        {
+          weights_float[offset + inner_it] =
+            scale *
+            static_cast<float>(static_cast<int32_t>(weights_uint4[offset + inner_it]) - zero_point);
+        }
+      }
+  }
+
   tflite::reference_ops::FullyConnected(
     params, getTensorShape(input()), getTensorData<float>(input()), getTensorShape(scratch()),
     getTensorData<float>(scratch()), getTensorShape(bias()), getTensorData<float>(bias()),
