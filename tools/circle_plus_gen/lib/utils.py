7053ac0e183c306f6dcb2407a7dacacc4b31444c
sseung <zetwhite@naver.com>
2024-05-16 23:12:44 +0900

    [tools/circle_plus_gen] Extend Namer to support various naming
    This PR extends namer to support multiple naming.
    This is to accept various string while loading json file.
    
    ONE-DCO-1.0-Signed-off-by: seunghui youn <sseung.youn@samsung.com>

--- tools/circle_plus_gen/lib/utils.py
+++ tools/circle_plus_gen/lib/utils.py
@@ -1,13 +1,27 @@
+from typing import Union, Type, List
 from schema import circle_traininfo_generated as ctr_gen
+
+# yapf:disable
+# type alias
+OPTIM_OPTIONS_T = Union[Type[ctr_gen.SGDOptionsT],
+                      Type[ctr_gen.AdamOptionsT]]
+
+LOSSFN_OPTIONS_T = Union[Type[ctr_gen.SparseCategoricalCrossentropyOptionsT],
+                         Type[ctr_gen.CategoricalCrossentropyOptionsT],
+                         Type[ctr_gen.MeanSquaredErrorOptionsT]]
+# yapf:enable
 
 
 class OptimizerNamer:
     '''Return name(string) based on ModelTraining.OptimizerOpt'''
-    names = {ctr_gen.SGDOptionsT: 'sgd', ctr_gen.AdamOptionsT: 'adam'}
+    names = {
+        ctr_gen.SGDOptionsT: ['sgd', 'stocasticgradientdescent'],
+        ctr_gen.AdamOptionsT: ['adam']
+    }
 
-    def __call__(cls, opt):
+    def __call__(self, opt: OPTIM_OPTIONS_T) -> List[str]:
         try:
-            name = cls.names[type(opt)]
+            name = self.names[opt]
         except:
             print(f"unknown optimizer {type(opt)}")
         return name
@@ -15,15 +29,21 @@ class OptimizerNamer:
 
 class LossNamer:
     '''Return name(string) based on ModelTraining.LossfnOpt'''
-    names = {
-        ctr_gen.SparseCategoricalCrossentropyOptionsT: 'sparse categorical crossentropy',
-        ctr_gen.CategoricalCrossentropyOptionsT: 'categorical crossentorpy',
-        ctr_gen.MeanSquaredErrorOptionsT: 'mean squared error'
-    }
 
-    def __call__(cls, lossfn):
+    # yapf:disable
+    names = {
+        ctr_gen.SparseCategoricalCrossentropyOptionsT:
+            ['sparse categorical crossentropy', 'sparsecategoricalcrossentropy', 'sparsecce'],
+        ctr_gen.CategoricalCrossentropyOptionsT:
+            ['categorical crossentropy', 'categoricalcrossentropy', 'cce'],
+        ctr_gen.MeanSquaredErrorOptionsT:
+            ['mean squared error', 'mse']
+    }
+    # yapf:eanble
+
+    def __call__(self, lossfn:LOSSFN_OPTIONS_T) -> List[str]:
         try:
-            name = cls.names[type(lossfn)]
+            name = self.names[lossfn]
         except:
             print(f"unknown lossfn {type(lossfn)}")
         return name
@@ -32,13 +52,13 @@ class LossNamer:
 class LossReductionNamer:
     '''Return name(string) based on ModelTraining.LossReductionType '''
     names = {
-        ctr_gen.LossReductionType.SumOverBatchSize: 'SumOverBatchSize',
-        ctr_gen.LossReductionType.Sum: 'Sum',
+        ctr_gen.LossReductionType.SumOverBatchSize: ['sum over batch size', 'sumoverbatchsize'],
+        ctr_gen.LossReductionType.Sum: ['sum'],
     }
 
-    def __call__(cls, rdt):
+    def __call__(self, rdt:ctr_gen.LossReductionType) -> List[str]:
         try:
-            name = cls.names[rdt]
+            name = self.names[rdt]
         except:
             print(f"unknown loss reduction type {rdt}")
         return name
