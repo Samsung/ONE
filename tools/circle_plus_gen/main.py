f371681fc3f57369248c4df701e0e6577360074a
sseung <zetwhite@naver.com>
2024-06-11 18:13:33 +0900

    tmp

--- tools/circle_plus_gen/main.py
+++ tools/circle_plus_gen/main.py
@@ -14,9 +14,11 @@ def get_cmd_args():
 
     parser.add_argument('input', help='input circle file')
     parser.add_argument(
-        'hyperparameters', nargs='?', help='training hyperparameters json file')
+        '--hyperparameters', 
+        nargs='?', 
+        help='training hyperparameters json file')
     parser.add_argument(
-        'output',
+        '--output',
         nargs='?',
         help='output circle file, if not given input circle file will be overwritten')
     parser.add_argument(
@@ -28,20 +30,27 @@ def get_cmd_args():
     return args
 
 
-def print_training_hparameters(in_circle_file) -> typing.NoReturn:
+def print_training_hparameters(circle_model : CirclePlus):
     '''
-    if in_circle_file has training hyperparameters, print it out
+    if circle_model has training parameters, print it out
     '''
-    print(f"check hyperparameters in {in_circle_file}")
-
-    circle_model: CirclePlus = CirclePlus.from_file(in_circle_file)
     tinfo: typing.Union[TrainParam, None] = circle_model.get_train_param()
-
+    operators = circle_model.get_operators(subgraph_idx=0)
+    
     if tinfo == None:
         print("No hyperparameters")
     else:
         print(tinfo.dump_as_json())
-
+        print()
+        
+        print("trainable operators :")
+        trainable = tinfo.get_trainable()
+        if len(trainable) == 0:
+            print("\tNone") 
+        else:
+            for i in tinfo.get_trainable():
+                print(f"\t[{i}] {operators[i]}")
+       
 
 def to_trainable_index(s: str, num_op: int) -> typing.List[int]:
     s = s.lower()
@@ -51,12 +60,19 @@ def to_trainable_index(s: str, num_op: int) -> typing.List[int]:
     elif s == "none":
         return []
     elif s.startswith("last"):
-        last_n = s[4:]  #remvoe 'last'
-        start_idx = num_op - int(last_n)
-        if start_idx < 0:
-            raise RuntimeError(
-                f"number of operators({num_op}) < number of trainable operators({last_n})"
-            )
+        try:
+            last_n = int(s[4:])  #remvoe 'last'
+        except ValueError as e:
+            print(e)
+            raise ValueError(f"lastN: N is not integer value")
+        
+        if last_n < 0: 
+            raise ValueError(f"lastN : N is negative value")
+        
+        if last_n > num_op : 
+            raise ValueError(f"number of operators({num_op}) < number of trainable operators({last_n})")
+        
+        start_idx = num_op - last_n
         return list(range(start_idx, num_op))
     else:
         raise RuntimeError(f"not supported train {s}")
@@ -76,26 +92,62 @@ def inject_hparams(in_file, hparams_file, trainable: str, out_file=None) -> None
     tparams: TrainParam = TrainParam.from_json(hparams_file)
     tparams.set_trainable(to_trainable_index(trainable, len(operators)))
 
-    print("load training hyperparameters")
-    print(tparams.dump_as_json())
-
-    print("trainable operators :")
-    for i in tparams.get_trainable():
-        print(f"\t[{i}] {operators[i]}")
-    print()
-
     circle_model.set_train_param(tparams)
     print("succesfully add hyperparameters to the circle file")
+    print_training_hparameters(circle_model)
 
-    circle_model.export(out_file)
-    print(f"saved in {out_file}")
+    # circle_model.export(out_file)
+    # print(f"saved in {out_file}")
+
+
+def inject_hparams(circle_mode: CirclePlus, hparams_file) -> CirclePlus:
+    '''
+    Inject hparams_file's contents into in_file's circle model, and save it as out_file 
+    '''
+    tparams: TrainParam = TrainParam.from_json(hparams_file)
+
+    circle_model.set_train_param(tparams)
+
+    # circle_model.export(out_file)
+    # print(f"saved in {out_file}")
+
 
 
 if __name__ == "__main__":
     args = get_cmd_args()
+    
+    circle_model : CirclePlus = CirclePlus.from_file(args.input)
+    
+    if args.hyperparameters is not None:
+        tparams : TrainParam = TrainParam.from_json(args.hyperparameters)
+        circle_model.set_train_param(tparams)
+        
+    if args.train is not None:
+        tparam = circle_model.get_train_param()
+        if tparam is None: 
+            tparam = TrainParam()
+        
+        num_operators = len(circle_model.get_operators())
+        indexes = to_trainable_index(args.train, num_operators)
+        tparam.set_trainable(indexes)
+        
+    print_training_hparameters(circle_model)
 
-    if args.hyperparameters is None:
-        print_training_hparameters(args.input)
+    # if out_file isn't given, rewrite in_file
+    out_file = args.output
+    if out_file is None:
+        out_file = args.input
+    
+    
+    if args.output is None:
+        
+
+    if args.output(attrs=None, header='Set-Cookie:')
 
     else:
+        operators = circle_model.get_operators()
+        op_indexes = to_trainable_index(args.train, len(operators))
+        
+        inject_hparams()
+        
         inject_hparams(args.input, args.hyperparameters, args.train, args.output)
