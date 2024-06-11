a3dbe829b37b26712c079f84b492f15dd7121cb9
Balyshev Artem <43214667+BalyshevArtem@users.noreply.github.com>
2024-06-11 18:35:32 +0300

    [onert-micro] Introduce OMTraining entities (#13145)
    This pr introduces OMTrainingInterpreter, OMTrainingContext and OMTrainingRuntimeModule entities.
    
    ONE-DCO-1.0-Signed-off-by: Artem Balyshev <a.balyshev@samsung.com>

--- onert-micro/onert-micro/include/OMConfig.h
+++ onert-micro/onert-micro/include/OMConfig.h
@@ -17,13 +17,86 @@
 #ifndef ONERT_MICRO_CONFIG_H
 #define ONERT_MICRO_CONFIG_H
 
+#include <stdint.h>
+#include <stdlib.h>
+
 namespace onert_micro
 {
 
+/*
+ * OMTrainOptimizer - enum to store optimizers supported by training with onert-micro
+ */
+enum OMTrainOptimizer
+{
+  SGD,
+  ADAM,
+};
+
+/*
+ * OMMetrics - enum to store metrics supported by training with onert-micro
+ */
+enum OMMetrics
+{
+  MSE_METRICS,
+  MAE_METRICS,
+  CROSS_ENTROPY_METRICS,
+  ACCURACY,
+};
+
+/*
+ * OMLoss - enum to store loss supported by training with onert-micro
+ */
+enum OMLoss
+{
+  BINARY_CROSS_ENTROPY,
+  CROSS_ENTROPY,
+  MSE,
+  MAE,
+};
+
+/*
+ * OMTrainingContext contains training specific parameters
+ * batch_size - batch size for training (Note: 1 - default one)
+ * num_of_train_layers - number of trainable last layers (Note: 0 - all layers will be trained)
+ * optimizer - optimizer which onert-micro training will be used (Note: SGD - default one)
+ * loss - loss which onert-micro training will be used (Note: CROSS_ENTROPY - default one)
+ * learning_rate - used by all optimizers
+ * beta - used by ADAM optimizer
+ * beta_squares - used by ADAM optimizer
+ * epsilon - used by ADAM optimizer
+ * num_Step - used by ADAM optimizer
+ */
+struct OMTrainingContext
+{
+  uint32_t batch_size = 1;
+  uint32_t num_of_train_layers = 0;
+  OMTrainOptimizer optimizer = SGD;
+  OMLoss loss = MSE;
+  float learning_rate = 0.001f;
+  float beta = 0.9f;
+  float beta_squares = 0.9f;
+  float epsilon = 10e-8;
+  uint32_t num_step = 0;
+  uint32_t num_epoch = 0;
+};
+
+/*
+ * OMConfig - main config to store information for runtime and for training runtime
+ * keep_input - will the allocated memory be saved for the input data, or can it be deleted after
+ * use cmsis_nn - will CMSIS NN kernels be used or not (needed to some internal settings) wof_ptr -
+ * a pointer to the data that stores weights separate from the model train_mode - a flag to indicate
+ * whether we are currently in training mode or not
+ */
 struct OMConfig
 {
   bool keep_input = false;
   bool cmsis_nn = false;
+  // For case with divided weights and circle file
+  char *wof_ptr = nullptr;
+  bool train_mode = false;
+  char *model_ptr = nullptr;
+  size_t model_size = 0;
+  OMTrainingContext training_context = {};
 };
 
 } // namespace onert_micro
