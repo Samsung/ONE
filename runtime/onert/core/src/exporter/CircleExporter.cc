0aabf8b18cedddea2b6610a8ea75e7dc34d7814d
Jiyoung Yun <jy910.yun@samsung.com>
2024-06-18 15:09:01 +0900

    Fix out of range issue
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

--- runtime/onert/core/src/exporter/CircleExporter.cc
+++ runtime/onert/core/src/exporter/CircleExporter.cc
@@ -57,19 +57,35 @@ CircleExporter::~CircleExporter() { finish(); }
 
 void CircleExporter::updateWeight(const std::unique_ptr<exec::Execution> &exec)
 {
+  static std::mutex mutex;
+
   exec->iterateTrainableTensors(
     [&](const ir::OperandIndex &idx, const backend::train::ITrainableTensor *tensor) {
       const auto &subgs = _model->subgraphs;
       if (subgs.size() != 1)
         throw std::runtime_error("Circle does not has valid subgraph or has multiple subgraphs");
 
-      const auto &subg = subgs.at(0); // Get 1st subgraph
-      if (!idx.valid() || idx.value() >= subg->tensors.size())
-        throw std::runtime_error("Trainable tensor index is out of range");
+      if (!idx.valid())
+        throw std::runtime_error("Trainable tensor is invalid");
 
-      const auto buf_idx = subg->tensors.at(idx.value())->buffer;
-      if (buf_idx >= _model->buffers.size())
-        throw std::runtime_error("Buffer for trainable tensors is invalid");
+      uint32_t buf_idx = -1;
+      const auto &subg = subgs.at(0); // Get 1st subgraph
+      if (idx.value() >= subg->tensors.size())
+      {
+        std::lock_guard<std::mutex> guard(mutex);
+        auto buffer = std::make_unique<::circle::BufferT>();
+        buffer->size = tensor->total_size();
+        buffer->data.resize(buffer->size);
+
+        buf_idx = _model->buffers.size();
+        _model->buffers.push_back(std::move(buffer));
+      }
+      else
+      {
+        buf_idx = subg->tensors.at(idx.value())->buffer;
+        if (buf_idx >= _model->buffers.size())
+          throw std::runtime_error("Buffer for trainable tensors is invalid");
+      }
 
       const auto &buffer = _model->buffers.at(buf_idx);
 
