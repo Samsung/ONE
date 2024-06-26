98f85b655e96e16989b3476d2015a00059d31c77
Artem Balyshev <a.balyshev@samsung.com>
2024-06-26 12:04:22 +0300

    [onert-micro] Reduce Relu code duplication
    This pr reduces code duplication for Relu.
    
    ONE-DCO-1.0-Signed-off-by: Artem Balyshev <a.balyshev@samsung.com>

--- onert-micro/onert-micro/src/execute/kernels/Relu6.cpp
+++ onert-micro/onert-micro/src/execute/kernels/Relu6.cpp
@@ -14,85 +14,14 @@
  * limitations under the License.
  */
 
-#include "OMStatus.h"
-
-#include "core/OMUtils.h"
-
-#include "execute/OMKernelExecutionBuilder.h"
-#include "execute/OMRuntimeKernel.h"
-
-#include "PALReluCommon.h"
+#include "execute/kernels/ReluCommon.h"
 
 using namespace onert_micro;
 using namespace onert_micro::execute;
 
-namespace
-{
-
-constexpr uint32_t inputTensorIdx = 0;
-constexpr uint32_t outputTensorIdx = 0;
-
-} // namespace
-
 // NOTE: doesnt currently support dynamic shapes
 OMStatus onert_micro::execute::execute_kernel_CircleRelu6(const OMExecuteArgs &execute_args)
 {
-  core::OMRuntimeContext &runtime_context = execute_args.runtime_context;
-  core::OMRuntimeStorage &runtime_storage = execute_args.runtime_storage;
-  uint16_t op_index = execute_args.kernel_index;
-
-  const circle::Tensor *input = nullptr;
-  const circle::Tensor *output = nullptr;
-
-  uint8_t *input_data = nullptr;
-  uint8_t *output_data = nullptr;
-
-  OMStatus status = Ok;
-
-  OMRuntimeKernel runtime_kernel;
-  runtime_kernel.readKernel(op_index, runtime_context);
-
-  input = runtime_kernel.inputs[inputTensorIdx];
-  output = runtime_kernel.outputs[outputTensorIdx];
-
-  assert(input != nullptr);
-  assert(output != nullptr);
-
-  status = runtime_kernel.getDataFromStorage(op_index, runtime_storage, runtime_context);
-  if (status != Ok)
-    return status;
-
-  input_data = runtime_kernel.inputs_data[inputTensorIdx];
-  output_data = runtime_kernel.outputs_data[outputTensorIdx];
-
-  assert(input_data != nullptr);
-  assert(output_data != nullptr);
-
-  switch (input->type())
-  {
-#ifndef DIS_FLOAT
-    case circle::TensorType_FLOAT32:
-    {
-
-      core::OMRuntimeShape input_shape(input);
-      core::OMRuntimeShape output_shape(output);
-
-      const float *input_data_float = core::utils::castInputData<float>(input_data);
-      float *output_data_float = core::utils::castOutputData<float>(output_data);
-
-      assert(output_data_float);
-      const int flat_size = input_shape.flatSize();
-
-      status = pal::ReLUCommon(flat_size, input_data_float, output_data_float, 0.0f, true);
-    }
-    break;
-#endif // DIS_FLOAT
-    default:
-    {
-      status = UnsupportedType;
-      assert(false && "Unsupported type.");
-    }
-  }
-
-  return status;
+  bool is_relu_6 = true;
+  return execute_relu_common(execute_args, is_relu_6);
 }
