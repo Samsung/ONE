c782a93d38a4f90e3d34613dfde9a7609038b88e
Jang Jiseob <ragmani0216@gmail.com>
2024-07-05 11:54:35 +0900

    [onert] Introduce UseDefChain (#13338)
    This commit introduces UseDefChain that contains use-def edges between operands and operartions.
    
    ONE-DCO-1.0-Signed-off-by: ragmani <ragmani0216@gmail.com>

---
+++ runtime/onert/core/include/ir/train/UseDefChain.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2024 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ONERT_IR_TRAIN_USEDEFCHAIN_H__
+#define __ONERT_IR_TRAIN_USEDEFCHAIN_H__
+
+#include "ir/Operand.h"
+#include "ir/train/Index.h"
+
+#include <set>
+
+namespace onert
+{
+namespace ir
+{
+namespace train
+{
+
+class UseDefChain
+{
+public:
+  explicit UseDefChain(const Operand &operand) : _operand{operand}
+  {
+    // DO NOTHING
+  }
+  explicit UseDefChain(const UseDefChain &other)
+    : _operand{other._operand}, _uses{other._uses}, _defs{other._defs}
+  {
+    // DO NOTHING
+  }
+  UseDefChain(UseDefChain &&other)
+    : _operand{other._operand}, _uses{std::move(other._uses)}, _defs{std::move(other._defs)}
+  {
+    // DO NOTHING
+  }
+  ~UseDefChain() = default;
+
+public:
+  UseDefChain &operator=(const UseDefChain &other) = delete;
+  UseDefChain &operator=(UseDefChain &&other) = delete;
+
+public:
+  bool operator==(const UseDefChain &other) const;
+
+public:
+  const std::set<TrainingOperationIndex> &getTrainingUses() const { return _uses; }
+  const std::set<TrainingOperationIndex> &getTrainingDefs() const { return _defs; }
+  void insertTrainingUse(const TrainingOperationIndex &idx);
+  void removeTrainingUse(const TrainingOperationIndex &idx);
+  void insertTrainingDef(const TrainingOperationIndex &idx);
+  void removeTrainingDef(const TrainingOperationIndex &idx);
+  void clearTrainingUseDefs();
+
+public:
+  const Operand &operand() const { return _operand; };
+
+private:
+  const Operand &_operand;
+
+private:
+  std::set<TrainingOperationIndex> _uses;
+  std::set<TrainingOperationIndex> _defs;
+};
+
+} // namespace train
+} // namespace ir
+} // namespace onert
+
+#endif // __ONERT_IR_TRAIN_USEDEFCHAIN_H__
---
+++ runtime/onert/core/src/ir/train/UseDefChain.cc
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2024 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ir/train/UseDefChain.h"
+
+namespace onert
+{
+namespace ir
+{
+namespace train
+{
+
+void UseDefChain::insertTrainingUse(const TrainingOperationIndex &idx) { _uses.insert(idx); }
+
+void UseDefChain::removeTrainingUse(const TrainingOperationIndex &idx) { _uses.erase(idx); }
+
+void UseDefChain::insertTrainingDef(const TrainingOperationIndex &idx)
+{
+  // defs must be valid
+  assert(idx.valid());
+  _defs.insert(idx);
+}
+
+void UseDefChain::removeTrainingDef(const TrainingOperationIndex &idx) { _defs.erase(idx); }
+
+void UseDefChain::clearTrainingUseDefs()
+{
+  _uses.clear();
+  _defs.clear();
+}
+
+bool UseDefChain::operator==(const UseDefChain &other) const
+{
+  return &_operand == &other._operand && _uses == other._uses && _defs == other._defs;
+}
+
+} // namespace train
+} // namespace ir
+} // namespace onert
