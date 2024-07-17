ac5a8e9186b7037006f07a47b6af478d24db322f
Hyeongseok Oh <hseok82.oh@samsung.com>
2024-07-17 10:33:28 +0900

    [infra] Bump up CpuInfo (#13450)
    This commit updates CpuInfo version to resolve build fail on RISC-V.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/cmake/packages/CpuInfoSourceConfig.cmake
+++ infra/cmake/packages/CpuInfoSourceConfig.cmake
@@ -8,8 +8,8 @@ function(_CpuInfoSource_import)
   nnas_include(OptionTools)
 
   envoption(EXTERNAL_DOWNLOAD_SERVER "https://github.com")
-  # CPUINFO commit from tflite v2.16.1
-  envoption(CPUINFO_URL ${EXTERNAL_DOWNLOAD_SERVER}/pytorch/cpuinfo/archive/ef634603954d88d2643d5809011288b890ac126e.tar.gz)
+  # CPUINFO commit for RISC-V bug fix
+  envoption(CPUINFO_URL ${EXTERNAL_DOWNLOAD_SERVER}/pytorch/cpuinfo/archive/6543fec09b2f04ac4a666882998b534afc9c1349.tar.gz)
   ExternalSource_Download(CPUINFO
     DIRNAME CPUINFO
     URL ${CPUINFO_URL})
