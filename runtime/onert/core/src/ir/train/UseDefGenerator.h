fc9940c1fc1d725401f889edd9006bdfbea14d1b
Jang Jiseob <ragmani0216@gmail.com>
2024-07-22 17:48:27 +0900

    [onert] Add generating training usedefs for Pad op (#13457)
    This commit adds generating training usedefs for Pad op.
    
    ONE-DCO-1.0-Signed-off-by: ragmani <ragmani0216@gmail.com>

--- runtime/onert/core/src/ir/train/UseDefGenerator.h
+++ runtime/onert/core/src/ir/train/UseDefGenerator.h
@@ -68,6 +68,7 @@ public:
   void visit(const train::operation::DepthwiseConv2D &node) override;
   void visit(const train::operation::Loss &node) override;
   void visit(const train::operation::Reshape &node) override;
+  void visit(const train::operation::Pad &node) override;
 
 private:
   void insertUse(const TrainingOperandIndex &operand_index, const TrainingOperationIndex &op_index);
