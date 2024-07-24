665206d0b11e60054e9230193ec0a7e1105160de
Hyeongseok Oh <hseok82.oh@samsung.com>
2024-07-24 13:54:30 +0900

    Remove layout test

--- infra/scripts/test_ubuntu_runtime_mixed.sh
+++ infra/scripts/test_ubuntu_runtime_mixed.sh
@@ -26,38 +26,3 @@ echo ""
 pushd ${ROOT_PATH} > /dev/null
 ./Product/out/test/FillFrom_runner
 popd > /dev/null
-
-# NOTE Fixed backend assignment by type of operation
-# TODO Enhance this with randomized test
-BACKENDS=(acl_cl acl_neon cpu)
-
-# Get the intersect of framework test list files
-TESTLIST_PREFIX="Product/out/test/list/tflite_comparator.${TEST_ARCH}"
-SKIPLIST_PREFIX="Product/out/nnapi-gtest/nnapi_gtest.skip.${TEST_ARCH}-${TEST_OS}"
-sort $TESTLIST_PREFIX.${BACKENDS[0]}.list > $TESTLIST_PREFIX.intersect.list
-sort $SKIPLIST_PREFIX.${BACKENDS[0]} > $SKIPLIST_PREFIX.union
-for BACKEND in "${BACKENDS[@]:1}"; do
-    comm -12 <(sort $TESTLIST_PREFIX.intersect.list) <(sort $TESTLIST_PREFIX.$BACKEND.list) > $TESTLIST_PREFIX.intersect.next.list
-    comm <(sort $SKIPLIST_PREFIX.union) <(sort $SKIPLIST_PREFIX.$BACKEND) | tr -d "[:blank:]" > $SKIPLIST_PREFIX.union.next
-    mv $TESTLIST_PREFIX.intersect.next.list $TESTLIST_PREFIX.intersect.list
-    mv $SKIPLIST_PREFIX.union.next $SKIPLIST_PREFIX.union
-done
-popd > /dev/null
-
-# Fail on NCHW layout (acl_cl, acl_neon)
-# TODO Fix bug
-echo "GeneratedTests.*weights_as_inputs*" >> $SKIPLIST_PREFIX.union
-echo "GeneratedTests.logical_or_broadcast_4D_2D_nnfw" >> $SKIPLIST_PREFIX.union
-echo "GeneratedTests.mean" >> $SKIPLIST_PREFIX.union
-echo "GeneratedTests.add_broadcast_4D_2D_after_nops_float_nnfw" >> $SKIPLIST_PREFIX.union
-echo "GeneratedTests.argmax_*" >> $SKIPLIST_PREFIX.union
-echo "GeneratedTests.squeeze_relaxed" >> $SKIPLIST_PREFIX.union
-
-# Run the test
-export OP_BACKEND_Conv2D="cpu"
-export OP_BACKEND_Pool2D="acl_cl"
-export OP_BACKEND_FullyConnected="acl_neon"
-export ACL_LAYOUT="NCHW"
-export RUY_THREADS=4
-NNAPIGTest "acl_cl;acl_neon;cpu" "Product/out/nnapi-gtest/nnapi_gtest.skip.${TEST_ARCH}-${TEST_OS}.union" "report/mixed"
-TFLiteModelVerification "acl_cl;acl_neon;cpu" "${TESTLIST_PREFIX}.intersect.list" "report/mixed"
