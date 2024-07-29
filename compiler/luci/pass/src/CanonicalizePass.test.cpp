ad2feea7c4be15fa3d5365de02105c815ec7e8be
SaeHie Park <saehie.park@gmail.com>
2024-07-29 06:23:16 +0000

    [luci/pass] Canonicalize PadV2 paddings
    This will enable Canonicalize PadV2 paddings dtype.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/pass/src/CanonicalizePass.test.cpp
+++ compiler/luci/pass/src/CanonicalizePass.test.cpp
@@ -43,10 +43,13 @@ struct PadGraphlet
   void init(loco::Graph *g)
   {
     _pad = g->nodes()->create<luci::CirclePad>();
+    _padv2 = g->nodes()->create<luci::CirclePadV2>();
     _paddings_s32 = g->nodes()->create<luci::CircleConst>();
     _paddings_s64 = g->nodes()->create<luci::CircleConst>();
+    // NOTE PadV2.constant_values is not set as test doesn't use this
 
     _pad->name("pad");
+    _padv2->name("padv2");
     _paddings_s32->name("paddings_s32");
     _paddings_s64->name("paddings_s64");
 
@@ -75,6 +78,7 @@ struct PadGraphlet
   }
 
   luci::CirclePad *_pad = nullptr;
+  luci::CirclePadV2 *_padv2 = nullptr;
   luci::CircleConst *_paddings_s32 = nullptr;
   luci::CircleConst *_paddings_s64 = nullptr;
 };
@@ -93,6 +97,23 @@ public:
     _pad->paddings(_paddings_s64);
 
     output()->from(_pad);
+  }
+};
+
+class CanonicalizePadV2TestGraph : public TestIOGraph, public PadGraphlet
+{
+public:
+  CanonicalizePadV2TestGraph() = default;
+
+  void init(void)
+  {
+    TestIOGraph::init({1, 3, 3, 2}, {1, 5, 5, 2});
+    PadGraphlet::init(g());
+
+    _padv2->input(input());
+    _padv2->paddings(_paddings_s64);
+
+    output()->from(_padv2);
   }
 };
 
@@ -147,6 +168,59 @@ TEST(CanonicalizePassPadTest, paddings_32_over_NEG)
   EXPECT_FALSE(pass.run(g.g()));
 
   luci::CircleConst *paddings = dynamic_cast<luci::CircleConst *>(g._pad->paddings());
+  EXPECT_NE(nullptr, paddings);
+  EXPECT_EQ(paddings->dtype(), loco::DataType::S64);
+}
+
+TEST(CanonicalizePassPadV2Test, paddings_64_to_32)
+{
+  CanonicalizePadV2TestGraph g;
+  luci::CanonicalizePass pass;
+
+  g.init();
+
+  luci::CircleConst *paddings = dynamic_cast<luci::CircleConst *>(g._padv2->paddings());
+  EXPECT_NE(nullptr, paddings);
+  EXPECT_EQ(paddings->dtype(), loco::DataType::S64);
+
+  EXPECT_TRUE(pass.run(g.g()));
+
+  paddings = dynamic_cast<luci::CircleConst *>(g._padv2->paddings());
+  EXPECT_NE(nullptr, paddings);
+  EXPECT_EQ(paddings->dtype(), loco::DataType::S32);
+}
+
+TEST(CanonicalizePassPadV2Test, paddings_32_NEG)
+{
+  CanonicalizePadV2TestGraph g;
+  luci::CanonicalizePass pass;
+
+  g.init();
+  g._padv2->paddings(g._paddings_s32);
+
+  luci::CircleConst *paddings = dynamic_cast<luci::CircleConst *>(g._padv2->paddings());
+  EXPECT_NE(nullptr, paddings);
+  EXPECT_EQ(paddings->dtype(), loco::DataType::S32);
+
+  EXPECT_FALSE(pass.run(g.g()));
+
+  paddings = dynamic_cast<luci::CircleConst *>(g._padv2->paddings());
+  EXPECT_NE(nullptr, paddings);
+  EXPECT_EQ(paddings->dtype(), loco::DataType::S32);
+}
+
+TEST(CanonicalizePassPadV2Test, paddings_32_over_NEG)
+{
+  CanonicalizePadV2TestGraph g;
+  luci::CanonicalizePass pass;
+
+  g.init();
+  g._paddings_s64->at<loco::DataType::S64>(2) =
+    static_cast<int64_t>(std::numeric_limits<int32_t>::max()) + 100;
+
+  EXPECT_FALSE(pass.run(g.g()));
+
+  luci::CircleConst *paddings = dynamic_cast<luci::CircleConst *>(g._padv2->paddings());
   EXPECT_NE(nullptr, paddings);
   EXPECT_EQ(paddings->dtype(), loco::DataType::S64);
 }
