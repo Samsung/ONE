19b0b2b2f3a55b33a726f0e414fe181e949dde86
SaeHie Park <saehie.park@gmail.com>
2024-07-29 16:54:02 +0900

    [luci/pass] Canonicalize PadV2 paddings (#13542)
    This will enable Canonicalize PadV2 paddings dtype.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/pass/src/CanonicalizePass.cpp
+++ compiler/luci/pass/src/CanonicalizePass.cpp
@@ -73,6 +73,51 @@ bool paddings_to_s32(luci::CirclePad *pad)
   return true;
 }
 
+/**
+ * Convert S64 CircleConst paddings to S32
+ */
+bool paddings_to_s32(luci::CirclePadV2 *padv2)
+{
+  // check conditions
+  auto paddings = dynamic_cast<luci::CircleConst *>(padv2->paddings());
+  CHECK_OR_FALSE(paddings);
+  CHECK_OR_FALSE(paddings->dtype() == loco::DataType::S64);
+
+  // TODO relocate to helpers/CreateCircleConst.h when necessary
+  auto num_elements = paddings->size<loco::DataType::S64>();
+  auto hval = static_cast<int64_t>(std::numeric_limits<int32_t>::max());
+  auto lval = static_cast<int64_t>(std::numeric_limits<int32_t>::lowest());
+  for (uint32_t i = 0; i < num_elements; i++)
+  {
+    auto v64 = paddings->at<loco::DataType::S64>(i);
+    CHECK_OR_FALSE(v64 <= hval);
+    CHECK_OR_FALSE(v64 >= lval);
+  }
+
+  auto paddings_s32 = padv2->graph()->nodes()->create<luci::CircleConst>();
+  paddings_s32->name(paddings->name() + "_S32");
+  paddings_s32->dtype(loco::DataType::S32);
+  paddings_s32->rank(paddings->rank());
+  for (uint32_t i = 0; i < paddings->rank(); i++)
+    paddings_s32->dim(i).set(paddings->dim(i).value());
+  paddings_s32->shape_status(luci::ShapeStatus::VALID);
+  luci::add_origin(paddings_s32, luci::get_origin(paddings));
+
+  paddings_s32->size<loco::DataType::S32>(num_elements);
+  for (uint32_t i = 0; i < num_elements; i++)
+  {
+    auto v64 = paddings->at<loco::DataType::S64>(i);
+    paddings_s32->at<loco::DataType::S32>(i) = static_cast<int32_t>(v64);
+  }
+
+  // replace paddings with S32 dtype
+  padv2->paddings(paddings_s32);
+
+  return true;
+}
+
+// TODO merge both paddings_to_s32 with template
+
 } // namespace
 
 namespace luci
@@ -89,6 +134,11 @@ bool CanonicalizePass::run(loco::Graph *g)
     if (auto pad = dynamic_cast<luci::CirclePad *>(node))
     {
       if (paddings_to_s32(pad))
+        changed = true;
+    }
+    else if (auto padv2 = dynamic_cast<luci::CirclePadV2 *>(node))
+    {
+      if (paddings_to_s32(padv2))
         changed = true;
     }
 
