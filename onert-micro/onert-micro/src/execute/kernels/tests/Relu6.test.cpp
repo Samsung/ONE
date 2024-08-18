f53f10ccd3294373189d09e6ecab101c012c6e79
chunseoklee <chunseoklee@gmail.com>
2024-08-18 11:21:26 +0900

    [onert-micro] Relu6 test added

--- onert-micro/onert-micro/src/execute/kernels/tests/Relu6.test.cpp
+++ onert-micro/onert-micro/src/execute/kernels/tests/Relu6.test.cpp
@@ -17,6 +17,7 @@
 #include "execute/OMTestUtils.h"
 #include "test_models/relu6/FloatReLU6Kernel.h"
 #include "test_models/relu6/NegReLU6Kernel.h"
+#include "test_models/relu6/Int8ReLU6Kernel.h"
 
 namespace onert_micro
 {
@@ -39,6 +40,14 @@ TEST_F(ReLU6Test, Float_P)
     onert_micro::execute::testing::checkKernel<float>(1, &test_data_kernel);
   EXPECT_THAT(output_data_vector,
               FloatArrayNear(test_data_kernel.get_output_data_by_index(0), 0.0001f));
+}
+
+TEST_F(ReLU6Test, S8_P)
+{
+  onert_micro::test_model::TestDataS8ReLU6 test_data_kernel;
+  std::vector<int8_t> output_data_vector =
+    onert_micro::execute::testing::checkKernel<int8_t>(1, &test_data_kernel);
+  EXPECT_THAT(output_data_vector, test_data_kernel.get_output_data_by_index(0));
 }
 
 TEST_F(ReLU6Test, Input_output_type_mismatch_NEG)
