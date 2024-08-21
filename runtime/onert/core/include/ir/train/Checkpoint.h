34257bfe11c57c51ef0a1c9c5fe98712a4d100a8
Jiyoung Giuliana Yun <jy910.yun@samsung.com>
2024-08-21 10:07:29 +0900

    [onert] Add ir namespace to checkpoint header file (#13722)
    This commit adds the missing ir namespace to checkpoint header file.
    
    ONE-DCO-1.0-Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>

--- runtime/onert/core/include/ir/train/Checkpoint.h
+++ runtime/onert/core/include/ir/train/Checkpoint.h
@@ -19,6 +19,8 @@
 
 namespace onert
 {
+namespace ir
+{
 namespace train
 {
 namespace checkpoint
@@ -46,6 +48,7 @@ constexpr uint8_t SCHEMA_VERSION = 1;
 
 } // namespace checkpoint
 } // namespace train
+} // namespace ir
 } // namespace onert
 
 #endif // __ONERT_IR_TRAIN_CHECKPOINT_H__
