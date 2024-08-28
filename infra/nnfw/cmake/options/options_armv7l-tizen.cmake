c63071e6e0161a36c658d209d84d6d0e3b5377c5
Hyeongseok Oh <hseok82.oh@samsung.com>
2024-08-28 20:55:42 +0900

    [onert/contrib] Disable logging runtime build as default
    This commit disables logging runtime build in contrib directory as default.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/options/options_armv7l-tizen.cmake
+++ infra/nnfw/cmake/options/options_armv7l-tizen.cmake
@@ -7,6 +7,5 @@ option(DOWNLOAD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
 option(DOWNLOAD_NEON2SSE "Download NEON2SSE library source" OFF)
 option(DOWNLOAD_GTEST "Download Google Test source and build Google Test" OFF)
 
-option(BUILD_LOGGING "Build logging runtime" OFF)
 option(GENERATE_RUNTIME_NNAPI_TESTS "Generate NNAPI operation gtest" OFF)
 option(ENVVAR_ONERT_CONFIG "Use environment variable for onert configuration" OFF)
