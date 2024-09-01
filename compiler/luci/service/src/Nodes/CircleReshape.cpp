f092ed0c6460e37c1f01d247a79ad44fadeeec8b
양종원 <yjw963@gmail.com>
2024-09-01 23:56:41 +0900

    WIP

--- compiler/luci/service/src/Nodes/CircleReshape.cpp
+++ compiler/luci/service/src/Nodes/CircleReshape.cpp
@@ -43,57 +43,120 @@ luci::CircleNode *CloneNodeLet<CN::OPQR>::visit(const luci::CircleReshape *node)
 namespace sinf
 {
 
+/**
+ * @note  CircleReshape has new shape info in two places: 2nd input and attribute.
+ *        This shape inference uses shape from input 'shape' node when it's constant.
+ *        If not, shape will be from node itself. shape from attribute is not used.
+ *
+ * TODO Change this policy when not appropriate
+ */
 loco::TensorShape Algorithm::visit(const luci::CircleReshape *node)
 {
   LOGGER(l);
 
   const loco::DataType S32 = loco::DataType::S32;
 
-  loco::TensorShape shape_by_input;
+  // loco::TensorShape shape_by_input;
+  // {
+  //   LUCI_ASSERT(node->shape(), "2nd input shape() should not be nullptr");
+
+  //   // Only support node's shape() is CircleConst with S32
+  //   // TODO support other node with other types
+  //   auto const_shape_node = dynamic_cast<luci::CircleConst *>(node->shape());
+  //   if (const_shape_node != nullptr)
+  //   {
+  //     LUCI_ASSERT(const_shape_node->dtype() == S32, "Only support int32 CircleConst");
+
+  //     shape_by_input.rank(const_shape_node->size<S32>());
+
+  //     for (uint32_t axis = 0; axis < shape_by_input.rank(); ++axis)
+  //     {
+  //       shape_by_input.dim(axis) = const_shape_node->at<S32>(axis);
+  //       if (const_shape_node->at<S32>(axis) < 0)
+  //         shape_by_input.dim(axis).unset();
+  //     }
+  //   }
+  //   else
+  //   {
+  //     // We use shape from the node itself
+  //     shape_by_input = own_shape(node);
+  //   }
+  // }
+
+  // loco::TensorShape shape_by_attr;
+  // {
+  //   shape_by_attr.rank(node->newShape()->rank());
+
+  //   for (uint32_t axis = 0; axis < shape_by_attr.rank(); ++axis)
+  //   {
+  //     shape_by_attr.dim(axis) = node->newShape()->dim(axis);
+  //   }
+  // }
+
+  // if (!(shape_by_input == shape_by_attr))
+  // {
+  //   INFO(l) << "CircleReshape: Two new shape information mismatched : " << std::endl;
+  //   INFO(l) << "   shape_by_input : " << shape_by_input << std::endl;
+  //   INFO(l) << "   shape_by_attr : " << shape_by_attr << std::endl;
+  // }
+
+  // loco::TensorShape output_shape = shape_by_input;
+
+  loco::TensorShape output_shape;
   {
-    LUCI_ASSERT(node->shape(), "2nd input shape() should not be nullptr");
-
-    // Only support node's shape() is CircleConst with S32
-    // TODO support other node with other types
-    auto const_shape_node = dynamic_cast<luci::CircleConst *>(node->shape());
-    if (const_shape_node != nullptr)
+    const auto *shape_by_attr = node->newShape();
+    
+    if (shape_by_attr != nullptr)
     {
-      LUCI_ASSERT(const_shape_node->dtype() == S32, "Only support int32 CircleConst");
-
-      shape_by_input.rank(const_shape_node->size<S32>());
-
-      for (uint32_t axis = 0; axis < shape_by_input.rank(); ++axis)
+      output_shape.rank(shape_by_attr->rank());
+      for (uint32_t axis = 0; axis < output_shape.rank(); ++axis)
       {
-        shape_by_input.dim(axis) = const_shape_node->at<S32>(axis);
-        if (const_shape_node->at<S32>(axis) < 0)
-          shape_by_input.dim(axis).unset();
+        output_shape.dim(axis) = shape_by_attr->dim(axis);
+        if (shape_by_attr->dim(axis) < 0)
+          output_shape.dim(axis).unset();
       }
     }
     else
     {
-      // We use shape from the node itself
-      shape_by_input = own_shape(node);
+      node->shape();
     }
   }
 
-  loco::TensorShape shape_by_attr;
+  loco::TensorShape output_shape;
   {
-    shape_by_attr.rank(node->newShape()->rank());
+    LUCI_ASSERT(node->shape(), "2nd input shape() should not be nullptr");
+    // Only support node's shape() is CircleConst with S32
+    // https://github.com/tensorflow/tensorflow/blob/2fe29379f6449d263b787eb636020fd81017b8bf/tensorflow/lite/kernels/reshape.cc#L130
 
-    for (uint32_t axis = 0; axis < shape_by_attr.rank(); ++axis)
+    auto const_shape_node = dynamic_cast<luci::CircleConst *>(node->shape());
+    std::cout << "node->shape(): " << node->shape() << std::endl;
+    std::cout << "node->newShape(): " << node->newShape() << std::endl;
+    std::cout << "const_shape_node: " << const_shape_node << std::endl;
+
+
+    if (const_shape_node != nullptr)
     {
-      shape_by_attr.dim(axis) = node->newShape()->dim(axis);
+      LUCI_ASSERT(const_shape_node->dtype() == S32, "Only support int32 CircleConst");
+      output_shape.rank(const_shape_node->size<S32>());
+      for (uint32_t axis = 0; axis < output_shape.rank(); ++axis)
+      {
+        output_shape.dim(axis) = const_shape_node->at<S32>(axis);
+        if (const_shape_node->at<S32>(axis) < 0)
+          output_shape.dim(axis).unset();
+      }
+    }
+    else
+    {
+      std::cout << "const_shape_node IS nullptr " << std::endl;
+      std::cout << "node->newShape(): " << node->newShape() << std::endl;
+      std::cout << "node->newShape()->rank(): " << node->newShape()->rank() << std::endl;
+      output_shape.rank(node->newShape()->rank());
+      for (uint32_t axis = 0; axis < output_shape.rank(); ++axis)
+      {
+        output_shape.dim(axis) = node->newShape()->dim(axis);
+      }
     }
   }
-
-  if (!(shape_by_input == shape_by_attr))
-  {
-    INFO(l) << "CircleReshape: Two new shape information mismatched : " << std::endl;
-    INFO(l) << "   shape_by_input : " << shape_by_input << std::endl;
-    INFO(l) << "   shape_by_attr : " << shape_by_attr << std::endl;
-  }
-
-  loco::TensorShape output_shape = shape_by_input;
 
   // One of the dimensions of input shape can have special value -1,
   // which should not be inferred.
