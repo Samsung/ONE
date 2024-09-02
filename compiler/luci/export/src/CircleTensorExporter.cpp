df118e3fb3b93b91945783ae11561403c85b9e78
SaeHie Park <saehie.park@gmail.com>
2024-09-03 06:52:12 +0900

    [luci/export] Clear model data in prepareModelData (#13891)
    This will clear model data in prepareModelData method.
    - this is to clear before second pass
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/luci/export/src/CircleTensorExporter.cpp
+++ compiler/luci/export/src/CircleTensorExporter.cpp
@@ -680,6 +680,8 @@ namespace luci
 
 void prepareModelData(FlatBufferBuilder &builder, SerializedModelData &md)
 {
+  md.clear();
+
   // add one empty buffer
   //   note: this follows TFLite
   //   note: there's a comment in tflite fbs file
