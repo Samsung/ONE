481547b348efc1e7f6f6dbfc5bcacbf3f8b9ce6e
CS Park <67962828+pcs1265@users.noreply.github.com>
2024-09-02 16:10:07 +0900

    [luci/service] Migrate rsqrt shape inference rule (#13857)
    This commit migrates rsqrt shape inference rule to sinf::Algorithm
    
    ONE-DCO-1.0-Signed-off-by: Chansu Park <1265pcs@gmail.com>

--- compiler/luci/service/src/Nodes/CircleRsqrt.test.cpp
+++ compiler/luci/service/src/Nodes/CircleRsqrt.test.cpp
@@ -15,6 +15,7 @@
  */
 
 #include "luci/Service/CircleNodeClone.h"
+#include "luci/Service/CircleShapeInference.h"
 
 #include <gtest/gtest.h>
 
@@ -30,4 +31,83 @@ TEST(CloneNodeTest, clone_Rsqrt)
 
   auto cloned_rsqrt = dynamic_cast<luci::CircleRsqrt *>(cloned);
   ASSERT_NE(nullptr, cloned_rsqrt);
+}
+
+TEST(ShapeRuleTest, rsqrt_dynamic_shape)
+{
+  luci::CircleInput input;
+  luci::CircleRsqrt rsqrt;
+
+  loco::TensorShape shape;
+  luci::sinf::Rule shape_inf_rule;
+
+  input.shape({1, 1, 3, 4});
+  input.shape_status(luci::ShapeStatus::VALID);
+  input.dim(0).unset();
+  input.dim(1).unset();
+
+  rsqrt.x(&input);
+
+  ASSERT_TRUE(shape_inf_rule.infer(&rsqrt, shape));
+  ASSERT_EQ(shape.rank(), 4);
+  ASSERT_FALSE(shape.dim(0).known());
+  ASSERT_FALSE(shape.dim(1).known());
+  ASSERT_TRUE(shape.dim(2).known());
+  ASSERT_TRUE(shape.dim(3).known());
+
+  ASSERT_EQ(0, shape.dim(0).value());
+  ASSERT_EQ(0, shape.dim(1).value());
+  ASSERT_EQ(3, shape.dim(2).value());
+  ASSERT_EQ(4, shape.dim(3).value());
+}
+
+TEST(ShapeRuleTest, rsqrt_static_shape)
+{
+  luci::CircleInput input;
+  luci::CircleRsqrt rsqrt;
+
+  loco::TensorShape shape;
+  luci::sinf::Rule shape_inf_rule;
+
+  input.shape({3, 2, 3, 4});
+  input.shape_status(luci::ShapeStatus::VALID);
+
+  rsqrt.x(&input);
+
+  ASSERT_TRUE(shape_inf_rule.infer(&rsqrt, shape));
+  ASSERT_EQ(shape.rank(), 4);
+  ASSERT_TRUE(shape.dim(0).known());
+  ASSERT_TRUE(shape.dim(1).known());
+  ASSERT_TRUE(shape.dim(2).known());
+  ASSERT_TRUE(shape.dim(3).known());
+
+  ASSERT_EQ(3, shape.dim(0).value());
+  ASSERT_EQ(2, shape.dim(1).value());
+  ASSERT_EQ(3, shape.dim(2).value());
+  ASSERT_EQ(4, shape.dim(3).value());
+}
+
+TEST(ShapeRuleTest, rsqrt_nullptr_input_NEG)
+{
+  luci::CircleRsqrt rsqrt;
+
+  loco::TensorShape shape;
+  luci::sinf::Rule shape_inf_rule;
+
+  rsqrt.x(nullptr);
+  ASSERT_ANY_THROW(shape_inf_rule.infer(&rsqrt, shape));
+}
+
+TEST(ShapeRuleTest, rsqrt_shape_not_ready_NEG)
+{
+  luci::CircleInput input;
+  luci::CircleRsqrt rsqrt;
+
+  loco::TensorShape shape;
+  luci::sinf::Rule shape_inf_rule;
+
+  input.shape_status(luci::ShapeStatus::UNDEFINED);
+
+  rsqrt.x(&input);
+  ASSERT_FALSE(shape_inf_rule.infer(&rsqrt, shape));
 }
