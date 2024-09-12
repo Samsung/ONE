3a3e50bdb27ed315d35579fd2589b328e9257cf7
bokyeong-Lee <149753416+kyeong8139@users.noreply.github.com>
2024-09-12 12:41:03 +0900

    [luci/service] migrate Range shape inference rule to sinf::Algorithm (#13987)
    This commit migrate Range shape inference rule to sinf::Algorithm.
    
    ONE-DCO-1.0-Signed-off-by: bokyeong Lee <kyeong8139@gmail.com>

--- compiler/luci/service/src/Nodes/CircleRange.cpp
+++ compiler/luci/service/src/Nodes/CircleRange.cpp
@@ -14,7 +14,12 @@
  * limitations under the License.
  */
 
+#include "luci/Service/CircleShapeInference.h"
+
 #include "CircleCloneNode.h"
+#include "CircleShapeInferenceHelper.h"
+
+#include <cmath>
 
 namespace luci
 {
@@ -23,5 +28,67 @@ luci::CircleNode *CloneNodeLet<CN::OPQR>::visit(const luci::CircleRange *)
 {
   return _graph->nodes()->create<luci::CircleRange>();
 }
+
+namespace sinf
+{
+
+loco::TensorShape Algorithm::visit(const luci::CircleRange *node)
+{
+  loco::TensorShape output_shape;
+  output_shape.rank(1);
+
+  auto start_node = dynamic_cast<luci::CircleConst *>(node->start());
+  auto limit_node = dynamic_cast<luci::CircleConst *>(node->limit());
+  auto delta_node = dynamic_cast<luci::CircleConst *>(node->delta());
+
+  if (start_node == nullptr || limit_node == nullptr || delta_node == nullptr)
+  {
+    // We use shape from the node itself
+    loco::TensorShape shape;
+    shape.rank(node->rank());
+    for (uint32_t r = 0; r < node->rank(); ++r)
+    {
+      // TODO remove this copy from `use_own(node);`
+      // Shape inference rules in this file did not consider unknown dimension.
+      // If some node has unknown dimension, 0 is inserted and wrong shape
+      // inference was done as a result.
+      // To fix this, new shape inference algorithm is being implemented.
+      // Until new inference algorithm is fully implemented, unknown dimension
+      // would be represented as 1 along with TFLite expression.
+      shape.dim(r) = node->dim(r).known() ? node->dim(r).value() : 1;
+    }
+    return shape;
+  }
+
+  double start = 0, limit = 0, delta = 0;
+
+#define GET_RANGE_PARAM(DT)         \
+  start = start_node->scalar<DT>(); \
+  limit = limit_node->scalar<DT>(); \
+  delta = delta_node->scalar<DT>();
+
+  switch (start_node->dtype())
+  {
+    case loco::DataType::FLOAT32:
+      GET_RANGE_PARAM(loco::DataType::FLOAT32)
+      break;
+    case loco::DataType::S32:
+      GET_RANGE_PARAM(loco::DataType::S32)
+      break;
+    default:
+      INTERNAL_EXN("Range data type not supported");
+  }
+
+#undef GET_RANGE_PARAM
+
+  if (delta == 0)
+    INTERNAL_EXN("Delta can not be zero");
+
+  output_shape.dim(0) = ceil((limit - start) / delta);
+
+  return output_shape;
+}
+
+} // namespace sinf
 
 } // namespace luci
