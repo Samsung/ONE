7bf462cd170c1013dd96da6f726735d7e4d835e4
icodo98 <rsb98759@gmail.com>
2024-09-12 12:19:25 +0900

    [dredd] Introduce new rule for tensorshape Introduce new dredd-rule for check tensors shape.
    ONE-DCO-1.0-Signed-off-by: JuYoung Lee rsb98759@gmail.com

--- compiler/dredd-rule-lib/rule-lib.sh
+++ compiler/dredd-rule-lib/rule-lib.sh
@@ -252,4 +252,21 @@ const_count()
   echo ${ACTUAL}
 }
 
+tensor_shape()
+{
+  argc_check $# 1
+  file_path_check ${COMPILED_FILE}
+  file_path_check ${INSPECT_PROG_PATH}
+
+  set -o pipefail
+
+  ACTUAL=`init_error_log ; \
+          ${INSPECT_PROG_PATH} --tensor_shape ${COMPILED_FILE} | \
+          awk -v tensor_name="$1" '{ if ($1 == tensor_name) print $2}'`
+
+  check_success_exit_code $? 0
+  
+  echo ${ACTUAL}
+}
+
 # TODO define more qullity test function
