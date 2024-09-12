f0ff2f4d64e191ac5badca8f20de0ddae8798783
SaeHie Park <saehie.park@gmail.com>
2024-09-12 06:54:59 +0000

    [onnx-tools] Enable to show node I/O shapes
    This will enable onnx-dump to show node I/O shapes.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- compiler/onnx-tools/onnx-dump.py
+++ compiler/onnx-tools/onnx-dump.py
@@ -58,6 +58,13 @@ def _gather_value_infos(onnx_model):
     return vis
 
 
+def _type_format(type):
+    dtstr = _data_type_str(type.tensor_type.elem_type)
+    shape = type.tensor_type.shape
+    shape_ar = [dim.dim_value for dim in shape.dim]
+    return '{} {}'.format(dtstr, shape_ar)
+
+
 def _dump_header(onnx_model):
     print("[General] -----------------------------")
     print("IR version =", onnx_model.ir_version)
@@ -108,9 +115,17 @@ def _dump_nodes(onnx_model):
             print('    A {0}'.format(attribute))
 
         for inp in node.input:
-            print('    I "{0}"'.format(inp))
+            inp_vi_str = ''
+            if inp in vis:
+                inp_vi = vis[inp]
+                inp_vi_str = _type_format(inp_vi)
+            print('    I "{0}" {1}'.format(inp, inp_vi_str))
         for out in node.output:
-            print('    O "{0}"'.format(out))
+            out_vi_str = ''
+            if out in vis:
+                out_vi = vis[out]
+                out_vi_str = _type_format(out_vi)
+            print('    O "{0}" {1}'.format(out, out_vi_str))
 
     print("")
 
