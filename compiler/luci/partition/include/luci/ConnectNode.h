f2b14355ea1816972117afb26b8bb64ba8b1eb8f
seockho.kim <seockho.kim@samsung.com>
2024-09-13 18:24:32 +0900

    [luci/partition] Support RmsNorm operation
    This commit supports RmsNorm operation in luci partition.
    
    ONE-DCO-1.0-Signed-off-by: Seockho Kim seockho.kim@samsung.com

--- compiler/luci/partition/include/luci/ConnectNode.h
+++ compiler/luci/partition/include/luci/ConnectNode.h
@@ -187,6 +187,7 @@ public:
   void visit(const luci::CircleBCQGather *) final;
   void visit(const luci::CircleGRU *) final;
   void visit(const luci::CircleInstanceNorm *) final;
+  void visit(const luci::CircleRmsNorm *) final;
 
   // NOTE CircleInput and CircleOutput are not handled here as these need
   //      link with graph I/O
