a37e9009431aa1e166147afd3304d0413904eba7
seockho.kim <seockho.kim@samsung.com>
2024-09-24 11:02:03 +0900

    [circledump] Add RmsNorm to circledump
    This commit supports RmsNorm operation in circledump
    
    ONE-DCO-1.0-Signed-off-by: Seockho Kim seockho.kim@samsung.com

--- compiler/circledump/src/OpPrinter.cpp
+++ compiler/circledump/src/OpPrinter.cpp
@@ -825,6 +825,20 @@ public:
   }
 };
 
+class RmsNormPrinter : public OpPrinter
+{
+public:
+  void options(const circle::Operator *op, std::ostream &os) const override
+  {
+    if (auto *params = op->builtin_options_as_RmsNormOptions())
+    {
+      os << "    ";
+      os << "epsilon(" << params->epsilon() << ") ";
+      os << std::endl;
+    }
+  }
+};
+
 OpPrinterRegistry::OpPrinterRegistry()
 {
   _op_map[circle::BuiltinOperator_ADD] = make_unique<AddPrinter>();
@@ -912,6 +926,7 @@ OpPrinterRegistry::OpPrinterRegistry()
   _op_map[circle::BuiltinOperator_BCQ_GATHER] = make_unique<BCQGatherPrinter>();
   _op_map[circle::BuiltinOperator_GRU] = make_unique<GRUPrinter>();
   _op_map[circle::BuiltinOperator_INSTANCE_NORM] = make_unique<InstanceNormPrinter>();
+  _op_map[circle::BuiltinOperator_RMS_NORM] = make_unique<RmsNormPrinter>();
 }
 
 } // namespace circledump
