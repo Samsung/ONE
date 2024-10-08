1bdfbaf14ceeb49a5431851d422f22d75d148eac
Dayoung Lee <dayoung.lee@samsung.com>
2024-10-08 14:54:52 +0900

    Update compiler/luci/pass/src/SubstituteStridedSliceToReshapePass.test.cpp

--- compiler/luci/pass/src/SubstituteStridedSliceToReshapePass.test.cpp
+++ compiler/luci/pass/src/SubstituteStridedSliceToReshapePass.test.cpp
@@ -70,6 +70,7 @@ public:
                                  const std::initializer_list<uint32_t> vals) {
         auto node = build_rank1_const(graph, vals);
         node->name(name);
+
         return node;
       };
 
