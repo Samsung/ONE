67c203149d22a2cabf852d8cf7616ae65b416417
Ju Young <rsb98759@gmail.com>
2024-10-09 12:35:48 +0000

    [train] update casting to int16
    This commit updates padding type to int16
    
    ONE-DCO-1.0-Signed-off-by: JuYoung Lee rsb98759@gmail.com

--- runtime/onert/backend/train/ops/PoolLayer.cc
+++ runtime/onert/backend/train/ops/PoolLayer.cc
@@ -58,10 +58,10 @@ public:
       _op_params.stride_width = strideWidth;
       _op_params.filter_height = kernelHeight;
       _op_params.filter_width = kernelWidth;
-      assert(paddingTop < (1 << 8));
-      assert(paddingLeft < (1 << 8));
-      _op_params.padding_values.height = static_cast<uint8_t>(paddingTop);
-      _op_params.padding_values.width = static_cast<uint8_t>(paddingLeft);
+      assert(paddingTop < (1 << 15));
+      assert(paddingLeft < (1 << 15));
+      _op_params.padding_values.height = static_cast<int16_t>(paddingTop);
+      _op_params.padding_values.width = static_cast<int16_t>(paddingLeft);
       CalculateActivationRange<float>(activation, &_op_params.float_activation_min,
                                       &_op_params.float_activation_max);
     }
