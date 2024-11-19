a045e0514f67e88ab92114ac7785ff0d51633251
Jonghwa Lee <jonghwa3.lee@samsung.com>
2024-11-19 16:28:50 +0900

    [tools/model_explorer_circle] Introduce circle adapter for model explorer
    It adds the initial version of circle adapter for model explorer.
    It'll simply show a empty main graph when circle file is loaded.
    
    ONE-DCO-1.0-Signed-off-by: Jonghwa Lee <jonghwa3.lee@samsung.com>

---
+++ tools/model_explorer_circle/src/circle_adapter/main.py
@@ -0,0 +1,19 @@
+"""Model Explorer adapter for Circle models."""
+
+from typing import Dict
+from model_explorer import Adapter, AdapterMetadata, ModelExplorerGraphs, graph_builder
+
+
+class CicleAdapter(Adapter):
+    """Adapter class for Circle models."""
+    metadata = AdapterMetadata(id='circle-adapter',
+                               name='Circle adapter',
+                               description='Circle adapter!',
+                               fileExts=['circle'])
+
+    def __init__(self):
+        super().__init__()
+
+    def convert(self, model_path: str, settings: Dict) -> ModelExplorerGraphs:
+        graph = graph_builder.Graph(id='main')
+        return {'graphs': [graph]}
