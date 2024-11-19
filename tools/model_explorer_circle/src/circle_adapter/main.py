d40365729e9b5a0df5a57c2f8747c64ae0b73429
Jonghwa Lee <jonghwa3.lee@samsung.com>
2024-11-19 18:28:52 +0900

    [tools/model_explorer_circle] Load circle model with circle schema
    It will load circle model with circle schema which is generated by
    flatbuffer compiler.
    In the near future, the schema will be auto-generated with the latest
    instead of current builtin one.
    
    ONE-DCO-1.0-Signed-off-by: Jonghwa Lee <jonghwa3.lee@samsung.com>

--- tools/model_explorer_circle/src/circle_adapter/main.py
+++ tools/model_explorer_circle/src/circle_adapter/main.py
@@ -2,6 +2,7 @@
 
 from typing import Dict
 from model_explorer import Adapter, AdapterMetadata, ModelExplorerGraphs, graph_builder
+from circle_adapter import circle_schema_generated as circle_schema
 
 
 class CicleAdapter(Adapter):
@@ -13,7 +14,19 @@ class CicleAdapter(Adapter):
 
     def __init__(self):
         super().__init__()
+        self.model = None
+
+    def load_model(self, model_path: str) -> None:
+        """Load the model from the given path."""
+        with open(model_path, 'rb') as fp:
+            model_ = circle_schema.Model.GetRootAsModel(fp.read(), 0)
+
+        self.model = circle_schema.ModelT.InitFromObj(model_)
 
     def convert(self, model_path: str, settings: Dict) -> ModelExplorerGraphs:
+        """Convert the model to a set of graphs."""
         graph = graph_builder.Graph(id='main')
+
+        self.load_model(model_path)
+
         return {'graphs': [graph]}
