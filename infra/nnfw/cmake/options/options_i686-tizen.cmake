f9f99749715cefc6ccdba42a37b7a2b7252e9a8a
Hyeongseok Oh <hseok82.oh@samsung.com>
2024-12-16 14:52:26 +0900

    [infra/onert] Disable ACL build as default (#14453)
    This commit disables ACL build as default.
    It will be enabled when target is aarch32/64 and OS is android or linux.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/options/options_i686-tizen.cmake
+++ infra/nnfw/cmake/options/options_i686-tizen.cmake
@@ -1,9 +1,7 @@
 #
 # i686 tizen cmake options
 #
-option(BUILD_ARMCOMPUTE "Build ARM Compute from the downloaded source" OFF)
 option(BUILD_TENSORFLOW_LITE "Build TensorFlow Lite from the downloaded source" OFF)
-option(DOWNLOAD_ARMCOMPUTE "Download ARM Compute source" OFF)
 option(DOWNLOAD_GTEST "Download Google Test source and build Google Test" OFF)
 
 option(GENERATE_RUNTIME_NNAPI_TESTS "Generate NNAPI operation gtest" OFF)
