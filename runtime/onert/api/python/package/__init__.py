88bc6afe04c72aee1c1f3bb4fb4f361a869976c9
ragmani <ragmani0216@gmail.com>
2025-01-06 08:01:19 +0000

    [onert/python] Introduce BaseSession
    This commit introduces the class BaseSession.
      - Inroduce the class BaseSession to group common functionalities
      - Modify setup.py to make Python files easier to find.
    
    ONE-DCO-1.0-Signed-off-by: ragmani <ragmani0216@gmail.com>

--- runtime/onert/api/python/package/__init__.py
+++ runtime/onert/api/python/package/__init__.py
@@ -1,2 +1,8 @@
-__all__ = ['infer']
+# Define the public API of the onert package
+__all__ = ["infer", "tensorinfo"]
+
+# Import and expose the infer module's functionalities
 from . import infer
+
+# Import and expose tensorinfo
+from .common import tensorinfo as tensorinfo
