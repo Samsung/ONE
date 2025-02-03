83ea34830221a0811c9633b599a9fdf45115977d
chunseoklee <chunseok.lee@samsung.com>
2025-02-03 19:32:32 +0900

    Disable XNNPack backend on Tizen (#14597)
    This commit disables XNNPack backend build on Tizen.
    
    Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>
    Co-authored-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- infra/nnfw/cmake/options/options_armv7hl-tizen.cmake
+++ infra/nnfw/cmake/options/options_armv7hl-tizen.cmake
@@ -7,3 +7,5 @@ option(DOWNLOAD_GTEST "Download Google Test source and build Google Test" OFF)
 
 option(GENERATE_RUNTIME_NNAPI_TESTS "Generate NNAPI operation gtest" OFF)
 option(ENVVAR_ONERT_CONFIG "Use environment variable for onert configuration" OFF)
+
+option(BUILD_XNNPACK "Build XNNPACK" OFF)
