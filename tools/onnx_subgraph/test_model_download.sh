950c2e48483f9310748fd55cb615a6b6f3afd7ab
chenyx113 <chenyouxin113@qq.com>
2025-02-18 19:50:08 +0800

    [tools/onnx-subgraph]update code as PR reviews
    modify the indent issue, remove useless code, add shebang in bash script,
    modify the typing mistake
    
    ONE-DCO-1.0-Signed-off-by: Youxin Chen <yx113.chen@samsung.com>

--- tools/onnx_subgraph/test_model_download.sh
+++ tools/onnx_subgraph/test_model_download.sh
@@ -1,15 +1,10 @@
-pip install onnx onnxsim
+#!/bin/bash
 
-if [ ! -d "./models/" ];then
-  mkdir ./models/
-  else
-  echo "./models path existing"
-fi
+pip install --user onnx onnxsim
+
+mkdir -p models
 
 cd ./models
 wget https://media.githubusercontent.com/media/onnx/models/refs/heads/main/Computer_Vision/resnext26ts_Opset16_timm/resnext26ts_Opset16.onnx --no-check-certificate
-#wget https://media.githubusercontent.com/media/onnx/models/refs/heads/main/Natural_Language_Processing/xmod_Opset16_transformers/xmod_Opset16.onnx --no-check-certificate
 
 onnxsim resnext26ts_Opset16.onnx ../resnet-test.onnx
-#onnxsim xmod_Opset16.onnx ../xmod-transformer-test.onnx
-
