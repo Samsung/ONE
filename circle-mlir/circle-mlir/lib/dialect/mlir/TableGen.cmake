97d16a59c608c64117bfb14d43f5390dd6f77176
SaeHie Park <saehie.park@gmail.com>
2025-02-18 13:03:15 +0900

    [circle-mlir] Introduce CircleOpInterfaces
    This will introduce CircleOpInterfaces td(target description) file and
    generate necessary include files.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- circle-mlir/circle-mlir/lib/dialect/mlir/TableGen.cmake
+++ circle-mlir/circle-mlir/lib/dialect/mlir/TableGen.cmake
@@ -2,3 +2,9 @@
 file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/mlir)
 
 include_directories("${LLVM_INST_INC}")
+
+set(LLVM_TARGET_DEFINITIONS mlir/CircleOpInterfaces.td)
+mlir_tablegen(mlir/CircleOpInterface.h.inc -gen-op-interface-decls)
+mlir_tablegen(mlir/CircleOpInterface.cc.inc -gen-op-interface-defs)
+mlir_tablegen(mlir/CircleOpsDialect.h.inc -gen-dialect-decls)
+mlir_tablegen(mlir/CircleOpsDialect.cc.inc -gen-dialect-defs)
