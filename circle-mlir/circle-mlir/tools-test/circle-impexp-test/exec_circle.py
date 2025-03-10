9a86a1026bc56e91a24fc694e12e98b115949f29
SaeHie Park <saehie.park@gmail.com>
2025-03-10 13:34:13 +0900

    [circle-mlir/tools-test] Introduce exec_circle
    This will introduce exec_circle script in circle-impexp-test to
    execute circle model with circle-interpreter.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ circle-mlir/circle-mlir/tools-test/circle-impexp-test/exec_circle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+
+from pathlib import Path
+
+
+def exec_model(circle_model):
+    # Execute circle-interpreter
+    one_compiler_root = os.getenv('ONE_COMPILER_ROOT')
+    if not one_compiler_root:
+        one_compiler_root = '/usr/share/one'
+    driver = Path(one_compiler_root) / 'bin/circle-interpreter'
+    input_data = circle_model + '.input'
+    output_res_data = circle_model + '.output'
+    return subprocess.run([driver, circle_model, input_data, output_res_data], check=True)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        filepath = Path(sys.argv[0])
+        sys.exit('Usage: ' + filepath.name + ' [model.cirle]')
+
+    exec_model(sys.argv[1])
