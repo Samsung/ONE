80163d5750dc7b6a64bcdeb88d2ab90eef0dce06
SaeHie Park <saehie.park@gmail.com>
2025-03-24 18:41:54 +0900

    [circle-mlir/pass] Enable conversion of Sqrt, Tanh (#14878)
    This will enable conversion of Sqrt and Tanh Ops.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>
    Co-authored-by: Seungho Henry Park <shs.park@samsung.com>

--- circle-mlir/circle-mlir/lib/pass/src/ConvertONNXToCirclePass.cpp
+++ circle-mlir/circle-mlir/lib/pass/src/ConvertONNXToCirclePass.cpp
@@ -34,6 +34,8 @@
 #include "ops/ReluOp.h"
 #include "ops/ReshapeOp.h"
 #include "ops/ShapeOp.h"
+#include "ops/SqrtOp.h"
+#include "ops/TanhOp.h"
 #include "ops/TransposeOp.h"
 
 #include <circle-mlir/dialect/CircleDialect.h>
