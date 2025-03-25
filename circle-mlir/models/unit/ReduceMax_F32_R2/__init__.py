d5836226528ce4ba32672b010e73a1a74a9a9c92
SaeHie Park <saehie.park@gmail.com>
2025-03-25 15:33:08 +0900

    [circle-mlir/models] Introduce ReduceMax (#14895)
    This will test models for ReduceMax Op.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ circle-mlir/models/unit/ReduceMax_F32_R2/__init__.py
@@ -0,0 +1,18 @@
+import torch
+
+
+# Generate ReduceMax operator with Float32, Rank-2
+class net_ReduceMax(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return torch.max(input)
+
+    def onnx_opset_version(self):
+        return 13
+
+
+_model_ = net_ReduceMax()
+
+_inputs_ = torch.randn(10, 12)
