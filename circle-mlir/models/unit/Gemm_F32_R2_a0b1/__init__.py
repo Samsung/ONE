606a874e58ffa6c934bfecb606b58d4a41f91835
SaeHie Park <saehie.park@gmail.com>
2025-03-26 18:12:28 +0900

    [circle-mlir/models] Add Gemm (#14921)
    This will add test models for Gemm Op.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ circle-mlir/models/unit/Gemm_F32_R2_a0b1/__init__.py
@@ -0,0 +1,26 @@
+import torch
+
+
+# Generate Gemm operator with Float32, Rank-2
+# Gemm with alpha 1.0, beta 1.0, transA=0, transB=0
+class net_Gemm(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mat2 = torch.randn(4, 5)
+        self.bias = torch.randn(3, 5)
+
+    def forward(self, input):
+        beta = 1.0
+        alpha = 0.0
+        # torch.addmm(input, mat1i, mat2i, *, beta=1, alpha=1, out=None)
+        # out=βinput + α(mat1i ​@ mat2i​)
+        return torch.addmm(self.bias, input, self.mat2, beta=beta, alpha=alpha)
+
+    def onnx_opset_version(self):
+        # TODO set to appropriate value
+        return 8
+
+
+_model_ = net_Gemm()
+
+_inputs_ = torch.randn(3, 4)
