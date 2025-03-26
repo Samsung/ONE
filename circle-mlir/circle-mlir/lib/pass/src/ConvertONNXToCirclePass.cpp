33bce88d6b92073c0fad7f4105dda172ccf22c41
SaeHie Park <saehie.park@gmail.com>
2025-03-26 14:56:38 +0900

    DRAFT cicle-mlir enable Cos,Cumsum, FC
    on-going draft to enable Cos, CumSum and FullyConnected.
    
    Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- circle-mlir/circle-mlir/lib/pass/src/ConvertONNXToCirclePass.cpp
+++ circle-mlir/circle-mlir/lib/pass/src/ConvertONNXToCirclePass.cpp
@@ -28,7 +28,11 @@
 #include "ops/ClipOp.h"
 #include "ops/ConstantOp.h"
 #include "ops/ConvOp.h"
+#include "ops/CosOp.h"
+#include "ops/CumsumOp.h"
+#include "ops/GemmOp.h"
 #include "ops/LeakyReluOp.h"
+#include "ops/MatMulOp.h"
 #include "ops/NoneOp.h"
 #include "ops/PadOp.h"
 #include "ops/PReluOp.h"
@@ -167,7 +171,11 @@ void ConvertONNXToCirclePass::runOnOperation()
   patterns.insert<ConvClip>(typeConverter, context);
   patterns.insert<ConvConstant>(typeConverter, context);
   patterns.insert<ConvConv>(typeConverter, context);
+  patterns.insert<ConvCos>(typeConverter, context);
+  patterns.insert<ConvCumsum>(typeConverter, context);
+  patterns.insert<ConvGemm>(typeConverter, context);
   patterns.insert<ConvLeakyRelu>(typeConverter, context);
+  patterns.insert<ConvMatMul>(typeConverter, context);
   patterns.insert<ConvNone>(typeConverter, context);
   patterns.insert<ConvPad>(typeConverter, context);
   patterns.insert<ConvPRelu>(typeConverter, context);
