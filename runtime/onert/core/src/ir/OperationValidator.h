399da606306d5f8ca6f1fa4158766d60ecff67ba
Sanggyu Lee <takepencil@naver.com>
2025-03-28 14:31:16 +0900

    [onert] Add typecheck for Transpose
    It adds typecheck (input type = output type) for Transpose.
    Also, it adds 1 negative testcase.
    
    ONE-DCO-1.0-Signed-off-by: Sanggyu Lee <sg5.lee@samsung.com>

--- runtime/onert/core/src/ir/OperationValidator.h
+++ runtime/onert/core/src/ir/OperationValidator.h
@@ -75,6 +75,7 @@ public:
   void visit(const operation::SquaredDifference &node) override;
   void visit(const operation::StatelessRandomUniform &node) override;
   void visit(const operation::StridedSlice &node) override;
+  void visit(const operation::Transpose &node) override;
   void visit(const operation::TransposeConv &node) override;
   void visit(const operation::Unpack &node) override;
   void visit(const operation::While &node) override;
