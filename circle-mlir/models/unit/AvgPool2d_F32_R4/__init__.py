5f8ca5cc2a82b657a7d6dfbfcbd2c72d4fb8f653
SaeHie Park <saehie.park@gmail.com>
2025-03-31 11:31:36 +0900

    [circle-mlir/models] Introduce AvgPool2d and MaxPool2d models (#14995)
    This will introduce AvgPool2d and MaxPool2d test models.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ circle-mlir/models/unit/AvgPool2d_F32_R4/__init__.py
@@ -0,0 +1,21 @@
+import torch
+
+
+# Generate AvgPool2d operator with Float32, Rank-4
+# NOTE this will generate Pad - AveragePool in ONNX
+class net_AvgPool2d(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.op = torch.nn.AvgPool2d(2)
+
+    def forward(self, input):
+        return self.op(input)
+
+    def onnx_opset_version(self):
+        # TODO set to appropriate value
+        return 7
+
+
+_model_ = net_AvgPool2d()
+
+_inputs_ = torch.randn(1, 2, 4, 4)
