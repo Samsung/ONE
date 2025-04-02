cbcc86491eb497a19621a03ff97516209713a7de
SaeHie Park <saehie.park@gmail.com>
2025-04-02 09:08:53 +0900

    [circle-mlir/models] Introduce ConvTranspose2d
    This will introduce ConvTranspose2d test models.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ circle-mlir/models/unit/ConvTranspose2d_F32_R4_p10/__init__.py
@@ -0,0 +1,20 @@
+import torch
+
+
+# Generate ConvTranspose2d operator with Float32, Rank-4, padding (1, 0)
+class net_ConvTranspose2d(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.op = torch.nn.ConvTranspose2d(4, 6, 1, padding=(1, 0))
+
+    def forward(self, input):
+        return self.op(input)
+
+    def onnx_opset_version(self):
+        # TODO set to appropriate value
+        return 14
+
+
+_model_ = net_ConvTranspose2d()
+
+_inputs_ = torch.randn(1, 4, 16, 9)
