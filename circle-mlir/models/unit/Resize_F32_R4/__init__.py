74a698e9f163c2617f57f43b91627c79a1c0149e
SaeHie Park <saehie.park@gmail.com>
2025-04-03 16:23:56 +0900

    [circle-mlir/models] Add Resize related
    This will add Resize related test models.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>
    Co-authored-by: Seungho Henry Park <shs.park@samsung.com>

---
+++ circle-mlir/models/unit/Resize_F32_R4/__init__.py
@@ -0,0 +1,20 @@
+import torch
+
+
+# NOTE this is same as Upsample_F32_R4 but with onnx_opset_version 10
+class net_Resize(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.op = torch.nn.Upsample(scale_factor=(2.0, 2.0))
+
+    def forward(self, input):
+        return self.op(input)
+
+    def onnx_opset_version(self):
+        # NOTE return 9 produces `Upsample` Op
+        return 10
+
+
+_model_ = net_Resize()
+
+_inputs_ = torch.randn(1, 3, 16, 9)
