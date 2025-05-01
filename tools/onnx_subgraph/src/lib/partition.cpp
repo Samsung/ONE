9c5867c82f8eebdb76a8cc1e5f45ca504bbb3b16
chenyx113 <chenyouxin113@qq.com>
2025-05-01 10:14:07 +0800

    [tools/onnx-subgraph] Add subgraphs information printing function
    Add subgraphs information printing function
    
    ONE-DCO-1.0-Signed-off-by: Youxin Chen <yx113.chen@samsung.com>

--- tools/onnx_subgraph/src/lib/partition.cpp
+++ tools/onnx_subgraph/src/lib/partition.cpp
@@ -21,6 +21,62 @@
 
 #define MAX_DEPTH 1000
 
+/**
+ * Prints the subgraph information of an ONNX model to specified files.
+ *
+ * @param Subgraphs A vector containing subgraph information.
+ * @param SubgraphFileName The filename for the output of subgraph information.
+ * @param OtherSubgraphs A vector containing other subgraph information.
+ * @param OtherSubgraphFileName The filename for the output of other subgraph information.
+ */
+void PrintSubgraphs(std::vector<onnx::GraphProto> Subgraphs, char *SubgraphFileName,
+                    std::vector<onnx::GraphProto> OtherSubgraphs, char *OtherSubgraphFileName)
+{
+  int nodeSum = 0;
+
+  std::ofstream outFile(SubgraphFileName);
+  if (!outFile.is_open())
+  {
+    std::cerr << "Error opening file." << std::endl;
+    exit(-1);
+  }
+
+  int id = 0;
+  for (const auto &vec : Subgraphs)
+  {
+    outFile << " subgraph" << id << ":";
+    for (const auto &node : vec.node())
+    {
+      outFile << node.name() << " ";
+    }
+
+    id++;
+    outFile << std::endl;
+    nodeSum += vec.node_size();
+  }
+
+  std::ofstream outFileOther(OtherSubgraphFileName);
+  if (!outFileOther.is_open())
+  {
+    std::cerr << "Error opening file." << std::endl;
+    exit(-1);
+  }
+
+  std::cout << "before:" << std::endl;
+  for (const auto &vec : OtherSubgraphs)
+  {
+    outFileOther << " subgraph" << id << ":";
+    for (const auto &node : vec.node())
+    {
+      outFileOther << node.name() << " ";
+    }
+
+    id++;
+    outFileOther << std::endl;
+    nodeSum += vec.node_size();
+  }
+}
+
 void PartitionGraph(const onnx::GraphProto &g, Device &d, PartitionStrategy strategy,
                     const std::unordered_map<std::string, NodeIOSize> &node_io_size)
 {
