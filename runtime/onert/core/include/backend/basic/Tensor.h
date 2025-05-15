ee3708648f6f0fba6c300b749e6aac46252ddad0
Jang Jiseob <ragmani0216@gmail.com>
2025-05-15 09:59:44 +0900

    [onert] Initialize Tensor size to 0 for dynamic shapes (#15366)
    This commit initialize Tensor size to 0 for dynamic shapes. instead of total size.
    
    ONE-DCO-1.0-Signed-off-by: ragmani <ragmani0216@gmail.com>

--- runtime/onert/core/include/backend/basic/Tensor.h
+++ runtime/onert/core/include/backend/basic/Tensor.h
@@ -36,8 +36,10 @@ public:
   virtual ~Tensor();
 
 public:
+  // Initialize _size to 0 because actual buffer size may be unknown until inference for dynamic
+  // shapes including unknown dimensions
   Tensor(const ir::OperandInfo &info, DynamicMemoryManager *dynamic_mem_mgr)
-    : IPortableTensor(info), _buffer(nullptr), _size(info.total_size()), _num_references(0),
+    : IPortableTensor(info), _buffer(nullptr), _size(0), _num_references(0),
       _dynamic_mem_mgr(dynamic_mem_mgr), _allocator(nullptr)
   {
     // DO NOTHING
