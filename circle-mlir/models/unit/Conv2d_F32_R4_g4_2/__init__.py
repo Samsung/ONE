dd43b6b0d88df346355c7c635f403b33a9a0e2f4
SaeHie Park <saehie.park@gmail.com>
2025-07-07 13:50:26 +0900

    [circle-mlir/models] Revise Conv2d_F32_R4_g4
    This will revise Conv2d_F32_R4_g4 models to have Conv2d_F32_R4_g4 method with rewrite validation.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

--- circle-mlir/models/unit/Conv2d_F32_R4_g4_2/__init__.py
+++ circle-mlir/models/unit/Conv2d_F32_R4_g4_2/__init__.py
@@ -20,3 +20,35 @@ class net_Conv2d(torch.nn.Module):
 _model_ = net_Conv2d()
 
 _inputs_ = torch.randn(1, 8, 6, 3)
+
+
+def check_circle_operators(opsDict, operators):
+    # Conv2d count should be 4
+    # Split count should be 1
+    # Concatenation count should be 1
+    # Add count should be 1
+    if not 'Circle.conv_2d' in operators:
+        print('ERROR: Circle.conv_2d NOT exist')
+        return 1
+    if not 'Circle.split' in operators:
+        print('ERROR: Circle.split NOT exist')
+        return 1
+    if not 'Circle.concatenation' in operators:
+        print('ERROR: Circle.concatenation NOT exist')
+        return 1
+    if not 'Circle.add' in operators:
+        print('ERROR: Circle.add NOT exist')
+        return 1
+    if opsDict['Circle.conv_2d'] != 4:
+        print('ERROR: Circle.conv_2d NOT 4')
+        return 1
+    if opsDict['Circle.split'] != 1:
+        print('ERROR: Circle.split NOT 1')
+        return 1
+    if opsDict['Circle.concatenation'] != 1:
+        print('ERROR: Circle.concatenation NOT 1')
+        return 1
+    if opsDict['Circle.add'] != 1:
+        print('ERROR: Circle.add NOT 1')
+        return 1
+    return 0
