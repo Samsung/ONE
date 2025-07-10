509919d5a8395e44f3f2620cc2fce61565d7e50d
Hyeongseok Oh <hseok82.oh@samsung.com>
2025-07-10 17:05:40 +0900

    [onert] Introduce set input & output type API
    This commit introduces set input & output type experimental API to support passing float data to quantized model.
    New APIs should be called before preparing.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- runtime/onert/api/nnfw/src/nnfw_api.cc
+++ runtime/onert/api/nnfw/src/nnfw_api.cc
@@ -131,6 +131,18 @@ NNFW_STATUS nnfw_set_output_layout(nnfw_session *session, uint32_t index, NNFW_L
   return session->set_output_layout(index, layout);
 }
 
+NNFW_STATUS nnfw_set_input_type(nnfw_session *session, uint32_t index, NNFW_TYPE type)
+{
+  NNFW_RETURN_ERROR_IF_NULL(session);
+  return session->set_input_type(index, type);
+}
+
+NNFW_STATUS nnfw_set_output_type(nnfw_session *session, uint32_t index, NNFW_TYPE type)
+{
+  NNFW_RETURN_ERROR_IF_NULL(session);
+  return session->set_output_type(index, type);
+}
+
 NNFW_STATUS nnfw_input_tensorinfo(nnfw_session *session, uint32_t index,
                                   nnfw_tensorinfo *tensor_info)
 {
