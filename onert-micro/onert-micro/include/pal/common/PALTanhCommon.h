79691681441f491f28bf37f19e7bebdae8e1e520
s.malakhov <s.malakhov@partner.samsung.com>
2025-07-31 16:38:40 +0300

    [onert-micro] This commit adds int8 type support for Tanh kernel
    Supporting kernels. Support of int8 type for Tanh kernel.
    
    ONE-DCO-1.0-Signed-off-by: s.malakhov <s.malakhov@partner.samsung.com>

--- onert-micro/onert-micro/include/pal/common/PALTanhCommon.h
+++ onert-micro/onert-micro/include/pal/common/PALTanhCommon.h
@@ -48,6 +48,34 @@ inline OMStatus Tanh(const core::OMRuntimeShape &input_shape, const T *input_dat
 
   return Ok;
 }
+
+template <typename T>
+inline OMStatus QuantizedTanh(const core::OMRuntimeShape &input_shape,
+                              const onert_micro::core::QuantizationParams &input_qparams,
+                              const T *input_data, const core::OMRuntimeShape &output_shape,
+                              const onert_micro::core::QuantizationParams &output_qparams,
+                              T *output_data)
+{
+  const uint32_t flat_size = input_shape.flatSize();
+  for (int i = 0; i < flat_size; i++)
+  {
+    // Dequantize.
+    float val = static_cast<float>((input_data[i] - static_cast<T>(input_qparams.zero_point)) *
+                                   input_qparams.scale);
+    // float result
+    float result = std::tanh(val);
+
+    // Quantize float to output type
+    result = result / output_qparams.scale + output_qparams.zero_point;
+    result = std::max<float>(std::numeric_limits<T>::min(), result);
+    result = std::min<float>(std::numeric_limits<T>::max(), result);
+
+    output_data[i] = static_cast<T>(result);
+  }
+
+  return Ok;
+}
+
 } // namespace pal
 } // namespace execute
 } // namespace onert_micro
