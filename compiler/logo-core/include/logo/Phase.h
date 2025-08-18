47b4bcc1bcc860fdc18e2769b38be4b6653683be
tomdol <t.dolbniak@partner.samsung.com>
2025-08-18 11:10:50 +0200

    [logo-core] Fix a warning when building with clang (#15921)
    This commit fixes a warning emitted by clang for the Phase.h header file
    
    There's a mismatch between the original template declaration and the following
    specializations of the PhaseEventInfo class/struct. The changes in this commit
    fix the following warning (which causes a build break when compiling in strict)
    
    logo/Phase.h:44:13: error: 'PhaseEventInfo' defined as a class template here but previously declared as a struct template;
    
    ONE-DCO-1.0-Signed-off-by: Tomasz Dołbniak <t.dolbniak@partner.samsung.com>

--- compiler/logo-core/include/logo/Phase.h
+++ compiler/logo-core/include/logo/Phase.h
@@ -39,7 +39,7 @@ enum class PhaseEvent
   PassEnd,
 };
 
-template <PhaseEvent E> struct PhaseEventInfo;
+template <PhaseEvent E> class PhaseEventInfo;
 
 template <> class PhaseEventInfo<PhaseEvent::PhaseBegin>
 {
