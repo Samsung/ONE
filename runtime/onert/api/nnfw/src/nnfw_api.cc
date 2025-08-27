37d242b5be2904bb1f13782ec311354375da3679
Hyeongseok Oh <hseok82.oh@samsung.com>
2025-08-27 14:42:19 +0900

    [onert] Add signature setting API to nnfw_session (#15947)
    This commit adds signature setting API skeleton in nnfw_session and uses it in API implementation.
    The functions include parameter validation and state checking, but return NYI errors as the implementation is not complete yet.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- runtime/onert/api/nnfw/src/nnfw_api.cc
+++ runtime/onert/api/nnfw/src/nnfw_api.cc
@@ -245,6 +245,18 @@ NNFW_STATUS nnfw_set_workspace(nnfw_session *session, const char *dir)
   return session->set_workspace(dir);
 }
 
+NNFW_STATUS nnfw_set_signature_for_tensorinfo(nnfw_session *session, const char *signature)
+{
+  NNFW_RETURN_ERROR_IF_NULL(session);
+  return session->set_signature_for_tensorinfo(signature);
+}
+
+NNFW_STATUS nnfw_set_signature_run(nnfw_session *session, const char *signature)
+{
+  NNFW_RETURN_ERROR_IF_NULL(session);
+  return session->set_signature_run(signature);
+}
+
 // Training
 
 NNFW_STATUS nnfw_train_get_traininfo(nnfw_session *session, nnfw_train_info *info)
