7c3c32df5967744f3f4e8485d13952a8997fb895
Hyeongseok Oh <hseok82.oh@samsung.com>
2025-09-11 10:11:24 +0900

    [infra/onert] Build host flatc on build process
    This commit updates FlatBuffers cmake configuration to build or use host flatc and generate headers on build process.
    It moves traininfo schema file and adds symbolic link to circle schema into "libs/circle-schema" directory, and removes pre-generated headers.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- packaging/nnfw.spec
+++ packaging/nnfw.spec
@@ -39,6 +39,7 @@ Source3016: XNNPACK.tar.gz
 %{!?nproc:          %define nproc           %{?!jobs:4}%{?jobs}}
 
 BuildRequires:  cmake
+BuildRequires:  pkgconfig(flatbuffers)
 
 Requires(post): /sbin/ldconfig
 Requires(postun): /sbin/ldconfig
@@ -189,6 +190,7 @@ tar -xf %{SOURCE3015} -C ./externals
 %if %{odc_build} == 1
 %{nncc_env} ./nncc configure -DBUILD_GTEST=OFF -DENABLE_TEST=OFF -DEXTERNALS_BUILD_THREADS=%{nproc} -DCMAKE_BUILD_TYPE=%{build_type} -DTARGET_ARCH=%{target_arch} -DTARGET_OS=tizen \
         -DCMAKE_INSTALL_PREFIX=$(pwd)/%{overlay_path} \
+        -DDOWNLOAD_FLATBUFFERS=OFF \
 	-DBUILD_WHITELIST="luci;foder;pepper-csv2vec;loco;locop;logo;logo-core;mio-circle;luci-compute;oops;hermes;hermes-std;angkor;pp;pepper-strcast;pepper-str"
 %{nncc_env} ./nncc build %{build_jobs}
 cmake --install %{nncc_workspace}
--- runtime/infra/cmake/packages/FlatBuffersConfig.cmake
+++ runtime/infra/cmake/packages/FlatBuffersConfig.cmake
@@ -4,12 +4,15 @@ function(_FlatBuffers_import)
     return()
   endif()
 
-  find_package(Flatbuffers 23.5.26 QUIET)
-  if(Flatbuffers_FOUND)
-    message(STATUS "Flatbuffers: found Flatbuffers")
-    set(FlatBuffers_FOUND TRUE PARENT_SCOPE)
-    return()
-  endif(Flatbuffers_FOUND)
+  # Don't use pre-installed FlatBuffers when cross-compiling
+  if(NOT CMAKE_CROSSCOMPILING)
+    find_package(Flatbuffers 23.5.26 QUIET)
+    if(Flatbuffers_FOUND)
+      message(STATUS "Flatbuffers: found Flatbuffers")
+      set(FlatBuffers_FOUND TRUE PARENT_SCOPE)
+      return()
+    endif(Flatbuffers_FOUND)
+  endif(NOT CMAKE_CROSSCOMPILING)
 
   nnfw_find_package(FlatBuffersSource QUIET)
 
@@ -22,7 +25,9 @@ function(_FlatBuffers_import)
   set(FLATBUFFERS_BUILD_TESTS OFF)
   set(FLATBUFFERS_STATIC_FLATC ON)
   set(FLATBUFFERS_INSTALL OFF)
-  set(FLATBUFFERS_BUILD_FLATC OFF)
+  if(CMAKE_CROSSCOMPILING)
+    set(FLATBUFFERS_BUILD_FLATC OFF)
+  endif(CMAKE_CROSSCOMPILING)
   add_subdirectory(${FlatBuffersSource_DIR} ${CMAKE_BINARY_DIR}/externals/flatbuffers)
   if(NOT TARGET flatbuffers)
     message(STATUS "Flatbuffers: failed to build FlatBuffers")
@@ -33,6 +38,42 @@ function(_FlatBuffers_import)
   set_property(TARGET flatbuffers PROPERTY POSITION_INDEPENDENT_CODE ON)
   target_compile_options(flatbuffers PUBLIC $<$<CONFIG:Debug>:-Wno-sign-compare>)
   add_library(flatbuffers::flatbuffers ALIAS flatbuffers)
+
+  if(CMAKE_CROSSCOMPILING)
+    # Build flatc for host manually: set buildtool to gcc/g++ explicitly
+    message(STATUS "Flatbuffers: build flatbuffers for host...")
+    execute_process(
+      COMMAND cmake -S ${FlatBuffersSource_DIR} -B ${CMAKE_BINARY_DIR}/externals/flatc-host
+        -DFLATBUFFERS_BUILD_FLATC=ON -DFLATBUFFERS_BUILD_FLATLIB=OFF -DFLATBUFFERS_BUILD_TESTS=OFF
+        -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
+      RESULT_VARIABLE FLATC_CONFIG_RESULT
+    )
+    if (NOT FLATC_CONFIG_RESULT EQUAL 0)
+      message(FATAL_ERROR "Flatbuffers: failed to configure host flatc")
+      set(FlatBuffers_FOUND FALSE PARENT_SCOPE)
+      return()
+    endif()
+
+    set(NUM_BUILD_THREADS 1)
+    if(DEFINED EXTERNALS_BUILD_THREADS)
+      set(NUM_BUILD_THREADS ${EXTERNALS_BUILD_THREADS})
+    endif(DEFINED EXTERNALS_BUILD_THREADS)
+    execute_process(
+      COMMAND cmake --build ${CMAKE_BINARY_DIR}/externals/flatc-host -j ${NUM_BUILD_THREADS}
+      RESULT_VARIABLE FLATC_BUILD_RESULT
+    )
+    if (NOT FLATC_BUILD_RESULT EQUAL 0)
+      message(FATAL_ERROR "Flatbuffers: failed to build host flatc")
+      set(FlatBuffers_FOUND FALSE PARENT_SCOPE)
+      return()
+    endif()
+
+    add_executable(flatbuffers::flatc IMPORTED GLOBAL)
+    set_property(TARGET flatbuffers::flatc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+    set_target_properties(flatbuffers::flatc PROPERTIES
+      IMPORTED_LOCATION_RELEASE "${CMAKE_BINARY_DIR}/externals/flatc-host/flatc"
+    )
+  endif()
 
   message(STATUS "Flatbuffers: built FlatBuffers from source")
   set(FlatBuffers_FOUND TRUE PARENT_SCOPE)
