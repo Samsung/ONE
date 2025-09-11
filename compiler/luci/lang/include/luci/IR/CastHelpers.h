eef9b2042d74540ddb6712c5674af20ebf2d19e6
SaeHie Park <saehie.park@gmail.com>
2025-09-12 08:59:59 +0900

    [luci/lang] Introduce must_cast with stack dump
    This will introduce luci::must_cast that shows call stack dump when failed.
    
    ONE-DCO-1.0-Signed-off-by: SaeHie Park <saehie.park@gmail.com>

---
+++ compiler/luci/lang/include/luci/IR/CastHelpers.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2025 Samsung Electronics Co., Ltd. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __LUCL_IR_CAST_HELPERS_H__
+#define __LUCL_IR_CAST_HELPERS_H__
+
+#include <string>
+#include <stdexcept>
+#include <typeinfo>
+
+namespace luci
+{
+
+__attribute__((noinline)) std::string _callstacks(void);
+
+template <typename T, typename ARG> inline T _must_cast(ARG arg)
+{
+  auto cast_arg = dynamic_cast<T>(arg);
+  if (cast_arg == nullptr)
+  {
+    std::string msg = "luci::must_cast() failed to cast: ";
+    msg += typeid(T).name();
+    msg += _callstacks();
+    throw std::invalid_argument(msg.c_str());
+  }
+  return cast_arg;
+}
+
+} // namespace luci
+
+#endif // __LUCL_IR_CAST_HELPERS_H__
