757260cbf595803a48b512b336fb7e91e5e53bc4
Hyeongseok Oh <hseok82.oh@samsung.com>
2025-09-17 09:00:59 +0900

    Libraries for cker

--- runtime/infra/cmake/packages/RuySourceConfig.cmake
+++ runtime/infra/cmake/packages/RuySourceConfig.cmake
@@ -7,12 +7,12 @@ function(_RuySource_import)
   nnfw_include(ExternalSourceTools)
   nnfw_include(OptionTools)
 
-  # Exact version used by TensorFlow v2.16.1.
+  # Exact version used by TensorFlow v2.18.1.
   # See tensorflow/third_party/ruy/workspace.bzl
   envoption(EXTERNAL_DOWNLOAD_SERVER "https://github.com")
   envoption(RUY_URL ${EXTERNAL_DOWNLOAD_SERVER}/google/ruy/archive/3286a34cc8de6149ac6844107dfdffac91531e72.zip)
 
-  ExternalSource_Download(RUY DIRNAME TENSORFLOW-2.16.1-RUY ${RUY_URL})
+  ExternalSource_Download(RUY DIRNAME RUY ${RUY_URL})
 
   set(RuySource_DIR ${RUY_SOURCE_DIR} PARENT_SCOPE)
   set(RuySource_FOUND TRUE PARENT_SCOPE)
