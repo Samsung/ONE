d13996b055fbaeb1ed759a9bcecb10fb8fbdb8ff
Hyeongseok Oh <hseok82.oh@samsung.com>
2025-09-17 10:41:46 +0900

    version up tflite 2.18.1

--- runtime/infra/cmake/packages/TensorFlowSourceConfig.cmake
+++ runtime/infra/cmake/packages/TensorFlowSourceConfig.cmake
@@ -8,9 +8,9 @@ function(_TensorFlowSource_import)
   nnfw_include(OptionTools)
 
   envoption(EXTERNAL_DOWNLOAD_SERVER "https://github.com")
-  envoption(TENSORFLOW_2_16_1_URL ${EXTERNAL_DOWNLOAD_SERVER}/tensorflow/tensorflow/archive/v2.16.1.tar.gz)
+  envoption(TENSORFLOW_URL ${EXTERNAL_DOWNLOAD_SERVER}/tensorflow/tensorflow/archive/v2.18.1.tar.gz)
 
-  ExternalSource_Download(TENSORFLOW DIRNAME TENSORFLOW-2.16.1 ${TENSORFLOW_2_16_1_URL})
+  ExternalSource_Download(TENSORFLOW DIRNAME TENSORFLOW ${TENSORFLOW_URL})
 
   set(TensorFlowSource_DIR ${TENSORFLOW_SOURCE_DIR} PARENT_SCOPE)
   set(TensorFlowSource_FOUND TRUE PARENT_SCOPE)
