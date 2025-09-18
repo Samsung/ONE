64ef626a1f0aadc721f81e71b3c3d2a842872405
Hyeongseok Oh <hseok82.oh@samsung.com>
2025-09-18 11:52:38 +0900

    [infra/onert] Bump up libraries for compute (#16101)
    This commit bumps up libries for compute
    - Eigen
    - GEMMLowp
    - RUY
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- runtime/infra/cmake/packages/EigenSourceConfig.cmake
+++ runtime/infra/cmake/packages/EigenSourceConfig.cmake
@@ -7,12 +7,12 @@ function(_EigenSource_import)
   nnfw_include(ExternalSourceTools)
   nnfw_include(OptionTools)
 
-  # Exact version used by TensorFlow v2.16.1.
+  # Exact version used by TensorFlow v2.18.1.
   # See tensorflow/third_party/eigen3/workspace.bzl.
   envoption(EXTERNAL_DOWNLOAD_SERVER "https://gitlab.com")
-  envoption(EIGEN_URL ${EXTERNAL_DOWNLOAD_SERVER}/libeigen/eigen/-/archive/aa6964bf3a34fd607837dd8123bc42465185c4f8/eigen-aa6964bf3a34fd607837dd8123bc42465185c4f8.tar.gz)
+  envoption(EIGEN_URL ${EXTERNAL_DOWNLOAD_SERVER}/libeigen/eigen/-/archive/33d0937c6bdf5ec999939fb17f2a553183d14a74/eigen-33d0937c6bdf5ec999939fb17f2a553183d14a74.tar.gz)
 
-  ExternalSource_Download(EIGEN DIRNAME TENSORFLOW-2.16.1-EIGEN ${EIGEN_URL})
+  ExternalSource_Download(EIGEN DIRNAME EIGEN ${EIGEN_URL})
 
   set(EigenSource_DIR ${EIGEN_SOURCE_DIR} PARENT_SCOPE)
   set(EigenSource_FOUND TRUE PARENT_SCOPE)
