a54a8f7d957a8040ef3a468aaafed9349e9617a5
Chunseok Lee <chunseok.lee@samsung.com>
2025-10-10 14:03:57 +0900

    fix format

--- onert-micro/onert-micro/include/pal/common/OMLog.h
+++ onert-micro/onert-micro/include/pal/common/OMLog.h
@@ -5,16 +5,18 @@
 #include <stdlib.h>
 
 // Simple logging macro – can be replaced later with the project's logger
-#define OM_LOG_ERROR(msg) \
-  do { \
+#define OM_LOG_ERROR(msg)                                            \
+  do                                                                 \
+  {                                                                  \
     fprintf(stderr, "[ERROR] %s:%d: %s\n", __FILE__, __LINE__, msg); \
   } while (0)
 
 // Macro that logs a message and returns the given error code
 #define OM_LOG_AND_RETURN(err, msg) \
-  do { \
-    OM_LOG_ERROR(msg); \
-    return err; \
+  do                                \
+  {                                 \
+    OM_LOG_ERROR(msg);              \
+    return err;                     \
   } while (0)
 
 #endif // ONERT_MICRO_OMLOG_H_
