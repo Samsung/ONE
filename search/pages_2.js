a16268f40a08947efe308ca5a1a93c574e6e8903
hseok-oh <hseok-oh@users.noreply.github.com>
2025-10-12 19:36:35 +0000

    Deploying to gh-pages from @ Samsung/ONE@d03fd20640afcf460dd3d5ec19d37b6b25b2ac34 🚀

--- search/pages_2.js
+++ search/pages_2.js
@@ -2,13 +2,14 @@ var searchData=
 [
   ['cnn_20model_20on_20mnist_20using_20onert_0',['Training a simple CNN model on MNIST using ONERT',['../md_docs_2runtime_2training__cnn__on__mnist.html',1,'']]],
   ['code_1',['How to Remote Debugging with Visual Studio Code',['../md_docs_2howto_2how-to-remote-debugging-with-visual-studio-code.html',1,'']]],
-  ['compilation_2',['On-Device Compilation',['../md_docs_2runtime_2on-device-compilation.html',1,'']]],
-  ['compiler_3',['compiler',['../md_docs_2howto_2how-to-build-compiler.html',1,'How to Build Compiler'],['../md_docs_2howto_2how-to-introduce-a-new-operation-into-compiler.html',1,'How To Introduce a New Operation Into Compiler']]],
-  ['compute_4',['Compute',['../md_docs_2runtime_2compute.html',1,'']]],
-  ['contribute_5',['How to Contribute',['../md_docs_2howto_2how-to-contribute.html',1,'']]],
-  ['controlflow_20operations_6',['Controlflow Operations',['../md_docs_2runtime_2controlflow-operations.html',1,'']]],
-  ['core_7',['Core',['../md_docs_2runtime_2core.html',1,'']]],
-  ['cross_20build_20runtime_20for_20aarch64_8',['How to Cross-build Runtime for AARCH64',['../md_docs_2howto_2how-to-cross-build-runtime-for-aarch64.html',1,'']]],
-  ['cross_20build_20runtime_20for_20android_9',['How to Cross-build Runtime for Android',['../md_docs_2howto_2how-to-cross-build-runtime-for-android.html',1,'']]],
-  ['cross_20build_20runtime_20for_20arm_10',['How to Cross-build Runtime for ARM',['../md_docs_2howto_2how-to-cross-build-runtime-for-arm.html',1,'']]]
+  ['coding_20rules_2',['Coding Rules',['../md_runtime_2coding-rules.html',1,'']]],
+  ['compilation_3',['On-Device Compilation',['../md_docs_2runtime_2on-device-compilation.html',1,'']]],
+  ['compiler_4',['compiler',['../md_docs_2howto_2how-to-build-compiler.html',1,'How to Build Compiler'],['../md_docs_2howto_2how-to-introduce-a-new-operation-into-compiler.html',1,'How To Introduce a New Operation Into Compiler']]],
+  ['compute_5',['Compute',['../md_docs_2runtime_2compute.html',1,'']]],
+  ['contribute_6',['How to Contribute',['../md_docs_2howto_2how-to-contribute.html',1,'']]],
+  ['controlflow_20operations_7',['Controlflow Operations',['../md_docs_2runtime_2controlflow-operations.html',1,'']]],
+  ['core_8',['Core',['../md_docs_2runtime_2core.html',1,'']]],
+  ['cross_20build_20runtime_20for_20aarch64_9',['How to Cross-build Runtime for AARCH64',['../md_docs_2howto_2how-to-cross-build-runtime-for-aarch64.html',1,'']]],
+  ['cross_20build_20runtime_20for_20android_10',['How to Cross-build Runtime for Android',['../md_docs_2howto_2how-to-cross-build-runtime-for-android.html',1,'']]],
+  ['cross_20build_20runtime_20for_20arm_11',['How to Cross-build Runtime for ARM',['../md_docs_2howto_2how-to-cross-build-runtime-for-arm.html',1,'']]]
 ];
