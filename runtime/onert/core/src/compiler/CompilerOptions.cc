75fd9bfa0766f9ff4ee0da18c3cdadf6a787ae68
Hyeongseok Oh <hseok82.oh@samsung.com>
2025-10-27 15:19:04 +0900

    [onert] Remove backend assignment workarounds
    This commit removes workarounds for BCQ and Bulk operations that were forcing specific backend assignments.
    Now these operations are assigned by backend ordering and validator automatically.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- runtime/onert/core/src/compiler/CompilerOptions.cc
+++ runtime/onert/core/src/compiler/CompilerOptions.cc
@@ -119,14 +119,6 @@ void CompilerOptions::forceInternalOptions()
 
   // Set builtin backend for custom operator
   manual_scheduler_options.opcode_to_backend[ir::OpCode::Custom] = builtin_id;
-
-  // FIXME This is a workaround for bcq operations, should remove it
-  manual_scheduler_options.opcode_to_backend[ir::OpCode::BCQFullyConnected] = "bcq";
-  manual_scheduler_options.opcode_to_backend[ir::OpCode::BCQGather] = "bcq";
-  manual_scheduler_options.opcode_to_backend[ir::OpCode::BCQUnembedding] = "bcq";
-
-  // FIXME This is a workaround for bulk operations, should remove it
-  manual_scheduler_options.opcode_to_backend[ir::OpCode::Bulk] = "trix";
 }
 
 void CompilerOptions::verboseOptions()
