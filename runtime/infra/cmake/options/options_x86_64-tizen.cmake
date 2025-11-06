bddcd02fa7af737a855cbb75c33e483241b8ecce
Hyeongseok Oh <hseok82.oh@samsung.com>
2025-11-06 16:02:51 +0900

    [infra/onert] Disable SentencePiece as default all tizen platforms (#16272)
    This commit disables SentencePiece as default for all tizen platforms.
    BUILD_GGMA_API also disabled, but enabled by nnfw.spec.
    
    ONE-DCO-1.0-Signed-off-by: Hyeongseok Oh <hseok82.oh@samsung.com>

--- runtime/infra/cmake/options/options_x86_64-tizen.cmake
+++ runtime/infra/cmake/options/options_x86_64-tizen.cmake
@@ -7,3 +7,7 @@ option(DOWNLOAD_GTEST "Download Google Test source and build Google Test" OFF)
 option(ENVVAR_ONERT_CONFIG "Use environment variable for onert configuration" OFF)
 
 option(BUILD_XNNPACK "Build XNNPACK" OFF)
+
+option(BUILD_GGMA_API "Build GGMA API for Generative AI" OFF)
+option(DOWNLOAD_SENTENCEPIECE "Download SentencePiece source" OFF)
+option(BUILD_SENTENCEPIECE "Build SentencePiece library from the source" OFF)
