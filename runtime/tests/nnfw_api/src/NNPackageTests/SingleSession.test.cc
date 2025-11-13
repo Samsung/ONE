f63a6980a48377e009dc224285c89fc1dfa43e3c
Arkadiusz Bokowy <a.bokowy@samsung.com>
2025-11-13 08:43:46 +0100

    Test deprecated OneRT APIs with a real session instead of NULL

--- runtime/tests/nnfw_api/src/NNPackageTests/SingleSession.test.cc
+++ runtime/tests/nnfw_api/src/NNPackageTests/SingleSession.test.cc
@@ -128,12 +128,3 @@ TEST_F(ValidationTestSingleSession, neg_internal_set_config)
 {
   ASSERT_EQ(nnfw_set_config(nullptr, "GRAPH_DOT_DUMP", "0"), NNFW_STATUS_UNEXPECTED_NULL);
 }
-
-TEST_F(ValidationTestSessionCreated, neg_deprecated_api)
-{
-  EXPECT_EQ(nnfw_apply_tensorinfo(nullptr, 0, nnfw_tensorinfo{}), NNFW_STATUS_DEPRECATED_API);
-  EXPECT_EQ(nnfw_prepare_pipeline(nullptr, nullptr), NNFW_STATUS_DEPRECATED_API);
-  EXPECT_EQ(nnfw_push_pipeline_input(nullptr, nullptr, nullptr), NNFW_STATUS_DEPRECATED_API);
-  EXPECT_EQ(nnfw_pop_pipeline_output(nullptr, nullptr), NNFW_STATUS_DEPRECATED_API);
-  EXPECT_EQ(nnfw_set_op_backend(nullptr, nullptr, nullptr), NNFW_STATUS_DEPRECATED_API);
-}
