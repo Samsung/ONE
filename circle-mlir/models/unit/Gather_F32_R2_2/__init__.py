8400ed652b4898bd06b3f20ea497cb06194c1046
Seungho Henry Park <shs.park@samsung.com>
2026-01-23 09:28:15 +0900

    [circle-mlir] Add test models for GatherOp pass (#16352)
    This adds test models to cover int32 indices in the GatherOp pass.
    These tests will be enabled after the related pass is updated.
    
    ONE-DCO-1.0-Signed-off-by: Seungho Henry Park <shs.park@samsung.com>

---
+++ circle-mlir/models/unit/Gather_F32_R2_2/__init__.py
@@ -0,0 +1,23 @@
+import torch
+import numpy as np
+
+
+# Generate Gather operator with Float32, Rank-2, no constant inputs
+class net_Gather(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input0, input1):
+        return input0[input1]
+
+    def onnx_opset_version(self):
+        # TODO set to appropriate value
+        return 14
+
+
+_model_ = net_Gather()
+
+# NOTE onnx2circle-value-test's exec_onnx.py generates random int32 into the range [0, 100).
+#      Let's use the same range for this test to prevent index values out of data bounds.
+rand = np.random.randint(0, 100, size=(3, 9), dtype=np.int32)
+_inputs_ = (torch.randn(100, 9), torch.from_numpy(rand))
