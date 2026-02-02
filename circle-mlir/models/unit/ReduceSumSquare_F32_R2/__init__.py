1bc99bb5fb95203a92ae0bd6227cd889ccaa2045
Seungho Henry Park <shs.park@samsung.com>
2026-02-02 17:04:39 +0900

    [circle-mlir/models] Add more test models for ReduceSumSquare operation
    This adds more test models for ReduceSumSquare operation.
    
    ONE-DCO-1.0-Signed-off-by: Seungho Henry Park <shs.park@samsung.com>

---
+++ circle-mlir/models/unit/ReduceSumSquare_F32_R2/__init__.py
@@ -0,0 +1,37 @@
+import torch
+import onnx
+
+
+# Generate ReduceSum operator with Float32, Rank-2
+class net_ReduceSum(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return torch.sum(input)
+
+    def onnx_opset_version(self):
+        return 13
+    
+    def post_process(self, model_path):
+        onnx_model = onnx.load(model_path)
+
+        # Find the ReduceSum node
+        target = None
+        for n in onnx_model.graph.node:
+            if n.op_type == "ReduceSum":
+                target = n
+                break
+        if target is None:
+            raise RuntimeError("ReduceSum node not found")
+
+        target.op_type = "ReduceSumSquare"
+        target.name = "ReduceSumSquare_0"
+
+        onnx.checker.check_model(onnx_model)
+        onnx.save(onnx_model, model_path)
+
+
+_model_ = net_ReduceSum()
+
+_inputs_ = torch.randn(10, 12)
---
+++ circle-mlir/models/unit/ReduceSumSquare_F32_R2_d1/__init__.py
@@ -0,0 +1,50 @@
+import torch
+import onnx
+
+
+# Generate ReduceSum operator with Float32, Rank-2, dim=1
+class net_ReduceSum(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return torch.sum(input)  # This will be replaced with ReduceSumSquare(axes=0, keepdim=False)
+
+    def onnx_opset_version(self):
+        return 13
+
+    def post_process(self, model_path):
+        onnx_model = onnx.load(model_path)
+
+        # Find the ReduceSum node and its index
+        idx = None
+        target = None
+        for i, n in enumerate(onnx_model.graph.node):
+            if n.op_type == "ReduceSum":
+                idx = i
+                target = n
+                break
+        if target is None:
+            raise RuntimeError("ReduceSum node not found")
+        
+        data_in = target.input[0]
+        data_out = target.output[0]
+
+        # Replace node with ReduceSumSquareV13
+        new_node = onnx.helper.make_node(
+            "ReduceSumSquare",
+            inputs=[data_in],
+            outputs=[data_out],
+            name="ReduceSumSquare_0",
+            axes=[1],  # dim=1
+            keepdims=0,  # keepdim=False
+        )
+        onnx_model.graph.node[idx].CopyFrom(new_node)
+
+        onnx.checker.check_model(onnx_model)
+        onnx.save(onnx_model, model_path)
+
+
+_model_ = net_ReduceSum()
+
+_inputs_ = torch.randn(10, 12)
