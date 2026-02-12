86e112ed82a99a6ebbe3ffc2321bef67aca1699d
Seungho Henry Park <shs.park@samsung.com>
2026-02-12 17:58:46 +0900

    [circle-mlir/models] Introduce test models for Range op
    This introduces test models for the Range operation.
    The tests will be activated once support for the Range pass is completed.
    
    Signed-off-by: Seungho Henry Park <shs.park@samsung.com>

---
+++ circle-mlir/models/unit/Range_F32_R0_2/__init__.py
@@ -0,0 +1,19 @@
+import torch
+
+
+# Generate Range operator with Float32, scalar
+class net_Range(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, limit, delta):
+        return torch.arange(0, limit, delta, dtype=torch.float32)
+
+    def onnx_opset_version(self):
+        return 11
+
+
+_model_ = net_Range()
+
+# produce float32 scalar with fixed number
+_inputs_ = (torch.tensor(10, dtype=torch.float32), torch.tensor(1, dtype=torch.float32))
