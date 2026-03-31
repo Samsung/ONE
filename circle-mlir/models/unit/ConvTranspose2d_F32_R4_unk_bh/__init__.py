79016cf873a5b0b22f715563019e3e5684376be5
Seungho Henry Park <shs.park@samsung.com>
2026-03-31 19:22:19 +0900

    [circle-mlir/models] Add test models for dynamic-shape ConvTranspose
    This adds test models for converting the ONNX `ConvTranspose` operation
    with dynamic-shape inputs.
    
    ONE-DCO-1.0-Signed-off-by: Seungho Henry Park <shs.park@samsung.com>

---
+++ circle-mlir/models/unit/ConvTranspose2d_F32_R4_unk_bh/__init__.py
@@ -0,0 +1,38 @@
+import torch
+
+
+# Generate ConvTranspose2d operator with Float32, Rank-4, unknown
+# input  : [N, 4, H, 10]
+# output : [N, 3, H, 10+7]
+# dynamic axes: N, H
+class net_ConvTranspose2d(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.op = torch.nn.ConvTranspose2d(
+            in_channels=4,
+            out_channels=3,
+            kernel_size=(1, 8),
+            stride=(1, 1),
+            padding=(0, 0),
+            dilation=(1, 1),
+            groups=1,
+            bias=True,
+        )
+
+    def forward(self, input):
+        return self.op(input)
+
+    def onnx_opset_version(self):
+        # TODO set to appropriate value
+        return 14
+
+
+_model_ = net_ConvTranspose2d()
+
+_inputs_ = (torch.Tensor(1, 4, 1, 1))
+
+_io_names_ = [['input'], ['output']]
+_dynamic_axes_ = {
+    "input": {0: "?", 2: "?"},
+    "output": {0: "?", 2: "?"}
+}
